53f8831dbd3c86cfc1410c01e3682eebc2b436b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:58 +0000

    Add package tox-battery/0.5.2/tox-battery-0.5.2.tar.gz

---
+++ packages/tox-battery/tox-battery-0.5.2.tar.gz/tox-battery-0.5.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+
+def content_of(fpath):
+    with open(fpath, 'r') as fd:
+        return fd.read()
+
+
+setuptools.setup(
+    name='tox-battery',
+    description='Additional functionality for tox',
+    long_description=content_of("README.rst"),
+    license='http://opensource.org/licenses/MIT',
+    version='0.5.2',
+    author='Volodymyr Vitvitskyi',
+    author_email='contact.volodymyr@gmail.com',
+    url='https://github.com/signalpillar/tox-battery',
+    packages=setuptools.find_packages(),
+    entry_points={'tox': [
+        'toxbat-requirements = toxbat.requirements',
+    ]},
+    install_requires=['tox'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'],
+)
