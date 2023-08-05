5a39e28b2611c8ce9292dfdb67eaa606d3d0ecca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:16 +0000

    Add package configcat-flag-reference-validator/1.0.9/configcat-flag-reference-validator-1.0.9.tar.gz

---
+++ packages/configcat-flag-reference-validator/configcat-flag-reference-validator-1.0.9.tar.gz/configcat-flag-reference-validator-1.0.9/configcat/reference_validator/reference_validator.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+
+log = logging.getLogger(sys.modules[__name__].__name__)
+
+
+class ReferenceValidator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def validate(remote_keys,
+                 reference_keys):
+        missing_references = remote_keys.difference(reference_keys)
+        missing_remote_keys = reference_keys.difference(remote_keys)
+
+        result = True
+        if len(missing_references) > 0 or len(missing_references) > 0:
+            result = False
+            log.warning("Found unused feature flags!")
+
+        if len(missing_references) > 0:
+            log.debug(
+                "Feature flag/Setting keys not found in source code (but present in ConfigCat): %s.",
+                missing_references)
+            for item in missing_references:
+                log.warning("Clean up '%s' from ConfigCat Dashboard.", item)
+
+        if len(missing_remote_keys) > 0:
+            log.debug(
+                "Feature flag/Setting keys not found in ConfigCat (but present in source code): %s.",
+                missing_remote_keys)
+            for item in missing_remote_keys:
+                log.warning("Clean up '%s' from code.", item)
+
+        return result
