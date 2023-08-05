9644993b3af63356a14d56ae489bea2f8d963a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package doctalk/0.0.4/doctalk-0.0.4-py3-none-any.whl

---
+++ packages/doctalk/doctalk-0.0.4-py3-none-any.whl/doctalk/params.py
@@ -0,0 +1,33 @@
+annotators=['tokenize','ssplit','pos','lemma','depparse','ner']+\
+           ['natlog','openie']
+openie='openie' in annotators
+
+sum_count=4
+key_count=10
+quiet=True
+max_answers=4
+answers_by_rank=True
+trace=1
+force=False
+from nltk.stem import PorterStemmer
+#stemmer=PorterStemmer()
+stemmer=None
+lower=True
+pers=True
+expand_query=2
+compounds=True
+svo_edges=True
+
+show_pics=1
+show_rels=0
+to_prolog=1
+
+# decides between '_' and ' ' as separator
+#def join(*xs) : return '_'.join(xs)
+def join(*xs) : return xs
+
+from inspect import currentframe
+def ppp(*args) :
+  cf = currentframe()
+  print('DEBUG at:',cf.f_back.f_lineno,end=': ')
+  print(*args)
