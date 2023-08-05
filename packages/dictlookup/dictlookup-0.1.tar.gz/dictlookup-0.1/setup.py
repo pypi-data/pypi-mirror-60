28387be0fcbdbda1c1be2628a4e712f6086d736c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package dictlookup/0.1/dictlookup-0.1.tar.gz

---
+++ packages/dictlookup/dictlookup-0.1.tar.gz/dictlookup-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dictlookup',
+      version='0.1',
+      description='Search a word in dictionary',
+      url='http://github.com/ibrahim45/dict-lookup',
+      author='Ibrahim sha',
+      author_email='kibrahimsha4@gmail.com',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
