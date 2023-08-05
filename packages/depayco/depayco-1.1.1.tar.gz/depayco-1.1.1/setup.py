d67e9a7212988bd99d42476ebf1d66be0d2c01ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:11 +0000

    Add package depayco/1.1.1/depayco-1.1.1.tar.gz

---
+++ packages/depayco/depayco-1.1.1.tar.gz/depayco-1.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='depayco',
+    version='1.1.1',
+    license='GPL',
+    author='Alonso',
+    author_email='alons45@gmail.com',
+    description='Django integration with ePayco (epayco.co)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ma1onso/depayco',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyepayco==1.0.0',
+        # TODO: don't repeat dependencies from pyepayco
+        'requests >= 2.4.3',
+        'pycrypto >= 2.3'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+    ],
+    python_requires='>=3.6',
+)
