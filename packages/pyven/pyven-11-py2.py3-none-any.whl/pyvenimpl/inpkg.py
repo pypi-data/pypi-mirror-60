d313b579fe45cbec198276634a5a20adf95b1653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:52 +0000

    Add package pyven/11/pyven-11-py2.py3-none-any.whl

---
+++ packages/pyven/pyven-11-py2.py3-none-any.whl/pyvenimpl/inpkg.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pyven
+
+# Copyright 2013, 2014, 2015, 2016, 2017 Andrzej Cichocki
+
+# This file is part of pyven.
+#
+# pyven is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyven is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyven.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+if '__main__' == __name__:
+    from util import stderr
+else:
+    from .util import stderr
+
+def main():
+    for name in 'path', 'executable', 'argv':
+        stderr("%s: %s" % (name, getattr(sys, name)))
+
+if '__main__' == __name__:
+    main()
