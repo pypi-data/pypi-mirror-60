ccbbba4cf5af4ca9aae15683f423361b766ea0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:08 +0000

    Add package darkig/1.0/darkig-1.0.tar.gz

---
+++ packages/darkig/darkig-1.0.tar.gz/darkig-1.0/darkig/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+'''
+   :About:
+   __________________________________
+   #DEV : Nasir Ali
+   #Github : github.com/nasirxo
+   #Facebook : fb.com/nasir.xo
+   #Instagram : instagram.com/nasir.xoz
+
+   :IGHACK:
+   _____________________________________
+   Hack and Bruteforce instagram accounts
+
+'''
