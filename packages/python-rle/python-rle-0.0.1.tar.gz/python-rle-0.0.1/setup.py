3dc2aa7af1027faf036f025a4d7ad6a09b89702d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:59 +0000

    Add package python-rle/0.0.1/python-rle-0.0.1.tar.gz

---
+++ packages/python-rle/python-rle-0.0.1.tar.gz/python-rle-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='python-rle', 
+    version="0.0.1",
+    author="Tan Nian Wei",
+    author_email="tannianwei@aggienetwork.com",
+    description="Run-length encoding for data analysis in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tnwei/pyrle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
