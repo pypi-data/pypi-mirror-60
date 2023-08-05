b7f454868c94a817bc3328ca5e8eb279c3a3b6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:50 +0000

    Add package ezflags/1.3.3/ezflags-1.3.3.tar.gz

---
+++ packages/ezflags/ezflags-1.3.3.tar.gz/ezflags-1.3.3/README.rst
@@ -0,0 +1,75 @@
+ezflags
+=======
+
+.. image:: https://travis-ci.com/karx1/ezflags.svg?branch=master
+    :target: https://travis-ci.com/karx1/ezflags
+.. image:: https://badge.fury.io/py/ezflags.svg
+    :target: https://badge.fury.io/py/ezflags
+    :alt: PyPI package
+.. image:: https://readthedocs.org/projects/ezflags/badge/?version=latest
+	:target: https://ezflags.readthedocs.io/en/latest/?badge=latest
+	:alt: Documentation Status
+
+
+A tool that makes creating command line flags super easy.
+
+Built on `argparse <https://docs.python.org/3/library/argparse.html>`__,
+switching is no problem at all! You can even use the FlagParser as if it
+were a normal ArgumentParser for full integration with existing
+arguments.
+
+Install with:
+
+.. code:: bash
+
+   pip install ezflags
+
+Here’s a simple example:
+
+.. code:: py
+
+   # main.py
+   import ezflags
+
+   parser = ezflags.FlagParser()
+   parser.add_flag('--flag', '-f', value=True, help="A demo flag.")
+
+   flags = parser.parse_flags()
+   print(flags.flag)
+
+To integrate with ArgumentParser:
+
+.. code:: py
+
+   import ezflags
+
+   parser = ezflags.FlagParser()
+   parser.add_flag('--flag', '-f', value=True, help="A demo flag.")
+   parser.add_argument('--arg', '-a', help="A demo argument.")
+
+   args = parser.parse_args() # Flags are included, too!
+   print(args.flag)
+   print(args.arg)
+
+This can be invoked as such:
+
+.. code:: bash
+
+   python main.py --flag
+   # With ArgumentParser()
+   python main.py --flag --arg arg
+
+View the full documentation
+`here <https://ezflags.readthedocs.io/en/latest/>`__.
+
+Supports
+--------
+
+Supports Python 3.6 and up.
+
+License
+-------
+
+MIT license. See the
+`LICENSE <https://github.com/karx1/ezflags/blob/master/LICENSE>`__ file
+for more details.
---
+++ packages/ezflags/ezflags-1.3.3.tar.gz/ezflags-1.3.3/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import ezflags
+
+# MIT License
+#
+# Copyright (c) 2019-2020 karx1
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+with open("README.rst") as f:
+    readme = f.read()
+
+extras_require = {"docs": ["sphinx", "sphinx-rtd-theme"]}
+
+setup(
+    name="ezflags",
+    version=ezflags.__version__,
+    packages=["ezflags"],
+    url="https://github.com/karx1/ezflags",
+    project_urls={"Documentation": "https://ezflags.readthedocs.io/en/latest"},
+    license="MIT",
+    author="karx",
+    author_email="nerdstep710@gmail.com",
+    description="A tool that makes creating command line flags super easy.",
+    long_description=readme,
+    python_requires=">=3.6",
+    extras_require=extras_require,
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
+)
