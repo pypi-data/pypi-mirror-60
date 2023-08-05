2101d58e0dcbf0babd207d9e5e4cf886b20e7048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package pandapy/1.7/pandapy-1.7.tar.gz

---
+++ packages/pandapy/pandapy-1.7.tar.gz/pandapy-1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Command
+import os
+import sys
+
+setup(name='pandapy',
+      version='1.7',
+      description='Structured Numpy with Pandas a Click Away',
+      url='https://github.com/firmai/pandapy',
+      author='snowde',
+      author_email='d.snow@firmai.org',
+      license='MIT',
+      packages=['pandapy'],
+      install_requires=[
+          'pandas',
+          'numpy',
+          'scipy',
+          'numpy-groupies',
+          'numba',
+          'datetime',
+          'ipython'
+
+      ],
+      zip_safe=False)
