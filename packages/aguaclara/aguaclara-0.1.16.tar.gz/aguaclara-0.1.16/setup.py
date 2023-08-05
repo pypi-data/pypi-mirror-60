8998226a69a675ef9cec03362950ba7e49c6267a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:15 +0000

    Add package aguaclara/0.1.16/aguaclara-0.1.16.tar.gz

---
+++ packages/aguaclara/aguaclara-0.1.16.tar.gz/aguaclara-0.1.16/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aguaclara',
+    version = '0.1.16',
+    description = (
+        'An open-source Python package for designing and performing research '
+        'on AguaClara water treatment plants.'
+    ),
+    url = 'https://github.com/AguaClara/aguaclara',
+    author = 'AguaClara Cornell',
+    author_email = 'aguaclara@cornell.edu',
+    license = 'MIT',
+    packages = find_packages(),
+
+    # Ensure that this matches Pipfile > [packages]
+    install_requires = [
+        'matplotlib==3.0.3', # Supports Python 3.5
+        'urllib3',
+        'pint==0.8.1',
+        'pandas',
+        'scipy',
+    ],
+
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    include_package_data=True,
+    test_suite="tests",
+    zip_safe=False
+)
