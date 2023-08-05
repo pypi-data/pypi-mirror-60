c0afd42f7f994d2b0c2b3282a867ae75e470c79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:28 +0000

    Add package UCS633-topsis-SumitJain/2.0.1/UCS633-topsis-SumitJain-2.0.1.tar.gz

---
+++ packages/UCS633-topsis-SumitJain/UCS633-topsis-SumitJain-2.0.1.tar.gz/UCS633-topsis-SumitJain-2.0.1/packagesj/__init__.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 20:22:17 2020
+
+@author: Sumit 
+"""
+
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+import math
+from sys import argv
+
+
+def topsis():
+    if (len(argv[2].split(",")) != len(argv[3].split(","))):
+        print("Please Enter Valid Command Line Arguments!!")
+        exit(0)
+    else:
+        file = open(argv[1], 'rb')
+        ws = argv[2]
+        ws1 = ws.split(",")
+        wt = list(map(int, ws1))
+        op = argv[3].split(",")
+        sqrs = []
+
+        dataset = pd.read_csv(file)
+        names = dataset.iloc[:, 0]
+        data = dataset.iloc[:, 1:].values
+        r = data.shape[0]
+        c = data.shape[1]
+
+        for i in range(0, c):
+            s1 = 0
+            for j in range(0, r):
+                s1 = s1 + math.pow(data[j][i], 2)
+            s1 = math.sqrt(s1)
+            sqrs.append(s1)
+
+        s2 = 0
+        for k in range(0, c):
+            s2 = s2 + wt[k]
+
+        for k in range(0, c):
+            wt[k] = wt[k] / s2
+
+        for i in range(0, c):
+            for j in range(0, r):
+                data[j][i] = (data[j][i] / sqrs[i]) * wt[i]
+
+        mx1 = []
+        mn1 = []
+        bst = []
+        wrst = []
+        l1 = []
+        for i in range(0, c):
+            l1 = data[:, i]
+            mx1 = max(l1)
+            mn1 = min(l1)
+            if (op[i] == '+'):
+                bst.append(mx1)
+                wrst.append(mn1)
+            elif (op[i] == '-'):
+                bst.append(mn1)
+                wrst.append(mx1)
+
+        sp = []
+        sn = []
+        for i in range(0, r):
+            sump = 0
+            sumn = 0
+            for j in range(0, c):
+                sump = sump + math.pow((data[i][j] - bst[j]), 2)
+                sumn = sumn + math.pow((data[i][j] - wrst[j]), 2)
+            sp.append(math.sqrt(sump))
+            sn.append(math.sqrt(sumn))
+        prfm = []
+        for i in range(0, r):
+            prfm.append(sn[i] / (sp[i] + sn[i]))
+
+        print("The Preferential order(Rank) is : ")
+        rl = list(map(int, len(prfm) - st.rankdata(prfm) + 1))
+        for i in range(len(rl)):
+            print(names[i], "-->", rl[i])
+
+        res = max(prfm)
+
+        for i in range(0, len(prfm)):
+            if prfm[i] == res:
+                index = i
+        print("Best Option is  : ", names[index])
+
+
+if __name__ == '__main__':
+    topsis()
+
+
+
+
+
---
+++ packages/UCS633-topsis-SumitJain/UCS633-topsis-SumitJain-2.0.1.tar.gz/UCS633-topsis-SumitJain-2.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        README=f.read()
+    return README
+
+setuptools.setup(
+    name="UCS633-topsis-SumitJain",
+    version="2.0.1",
+    author="Sumit Jain",
+    author_email="sumitjaini1998@gmail.com",
+    description="A python package to implement topsis(MCDM)",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SumitJain01/MyProjects",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+	 "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    #packages=["packagesj"],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=["pandas","numpy","scipy"],
+    entry_points={
+        "console_scripts":[
+            "UCS633-topsis-SumitJain-101703570=packagesj.__init__:topsis",
+        ]
+    },
+)
