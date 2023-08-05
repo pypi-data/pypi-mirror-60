704ee2b201ef83faf389d442b88801cb5002e8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:38 +0000

    Add package torchio/0.12.9/torchio-0.12.9-py2.py3-none-any.whl

---
+++ packages/torchio/torchio-0.12.9-py2.py3-none-any.whl/torchio/transforms/__init__.py
@@ -0,0 +1,21 @@
+from .transform import Transform
+from .interpolation import Interpolation
+
+# Augmentation
+from .augmentation.spatial import RandomFlip
+from .augmentation.spatial import RandomAffine
+from .augmentation.spatial import RandomElasticDeformation
+
+from .augmentation.intensity import RandomNoise
+from .augmentation.intensity import RandomMotion
+from .augmentation.intensity import RandomBiasField
+
+# Preprocessing
+from .preprocessing import Pad
+from .preprocessing import Crop
+from .preprocessing import Rescale
+from .preprocessing import Resample
+from .preprocessing import ToCanonical
+from .preprocessing import ZNormalization
+from .preprocessing import CenterCropOrPad
+from .preprocessing import HistogramStandardization
