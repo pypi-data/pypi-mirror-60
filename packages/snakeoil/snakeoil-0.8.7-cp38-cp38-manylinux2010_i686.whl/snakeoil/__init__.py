c9ef77b741215b8f33ff6025527f82e0295f6a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:54 +0000

    Add package snakeoil/0.8.7/snakeoil-0.8.7-cp38-cp38-manylinux2010_i686.whl

---
+++ packages/snakeoil/snakeoil-0.8.7-cp38-cp38-manylinux2010_i686.whl/snakeoil/__init__.py
@@ -0,0 +1,17 @@
+"""
+snakeoil library
+
+This library is a bit of a grabbag of the following:
+
+* implementations that make nasty/hard problems simple in usage
+* standard lib fixups; a new style UserDict base class for example that
+  is designed around iter* overriding, rather than sequence methods as
+  UserDict is.
+* python version compatibility; snakeoil supports 3.4 through 3.7,
+  exposing fallback implementations of desirable functionality in older python
+  versions.
+* optimized implementations of common patterns
+"""
+
+__title__ = 'snakeoil'
+__version__ = '0.8.7'
