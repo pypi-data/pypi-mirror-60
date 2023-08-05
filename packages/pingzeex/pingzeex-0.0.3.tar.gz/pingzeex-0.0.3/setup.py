17891e532a31349cd9f649aa3624ad38e989edab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:47 +0000

    Add package pingzeex/0.0.3/pingzeex-0.0.3.tar.gz

---
+++ packages/pingzeex/pingzeex-0.0.3.tar.gz/pingzeex-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+import os
+
+print(os.getcwd())
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+
+setup(
+	name='pingzeex',
+	version='0.0.3',
+	description='Pingzeex Python Library',
+	py_modules=["pingzeex"],
+	package_dir={'':'src'},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		],
+		long_description=long_description,
+		long_description_content_type="text/markdown",
+		url= "https://github.com/nishgaba-ai/pingzeex",
+		author="Nishchal Gaba",
+		author_email="nishgaba.ai@gmail.com",
+)
