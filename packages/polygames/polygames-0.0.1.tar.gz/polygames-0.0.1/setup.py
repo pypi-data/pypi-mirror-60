1ece7aa6b8c14964d02eb2d4a6883c05658f7981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package polygames/0.0.1/polygames-0.0.1.tar.gz

---
+++ packages/polygames/polygames-0.0.1.tar.gz/polygames-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name="polygames",
+      version="0.0.1",
+      license="MIT",
+      description="TBD",
+      author="TBD",
+      packages=find_packages(),
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Intended Audience :: Science/Research",
+                   "Topic :: Scientific/Engineering",
+                   "Programming Language :: Python"],
+      install_requires=[],
+)
