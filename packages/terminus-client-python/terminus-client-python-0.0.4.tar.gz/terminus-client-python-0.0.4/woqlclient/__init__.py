a602469a78bb9cdb11ca5a3309be17fefd4c306f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package terminus-client-python/0.0.4/terminus-client-python-0.0.4.tar.gz

---
+++ packages/terminus-client-python/terminus-client-python-0.0.4.tar.gz/terminus-client-python-0.0.4/woqlclient/__init__.py
@@ -0,0 +1,13 @@
+"""
+    woqlClient Library
+    ~~~~~~~~~~~~~~~~~~~~~
+
+"""
+from .woqlClient import *
+from .woql import WOQLQuery
+
+__all__ = ['woqlClient', 'woql']
+
+name = "woqlclient"
+__version__ = "0.0.4"
+# cmdclass = {'test': PyTest}
