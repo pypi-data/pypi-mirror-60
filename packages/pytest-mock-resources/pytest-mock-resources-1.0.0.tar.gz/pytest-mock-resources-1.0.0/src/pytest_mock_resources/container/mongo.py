1c78e31d26c338c840cee15f7ed0f72e337be261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:19 +0000

    Add package pytest-mock-resources/1.0.0/pytest-mock-resources-1.0.0.tar.gz

---
+++ packages/pytest-mock-resources/pytest-mock-resources-1.0.0.tar.gz/pytest-mock-resources-1.0.0/src/pytest_mock_resources/container/mongo.py
@@ -0,0 +1,45 @@
+import pytest
+
+from pytest_mock_resources.container import (
+    ContainerCheckFailed,
+    get_container_fn,
+    get_docker_host,
+    IN_CI,
+)
+
+# XXX: To become overwritable via pytest config.
+config = {
+    "port": 27017 if IN_CI else 28017,
+    "root_database": "dev-mongo",
+    "image": "mongo:3.6",
+}
+
+
+def get_pymongo_client():
+    from pymongo import MongoClient
+
+    uri = "mongodb://{}:{}".format(get_docker_host(), config["port"])
+
+    return MongoClient(uri)
+
+
+def check_mongo_fn():
+    from pymongo.errors import ConnectionFailure
+
+    try:
+        client = get_pymongo_client()
+        db = client[config["root_database"]]
+        db.command("ismaster")
+    except ConnectionFailure:
+        raise ContainerCheckFailed(
+            "Unable to connect to a presumed MongoDB test container via given config: {}".format(
+                config
+            )
+        )
+
+
+_mongo_container = pytest.fixture("session")(
+    get_container_fn(
+        "_mongo_container", config["image"], {27017: config["port"]}, {}, check_mongo_fn
+    )
+)
