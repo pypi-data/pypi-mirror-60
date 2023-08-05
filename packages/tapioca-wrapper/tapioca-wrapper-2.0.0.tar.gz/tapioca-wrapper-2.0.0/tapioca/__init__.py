83017f1253113c61aa2fa8ae5cbb48aef2bf48d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package tapioca-wrapper/2.0.0/tapioca-wrapper-2.0.0.tar.gz

---
+++ packages/tapioca-wrapper/tapioca-wrapper-2.0.0.tar.gz/tapioca-wrapper-2.0.0/tapioca/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+__author__ = 'Filipe Ximenes'
+__email__ = 'filipeximenes@gmail.com'
+__version__ = '2.0.0'
+
+
+from .adapters import (
+    generate_wrapper_from_adapter,
+    TapiocaAdapter,
+    FormAdapterMixin, JSONAdapterMixin)
