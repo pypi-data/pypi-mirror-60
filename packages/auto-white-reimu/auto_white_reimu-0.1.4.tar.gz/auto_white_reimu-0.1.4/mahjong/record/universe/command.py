48e4133ff0cb60b7b70ac6c80508a94c1a43bc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:33 +0000

    Add package auto-white-reimu/0.1.4/auto_white_reimu-0.1.4.tar.gz

---
+++ packages/auto-white-reimu/auto_white_reimu-0.1.4.tar.gz/auto_white_reimu-0.1.4/mahjong/record/universe/command.py
@@ -0,0 +1,182 @@
+import ast
+from collections import namedtuple
+from typing import List, TypeVar, Callable, Iterable
+
+import numpy
+import pandas
+from loguru import logger
+
+from mahjong.record.universe.property_manager import prop_manager
+from mahjong.record.universe.format import View, Update
+
+
+def is_empty(x):
+    return x is None or x != x
+
+
+def norm_empty(x):
+    return None if is_empty(x) else x
+
+
+def norm_value_str(x: str):  # FIXME: take caution about type with str
+    return ast.literal_eval(x) if x != "" else None
+
+
+class GameProperty:
+    def __init__(self, view_property: View, update_method: Update):
+        self.view_property = view_property
+        self.update_method = update_method
+        # self.default_ctor = default_ctor
+
+    @property
+    def scope(self):
+        return self.view_property.scope
+
+
+command_field_names = [
+    "timestamp",
+    "scope",
+    "sub_scope_id",
+    "property",
+    "update_method",
+    "value",
+]
+_Game_command = namedtuple(
+    "GameCommand_",
+    field_names=command_field_names
+)
+
+command_field_names_set = set(command_field_names)
+
+
+class GameCommand:
+    def __init__(self, *, prop: View, update: Update, sub_scope=None, value=None, timestamp=None):
+        self.timestamp = timestamp
+        # if value is None and prop.default_ctor is not None and prop.update_method != Update.CLEAR:
+        #     self.value = prop.default_ctor()
+        # else:
+        self.sub_scope_id = sub_scope
+        self.value = value
+        self.property = prop
+        self.update_method = update
+        self.prop = GameProperty(self.property, self.update_method)
+
+    # @staticmethod
+    # def multi_command(props: Iterable[GameProperty], sub_scope_id=None, value=None, timestamp=None):
+    #     return [GameCommand(
+    #         prop,
+    #         sub_scope_id=sub_scope_id,
+    #         value=value,
+    #         timestamp=timestamp
+    #     ) for prop in props]
+
+    def __str__(self):
+        return str(self.to_record())
+
+    def __repr__(self):
+        return "{%s}" % str(self)
+
+    @staticmethod
+    def clean(pandas_dataframe):
+        return pandas_dataframe.apply(GameCommand.pandas_columns_clean, axis="columns")
+
+
+    @staticmethod
+    def to_dataframe(command_list):
+        return pandas.DataFrame(
+            (x.to_record() for x in command_list),
+        )
+
+    @staticmethod
+    def read_clean_csv(csv_path):
+        return GameCommand.clean(pandas.read_csv(csv_path))
+
+    @staticmethod
+    def pandas_columns_clean(row):
+        # remove index
+        row = row[command_field_names]
+        series_ctor = type(row)
+        record = _Game_command(**row)
+        command = GameCommand.from_record(record)
+        target = numpy.array(command.to_raw_record(), dtype=object)
+        target_series = series_ctor(target)
+        target_series.index = command_field_names
+        return target_series
+
+    def to_raw_record(self):
+        return _Game_command(
+            timestamp=self.timestamp,
+            scope=self.prop.scope,
+            sub_scope_id=self.sub_scope_id,
+            property=self.prop.view_property,
+            update_method=self.prop.update_method,
+            value=self.value,
+        )
+
+    @staticmethod
+    def from_raw_record(record: _Game_command):
+        return GameCommand(
+            prop=record.property,
+            update=record.update_method,
+            sub_scope=norm_empty(record.sub_scope_id),
+            value=norm_empty(record.value),
+            timestamp=norm_empty(record.timestamp),
+        )
+
+    def to_record(self):
+        return _Game_command(
+            timestamp=self.timestamp,
+            scope=self.prop.scope.name,
+            sub_scope_id=self.sub_scope_id,
+            property=self.prop.view_property.name,
+            update_method=self.prop.update_method.name,
+            value=prop_manager.to_str(self.value, self.prop.view_property),
+        )
+
+    @staticmethod
+    def from_record(record: _Game_command):
+        view = View.by_name(record.scope)[record.property]
+        return GameCommand(
+            prop=view,
+            update=Update[record.update_method],
+            sub_scope=norm_empty(record.sub_scope_id),
+            value=prop_manager.from_str(record.value, view=view),
+            timestamp=norm_empty(record.timestamp),
+        )
+
+
+EventT = TypeVar('EventT')
+EventTransform = Callable[[EventT], Iterable[GameCommand]]
+
+
+class CommandTranslator:
+    def __init__(self):
+        self.defaults = []
+        self.defaults: List[EventTransform]
+
+    @staticmethod
+    def fallback_call(event, matchers: List[EventTransform]) -> List[GameCommand]:
+        for matcher in matchers:
+            value = list(matcher(event))
+            if value:
+                return value
+
+    # TODO: add preprocess and postprocess for event and command list. (to support timestamp attaching in tenhou).
+
+    def default_event(self, func: EventTransform) -> EventTransform:
+        self.defaults.append(func)
+        return func
+
+    def translate(self, event: EventT) -> List[GameCommand]:
+        pass
+
+    def __call__(self, event: EventT) -> List[GameCommand]:
+        return_value = self.translate(event)
+        if return_value:
+            return return_value
+        return_value = CommandTranslator.fallback_call(event, self.defaults)
+        if return_value:
+            return return_value
+        else:
+            logger.warning("event <{}> is not transformed to game commands.", event)
+            return []
---
+++ packages/auto-white-reimu/auto_white_reimu-0.1.4.tar.gz/auto_white_reimu-0.1.4/mahjong/record/universe/executor.py
@@ -0,0 +1,261 @@
+import operator
+
+from typing import Iterable
+
+from mahjong.record.universe.command import GameCommand
+from mahjong.record.universe.property_manager import prop_manager
+from mahjong.record.universe.format import *
+import mahjong.record.universe.format as mahjong_format
+import pandas as pd
+
+
+def get_enums_from(module):
+    for name in dir(module):
+        typ = getattr(module, name)
+        if isinstance(typ, type) and issubclass(typ, Enum):
+            yield name, typ
+
+
+enums = dict(get_enums_from(mahjong_format))
+
+
+def replace_enum_values(value_str):
+    if isinstance(value_str, str):
+        split = value_str.split(".")
+        if len(split) == 2:
+            typ, name = split
+            return enums[typ][name]
+    return value_str
+
+
+def null():
+    return None
+
+
+defaultExecutor = {
+    Update.RESET_DEFAULT: null,
+    Update.ADD: operator.add,
+    Update.REMOVE: operator.sub,
+}
+
+
+def fill_value_executor():
+    return {
+        Update.ADD: operator.add,
+        Update.REMOVE: operator.sub,
+    }
+
+
+def remove_all(lst: list, values: list):
+    lst_r = lst.copy()
+    for val in values:
+        lst_r.remove(val)
+    return lst_r
+
+
+listExecutor = {
+    Update.ADD: operator.add,
+    Update.REMOVE: remove_all,
+}
+
+setExecutor = {
+    Update.ADD: operator.or_,
+    Update.REMOVE: operator.sub,
+}
+
+
+# def setdefault(self, k, default):
+#     if k in self:
+#         return self
+#     self
+class CombinedCommandExecutor:
+    def __init__(self, executor_with_type):
+        self.executor_with_type = executor_with_type
+
+    def has_function(self, value, method: Update):
+        for typ, executor in self.executor_with_type:
+            if typ is None or isinstance(value, typ):
+                if method in executor:
+                    return True
+        return False
+
+    def execute_value(self, command: GameCommand, origin_value=None):
+        for typ, executor in self.executor_with_type:
+            if typ is None or isinstance(command.value, typ):
+                method = command.prop.update_method
+                if method.operand_num() == 2:
+                    return executor[method](origin_value, command.value)
+                elif method.operand_num() == 1:
+                    return executor[method](command.value)
+                elif method.operand_num() == 0:
+                    raise ValueError("Zero operand is not supported. Type of None value may be ambiguous.")
+
+
+def state_series_apply(x):
+    col_last = x.name[-1]
+    if hasattr(col_last, "type"):
+        def from_str_view(t):
+            return prop_manager.from_str(t, col_last)
+
+        return x.apply(from_str_view)
+    return x
+
+
+class GameExecutor:
+    @staticmethod
+    def read_clean_csv(csv_path):
+        df_states = pd.read_csv(csv_path, header=[0, 1, 2], skipinitialspace=True)
+        df_states = df_states.rename(columns=replace_enum_values)
+        df_states = df_states.apply(state_series_apply, axis="rows")
+        return df_states
+
+    def __init__(self):
+        self.executor = CombinedCommandExecutor([
+            (list, listExecutor),
+            (set, setExecutor),
+            (None, defaultExecutor),
+        ])
+        state_dict = {
+            enum: {
+                "single": {},
+            } if multi_value is None else {
+                key: {} for key in multi_value
+            } for enum, multi_value in ViewScope.scopes_with_multi_value().items()
+        }
+        all_dict = {
+            "global": {
+                "time": {
+
+                },
+            },
+            **state_dict
+        }
+        self.states = TransferDict(all_dict)
+
+    @staticmethod
+    def state_value(state_dict, command: GameCommand):
+        scope_key = command.prop.scope
+        multi_values = ViewScope.scopes_with_multi_value()[scope_key]
+        if multi_values is None:
+            return state_dict[scope_key]["single"]
+        else:
+            return state_dict[scope_key][multi_values(command.sub_scope_id)]
+
+    def execute(self, commands: Iterable[GameCommand]):
+        curr_state = self.states
+        for command in commands:
+            curr_state = self.execute_update_state(command, curr_state)
+            yield curr_state["global"]["time"].set("timestamp", command.timestamp)
+
+    def execute_as_dataframe(self, commands: Iterable[GameCommand]):
+        df = pd.DataFrame(x.flatten() for x in self.execute(commands))
+        df.columns = pd.MultiIndex.from_tuples(df.columns)
+        return df
+
+    def execute_update_state(self, command, curr_state):
+        method = command.prop.update_method
+        view = GameExecutor.state_value(curr_state, command)
+        view_property = command.prop.view_property
+        if method == Update.REPLACE:
+            result_state = view.set(
+                view_property,
+                command.value
+            )
+        elif method == Update.CLEAR:
+            result_state = view.drop(view_property)
+        elif method == Update.ADD or method == Update.REMOVE:
+            result_state = view.set(
+                view_property,
+                self.executor.execute_value(
+                    command, view[view_property]
+                ),
+            )
+        elif method == Update.RESET_DEFAULT:
+            result_state = view.set(
+                view_property,
+                prop_manager.get_default(view_property),
+            )
+        else:
+            raise ValueError("unrecognized", method)
+        return result_state
+
+
+class TransferDict:
+    def __init__(self, nested_dict, parent=None, parent_key=None):
+        self.parent: TransferDict = parent
+        self.parent_key = parent_key
+        self.nested_dict = {
+            k: self.transfer_value(k, v)  # fix construction (also copy TransferDict)
+            for k, v in nested_dict.items()
+        }
+
+    def flatten_iter(self):
+        for k, v in self.nested_dict.items():
+            initial = [k]
+            if isinstance(v, TransferDict):
+                for sub_k, sub_v in v.flatten_iter():
+                    yield tuple(initial + list(sub_k)), sub_v
+            else:
+                yield tuple(initial), v
+
+    def flatten(self):
+        return dict((k, prop_manager.to_str(v, k[-1])) for k, v in self.flatten_iter())
+
+    def __getattr__(self, item):
+        return getattr(self.nested_dict, item)
+
+    def __getitem__(self, item):
+        return self.nested_dict[item]
+
+    def __str__(self):
+        return str(self.nested_dict)
+
+    def __repr__(self):
+        return repr(self.nested_dict)
+
+    def _readonly(self, *args, **kwards):
+        raise NotImplemented
+
+    __setitem__ = __delattr__ = pop = update = popitem = _readonly
+
+    def set(self, key, value):
+        modified_dict = {
+            **self.nested_dict,
+            key: value
+        }
+        modified = TransferDict(modified_dict, parent=self.parent, parent_key=self.parent_key)
+        if self.parent is not None:
+            return self.parent.set(
+                self.parent_key, modified
+            )
+        else:
+            # modified._update_link()
+            return modified
+
+    # def _update_link(self):
+    #     for k,v in self.nested_dict.items():
+    #         if isinstance(v, TransferDict):
+    #             v.parent = self
+    #             v.parent_key =
+
+    def drop(self, key):
+        dict_cpy = self.nested_dict.copy()
+        del dict_cpy[key]
+        modified = TransferDict(dict_cpy)
+        return self.parent.set(
+            self.parent_key, modified
+        ) if self.parent is not None else modified
+
+    def setdefault(self, key, default):
+        if key not in self:
+            return self.set(key, default)
+        else:
+            return self
+
+    def transfer_value(self, k, v):
+        if isinstance(v, dict):
+            return TransferDict(v, self, k)
+        elif isinstance(v, TransferDict):
+            return TransferDict(v.nested_dict, self, k)
+        else:
+            return v
