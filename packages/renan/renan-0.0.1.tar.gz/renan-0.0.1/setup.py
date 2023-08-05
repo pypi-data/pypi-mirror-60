7e67d2913f41f7b1bb7a03156f6df3c6da5777d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:52 +0000

    Add package renan/0.0.1/renan-0.0.1.tar.gz

---
+++ packages/renan/renan-0.0.1.tar.gz/renan-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+MODULE_NAME = 'renan'         # package name used to install via pip.
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def requirements_from_pip(filename='requirements.txt'):
+    with open(filename, 'r') as pip:
+        return [l.strip() for l in pip if not l.startswith('#') and l.strip()]
+
+
+setup(
+    name=MODULE_NAME,
+    version="0.0.1",
+    author="Renan Oliveira Biazotto",
+    author_email="renan.biazotto@timeenergy.com.br",
+    description="My first package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    install_requires=requirements_from_pip(),
+    extras_require={"test_deps": requirements_from_pip('requirements_test.txt')},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
