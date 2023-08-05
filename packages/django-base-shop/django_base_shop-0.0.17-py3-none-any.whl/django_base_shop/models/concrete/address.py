6beb7e3c466fe8940c2da64b6628ec16a43db433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:39 +0000

    Add package django-base-shop/0.0.17/django_base_shop-0.0.17-py3-none-any.whl

---
+++ packages/django-base-shop/django_base_shop-0.0.17-py3-none-any.whl/django_base_shop/models/concrete/address.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from .country import Country
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=20)
+
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+
+    @property
+    def formatted(self) -> str:
+        return f"{self.street}\n{self.city} {self.state} {self.postcode}\n{self.country.name}"
