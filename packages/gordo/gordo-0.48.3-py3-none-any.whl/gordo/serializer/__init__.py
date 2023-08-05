6c4b6435350a6205b2af0975092d310026769fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:04 +0000

    Add package gordo/0.48.3/gordo-0.48.3-py3-none-any.whl

---
+++ packages/gordo/gordo-0.48.3-py3-none-any.whl/gordo/serializer/__init__.py
@@ -0,0 +1,3 @@
+from .pipeline_from_definition import pipeline_from_definition
+from .pipeline_into_definition import pipeline_into_definition
+from .serializer import dump, dumps, load, loads, load_metadata
