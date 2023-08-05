d9b16a09da1896d96e38531e5b37983c5a97b11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:10 +0000

    Add package search-engine-scraper/0.3/search_engine_scraper-0.3.tar.gz

---
+++ packages/search-engine-scraper/search_engine_scraper-0.3.tar.gz/search_engine_scraper-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='search_engine_scraper',
+      version='0.3',
+      description='A module to scrape popular search engines',
+      long_description=long_description,
+       long_description_content_type='text/markdown',
+      url='https://github.com/deepsidh9/Search-Engine-Scraper',
+      author='Deep Singh Dhillon',
+      author_email='deep.barca@gmail.com',
+      license='MIT',
+      packages=['search_engine_scraper'],
+      install_requires=[
+          'requests',
+          'lxml'
+      ],
+      include_package_data=True,
+      package_data={'': ['search_engine_scraper/*.txt']},
+      zip_safe=False)
