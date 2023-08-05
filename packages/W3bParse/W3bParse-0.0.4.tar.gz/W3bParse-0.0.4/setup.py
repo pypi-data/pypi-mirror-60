04f21b15fd7e0b33502da50b3e853e230932d383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package W3bParse/0.0.4/W3bParse-0.0.4.tar.gz

---
+++ packages/W3bParse/W3bParse-0.0.4.tar.gz/W3bParse-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="W3bParse", # Replace with your own username
+    version="0.0.4",
+    author="Colby Sehnert",
+    author_email="colbysehnert@gmail.com",
+    description="A simple web parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(where='WebParse'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+	'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+	'Programming Language :: Python',
+    ],
+    python_requires='>=3.8',
+)
