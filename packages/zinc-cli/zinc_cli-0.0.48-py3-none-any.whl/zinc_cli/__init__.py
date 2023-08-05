fe109f3a3276f14d4550ac568f10738a401a6b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:05 +0000

    Add package zinc-cli/0.0.48/zinc_cli-0.0.48-py3-none-any.whl

---
+++ packages/zinc-cli/zinc_cli-0.0.48-py3-none-any.whl/zinc_cli/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+__version__ = "0.0.48"
+
+from .zinc import main
+from .commands import zinc_create, zinc_transform
+
+print("Imported ZINC CLI")
