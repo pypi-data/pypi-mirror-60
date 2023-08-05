35da1006b6010bb81f72eab9338639e71c1e8f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:30 +0000

    Add package django-jsonschema-form/1.0.3/django-jsonschema-form-1.0.3.tar.gz

---
+++ packages/django-jsonschema-form/django-jsonschema-form-1.0.3.tar.gz/django-jsonschema-form-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    author="Collabo Software Ltda",
+    author_email="basask@collabo.com.br",
+    description="Django admin widgets to render jsonschema as forms",
+    name="django-jsonschema-form",
+    long_description="Django admin widgets to render jsonschema as forms",
+    long_description_content_type='text/markdown',
+    version="1.0.3",
+    url="https://www.collabo.com.br/",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    package_data={
+        'jsonschemaform': ['templates/*.html', 'static/**/*.js'],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
