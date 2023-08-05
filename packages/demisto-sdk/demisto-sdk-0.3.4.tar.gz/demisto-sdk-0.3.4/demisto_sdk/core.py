a0b0ced6d0cf8c698da14ea85da7555b2496508b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package demisto-sdk/0.3.4/demisto-sdk-0.3.4.tar.gz

---
+++ packages/demisto-sdk/demisto-sdk-0.3.4.tar.gz/demisto-sdk-0.3.4/demisto_sdk/core.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+
+# DemistoSDK
+# Python client that invokes functions in the Demisto SDK.
+#
+# Author:       Demisto
+# Version:      0.3.4
+#
+
+
+class DemistoSDK:
+    """
+    The core class for the SDK.
+    """
+
+    def __init__(self):
+        self.configuration = None
