2e0317ad1582c51803c0c0837f457aa95a03a3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:43 +0000

    Add package horno/1.0.2.12/horno-1.0.2.12.tar.gz

---
+++ packages/horno/horno-1.0.2.12.tar.gz/horno-1.0.2.12/horno/datos/Colecciones.py
@@ -0,0 +1,47 @@
+import random
+import re
+
+from horno.utiles.Singleton import Singleton
+
+
+#=============================================================================================
+class Textos (metaclass=Singleton):
+
+    #------------------------------------------------------------------------------------------
+    def __init__(self):
+
+        ''
+
+    #------------------------------------------------------------------------------------------
+    def ContieneDigitos(self, texto):
+    
+        return not re.match(".*[0-9]+.*", texto) is None
+
+#=============================================================================================
+class Listas (metaclass=Singleton):
+
+    #------------------------------------------------------------------------------------------
+    def __init__(self):
+
+        ''
+
+    #------------------------------------------------------------------------------------------
+    def ElegirValor(self, key, default, dic):
+        
+        return dic.get(key, default)
+
+    #------------------------------------------------------------------------------------------
+    def ElegirPrimeroNoVacio(self, default, lista):
+        
+        lista_no_vacios = [e for e in lista if len(e)]
+        return lista_no_vacios[0] if len(lista_no_vacios) else default
+
+    #------------------------------------------------------------------------------------------
+    def Unique(self, lista):
+        
+        return [e for n, e in enumerate(lista) if e not in lista[n + 1:]]
+
+    #------------------------------------------------------------------------------------------
+    def RandomEn(self, valor):
+        
+        return random.choice(valor)
