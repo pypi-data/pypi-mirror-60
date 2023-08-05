20315e5259d7f75ae9fba6e16b8ca5eb33207a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package hgvs/1.4.0/hgvs-1.4.0.tar.gz

---
+++ packages/hgvs/hgvs-1.4.0.tar.gz/hgvs-1.4.0/tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+
+import hgvs.easy
+from hgvs.extras.babelfish import Babelfish
+
+
+@pytest.fixture(scope="session")
+def parser():
+    return hgvs.easy.parser
+
+
+@pytest.fixture(scope="session")
+def am37():
+    return hgvs.easy.am37
+
+
+@pytest.fixture(scope="session")
+def am38():
+    return hgvs.easy.am38
+
+
+@pytest.fixture(scope="session")
+def hdp():
+    return hgvs.easy.hdp
+
+
+@pytest.fixture(scope="session")
+def babelfish38(hdp):
+    return Babelfish(hdp, assembly_name="GRCh38")
