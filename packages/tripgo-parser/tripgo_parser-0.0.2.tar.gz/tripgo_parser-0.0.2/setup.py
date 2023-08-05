1aaed84a50c03f7cf53cfc2da5c4e2f25c83cbc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:15 +0000

    Add package tripgo-parser/0.0.2/tripgo_parser-0.0.2.tar.gz

---
+++ packages/tripgo-parser/tripgo_parser-0.0.2.tar.gz/tripgo_parser-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tripgo_parser',
+    version='0.0.2',
+    description='Parser for use with the TripGo API',
+    url='https://github.com/leongjoseph/tripgo-parser',
+    author='Joseph Leong',
+    author_email='leong.joseph@me.com',
+    license='MIT',
+    packages=['tripgo'],
+    install_requires=['numpy','pandas','requests']  # include all imports
+)
