dae4168c2fe2cb5376ce5527bc9f3073af660eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:38 +0000

    Add package bmrs/1.2.84/bmrs-1.2.84.tar.gz

---
+++ packages/bmrs/bmrs-1.2.84.tar.gz/bmrs-1.2.84/sample_client.py
@@ -0,0 +1,16 @@
+'''This could be your example consumer file from which you will receive and process the API responses'''
+import bmrs
+
+
+def on_message(json_response):
+    '''Receive a JSON response and do stuff with it'''
+    print(list(json_response.keys()))
+    # print(json_response)
+
+
+# connect to the bmrs API using your credentials
+bmrs.connect_to_api(
+        api_key = 'YOUR API KEY HERE',
+        client_id = 'YOUR_CLIENT_ID_HERE',
+        listener=on_message
+    )
---
+++ packages/bmrs/bmrs-1.2.84.tar.gz/bmrs-1.2.84/test_bmrs.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestBmrsReceiver(unittest.TestCase):
+    def test_connect_to_api(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
