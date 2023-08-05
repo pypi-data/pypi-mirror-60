845eaa803e54c107f23fb95ad501aaa57b3b9da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:03 +0000

    Add package botsic/0.0.3/botsic-0.0.3.tar.gz

---
+++ packages/botsic/botsic-0.0.3.tar.gz/botsic-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='botsic',
+    version='0.0.3',
+    description='Creates bots',
+    url='https://github.com/ifosch/botsic',
+    download_url='https://github.com/ifosch/botsic/archive/0.0.3.tar.gz',
+    author='Ignasi Fosch',
+    author_email='natx@y10k.ws',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=['botsic'],
+    include_package_data=True,
+    install_requires=['python-telegram-bot', ],
+    scripts=['bin/botsic'],
+)
