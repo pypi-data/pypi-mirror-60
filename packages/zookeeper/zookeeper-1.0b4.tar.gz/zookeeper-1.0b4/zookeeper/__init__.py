b4236bbcdb48643cf7331a5b935e551f5ac5df62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:51 +0000

    Add package zookeeper/1.0b4/zookeeper-1.0b4.tar.gz

---
+++ packages/zookeeper/zookeeper-1.0b4.tar.gz/zookeeper-1.0b4/zookeeper/__init__.py
@@ -0,0 +1,3 @@
+from zookeeper.core import cli, component, configure, task
+
+__all__ = ["cli", "component", "configure", "task"]
---
+++ packages/zookeeper/zookeeper-1.0b4.tar.gz/zookeeper-1.0b4/zookeeper/tf/experiment.py
@@ -0,0 +1,25 @@
+from typing import Optional, Union
+
+from tensorflow import keras
+
+from zookeeper.tf.dataset import Dataset
+from zookeeper.tf.model import Model
+from zookeeper.tf.preprocessing import Preprocessing
+
+
+class Experiment:
+    """
+    A wrapper around a Keras experiment. Subclasses must implement their
+    training loop in `run`.
+    """
+
+    # Nested components
+    dataset: Dataset
+    preprocessing: Preprocessing
+    model: Model
+
+    # Parameters
+    epochs: int
+    batch_size: int
+    loss: Optional[Union[keras.losses.Loss, str]]
+    optimizer: Union[keras.optimizers.Optimizer, str]
