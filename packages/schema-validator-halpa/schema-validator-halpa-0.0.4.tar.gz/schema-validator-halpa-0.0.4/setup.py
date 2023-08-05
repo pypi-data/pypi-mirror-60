98103df759aa503c58eb5bcf3803ef95f17f9fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:00 +0000

    Add package schema-validator-halpa/0.0.4/schema-validator-halpa-0.0.4.tar.gz

---
+++ packages/schema-validator-halpa/schema-validator-halpa-0.0.4.tar.gz/schema-validator-halpa-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="schema-validator-halpa",
+    version="0.0.4",
+    author="Daniel Leppänen",
+    author_email="daniel.leppanen@gmail.com",
+    description="A utility to validate JSON schemas with jsonschema with simplejson",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/halpdesk/python-halpa-schema-validator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    build_requires=['simplejson>=3.17.0', 'jsonschema>=3.2.0'],
+    install_requires=['simplejson>=3.17.0', 'jsonschema>=3.2.0']
+)
