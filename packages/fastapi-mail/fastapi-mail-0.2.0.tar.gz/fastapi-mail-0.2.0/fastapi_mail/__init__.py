e128946b202b196aa7788e9834829df5a7fac4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:29 +0000

    Add package fastapi-mail/0.2.0/fastapi-mail-0.2.0.tar.gz

---
+++ packages/fastapi-mail/fastapi-mail-0.2.0.tar.gz/fastapi-mail-0.2.0/fastapi_mail/__init__.py
@@ -0,0 +1,12 @@
+
+from fastapi_mail.fastmail import FastMail
+from fastapi_mail.version import VERSION
+
+__author__ = "sabuhi.shukurov@gmail.com"
+
+
+
+
+__all__ = [
+    "FastMail", "VERSION"
+]
