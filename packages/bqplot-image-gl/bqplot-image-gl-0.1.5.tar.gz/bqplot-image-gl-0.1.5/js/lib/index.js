0d41ac68772125dd3195e44aaedb1f09a225bb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:18 +0000

    Add package bqplot-image-gl/0.1.5/bqplot-image-gl-0.1.5.tar.gz

---
+++ packages/bqplot-image-gl/bqplot-image-gl-0.1.5.tar.gz/bqplot-image-gl-0.1.5/js/lib/index.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6 */
+
+// Export widget models and views, and the npm package version number.
+module.exports = require('./imagegl.js');
+module.exports.version = require('../package.json').version;
