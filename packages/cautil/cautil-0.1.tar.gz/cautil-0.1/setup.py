ae4c726a87682b45dc3adbbb23833897fb323355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:47 +0000

    Add package cautil/0.1/cautil-0.1.tar.gz

---
+++ packages/cautil/cautil-0.1.tar.gz/cautil-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cautil',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/whirldata/cautil',
+      author='Whirldata',
+      author_email='info@whirldatascience.com.com',
+      license='MIT',
+      packages=['CAUtil'],
+      zip_safe=False)
