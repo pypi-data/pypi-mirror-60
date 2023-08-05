42308dd42cce03466f07211f0a2e4d950c527da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:39 +0000

    Add package datafaker/0.6.4/datafaker-0.6.4.tar.gz

---
+++ packages/datafaker/datafaker-0.6.4.tar.gz/datafaker-0.6.4/datafaker/drivers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def load_sqlalchemy(connet):
+    """
+    返回db session
+    :param connet:
+    :return:
+    """
+    engine = create_engine(connet, pool_recycle=1800)
+    DBSession = sessionmaker(engine)
+    session = DBSession()
+    return session
