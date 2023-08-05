b2ab70230ff07f86d667b75ee962430de00f8979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:07 +0000

    Add package django-cached-fields/0.1.1/django-cached-fields-0.1.1.tar.gz

---
+++ packages/django-cached-fields/django-cached-fields-0.1.1.tar.gz/django-cached-fields-0.1.1/setup.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'django-cached-fields'
+DESCRIPTION = 'Easy value caching on Django.'
+URL = 'https://github.com/bartonip/django-cached-fields'
+EMAIL = 'notbartonip@gmail.com'
+AUTHOR = 'Barton Ip'
+REQUIRES_PYTHON = '>=3.0.0'
+VERSION = '0.1.1'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+    'django'
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+class RunTests(Command):
+    description = 'Run the django test suite from the tests dir.'
+    user_options = []
+    extra_env = {}
+    extra_args = []
+
+    def run(self):
+        for env_name, env_value in self.extra_env.items():
+            os.environ[env_name] = str(env_value)
+
+        this_dir = os.getcwd()
+        testproj_dir = os.path.join(this_dir, 'tests')
+        os.chdir(testproj_dir)
+        sys.path.append(testproj_dir)
+        from django.core.management import execute_from_command_line
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+        prev_argv = list(sys.argv)
+        try:
+            sys.argv = [__file__, 'test'] + self.extra_args
+            execute_from_command_line(argv=sys.argv)
+        finally:
+            sys.argv = prev_argv
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+class MakeMigrations(Command):
+    description = 'Run the django test suite from the tests dir.'
+    user_options = []
+    extra_env = {}
+    extra_args = []
+
+    def run(self):
+        for env_name, env_value in self.extra_env.items():
+            os.environ[env_name] = str(env_value)
+
+        this_dir = os.getcwd()
+        testproj_dir = os.path.join(this_dir, 'tests')
+        os.chdir(testproj_dir)
+        sys.path.append(testproj_dir)
+        from django.core.management import execute_from_command_line
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+        prev_argv = list(sys.argv)
+        try:
+            sys.argv = [__file__, 'makemigrations'] + self.extra_args
+            execute_from_command_line(argv=sys.argv)
+        finally:
+            sys.argv = prev_argv
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+class Migrate(Command):
+    description = 'Run the django test suite from the tests dir.'
+    user_options = []
+    extra_env = {}
+    extra_args = []
+
+    def run(self):
+        for env_name, env_value in self.extra_env.items():
+            os.environ[env_name] = str(env_value)
+
+        this_dir = os.getcwd()
+        testproj_dir = os.path.join(this_dir, 'tests')
+        os.chdir(testproj_dir)
+        sys.path.append(testproj_dir)
+        from django.core.management import execute_from_command_line
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+        prev_argv = list(sys.argv)
+        try:
+            sys.argv = [__file__, 'migrate'] + self.extra_args
+            execute_from_command_line(argv=sys.argv)
+        finally:
+            sys.argv = prev_argv
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+class StartApp(Command):
+    description = 'Run the django test suite from the tests dir.'
+    user_options = [('appname=', None, "Specify the django appname")]
+    extra_env = {}
+    extra_args = []
+
+    def run(self):
+        for env_name, env_value in self.extra_env.items():
+            os.environ[env_name] = str(env_value)
+        print(self.appname)
+        this_dir = os.getcwd()
+        testproj_dir = os.path.join(this_dir, 'tests')
+        os.chdir(testproj_dir)
+        sys.path.append(testproj_dir)
+        from django.core.management import execute_from_command_line
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+        prev_argv = list(sys.argv)
+        try:
+            sys.argv = [__file__, 'startapp', self.appname] + self.extra_args
+            execute_from_command_line(argv=sys.argv)
+        finally:
+            sys.argv = prev_argv
+
+    def initialize_options(self):
+        self.appname = ""
+
+    def finalize_options(self):
+        pass
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+        'test': RunTests,
+        'makemigrations': MakeMigrations,
+        'migrate': Migrate,
+        'startapp': StartApp
+    },
+)
+
