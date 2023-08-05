8a85db4a603f243f742115c42cca7bdc2260d5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:13 +0000

    Add package oxo-pkg/2.1.1/oxo_pkg-2.1.1-py3-none-any.whl

---
+++ packages/oxo-pkg/oxo_pkg-2.1.1-py3-none-any.whl/oxo_pkg/text/text.py
@@ -0,0 +1,16 @@
+
+# help & version text
+
+
+help_text = """
+here is a list of accepted arguments: 
+    --hard         :  activates hard-mode
+    --s-hard       :  activates super-hard-mode
+    --show-inputs  :  show all accepted move formats
+    --version      :  show current version number
+    --help         :  show help options
+"""
+
+
+def get_version_text(version_number):
+    return f"oxo version: {version_number}"
