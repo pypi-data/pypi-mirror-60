1e9b8633c96a3a7740aa9693139a4af37392e861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:35 +0000

    Add package python-collectd-mock/0.0.3/python-collectd-mock-0.0.3.tar.gz

---
+++ packages/python-collectd-mock/python-collectd-mock-0.0.3.tar.gz/python-collectd-mock-0.0.3/collectd/__init__.py
@@ -0,0 +1,8 @@
+from .dispatcher import Values
+from .registry import (
+    register_config,
+    register_init,
+    register_read,
+    register_write,
+    info,
+)
---
+++ packages/python-collectd-mock/python-collectd-mock-0.0.3.tar.gz/python-collectd-mock-0.0.3/collectd/registry.py
@@ -0,0 +1,21 @@
+
+
+def register_init(func):
+    pass
+
+
+def register_config(func):
+    pass
+
+
+def register_read(func):
+    pass
+
+
+def register_write(func):
+    pass
+
+
+def info(msg):
+    print(msg)
+
