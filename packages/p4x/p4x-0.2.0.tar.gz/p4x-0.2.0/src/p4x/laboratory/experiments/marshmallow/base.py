db7bc29734033376a5c0a7b510f4a71196e2d7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:19 +0000

    Add package p4x/0.2.0/p4x-0.2.0.tar.gz

---
+++ packages/p4x/p4x-0.2.0.tar.gz/p4x-0.2.0/src/p4x/laboratory/experiments/marshmallow/base.py
@@ -0,0 +1,29 @@
+from marshmallow import Schema, fields, post_load
+
+from p4x.laboratory.experiments import project as _project
+from abc import abstractmethod
+
+__author__ = "Philipp Tempel"
+__email__ = "p.tempel@tudelft.nl"
+
+
+class BaseSchema(Schema):
+    name = fields.String()
+    path = fields.String()
+
+    class Meta:
+        ordered = True
+
+    @property
+    @abstractmethod
+    def __model__(self):
+        raise NotImplementedError()
+
+    @post_load
+    def make_object(self, data, **kwargs):
+        return self.__model__(**data)
+
+
+__all__ = [
+        'BaseSchema',
+]
---
+++ packages/p4x/p4x-0.2.0.tar.gz/p4x-0.2.0/tests/test_undoredo.py
@@ -0,0 +1,30 @@
+import pathlib as pl
+
+from p4x import undoredo
+
+
+class UndoRedoTestSuite(object):
+
+    def test_create_file(self, tmp_path: pl.Path):
+        tmp_path = pl.Path('.').resolve() / 'file.txt'
+
+        def create_file():
+            tmp_path.touch()
+
+        def unlink_file():
+            tmp_path.unlink()
+
+        def write_file_text():
+            raise ValueError('invalid value')
+            tmp_path.write_text('asd')
+
+        def remove_file_text():
+            tmp_path.write_text('')
+
+        history = undoredo.History([
+                undoredo.Command(create_file, unlink_file),
+                undoredo.Command(write_file_text, remove_file_text)
+        ])
+
+        history.commit()
+        history.rollback()
