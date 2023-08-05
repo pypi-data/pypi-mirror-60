eaea608df7af7e0ac02876a5619cd658c86184d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package lastfm-now/0.0.3/lastfm-now-0.0.3.tar.gz

---
+++ packages/lastfm-now/lastfm-now-0.0.3.tar.gz/lastfm-now-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = "lastfm-now",
+    version = "0.0.3",
+    author = "Adam Cathersides",
+    author_email = "adamcathersides@gmail.com",
+    description = ("Display now playing from lastfm"),
+    url='https://github.com/adamcathersides/lastfm-now-playing/',
+    packages = ['lastfm_now'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data = True,
+    install_requires = [
+        'click',
+        'pylast'
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+    entry_points={
+          'console_scripts': [
+              'lastfm-now = lastfm_now.lastfm_now:run'
+          ]
+      }
+)
