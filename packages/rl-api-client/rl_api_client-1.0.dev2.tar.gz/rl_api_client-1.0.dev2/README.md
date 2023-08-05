4f472210f72d81661147034733c8ffa66b96b2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:55 +0000

    Add package rl-api-client/1.0.dev2/rl_api_client-1.0.dev2.tar.gz

---
+++ packages/rl-api-client/rl_api_client-1.0.dev2.tar.gz/rl_api_client-1.0.dev2/README.md
@@ -0,0 +1,12 @@
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
+
+
+# Overview
+
+RLAPIClient is meant to provide an easy method of using the RS REST API.
+
+Install:         `pip install rl-api-client`
+Build:           `make build`
+Publish to PyPi: `make publish`
---
+++ packages/rl-api-client/rl_api_client-1.0.dev2.tar.gz/rl_api_client-1.0.dev2/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# see https://github.com/pypa/sampleproject/blob/master/setup.py
+#   for instructions.
+
+_version = os.getenv('BUILD_VERSION', '100.0.0')
+
+setup(
+    name='rl_api_client',
+    version=_version,
+    author='Erez Atiya',
+    author_email='erez.atiya@redislabs.com',
+    description='RedisLabs Redis Enterprise Python API Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License'
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.22.0',
+        'urllib3==1.25.3',
+        'wheel'
+
+    ],
+    dependency_links=[]
+)
