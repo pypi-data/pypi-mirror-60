28f48d6d1951c95ed674d79f8fa8e1a5f906f576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:49 +0000

    Add package cautil/0.2/cautil-0.2.tar.gz

---
+++ packages/cautil/cautil-0.2.tar.gz/cautil-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cautil',
+      version='0.2',
+      description='The UTILITY for Cloud Agnostic',
+      url='http://github.com/whirldata/cautil',
+      author='Whirldata',
+      author_email='info@whirldatascience.com.com',
+      license='MIT',
+      packages=['CAUtil'],
+      zip_safe=False)
