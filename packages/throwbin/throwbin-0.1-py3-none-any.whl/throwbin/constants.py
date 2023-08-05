4a66042735a86baf1489e63ac41f011d12476fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:21 +0000

    Add package throwbin/0.1/throwbin-0.1-py3-none-any.whl

---
+++ packages/throwbin/throwbin-0.1-py3-none-any.whl/throwbin/constants.py
@@ -0,0 +1,14 @@
+AVAILABLE_SYNTAXES = [
+    "text",
+    "javascript",
+    "css",
+    "html",
+    "vue",
+    "php",
+    "c",
+    "csharp",
+    "java",
+    "lua",
+    "golang",
+    "dockerfile",
+]
---
+++ packages/throwbin/throwbin-0.1-py3-none-any.whl/throwbin/exceptions.py
@@ -0,0 +1,2 @@
+class ThrowBinException(Exception):
+    pass
