50ffeaf34c627937921ca00c052c96a6f356ca42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:03 +0000

    Add package hypercluster/0.1.13/hypercluster-0.1.13.tar.gz

---
+++ packages/hypercluster/hypercluster-0.1.13.tar.gz/hypercluster-0.1.13/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+files = ['snakemake/hypercluster.smk', 'snakemake/config.yml']
+setuptools.setup(
+    name="hypercluster",
+    version="0.1.13",
+    author="Lili Blumenberg, Ruggles Lab",
+    author_email="lili.blumenberg@gmail.com",
+    description="A package for automatic clustering hyperparameter optmization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liliblu/hypercluster",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+    ],
+    install_requires=[
+        "pandas >= 0.24.2",
+        "numpy >= 1.16.4",
+        "scipy >= 1.2.1",
+        "matplotlib >= 3.1.0",
+        "seaborn >= 0.9.0",
+        "scikit-learn >= 0.22.0",
+        "hdbscan >= 0.8.24",
+        "snakemake >= 5.8.2",
+        "python-igraph >=0.7.1",
+        "leidenalg >=0.7.0",
+        "louvain >=0.6.1"
+    ],
+    package_data={"hypercluster": files},
+    packages=setuptools.find_packages()
+)
