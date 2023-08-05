95480015853ea11402e2118f77bdbba6a4620f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:28 +0000

    Add package qtoggleserver/0.14.1/qtoggleserver-0.14.1.tar.gz

---
+++ packages/qtoggleserver/qtoggleserver-0.14.1.tar.gz/qtoggleserver-0.14.1/qtoggleserver/version.py
@@ -0,0 +1,3 @@
+
+VERSION = '0.14.1'
+VENDOR = 'qtoggle/qtoggleserver'
---
+++ packages/qtoggleserver/qtoggleserver-0.14.1.tar.gz/qtoggleserver-0.14.1/qtoggleserver/web/constants.py
@@ -0,0 +1,10 @@
+
+FRONTEND_DIR = 'frontend'
+FRONTEND_URL_PREFIX = 'frontend'
+
+APP_NAME = 'qtoggleserver'
+APP_PRETTY_NAME = 'qToggle Remote'
+APP_DESCRIPTION = 'Control your qToggle devices'
+
+BROWSER_BACKGROUND_COLOR = '#444444'
+BROWSER_THEME_COLOR = '#62abea'
