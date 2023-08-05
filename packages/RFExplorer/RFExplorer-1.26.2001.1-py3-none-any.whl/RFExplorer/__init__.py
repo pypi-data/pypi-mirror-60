213b2dd35905e53202198f788a7f9d9472c887dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:09 +0000

    Add package RFExplorer/1.26.2001.1/RFExplorer-1.26.2001.1-py3-none-any.whl

---
+++ packages/RFExplorer/RFExplorer-1.26.2001.1-py3-none-any.whl/RFExplorer/__init__.py
@@ -0,0 +1,25 @@
+#pylint: disable=trailing-whitespace, line-too-long, bad-whitespace, invalid-name, R0204, C0200
+#pylint: disable=superfluous-parens, missing-docstring, broad-except
+#pylint: disable=too-many-lines, too-many-instance-attributes, too-many-statements, too-many-nested-blocks
+#pylint: disable=too-many-branches, too-many-public-methods, too-many-locals, too-many-arguments
+
+#============================================================================
+#RF Explorer Python Libraries - A Spectrum Analyzer for everyone!
+#Copyright © 2010-20 Ariel Rocholl, www.rf-explorer.com
+#
+#This application is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 3.0 of the License, or (at your option) any later version.
+#
+#This software is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#=============================================================================
+
+from RFExplorer.RFExplorer import RFECommunicator
