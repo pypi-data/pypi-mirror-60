007939b05085f8d5dc2b9c7acc0e9abb9e31b035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:08 +0000

    Add package pyborg/2.0.0a6/pyborg-2.0.0a6.tar.gz

---
+++ packages/pyborg/pyborg-2.0.0a6.tar.gz/pyborg-2.0.0a6/pyborg/pyborg/util/irc.py
@@ -0,0 +1,16 @@
+from typing import Callable
+
+import venusian
+
+
+def command(internals: bool = False, pass_msg: bool = False) -> Callable:
+    """Wraps a python function into an irc command"""
+
+    def decorator(wrapped: Callable) -> Callable:
+        def callback(scanner: venusian.Scanner, name: str, ob: Callable) -> None:
+            scanner.registry.add(name, ob, internals, pass_msg)
+
+        venusian.attach(wrapped, callback)
+        return wrapped
+
+    return decorator
