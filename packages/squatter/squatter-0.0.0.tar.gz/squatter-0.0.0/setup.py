2047ba2a86dd4e24a5d3a7ba7f6bbf86d078507f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:16 +0000

    Add package squatter/0.0.0/squatter-0.0.0.tar.gz

---
+++ packages/squatter/squatter-0.0.0.tar.gz/squatter-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="squatter",
+    description="coming soon",
+    version="0.0.0",
+    author="John Reese",
+    author_email="john@noswap.com",
+    url="https://github.com/python-packaging/squatter",
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    license="MIT",
+)
