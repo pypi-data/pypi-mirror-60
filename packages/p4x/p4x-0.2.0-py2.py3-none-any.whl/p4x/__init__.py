ba8ac54388ad7f85b8262745a579a56c7ba2c201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:19 +0000

    Add package p4x/0.2.0/p4x-0.2.0-py2.py3-none-any.whl

---
+++ packages/p4x/p4x-0.2.0-py2.py3-none-any.whl/p4x/__init__.py
@@ -0,0 +1,10 @@
+from p4x import colorschemes, timing
+
+__author__ = "Philipp Tempel"
+__email__ = "p.tempel@tudelft.nl"
+__version__ = '0.2.0'
+
+__all__ = [
+        'colorschemes',
+        'timing',
+]
