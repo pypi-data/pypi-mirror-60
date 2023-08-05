d2df8a13e8f61ffc2ecadc67136711a515ae2900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package tradfri/0.0.4/tradfri-0.0.4.tar.gz

---
+++ packages/tradfri/tradfri-0.0.4.tar.gz/tradfri-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="tradfri",
+    version="0.0.4",
+    url="https://github.com/moroen/tradfricoap.git",
+    author="moroen",
+    author_email="moroen@gmail.com",
+    description="Controlling IKEA-Tradfri from the commmand line",
+    packages=["tradfricoap"],
+    include_package_data=True,
+    setup_requires=["cython"],
+    install_requires=["cython"],
+    scripts=["tradfricoap/tradfri"],
+)
