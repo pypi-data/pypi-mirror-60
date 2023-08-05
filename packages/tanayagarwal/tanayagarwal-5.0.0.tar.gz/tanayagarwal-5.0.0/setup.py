66ba2325e71720e89a82ff6e2d655b99ea8020a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:03 +0000

    Add package tanayagarwal/5.0.0/tanayagarwal-5.0.0.tar.gz

---
+++ packages/tanayagarwal/tanayagarwal-5.0.0.tar.gz/tanayagarwal-5.0.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tanayagarwal", # Replace with your own username
+    version="5.0.0",
+    author="Tanay Agarwal",
+    author_email="tanaygupta1234@gmail.com",
+    description="topsis approach for mcdm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=" ",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "tanayagarwal=data_project.topsisfinal:main",
+        ]
+    },
+    python_requires='>=3.5',
+)
