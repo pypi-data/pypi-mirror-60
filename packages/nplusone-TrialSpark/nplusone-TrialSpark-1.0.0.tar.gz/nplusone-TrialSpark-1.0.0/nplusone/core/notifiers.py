10c66c7b9b08afa5096240e1664e5e6d4ba8f7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package nplusone-TrialSpark/1.0.0/nplusone-TrialSpark-1.0.0.tar.gz

---
+++ packages/nplusone-TrialSpark/nplusone-TrialSpark-1.0.0.tar.gz/nplusone-TrialSpark-1.0.0/nplusone/core/notifiers.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import traceback
+
+from nplusone.core import exceptions
+
+
+class Notifier(object):
+
+    CONFIG_KEY = None
+    ENABLED_DEFAULT = False
+
+    @classmethod
+    def is_enabled(cls, config):
+        return (
+            config.get(cls.CONFIG_KEY) or
+            (
+                cls.CONFIG_KEY not in config and
+                cls.ENABLED_DEFAULT
+            )
+        )
+
+    def __init__(self, config):
+        self.config = config  # pragma: no cover
+
+    def notify(self, model, field):
+        pass  # pragma: no cover
+
+
+class LogNotifier(Notifier):
+
+    CONFIG_KEY = 'NPLUSONE_LOG'
+    ENABLED_DEFAULT = True
+
+    def __init__(self, config):
+        self.logger = config.get('NPLUSONE_LOGGER', logging.getLogger('nplusone'))
+        self.level = config.get('NPLUSONE_LOG_LEVEL', logging.DEBUG)
+
+    def notify(self, message):
+        self.logger.log(self.level, message.message)
+
+
+class ErrorNotifier(Notifier):
+
+    CONFIG_KEY = 'NPLUSONE_RAISE'
+    ENABLED_DEFAULT = False
+
+    def __init__(self, config):
+        self.error = config.get('NPLUSONE_ERROR', exceptions.NPlusOneError)
+
+    def notify(self, message):
+        stack = traceback.extract_stack()
+        relevant_frame = [frame for frame in reversed(stack) if 'spark' in frame.filename][0]
+        raise self.error(message.message + ', ' + str(relevant_frame)[len('<FrameSummary '): -1])
+
+
+def init(config):
+    return [
+        notifier(config) for notifier in (LogNotifier, ErrorNotifier)
+        if notifier.is_enabled(config)
+    ]
