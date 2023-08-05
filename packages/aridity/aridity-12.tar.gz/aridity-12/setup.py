64cee322e83326810c95ea99ef33c58e8b2d9067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:12 +0000

    Add package aridity/12/aridity-12.tar.gz

---
+++ packages/aridity/aridity-12.tar.gz/aridity-12/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+def long_description():
+    with open('README.md') as f:
+        return f.read()
+
+setuptools.setup(
+        name = 'aridity',
+        version = '12',
+        description = 'DRY config and template system',
+        long_description = long_description(),
+        long_description_content_type = 'text/markdown',
+        url = 'https://github.com/combatopera/aridity',
+        author = 'Andrzej Cichocki',
+        packages = setuptools.find_packages(),
+        py_modules = ['arid_config', 'aridity', 'processtemplate'],
+        install_requires = ['pyparsing'],
+        package_data = {'': ['*.pxd', '*.pyx', '*.pyxbld', '*.arid', '*.aridt']},
+        scripts = ['arid-config', 'aridity.py', 'processtemplate.py'])
