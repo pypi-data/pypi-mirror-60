bcec90a534e9d26aa3bc325570b0477ffa98ae78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:01 +0000

    Add package pydriosm/1.0.20/pydriosm-1.0.20-py3-none-any.whl

---
+++ packages/pydriosm/pydriosm-1.0.20-py3-none-any.whl/pydriosm/__init__.py
@@ -0,0 +1,9 @@
+from .download_BBBike import *
+from .download_GeoFabrik import *
+from .dump_GeoFabrik import *
+from .osm_psql import *
+from .read_GeoFabrik import *
+from .settings import *
+from .utils import *
+
+gdal_configurations(reset=False)
---
+++ packages/pydriosm/pydriosm-1.0.20-py3-none-any.whl/pydriosm/utils.py
@@ -0,0 +1,106 @@
+""" Utilities - Helper functions """
+
+import os
+import pickle
+
+import pkg_resources
+import pyhelpers.dir
+import rapidjson
+
+# ====================================================================================================================
+""" Change directory """
+
+
+# Change directory to "dat_GeoFabrik" and sub-directories
+def cd_dat_geofabrik(*sub_dir):
+    path = pyhelpers.dir.cd("dat_GeoFabrik")
+    for x in sub_dir:
+        path = os.path.join(path, x)
+    return path
+
+
+# Change directory to "dat_BBBike" and sub-directories
+def cd_dat_bbbike(*sub_dir):
+    path = pyhelpers.dir.cd("dat_BBBike")
+    for x in sub_dir:
+        path = os.path.join(path, x)
+    return path
+
+
+# Change directory to "dat" and sub-directories
+def cd_dat(*sub_dir):
+    path = pkg_resources.resource_filename(__name__, 'dat/')
+    for x in sub_dir:
+        path = os.path.join(path, x)
+    return path
+
+
+# ====================================================================================================================
+""" Save data """
+
+
+# Save Pickle file
+def save_pickle(pickle_data, path_to_pickle, verbose=True):
+    """
+    :param pickle_data: any object that could be dumped by the 'pickle' package
+    :param path_to_pickle: [str] local file path
+    :param verbose: [bool] (default: True)
+    :return: whether the data has been successfully saved
+    """
+    pickle_filename = os.path.basename(path_to_pickle)
+    pickle_dir = os.path.basename(os.path.dirname(path_to_pickle))
+    pickle_dir_parent = os.path.basename(os.path.dirname(os.path.dirname(path_to_pickle)))
+
+    if verbose:
+        print("{} \"{}\" ... ".format("Updating" if os.path.isfile(path_to_pickle) else "Saving",
+                                      " - ".join([pickle_dir_parent, pickle_dir, pickle_filename])), end="")
+
+    try:
+        os.makedirs(os.path.dirname(os.path.abspath(path_to_pickle)), exist_ok=True)
+        pickle_out = open(path_to_pickle, 'wb')
+        pickle.dump(pickle_data, pickle_out)
+        pickle_out.close()
+        print("Successfully.") if verbose else None
+    except Exception as e:
+        print("Failed. {}.".format(e))
+
+
+# Save JSON file
+def save_json(json_data, path_to_json, verbose=True):
+    """
+    :param json_data: any object that could be dumped by the 'json' package
+    :param path_to_json: [str] local file path
+    :param verbose: [bool] (default: True)
+    :return: whether the data has been successfully saved
+    """
+    json_filename = os.path.basename(path_to_json)
+    json_dir = os.path.basename(os.path.dirname(path_to_json))
+    json_dir_parent = os.path.basename(os.path.dirname(os.path.dirname(path_to_json)))
+
+    print("{} \"{}\" ... ".format("Updating" if os.path.isfile(path_to_json) else "Saving",
+                                  " - ".join([json_dir_parent, json_dir, json_filename])), end="") if verbose else None
+    try:
+        os.makedirs(os.path.dirname(os.path.abspath(path_to_json)), exist_ok=True)
+        json_out = open(path_to_json, 'w')
+        rapidjson.dump(json_data, json_out)
+        json_out.close()
+        print("Successfully.") if verbose else None
+    except Exception as e:
+        print("Failed. {}.".format(e))
+
+
+# ====================================================================================================================
+""" Misc """
+
+
+# Make a dictionary with keys and values being shape_type (in OSM .shp file) and shapely.geometry, respectively
+def osm_geom_types():
+    import shapely.geometry
+    shape_types = {'Point': shapely.geometry.Point,
+                   'LineString': shapely.geometry.LineString,
+                   'LinearRing': shapely.geometry.LinearRing,
+                   'MultiLineString': shapely.geometry.MultiLineString,
+                   'Polygon': shapely.geometry.Polygon,
+                   'MultiPolygon': shapely.geometry.MultiPolygon,
+                   'GeometryCollection': shapely.geometry.GeometryCollection}
+    return shape_types
