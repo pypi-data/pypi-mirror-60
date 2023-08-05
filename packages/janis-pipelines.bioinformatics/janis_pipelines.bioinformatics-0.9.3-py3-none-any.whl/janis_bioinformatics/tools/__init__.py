56ce99a3c9b54f3e074f47c357b0238650a96e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package janis-pipelines.bioinformatics/0.9.3/janis_pipelines.bioinformatics-0.9.3-py3-none-any.whl

---
+++ packages/janis-pipelines.bioinformatics/janis_pipelines.bioinformatics-0.9.3-py3-none-any.whl/janis_bioinformatics/tools/__init__.py
@@ -0,0 +1,24 @@
+from janis_bioinformatics.tools.bioinformaticstoolbase import (
+    BioinformaticsTool,
+    BioinformaticsWorkflow,
+)
+from janis_bioinformatics.tools import (
+    babrahambioinformatics,
+    bcftools,
+    bwa,
+    common,
+    cellranger,
+    cutadapt,
+    freebayes,
+    gatk4,
+    htslib,
+    illumina,
+    multiqc,
+    papenfuss,
+    pmac,
+    samtools,
+    ucsf,
+    validation,
+    variantcallers,
+    vcflib,
+)
---
+++ packages/janis-pipelines.bioinformatics/janis_pipelines.bioinformatics-0.9.3-py3-none-any.whl/janis_bioinformatics/tools/bwa/bwalatest.py
@@ -0,0 +1,3 @@
+from .bwa_0_7_15 import Bwa_0_7_15
+
+BwaLatest = Bwa_0_7_15
