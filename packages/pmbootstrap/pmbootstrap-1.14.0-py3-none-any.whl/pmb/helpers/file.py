acd6faee6d18a26c0d0a729d2f26784465cc9a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package pmbootstrap/1.14.0/pmbootstrap-1.14.0-py3-none-any.whl

---
+++ packages/pmbootstrap/pmbootstrap-1.14.0-py3-none-any.whl/pmb/helpers/file.py
@@ -0,0 +1,86 @@
+"""
+Copyright 2020 Oliver Smith
+
+This file is part of pmbootstrap.
+
+pmbootstrap is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pmbootstrap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pmbootstrap.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import time
+import pmb.helpers.run
+
+
+def replace(path, old, new):
+    text = ""
+    with open(path, "r", encoding="utf-8") as handle:
+        text = handle.read()
+
+    text = text.replace(old, new)
+
+    with open(path, "w", encoding="utf-8") as handle:
+        handle.write(text)
+
+
+def is_up_to_date(path_sources, path_target=None, lastmod_target=None):
+    """
+    Check if a file is up-to-date by comparing the last modified timestamps
+    (just like make does it).
+
+    :param path_sources: list of full paths to the source files
+    :param path_target: full path to the target file
+    :param lastmod_target: the timestamp of the target file. specify this as
+                           alternative to specifying path_target.
+    """
+
+    if path_target and lastmod_target:
+        raise RuntimeError(
+            "Specify path_target *or* lastmod_target, not both!")
+
+    lastmod_source = None
+    for path_source in path_sources:
+        lastmod = os.path.getmtime(path_source)
+        if not lastmod_source or lastmod > lastmod_source:
+            lastmod_source = lastmod
+
+    if path_target:
+        lastmod_target = os.path.getmtime(path_target)
+
+    return lastmod_target >= lastmod_source
+
+
+def is_older_than(path, seconds):
+    """
+    Check if a single file is older than a given amount of seconds.
+    """
+    if not os.path.exists(path):
+        return True
+    lastmod = os.path.getmtime(path)
+    return lastmod + seconds < time.time()
+
+
+def symlink(args, file, link):
+    """
+    Checks if the symlink is already present, otherwise create it.
+    """
+    if os.path.exists(link):
+        if (os.path.islink(link) and
+                os.path.realpath(os.readlink(link)) == os.path.realpath(file)):
+            return
+        raise RuntimeError("File exists: " + link)
+    elif os.path.islink(link):
+        os.unlink(link)
+
+    # Create the symlink
+    pmb.helpers.run.user(args, ["ln", "-s", file, link])
---
+++ packages/pmbootstrap/pmbootstrap-1.14.0-py3-none-any.whl/pmb/helpers/http.py
@@ -0,0 +1,69 @@
+"""
+Copyright 2020 Oliver Smith
+
+This file is part of pmbootstrap.
+
+pmbootstrap is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pmbootstrap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pmbootstrap.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import os
+import hashlib
+import shutil
+import logging
+import urllib.request
+import pmb.helpers.run
+
+
+def download(args, url, prefix, cache=True, loglevel=logging.INFO,
+             allow_404=False):
+    """ Download a file to disk.
+
+        :param url: the http(s) address of to the file to download
+        :param prefix: for the cache, to make it easier to find (cache files
+                       get a hash of the URL after the prefix)
+        :param loglevel: change to logging.DEBUG to only display the download
+                         message in 'pmbootstrap log', not in stdout. We use
+                         this when downloading many APKINDEX files at once, no
+                         point in showing a dozen messages.
+        :param allow_404: do not raise an exception when the server responds
+                          with a 404 Not Found error. Only display a warning on
+                          stdout (no matter if loglevel is changed).
+        :returns: path to the downloaded file in the cache or None on 404 """
+    # Create cache folder
+    if not os.path.exists(args.work + "/cache_http"):
+        pmb.helpers.run.user(args, ["mkdir", "-p", args.work + "/cache_http"])
+
+    # Check if file exists in cache
+    prefix = prefix.replace("/", "_")
+    path = (args.work + "/cache_http/" + prefix + "_" +
+            hashlib.sha256(url.encode("utf-8")).hexdigest())
+    if os.path.exists(path):
+        if cache:
+            return path
+        pmb.helpers.run.user(args, ["rm", path])
+
+    # Download the file
+    logging.log(loglevel, "Download " + url)
+    try:
+        with urllib.request.urlopen(url) as response:
+            with open(path, "wb") as handle:
+                shutil.copyfileobj(response, handle)
+    # Handle 404
+    except urllib.error.HTTPError as e:
+        if e.code == 404 and allow_404:
+            logging.warning("WARNING: file not found: " + url)
+            return None
+        raise
+
+    # Return path in cache
+    return path
