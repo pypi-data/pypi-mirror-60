0ba149b3462579844dde6bb17268988e781796c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:00 +0000

    Add package satnogs-config/0.4/satnogs_config-0.4-py3-none-any.whl

---
+++ packages/satnogs-config/satnogs_config-0.4-py3-none-any.whl/satnogsconfig/settings.py
@@ -0,0 +1,37 @@
+"""
+Settings module
+"""
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+try:
+    load_dotenv()
+except EnvironmentError:
+    # XXX: Workaround for readthedocs  # pylint: disable=fixme
+    pass
+
+ENV_PREFIX = 'SATNOGS_CONFIG_'
+
+SATNOGS_SETUP_UPGRADE_SCRIPT = os.getenv(
+    ENV_PREFIX + 'SATNOGS_SETUP_UPGRADE_SCRIPT', 'satnogs-upgrade'
+)
+SATNOGS_SETUP_STAMP_DIR = os.getenv(
+    ENV_PREFIX + 'SATNOGS_SETUP_STAMP_DIR',
+    str(Path.home().joinpath('.satnogs'))
+)
+SATNOGS_SETUP_BOOTSTRAP_STAMP = os.getenv(
+    ENV_PREFIX + 'SATNOGS_SETUP_BOOTSTRAP_STAMP', '.bootstrapped'
+)
+SATNOGS_SETUP_INSTALL_STAMP = os.getenv(
+    ENV_PREFIX + 'SATNOGS_SETUP_INSTALL_STAMP', '.installed'
+)
+CONFIG_FILE = os.getenv(
+    ENV_PREFIX + 'CONFIG_FILE',
+    str(Path.home().joinpath('.satnogs', 'config.yml'))
+)
+ANSIBLE_DIR = os.getenv(
+    ENV_PREFIX + 'ANSIBLE_DIR',
+    str(Path.home().joinpath('.satnogs', 'ansible'))
+)
