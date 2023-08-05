6b093d4389fcb32f548a4060788ad41db302f110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:13 +0000

    Add package solution-efe-client-member/1.0.0/solution_efe_client_member-1.0.0-py3-none-any.whl

---
+++ packages/solution-efe-client-member/solution_efe_client_member-1.0.0-py3-none-any.whl/solution_efe_client_member/memberService.py
@@ -0,0 +1,17 @@
+import socket
+import json
+from solution_efe_config import configConstants
+
+CLIENT_MEMBER=configConstants.CLIENTS['member']
+
+def memberServiceProfileById(data):
+    try:
+        event = {'event':'member-service-profile-by-id','data': data}
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((CLIENT_MEMBER['host'], CLIENT_MEMBER['port']))
+            sock.sendall(bytes(json.dumps(event) + "\n", "utf-8"))
+            received = str(sock.recv(1024), "utf-8")
+        response=json.loads(received)
+        return response
+    except Exception as e:
+        return {'code': 500,'status': False,'data':str(e)}
