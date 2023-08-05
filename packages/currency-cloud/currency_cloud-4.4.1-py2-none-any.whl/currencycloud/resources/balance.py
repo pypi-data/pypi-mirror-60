042d7c1ceb76c5dcae52ad5ef3d2db8743214a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package currency-cloud/4.4.1/currency_cloud-4.4.1-py2-none-any.whl

---
+++ packages/currency-cloud/currency_cloud-4.4.1-py2-none-any.whl/currencycloud/resources/balance.py
@@ -0,0 +1,8 @@
+'''This module provides the object representation of a CurrencyCloud Balance'''
+
+from currencycloud.resources.resource import Resource
+
+
+class Balance(Resource):
+    '''This class represents a CurrencyCloud Balance'''
+    pass
