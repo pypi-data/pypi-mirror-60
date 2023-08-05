6ccb7a63aa815fb5e817125062118bf6b06e2b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package disco/1.19.0/disco-1.19.0.tar.gz

---
+++ packages/disco/disco-1.19.0.tar.gz/disco-1.19.0/disco/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#    ____  __  ____   ___  __
+#   (    \(  )/ ___) / __)/  \
+#    ) D ( )( \___ \( (__(  O )
+#   (____/(__)(____/ \___)\__/
+
+"""
+DISCO SDK Library
+"""
+
+# from .gql import authentication
+from .job import Job
+from .cluster import Cluster
+from .asset import upload_file, Asset
+from .core.credentials import set_credentials
+from .repository import Repository
