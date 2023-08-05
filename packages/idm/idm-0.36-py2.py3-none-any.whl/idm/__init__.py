c5295f4ff01e096c9bd22104d8698fc3a755960c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package idm/0.36/idm-0.36-py2.py3-none-any.whl

---
+++ packages/idm/idm-0.36-py2.py3-none-any.whl/idm/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+import imp
+import __version__ as version
+__version__ 	= version.version
+__email__		= "licface@yahoo.com"
+__author__		= "licface@yahoo.com"
+
+from .idm import *
