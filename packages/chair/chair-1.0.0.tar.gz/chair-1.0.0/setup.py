397d2aaa218a306adf6212985eb5c4a53d065f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package chair/1.0.0/chair-1.0.0.tar.gz

---
+++ packages/chair/chair-1.0.0.tar.gz/chair-1.0.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="chair",
+    version="1.0.0",
+    author="Hakan Vergil",
+    author_email='hakanvergil@gmail.com',
+    url="https://pypi.org/project/chair/",
+    download_url=None,
+    description="Additional features for Android",
+    long_description=None,
+    license="MIT",
+    install_requires=None,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ],
+)
