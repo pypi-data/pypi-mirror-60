a38a1b21c96e60bdaa79e8230a00ca4e360e02ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:29 +0000

    Add package exhal/1.0.12/exhal-1.0.12.tar.gz

---
+++ packages/exhal/exhal-1.0.12.tar.gz/exhal-1.0.12/exhal/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+__title__ = "exhal"
+__version__ = "1.0.12"
+__author__ = "Nick Ficano"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2020 Nick Ficano"
+from .breathe import Breathe
