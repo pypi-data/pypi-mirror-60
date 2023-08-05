27332578a1dfaacb50ea5c2e55bc86b78d72ec79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:29 +0000

    Add package time-tracker-plus/1.0/time-tracker-plus-1.0.tar.gz

---
+++ packages/time-tracker-plus/time-tracker-plus-1.0.tar.gz/time-tracker-plus-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="time-tracker-plus",
+    version="1.0",
+    license='MIT',
+    author = 'Alex Davies',
+    author_email = 'traverse.da@gmail.com',
+    url = 'https://github.com/traverseda/ttp',
+    download_url = 'https://github.com/traverseda/ttp/archive/v1.0.tar.gz',
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': ['ttp=ttp.main:cli'],
+    },
+    install_requires=(
+        'appdirs',
+        'click',
+        'arrow',
+        'dateparser',
+        'jinja2',
+        'pygments',
+    )
+)
