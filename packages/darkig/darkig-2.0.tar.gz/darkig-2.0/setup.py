d5e8a0abd8f4c04921a4007ac32236faddafff5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:24 +0000

    Add package darkig/2.0/darkig-2.0.tar.gz

---
+++ packages/darkig/darkig-2.0.tar.gz/darkig-2.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+name='darkig',
+version='2.0',
+author='Nasir Ali',
+author_email='nasiralis1731@gmail.com',
+#author_url='https://facebook.com/nasir.xo',
+packages=['darkig'],
+scripts=['bin/darkig',],
+install_requires=['igramscraper',],
+)
+
+
+
+
