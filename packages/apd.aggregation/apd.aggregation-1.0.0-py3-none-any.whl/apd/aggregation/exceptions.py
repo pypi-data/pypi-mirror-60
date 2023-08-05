9e349d8b4627e9440bb01cccf6bd2b52ec41ee48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:33 +0000

    Add package apd.aggregation/1.0.0/apd.aggregation-1.0.0-py3-none-any.whl

---
+++ packages/apd.aggregation/apd.aggregation-1.0.0-py3-none-any.whl/apd/aggregation/exceptions.py
@@ -0,0 +1,6 @@
+class IncompatibleTriggerError(ValueError):
+    """An error that's raised when a trigger is passed
+    a data point that cannot be handled due to an incompatible
+    value being stored"""
+
+    pass
