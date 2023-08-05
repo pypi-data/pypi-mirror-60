c00f760a5c587f66a8a0a71e97cfa57664b6ae22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package oatspower/1.0.1/oatspower-1.0.1.tar.gz

---
+++ packages/oatspower/oatspower-1.0.1.tar.gz/oatspower-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+with open('README.md') as f:
+    readme = f.read()
+setup(name='oatspower',
+      version='1.0.1',
+      description='OATS: Optimisation and Analysis Toolbox for Power Systems',
+      long_description='README.md',
+      long_description_content_type='text/markdown',
+      url='https://github.com/bukhsh/oats',
+      author='Waqquas Bukhsh',
+      author_email='wbukhsh@gmail.com',
+      license='GPLv3+',
+      packages=['oats','oats.models','oats.testcases'],
+      package_data={'oats.testcases': ['*']},
+      install_requires =[
+        'pyomo',
+        'tabulate',
+        'PyYAML',
+        'ipython[notebook]',
+        'openpyxl',
+        'pymysql',
+        'pypyodbc',
+        'pyro4',
+        'sympy',
+        'xlrd',
+        'pandas',
+      ],
+      zip_safe=False)
