69f2bb42834e3adca7ddd2cbe36266ede0628721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:34 +0000

    Add package primitives/0.1.dev1/primitives-0.1.dev1.tar.gz

---
+++ packages/primitives/primitives-0.1.dev1.tar.gz/primitives-0.1.dev1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='primitives',
+    version='0.1.dev1',
+    description='Primitive tools designed with OOP in mind',
+    url='https://github.com/dry-python/primitives',
+    license='BSD-2-Clause',
+    author='Artem Malyshev',
+    author_email='proofit404@gmail.com',
+    packages=find_packages())
