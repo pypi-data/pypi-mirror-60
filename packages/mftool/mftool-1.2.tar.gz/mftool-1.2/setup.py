e0f9385f5d539b3d29613ef5d639b726eac4f442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:24 +0000

    Add package mftool/1.2/mftool-1.2.tar.gz

---
+++ packages/mftool/mftool-1.2.tar.gz/mftool-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension ,find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+	
+setup(
+    name="mftool",
+    version="1.2",
+    author="Sujit Nayakwadi",
+    author_email="nayakwadi.sujit@gmail.com",
+    description="Python library for extracting realtime Mutual funds data from AMFI (India)",
+    license="MIT",
+    keywords="amfi, quote, mutual-funds, funds, mutual, nse",
+    install_requires=['requests','bs4'],
+    url="http://mftool.readthedocs.io",
+    packages=find_packages(),
+	long_description = long_description,
+	long_description_content_type='text/markdown'
+)
