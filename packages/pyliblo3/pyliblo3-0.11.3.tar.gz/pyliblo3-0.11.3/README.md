4f596b9de5818e6f5004be0558804f8b7e7821fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:44 +0000

    Add package pyliblo3/0.11.3/pyliblo3-0.11.3.tar.gz

---
+++ packages/pyliblo3/pyliblo3-0.11.3.tar.gz/pyliblo3-0.11.3/README.md
@@ -0,0 +1,72 @@
+# pyliblo3 
+
+This is a fork of the original bindings for liblo, making it pip installable
+
+## Example
+
+
+### Simple Server
+
+```python
+
+import liblo
+server = liblo.Server(8080)
+
+def test_handler(path, args, types, src):
+    print(args)
+    
+server.add_method("/test", None, test_handler)
+
+while True:
+    server.recv(100)
+```
+
+
+## Dependencies
+
+* liblo >= 0.28
+
+
+## Installation
+
+
+### 1. Install liblo
+
+`liblo` must be installed previously
+
+#### Linux
+
+sudo apt install liblo-dev
+
+#### macOS
+
+brew install liblo
+
+#### Windows
+
+See http://liblo.sourceforge.net/README-platforms.html
+
+
+### 2. Install pyliblo3
+
+`pip install pyliblo3`
+
+
+-------
+
+
+## Original README
+
+
+	pyliblo - Python bindings for the liblo OSC library
+
+	Copyright (C) 2007-2011  Dominic Sacré  <dominic.sacre@gmx.de>
+
+	http://das.nasophon.de/pyliblo/
+
+
+	To install, run "./setup.py build", followed by "./setup.py install". This
+	will install both the python module and the send_osc/dump_osc scripts.
+
+	See doc/API.html and doc/examples.html for API documentation and some
+	example code.
---
+++ packages/pyliblo3/pyliblo3-0.11.3.tar.gz/pyliblo3-0.11.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension
+import platform as _platform
+import glob
+import os
+
+VERSION = '0.11.3'
+
+platform = _platform.system()
+
+include_dirs, library_dirs = [], []
+
+if platform == 'Darwin':
+    include_dirs.append("/usr/local/include/")
+    library_dirs.append("/usr/local/lib")
+elif platform == 'Linux':
+    include_dirs.extend(['usr/include', '/usr/local/include'])
+    library_dirs.append("/usr/local/lib")
+
+# read the contents of your README file
+thisdir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(thisdir, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='pyliblo3',
+    python_requires='>=3.7',
+    version=VERSION,
+    scripts=glob.glob("scripts/*.py"),
+    ext_modules=[
+        Extension(
+            'liblo', 
+            sources = ['src/liblo.pyx', 'src/liblo.pxd'],
+            extra_compile_args=[
+                '-fno-strict-aliasing',
+                '-Werror-implicit-function-declaration',
+                '-Wfatal-errors'],
+            libraries=['lo'],
+            library_dirs=library_dirs,
+            include_dirs=include_dirs)
+    ],
+    setup_requires=['setuptools>=18', 'cython'],
+    install_requires=['cython'],
+    author='Dominic Sacre',
+    author_email='dominic.sacre@gmx.de',
+    maintainer='Eduardo Moguillansky',
+    maintainer_email='eduardo.moguillansky@gmail.com',
+    url='https://github.com/gesellkammer/pyliblo3',
+    description='Python bindings for the liblo OSC library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='LGPL',
+)
