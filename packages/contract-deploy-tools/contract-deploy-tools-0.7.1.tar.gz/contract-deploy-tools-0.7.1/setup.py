a5ee55770c69610b7622a1986729dfb5ea61e6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:33 +0000

    Add package contract-deploy-tools/0.7.1/contract-deploy-tools-0.7.1.tar.gz

---
+++ packages/contract-deploy-tools/contract-deploy-tools-0.7.1.tar.gz/contract-deploy-tools-0.7.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="contract-deploy-tools",
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "py-solc",
+        "web3>=5.0.0b2",
+        "eth-tester[py-evm]",
+        "eth-utils",
+        "eth-keyfile",
+        "click",
+    ],
+    entry_points="""
+    [console_scripts]
+    deploy-tools=deploy_tools.cli:main
+    [pytest11]
+    deploy-tools=deploy_tools.plugin
+    """,
+)
