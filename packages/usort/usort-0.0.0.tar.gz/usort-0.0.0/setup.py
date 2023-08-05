430649836a38b48092661eb61230d9c79f3ff667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:25 +0000

    Add package usort/0.0.0/usort-0.0.0.tar.gz

---
+++ packages/usort/usort-0.0.0.tar.gz/usort-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="usort",
+    description="coming soon",
+    version="0.0.0",
+    author="John Reese",
+    author_email="john@noswap.com",
+    url="https://github.com/jreese/aioitertools",
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    license="MIT",
+)
