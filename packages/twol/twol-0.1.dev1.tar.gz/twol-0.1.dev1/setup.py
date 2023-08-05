11b1158ec2166ac159131c663f700ac518a96238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:17 +0000

    Add package twol/0.1.dev1/twol-0.1.dev1.tar.gz

---
+++ packages/twol/twol-0.1.dev1.tar.gz/twol-0.1.dev1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="twol", # of the PyPI project and package
+    version="0.1.dev1",
+    author="Kimmo Koskenniemi",
+    author_email="koskenni@gmail.com",
+    description="Tools for simplified two-level morphology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/koskenni/twol",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "twol-aligner = twolalign.aligner:main",
+            "twol-metric = twolalign.metric:main",
+            "twol-multialign = twolalign.multialign:main",
+            "twol-table2words = twolalign.table2words:step1",
+            "twol-words2zerofilled = twolalign.words2zerofilled:main",
+            "twol-zerofilled2raw = twolalign.zerofilled2raw:main",
+            "twol-raw2named = twolalign.raw2named:main",
+            "twol-comp = twol.twolcomp:main",
+            "twol-examples2fst = twol.twexamp:main",
+            "twol-discov =  twol.twdiscov:main"
+        ]
+    },
+    include_package_data=True,
+    python_requires='>=3.6',
+        install_requires=[
+        'hfst',
+        'TatSu',
+        'grapheme'
+    ]
+
+)
+
