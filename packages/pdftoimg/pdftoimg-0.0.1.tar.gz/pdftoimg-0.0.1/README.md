c559520f7ba97cf35baa5ec126aa193fe52fd2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:00 +0000

    Add package pdftoimg/0.0.1/pdftoimg-0.0.1.tar.gz

---
+++ packages/pdftoimg/pdftoimg-0.0.1.tar.gz/pdftoimg-0.0.1/README.md
@@ -0,0 +1,6 @@
+# [Pdf-To-Image](https://github.com/starkblaze01/Pdf-To-Image)
+Simple Command Line tool to convert PDF to Image.
+This package is still in the beta-version.
+
+# For Using the package
+- Run `pdftoimg -h`
---
+++ packages/pdftoimg/pdftoimg-0.0.1.tar.gz/pdftoimg-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdftoimg",
+    version="0.0.1",
+    author="starkblaze01",
+    author_email="mp.pathela@gmail.com",
+    description="Easily convert PDF to Image from command line",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/starkblaze01/Pdf-To-Image",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        "console_scripts": ['pdftoimg = pdftoimg.pdftoimg:main'],
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        'pdf2image>=1.11.0',
+    ],
+)
