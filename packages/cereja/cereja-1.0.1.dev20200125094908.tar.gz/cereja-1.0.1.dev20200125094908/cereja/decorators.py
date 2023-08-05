6ed21359dcd922db7a5fdd0bbfd0347dca27c176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:03 +0000

    Add package cereja/1.0.1.dev20200125094908/cereja-1.0.1.dev20200125094908.tar.gz

---
+++ packages/cereja/cereja-1.0.1.dev20200125094908.tar.gz/cereja-1.0.1.dev20200125094908/cereja/decorators.py
@@ -0,0 +1,57 @@
+import functools
+import time
+from abc import abstractmethod
+from typing import Callable, Any
+import abc
+import logging
+from cereja.concurrently import SyncToAsync, AsyncToSync
+
+logger = logging.getLogger(__name__)
+
+# exclude from the root import
+_explicit_exclude = ['time_exec', 'BaseDecorator', 'Decorator', 'logger']
+
+
+class BaseDecorator(abc.ABC):
+    def __init__(self):
+        self.func = None
+
+    def register(self, name, wrapper):
+        setattr(self, name, wrapper)
+        return self.__getattribute__(name)
+
+    @abstractmethod
+    def wrapper(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+class Decorator(BaseDecorator):
+    def wrapper(self, *args, **kwargs):
+        result = super().wrapper(*args, **kwargs)
+        return result
+
+
+def time_exec(func: Callable[[Any], Any]) -> Callable:
+    """
+    Decorator used to signal or perform a particular function.
+
+    :param func: Receives the function that will be executed as well as its parameters.
+    :return
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        first_time = time.time()
+        result = func(*args, **kwargs)
+        logger.info(f"[{func.__name__}] performed {time.time() - first_time}")
+        return result
+
+    return wrapper
+
+
+# Lowercase is more sensible for most things
+sync_to_async = SyncToAsync
+async_to_sync = AsyncToSync
+
+if __name__ == '__main__':
+    pass
