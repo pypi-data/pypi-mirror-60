1d0e10bc9c5d4a7bca79b5cbf6b55ee834cb94a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:30 +0000

    Add package tablign/0.2.2/tablign-0.2.2.tar.gz

---
+++ packages/tablign/tablign-0.2.2.tar.gz/tablign-0.2.2/tablign/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+from . import cli
+from .__about__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __version__,
+    __website__,
+)
+from .main import tablign
+
+__all__ = [
+    "__copyright__",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__website__",
+    "cli",
+    "tablign",
+]
