4778d99d62b7708db9ea74076869a8774ad7a2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package hello-kjmerf/0.0.4/hello-kjmerf-0.0.4.tar.gz

---
+++ packages/hello-kjmerf/hello-kjmerf-0.0.4.tar.gz/hello-kjmerf-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hello-kjmerf",
+    version="0.0.4",
+    author="Kevin Merfeld",
+    author_email="kevinjmerfeld@gmail.com",
+    description="A package that says hello to kjmerf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kjmerf/hello_kjmerf",
+    download_url="https://github.com/kjmerf/hello_kjmerf/archive/v_0_0_01.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
