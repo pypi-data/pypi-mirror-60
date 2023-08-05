b1a1c53be25ed72332e872f755326878132612a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:18 +0000

    Add package ahd/0.0.1/ahd-0.0.1.tar.gz

---
+++ packages/ahd/ahd-0.0.1.tar.gz/ahd-0.0.1/README.md
@@ -0,0 +1,6 @@
+# AHD; Ad-Hoc Dispatcher
+
+A system for creating ad-hoc commands with a simple syntax.
+
+**Currently just name-squatting while creating the initial build**
+
---
+++ packages/ahd/ahd-0.0.1.tar.gz/ahd-0.0.1/ahd/cli.py
@@ -0,0 +1,6 @@
+"""This file will contain all the functionality of the script
+
+Features:
+ * Create a keword to execute a bash/psl command
+ * Be able to specify a path to execute the command in
+"""
