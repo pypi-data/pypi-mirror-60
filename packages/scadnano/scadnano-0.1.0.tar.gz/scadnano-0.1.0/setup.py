5db06c2d09f3bbf5c5055ef9840bdcce5c8ac8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:46 +0000

    Add package scadnano/0.1.0/scadnano-0.1.0.tar.gz

---
+++ packages/scadnano/scadnano-0.1.0.tar.gz/scadnano-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scadnano',
+      packages=['scadnano'],
+      version='0.1.0',
+      license='MIT',
+      description="Python scripting library for generating designs readable by scadnano.",
+      author="David Doty",
+      author_email="doty@ucdavis.edu",
+      url="https://github.com/UC-Davis-molecular-computing/scadnano-python-package",
+      download_url = 'https://github.com/UC-Davis-molecular-computing/scadnano-python-package/archive/v0.1.0.zip', 
+    )
