aa802323119b0eb01234d1f9efc87df31f315b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:49 +0000

    Add package treebeard/0.0.3/treebeard-0.0.3.tar.gz

---
+++ packages/treebeard/treebeard-0.0.3.tar.gz/treebeard-0.0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+requirements = [
+    "click",
+    "google-cloud-storage",
+    "google-cloud-scheduler",
+    "halo",
+    "flask",
+    "papermill",
+    "gunicorn",
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="treebeard",
+    version="0.0.3",
+    author="Treebeard Technologies",
+    author_email="alex@treebeard.io",
+    description="Tools for notebook hosting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={"console_scripts": ["treebeard = treebeard.treebeard:cli"]},
+    install_requires=requirements,
+)
