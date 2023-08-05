aed2beb8db3c7067886383371001a8c68e0e972f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:29 +0000

    Add package NeuroSkyPy/1.4/NeuroSkyPy-1.4.tar.gz

---
+++ packages/NeuroSkyPy/NeuroSkyPy-1.4.tar.gz/NeuroSkyPy-1.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+version = "1.4"
+
+setup(
+      name="NeuroSkyPy",
+      packages=['NeuroSkyPy'],
+      version=version,
+      license="GNU General Public License v3.0",
+      description="Library for interfacing with Neurosky's Mindwave EEG headset",
+      author="Jorge Lopez Marcos",
+      author_email="jlomar2005@hotmail.com",
+      url="https://github.com/Jor-G-ete/NeuroSkyPy",
+      download_url="https://github.com/Jor-G-ete/NeuroSkyPy/archive/v"+version+".tar.gz",
+      project_urls={
+            "Documentation":"https://github.com/Jor-G-ete/NeuroSkyPy",
+            "Source Code":"https://github.com/Jor-G-ete/NeuroSkyPy/blob/master/NeuroSkyPy/NeuroSkyPy.py"
+      },
+      platforms="Windows",
+      keywords=["python3.7", "NeuroSky", "graphics", "threads"],
+      install_requires=[
+            'json',
+            'numpy',
+            'scikit-learn',
+            'matplotlib',
+            'pyserial',
+            'pyyaml'
+      ],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',  # Define that your audience are developers
+            'Topic :: Software Development :: Build Tools',
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+            'Programming Language :: Python :: 3.7',
+            "Natural Language :: English",
+            "Natural Language :: Spanish",
+            "Operating System :: Microsoft :: Windows :: Windows 10",
+            "Topic :: Scientific/Engineering :: Medical Science Apps."
+            ],
+      python_requires=">=3.7",
+      )
