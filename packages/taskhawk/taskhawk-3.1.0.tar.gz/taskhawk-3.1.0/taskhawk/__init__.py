7a21ee555e80e3285113e0dc592826b8a4c39e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package taskhawk/3.1.0/taskhawk-3.1.0.tar.gz

---
+++ packages/taskhawk/taskhawk-3.1.0.tar.gz/taskhawk-3.1.0/taskhawk/__init__.py
@@ -0,0 +1,18 @@
+"""
+taskhawk
+~~~~~~~~
+
+:copyright: (c) 2013-2017 by the Automatic Labs.
+"""
+
+
+# semantic versioning (http://semver.org/)
+VERSION = '3.1.0'
+
+
+from .models import Metadata, Priority  # noqa
+from .task_manager import AsyncInvocation, task, Task  # noqa
+from .consumer import listen_for_messages, process_messages_for_lambda_consumer  # noqa
+from .commands import requeue_dead_letter  # noqa
+from .exceptions import *  # noqa
+from .publisher import publish  # noqa
