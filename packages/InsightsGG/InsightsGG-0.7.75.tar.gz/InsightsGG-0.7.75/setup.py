0eed677165c761d1839d2a674b0a0df74a8f24c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:40 +0000

    Add package InsightsGG/0.7.75/InsightsGG-0.7.75.tar.gz

---
+++ packages/InsightsGG/InsightsGG-0.7.75.tar.gz/InsightsGG-0.7.75/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="InsightsGG",
+    version="0.7.75",
+    author="Aud",
+    author_email="audaciousxth@gmail.com",
+    description="The unofficial api for Insights.gg",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thoward02/InsightsGG",
+    packages= ["insightsGG"],
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
