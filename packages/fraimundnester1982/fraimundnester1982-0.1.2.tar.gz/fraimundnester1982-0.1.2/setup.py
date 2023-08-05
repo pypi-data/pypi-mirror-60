34d30130106fe19b432a421bc8cc54fbd40f0e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package fraimundnester1982/0.1.2/fraimundnester1982-0.1.2.tar.gz

---
+++ packages/fraimundnester1982/fraimundnester1982-0.1.2.tar.gz/fraimundnester1982-0.1.2/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='fraimundnester1982',
+    version='0.1.2',
+    py_module=['fraimundnester1982'],
+    author='Fabio Raimundo',
+    author_email='fabio.ap.raimundo@gmail.com',
+    url='http://www.headfirstlabs.com',
+    keywords='Listas aninhadas',
+    description='Listas aninhadas'
+)
