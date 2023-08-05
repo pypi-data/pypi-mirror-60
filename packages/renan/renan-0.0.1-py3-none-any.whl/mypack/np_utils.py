2d4aef6fb4e3d7011ea5e936bf7299bbd114bd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:52 +0000

    Add package renan/0.0.1/renan-0.0.1-py3-none-any.whl

---
+++ packages/renan/renan-0.0.1-py3-none-any.whl/mypack/np_utils.py
@@ -0,0 +1,14 @@
+"""
+Docstring.
+"""
+
+import numpy as np
+
+def multiply_array(array, scalar):
+    """
+    Docstring.
+    """
+
+    new_array = np.array(array) * scalar
+
+    return new_array
---
+++ packages/renan/renan-0.0.1-py3-none-any.whl/mypack/plot_utils.py
@@ -0,0 +1,15 @@
+"""
+Docstring.
+"""
+
+import matplotlib.pyplot as plt
+
+def plot_line(x_array, y_array):
+    """
+    Docstring.
+    """
+
+    plt.figure()
+    plt.plot(x_array, y_array)
+    plt.tight_layout()
+    plt.show()
