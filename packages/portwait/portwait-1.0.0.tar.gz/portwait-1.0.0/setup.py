2b3ab02e732b306ceff944060d40508821748415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package portwait/1.0.0/portwait-1.0.0.tar.gz

---
+++ packages/portwait/portwait-1.0.0.tar.gz/portwait-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'portwait',
+    scripts=['bin/portwait'],
+    version = '1.0.0',
+    description = 'CLI tool for waiting on ports to open',
+    author = 'KJ',
+    author_email = 'jdotpy@users.noreply.github.com',
+    url = 'https://github.com/jdotpy/portwait',
+    download_url = 'https://github.com/jdotpy/portwait/tarball/master',
+    keywords = ['tools'],
+    classifiers = [],
+)
