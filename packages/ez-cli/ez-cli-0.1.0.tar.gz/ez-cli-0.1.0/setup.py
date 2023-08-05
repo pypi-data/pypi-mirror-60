adea7daff711c78827ffdcc21efff20b120d8454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package ez-cli/0.1.0/ez-cli-0.1.0.tar.gz

---
+++ packages/ez-cli/ez-cli-0.1.0.tar.gz/ez-cli-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ez-cli",
+    version="0.1.0",
+    author="Harsh Verma",
+    author_email="harsh376@gmail.com",
+    description="A small cli",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harsh376/ez-cli",
+    packages=['ez_cli'],
+    install_requires=['invoke'],
+    entry_points={
+        'console_scripts': ['ezpz = ez_cli.main:program.run']
+    },
+    python_requires='>=3.7',
+)
