9fb25820a28cd1e522bcc6944746a4fde262cc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:52 +0000

    Add package arcbound/0.0.3/arcbound-0.0.3-py3-none-any.whl

---
+++ packages/arcbound/arcbound-0.0.3-py3-none-any.whl/arcbound/__init__.py
@@ -0,0 +1,9 @@
+"""
+Initializes the arcbound package. Load classes and functions into the arcbound
+namespace.
+"""
+
+from .arc import arc
+from .graph import graph
+from .requires import requires
+
