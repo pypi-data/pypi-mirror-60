4ee87066dcb0ec131b9b7b8abf8c3bb2ffcd6bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:52 +0000

    Add package lytest/0.0.20/lytest-0.0.20.tar.gz

---
+++ packages/lytest/lytest-0.0.20.tar.gz/lytest-0.0.20/lytest/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '0.0.20'
+
+# quickplotters so you can debug more easily
+try:
+    import lyipc.client as ipc
+    kqp = ipc.generate_display_function(None, 'debugging.gds')
+except ImportError:
+    def kqp(*args, **kwargs):
+        print('klayout quickplot is not available.\nPlease install lyipc.')
+try:
+    from lyipc.client import load as ipc_load
+except ImportError:
+    def ipc_load(*args, **kwargs):
+        pass
+
+try:
+    from phidl import quickplot2 as qp
+except ImportError:
+    def qp(*args, **kwargs):
+        print('phidl does not seem to be installed, so you cannot use qp')
+
+
+from lytest.kdb_xor import GeometryDifference, run_xor, xor_polygons_phidl
+from lytest.utest_buds import store_reference, difftest_it
+from lytest.containers import contained_phidlDevice, contained_pyaCell, contained_script
+import lytest.command_line
---
+++ packages/lytest/lytest-0.0.20.tar.gz/lytest-0.0.20/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+
+def readme():
+    with open('README.md', 'r') as fx:
+      return fx.read()
+
+
+setup(name='lytest',
+      version='0.0.20',
+      description='Regression testing for klayout and phidl',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',
+      author_email='alexander.tait@nist.gov',
+      license='MIT',
+      packages=['lytest'],
+      install_requires=['pytest', 'lygadgets>=0.1.15', 'lyipc>0.2.0', 'phidl'],
+      entry_points={'console_scripts': ['lytest=lytest.command_line:cm_main']},
+      cmdclass={},
+      )
