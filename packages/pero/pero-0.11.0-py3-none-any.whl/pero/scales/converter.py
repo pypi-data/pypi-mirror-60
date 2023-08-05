a13ece8b2a2556d6b91cc3c9e8ede7637f9105e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:20:59 +0000

    Add package pero/0.11.0/pero-0.11.0-py3-none-any.whl

---
+++ packages/pero/pero-0.11.0-py3-none-any.whl/pero/scales/converter.py
@@ -0,0 +1,78 @@
+#  Created byMartin.cz
+#  Copyright (c) Martin Strohalm. All rights reserved.
+
+import numpy
+from ..enums import *
+from ..properties import *
+
+
+class Converter(PropertySet):
+    """
+    Converter is a simple tool to convert from one value to another by custom
+    forward and reverse functions.
+    
+    Properties:
+        
+        forward: callable
+            Specifies the function to be used for forward conversion. The
+            function is expected to have just one argument for the input value
+            and should return converted output.
+        
+        reverse: callable
+            Specifies the function to be used for reverse conversion. The
+            function is expected to have just one argument for the input value
+            and should return reversed output.
+    """
+    
+    forward = FuncProperty(UNDEF)
+    reverse = FuncProperty(UNDEF)
+    
+    
+    def scale(self, value, *args, **kwargs):
+        """
+        Returns corresponding converted value for given input value.
+        
+        Args:
+            value: any or (any,)
+                Input value to be scaled.
+        
+        Returns:
+            any
+                Converted value.
+        """
+        
+        # apply array scaling
+        if isinstance(value, (numpy.ndarray, list, tuple)):
+            return tuple(map(self.scale, value))
+        
+        # no function defined
+        if self.forward is UNDEF:
+            return None
+        
+        # convert
+        return self.forward(value)
+    
+    
+    def invert(self, value, *args, **kwargs):
+        """
+        Returns corresponding reversed value for given output value.
+        
+        Args:
+            value: any or (any,)
+                Output value to be inverted.
+        
+        Returns:
+            any
+                Reversed value.
+        """
+        
+        # apply array scaling
+        if isinstance(value, (numpy.ndarray, list, tuple)):
+            return tuple(map(self.invert, value))
+
+        # no function defined
+        if self.reverse is UNDEF:
+            return None
+        
+        # convert
+        return self.reverse(value)
