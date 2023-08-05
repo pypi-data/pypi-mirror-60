8a821c3e13f319be0262e5cd30a6998756ad02f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:20:48 +0000

    Add package dgl/0.4.2/dgl-0.4.2-cp35-cp35m-macosx_10_6_x86_64.whl

---
+++ packages/dgl/dgl-0.4.2-cp35-cp35m-macosx_10_6_x86_64.whl/dgl/nn/mxnet/__init__.py
@@ -0,0 +1,5 @@
+"""Package for mxnet-specific NN modules."""
+from .conv import *
+from .glob import *
+from .softmax import *
+from .utils import Sequential
---
+++ packages/dgl/dgl-0.4.2-cp35-cp35m-macosx_10_6_x86_64.whl/dgl/nn/pytorch/__init__.py
@@ -0,0 +1,6 @@
+"""Package for pytorch-specific NN modules."""
+from .conv import *
+from .glob import *
+from .softmax import *
+from .factory import *
+from .utils import Sequential
