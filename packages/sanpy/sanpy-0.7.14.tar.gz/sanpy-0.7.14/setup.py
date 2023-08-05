859af4b3210fe2c93da98f00286610ab56765020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package sanpy/0.7.14/sanpy-0.7.14.tar.gz

---
+++ packages/sanpy/sanpy-0.7.14.tar.gz/sanpy-0.7.14/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="sanpy",
+    version="0.7.14",
+    author="Santiment",
+    author_email="admin@santiment.net",
+    description="Package for Santiment API access with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/santiment/sanpy",
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'requests',
+        'iso8601',
+        'setuptools',
+        'matplotlib'
+    ]
+)
