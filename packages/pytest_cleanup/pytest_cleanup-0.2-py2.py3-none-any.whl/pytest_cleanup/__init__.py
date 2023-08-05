b64bbef0ee8f79dc7693b633f3ae008255c73e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:58 +0000

    Add package pytest_cleanup/0.2/pytest_cleanup-0.2-py2.py3-none-any.whl

---
+++ packages/pytest_cleanup/pytest_cleanup-0.2-py2.py3-none-any.whl/pytest_cleanup/__init__.py
@@ -0,0 +1,8 @@
+"""Automated, comprehensive and well-organised pytest test cases."""
+
+__version__ = '0.2'
+
+from .recorder import Recorder, get_test_data_filename, user_function
+from .runtime import parametrize_stg_tests
+
+__all__ = ['parametrize_stg_tests', 'Recorder', 'get_test_data_filename', 'user_function']
