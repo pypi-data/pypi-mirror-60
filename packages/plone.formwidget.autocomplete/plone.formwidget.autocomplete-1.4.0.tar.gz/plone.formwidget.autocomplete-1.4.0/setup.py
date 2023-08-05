311898ecb75f95c031624d4fa52741be02fa6e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:40 +0000

    Add package plone.formwidget.autocomplete/1.4.0/plone.formwidget.autocomplete-1.4.0.tar.gz

---
+++ packages/plone.formwidget.autocomplete/plone.formwidget.autocomplete-1.4.0.tar.gz/plone.formwidget.autocomplete-1.4.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '1.4.0'
+
+setup(name='plone.formwidget.autocomplete',
+      version=version,
+      description="AJAX selection widget for Plone",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Plone :: 5.1",
+          "Framework :: Plone :: 5.2",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+        ],
+      keywords='Plone selection widget AJAX',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='https://github.com/plone/plone.formwidget.autocomplete/',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.formwidget'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.formwidget.query',
+          'plone.z3cform >= 0.7.4',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
