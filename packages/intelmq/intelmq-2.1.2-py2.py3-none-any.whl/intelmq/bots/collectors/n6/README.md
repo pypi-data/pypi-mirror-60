0540373d1e4575fa751528834d556e80329feaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:08 +0000

    Add package intelmq/2.1.2/intelmq-2.1.2-py2.py3-none-any.whl

---
+++ packages/intelmq/intelmq-2.1.2-py2.py3-none-any.whl/intelmq/bots/collectors/n6/README.md
@@ -0,0 +1,3 @@
+# intelmq n6stomp collector
+
+This parser is deprecated, please use the `intelmq.bots.collectors.stomp.collector` instead
---
+++ packages/intelmq/intelmq-2.1.2-py2.py3-none-any.whl/intelmq/bots/collectors/n6/collector_stomp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from intelmq.bots.collectors.stomp.collector import StompCollectorBot
+
+
+class n6stompCollectorBot(StompCollectorBot):
+    """ main class for the n6 STOMP protocol collector """
+
+    def init(self):
+        self.logger.warning("This module is deprecated and will be removed in "
+                            "version 2.0. Please use intelmq.bots.collectors."
+                            "stomp.collector instead.")
+        super().init()
+
+
+BOT = n6stompCollectorBot
