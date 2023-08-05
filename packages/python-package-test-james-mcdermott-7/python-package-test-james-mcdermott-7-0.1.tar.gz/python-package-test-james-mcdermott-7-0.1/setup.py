be152123eb428c4870cf892a1191879b9d92b520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:09 +0000

    Add package python-package-test-james-mcdermott-7/0.1/python-package-test-james-mcdermott-7-0.1.tar.gz

---
+++ packages/python-package-test-james-mcdermott-7/python-package-test-james-mcdermott-7-0.1.tar.gz/python-package-test-james-mcdermott-7-0.1/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-test-james-mcdermott-7',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/BillMills/python-package-example',
+    author='James McDermott'
+)
