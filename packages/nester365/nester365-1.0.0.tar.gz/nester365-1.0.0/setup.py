14c5ffaf4c1def6a9d4af1afee9529294bbf0415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:07 +0000

    Add package nester365/1.0.0/nester365-1.0.0.tar.gz

---
+++ packages/nester365/nester365-1.0.0.tar.gz/nester365-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+ name = 'nester365',
+ version = '1.0.0',
+ py_modules = ['nester365'],
+ author = 'sen3cuh',
+ author_email = 'almuhtad@ualberta.ca',
+ url = 'http://www.headfirstlabs.com',
+ description = 'A simple printer of nested lists',
+ )
