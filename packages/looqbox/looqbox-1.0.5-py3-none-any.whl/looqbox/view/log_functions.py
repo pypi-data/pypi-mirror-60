daf83959ff9e5414601badb5b773d43f37e99d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package looqbox/1.0.5/looqbox-1.0.5-py3-none-any.whl

---
+++ packages/looqbox/looqbox-1.0.5-py3-none-any.whl/looqbox/view/log_functions.py
@@ -0,0 +1,16 @@
+from looqbox.objects.looq_object import LooqObject
+from looqbox.objects.looq_query import ObjQuery
+from looqbox.global_calling import GlobalCalling
+
+
+__all__ = ["log_query"]
+
+
+def log_query(*args):
+    query_list = GlobalCalling.looq.query_list
+    queries = [*args]
+
+    for i in range(1, len(queries)):
+        query_list = query_list.append(queries[i])
+
+    print(query_list)
