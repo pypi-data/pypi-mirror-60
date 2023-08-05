963c5716d7d7fa73fe4279db02e2035e660769c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:52 +0000

    Add package sql-tools/1.0/sql_tools-1.0-py3-none-any.whl

---
+++ packages/sql-tools/sql_tools-1.0-py3-none-any.whl/sql_tools/mysql/connect.py
@@ -0,0 +1,18 @@
+from . import constants, mysqlException, tools
+
+
+def connect(host, user, pword, db=[], charset="", validate=True, raiseError=True):
+    charset = constants.__charset__ if not charset else charset
+    constants.__credentials__ = (host, user, pword, charset)
+
+    if tools.checkInstance(db, list, tuple):
+        constants.__db__.extend(db)
+    elif tools.checkInstance(db, str):
+        constants.__db__.append(db)
+    else:
+        raise mysqlException.DatabaseError("Invalid database dtype, it should be str or array")
+
+
+def disconnect(db=[], raiseError=True):
+    db = tools.parse(db)
+    constants.__db__ = [x for x in constants.__db__ if x not in db]
