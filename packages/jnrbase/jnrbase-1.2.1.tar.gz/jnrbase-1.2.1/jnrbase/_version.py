b1f145189590da204969a35eaa36c4b69ee8af6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:15 +0000

    Add package jnrbase/1.2.1/jnrbase-1.2.1.tar.gz

---
+++ packages/jnrbase/jnrbase-1.2.1.tar.gz/jnrbase-1.2.1/jnrbase/_version.py
@@ -0,0 +1,10 @@
+# This is jnrbase version 1.2.1 (2020-01-26)
+# pylint: skip-file
+
+dotted = '1.2.1'
+libtool = '12:21'
+hex = 0x010201
+date = '2020-01-26'
+tuple = (1, 2, 1)
+dict = {'major': 1, 'minor': 2, 'micro': 1}
+web = 'jnrbase/1.2.1'
