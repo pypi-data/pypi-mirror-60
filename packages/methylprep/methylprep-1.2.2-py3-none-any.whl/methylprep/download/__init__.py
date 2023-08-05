34346474218ec40b28781d9a7030ca7a5f8ac553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:36 +0000

    Add package methylprep/1.2.2/methylprep-1.2.2-py3-none-any.whl

---
+++ packages/methylprep/methylprep-1.2.2-py3-none-any.whl/methylprep/download/__init__.py
@@ -0,0 +1,16 @@
+from .process_data import (
+	run_series,
+	run_series_list
+	)
+from .miniml import (
+	convert_miniml,
+	build_composite_dataset
+	)
+
+
+__all__ = [
+    'run_series',
+    'run_series_list',
+	'convert_miniml',
+	'build_composite_dataset',
+]
