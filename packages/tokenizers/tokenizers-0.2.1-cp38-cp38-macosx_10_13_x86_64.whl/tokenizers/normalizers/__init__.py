2736e552df12287ab4a570833df5ed7f644caf96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:13 +0000

    Add package tokenizers/0.2.1/tokenizers-0.2.1-cp38-cp38-macosx_10_13_x86_64.whl

---
+++ packages/tokenizers/tokenizers-0.2.1-cp38-cp38-macosx_10_13_x86_64.whl/tokenizers/normalizers/__init__.py
@@ -0,0 +1,10 @@
+from .. import normalizers
+
+Normalizer = normalizers.Normalizer
+BertNormalizer = normalizers.BertNormalizer
+NFD = normalizers.NFD
+NFKD = normalizers.NFKD
+NFC = normalizers.NFC
+NFKC = normalizers.NFKC
+Sequence = normalizers.Sequence
+Lowercase = normalizers.Lowercase
