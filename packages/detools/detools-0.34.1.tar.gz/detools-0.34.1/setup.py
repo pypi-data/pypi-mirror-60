837c3fea74f2140a14a7cc7ffcf5da891c16178e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:54 +0000

    Add package detools/0.34.1/detools-0.34.1.tar.gz

---
+++ packages/detools/detools-0.34.1.tar.gz/detools-0.34.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import re
+from setuptools import setup
+from setuptools import find_packages
+from setuptools import Extension
+
+
+def find_version():
+    return re.search(r"^__version__ = '(.*)'$",
+                     open('detools/version.py', 'r').read(),
+                     re.MULTILINE).group(1)
+
+
+setup(name='detools',
+      version=find_version(),
+      description='Binary delta encoding tools.',
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist',
+      author_email='erik.moqvist@gmail.com',
+      license='BSD',
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+      ],
+      url='https://github.com/eerimoq/detools',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'humanfriendly',
+          'bitstruct',
+          'pyelftools',
+          'zstandard',
+          'lz4',
+          'heatshrink2'
+      ],
+      ext_modules=[
+          Extension(name="detools.csais", sources=["detools/sais.c"]),
+          Extension(name="detools.cbsdiff", sources=["detools/bsdiff.c"])
+      ],
+      test_suite="tests",
+      entry_points={
+          'console_scripts': ['detools=detools.__init__:_main']
+      })
