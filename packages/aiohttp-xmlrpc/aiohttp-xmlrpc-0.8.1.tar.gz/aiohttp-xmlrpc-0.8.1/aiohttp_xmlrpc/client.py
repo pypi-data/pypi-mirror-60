a7830024d02fe46d4680a49c18d5ccee9ef65627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:03 +0000

    Add package aiohttp-xmlrpc/0.8.1/aiohttp-xmlrpc-0.8.1.tar.gz

---
+++ packages/aiohttp-xmlrpc/aiohttp-xmlrpc-0.8.1.tar.gz/aiohttp-xmlrpc-0.8.1/aiohttp_xmlrpc/client.py
@@ -0,0 +1,111 @@
+# encoding: utf-8
+import asyncio
+import aiohttp.client
+import logging
+from lxml import etree
+from multidict import MultiDict
+
+from . import __version__, __pyversion__, exceptions
+from .exceptions import xml2py_exception
+from .common import py2xml, xml2py, schema
+
+
+log = logging.getLogger(__name__)
+
+
+class ServerProxy(object):
+    __slots__ = 'client', 'url', 'loop', 'headers', 'encoding'
+
+    USER_AGENT = u'aiohttp XML-RPC client (Python: {0}, version: {1})'.format(__pyversion__, __version__)
+
+    def __init__(self, url, client=None, headers=None, encoding=None, **kwargs):
+        self.headers = MultiDict(headers or {})
+
+        self.headers.setdefault('Content-Type', 'text/xml')
+        self.headers.setdefault('User-Agent', self.USER_AGENT)
+
+        self.encoding = encoding
+
+        self.url = str(url)
+        self.client = client or aiohttp.client.ClientSession(**kwargs)
+
+    @staticmethod
+    def _make_request(method_name, *args, **kwargs):
+        root = etree.Element('methodCall')
+        method_el = etree.Element('methodName')
+        method_el.text = method_name
+
+        root.append(method_el)
+
+        params_el = etree.Element('params')
+        root.append(params_el)
+
+        for arg in args:
+            param = etree.Element('param')
+            val = etree.Element('value')
+            param.append(val)
+            params_el.append(param)
+            val.append(py2xml(arg))
+
+        if kwargs:
+            param = etree.Element('param')
+            val = etree.Element('value')
+            param.append(val)
+            params_el.append(param)
+            val.append(py2xml(kwargs))
+
+        return root
+
+    @staticmethod
+    def _parse_response(body, method_name):
+        try:
+            if log.getEffectiveLevel() <= logging.DEBUG:
+                log.debug("Server response: \n%s", body.decode())
+
+            response = etree.fromstring(body)
+            schema.assertValid(response)
+        except etree.DocumentInvalid:
+            raise ValueError("Invalid body")
+
+        result = response.xpath('//params/param/value/*')
+        if result:
+            return xml2py(result[0])
+
+        fault = response.xpath('//fault/value/*')
+        if fault:
+            err = xml2py(fault[0])
+
+            raise xml2py_exception(
+                err.get('faultCode', exceptions.SystemError.code),
+                err.get('faultString', 'Unknown error'),
+                default_exc_class=exceptions.ServerError
+            )
+
+        raise exceptions.ParseError('Respond body for method "%s" '
+                                    'not contains any response.', method_name)
+
+    async def __remote_call(self, method_name, *args, **kwargs):
+        async with self.client.post(
+            str(self.url),
+            data=etree.tostring(
+                self._make_request(method_name, *args, **kwargs),
+                xml_declaration=True,
+                encoding=self.encoding
+            ),
+            headers=self.headers,
+        ) as response:
+            response.raise_for_status()
+
+            return self._parse_response((await response.read()), method_name)
+
+    def __getattr__(self, method_name):
+        return self[method_name]
+
+    def __getitem__(self, method_name):
+        def method(*args, **kwargs):
+            return self.__remote_call(method_name, *args, **kwargs)
+
+        return method
+
+    def close(self):
+        return self.client.close()
---
+++ packages/aiohttp-xmlrpc/aiohttp-xmlrpc-0.8.1.tar.gz/aiohttp-xmlrpc-0.8.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import absolute_import, print_function
+from setuptools import setup, find_packages
+import aiohttp_xmlrpc as module
+
+
+setup(
+    name=module.__name__.replace("_", "-"),
+    version=module.__version__,
+    author=module.__author__,
+    license=module.__license__,
+    description=module.description,
+    long_description=open('README.rst').read(),
+    platforms="all",
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    include_package_data=True,
+    zip_safe=False,
+    package_data={
+        module.__name__: ['xmlrpc.rng'],
+    },
+    packages=find_packages(exclude=('tests',)),
+    install_requires=(
+        'aiohttp',
+        'lxml',
+    ),
+    extras_require={
+        'develop': [
+            'pytest',
+            'pytest-cov',
+            'xmltodict',
+
+        ],
+        ':python_version < "3.5"': 'typing >= 3.5.3',
+    }
+)
