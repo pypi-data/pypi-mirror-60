1009b873ca45752a7fe2760206a34b735e9078c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:34 +0000

    Add package Compositions/1.0.1/Compositions-1.0.1-py3-none-any.whl

---
+++ packages/Compositions/Compositions-1.0.1-py3-none-any.whl/compositions/compositions.py
@@ -0,0 +1,21 @@
+class Compose(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, x):
+        return self.func(x)
+
+    def __mul__(self, neighbour):
+        return Compose(lambda x: self.func(neighbour.func(x)))
+
+
+if __name__ == "__main__":
+    # Syntax 1
+    @Compose
+    def f(x):
+        return x
+
+    # Syntax 2
+    g = Compose(lambda x: x)
+
+    print((f * g)(2))
