7822e7b61e0bf7d58dd3bc671bc261824f667e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package guillotina/6.0.0a8/guillotina-6.0.0a8.tar.gz

---
+++ packages/guillotina/guillotina-6.0.0a8.tar.gz/guillotina-6.0.0a8/guillotina/auth/__init__.py
@@ -0,0 +1,5 @@
+from .groups import GroupsUtility  # noqa
+from .utils import authenticate_request  # noqa
+from .utils import authenticate_user  # noqa
+from .utils import find_user  # noqa
+from .utils import set_authenticated_user  # noqa
---
+++ packages/guillotina/guillotina-6.0.0a8.tar.gz/guillotina-6.0.0a8/guillotina/db/cache/dummy.py
@@ -0,0 +1,25 @@
+from guillotina import configure
+from guillotina.db.cache.base import BaseCache
+from guillotina.db.interfaces import ITransaction
+from guillotina.db.interfaces import ITransactionCache
+
+
+@configure.adapter(for_=ITransaction, provides=ITransactionCache, name="dummy")
+class DummyCache(BaseCache):
+    async def get(self, **kwargs):
+        return None
+
+    async def set(self, value, **kwargs):
+        pass
+
+    async def clear(self):
+        pass
+
+    async def invalidate(self, ob):
+        pass
+
+    async def delete(self, key):
+        pass
+
+    async def delete_all(self, keys):
+        pass
