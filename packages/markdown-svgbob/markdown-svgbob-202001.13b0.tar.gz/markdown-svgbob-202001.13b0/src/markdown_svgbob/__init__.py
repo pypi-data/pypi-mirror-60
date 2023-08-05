25fe470b68ac31afc8efcd7a0b4a27a401cc6fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:47 +0000

    Add package markdown-svgbob/202001.13b0/markdown-svgbob-202001.13b0.tar.gz

---
+++ packages/markdown-svgbob/markdown-svgbob-202001.13b0.tar.gz/markdown-svgbob-202001.13b0/src/markdown_svgbob/__init__.py
@@ -0,0 +1,26 @@
+# This file is part of the markdown-svgbob project
+# https://gitlab.com/mbarkhau/markdown-svgbob
+#
+# Copyright (c) 2019 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+"""markdown_svgbob extension.
+
+This is an extension for Python-Markdown which
+uses svgbob to generate images from ascii
+diagrams in fenced code blocks.
+"""
+
+__version__ = "v202001.0013-beta"
+
+
+from markdown_svgbob.extension import SvgbobExtension
+from markdown_svgbob.wrapper import text2svg, get_bin_path
+
+get_svgbob_bin_path = get_bin_path
+
+
+def makeExtension(**kwargs) -> SvgbobExtension:
+    return SvgbobExtension(**kwargs)
+
+
+__all__ = ['makeExtension', '__version__', 'get_bin_path', 'get_svgbob_bin_path', 'text2svg']
