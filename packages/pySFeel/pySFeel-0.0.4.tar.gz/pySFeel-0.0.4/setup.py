d66a42abe54b16069eb6d4817cc81ff6ba1b880b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:34 +0000

    Add package pySFeel/0.0.4/pySFeel-0.0.4.tar.gz

---
+++ packages/pySFeel/pySFeel-0.0.4.tar.gz/pySFeel-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pySFeel',
+    version='0.0.4',
+    author='Russell McDonell',
+    author_email='russell.mcdonell@c-cost.com',
+    description='An implementation of S-FEEL using sly',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/russell.mcdonell/pySFeel',
+    packages='.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['sly','python-dateutil'],
+)
+
