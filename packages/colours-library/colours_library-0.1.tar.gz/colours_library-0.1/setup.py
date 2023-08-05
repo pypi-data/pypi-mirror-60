d14dd0634d4b4021600ddab14cfdecc2b6a32571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package colours-library/0.1/colours_library-0.1.tar.gz

---
+++ packages/colours-library/colours_library-0.1.tar.gz/colours_library-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='colours_library',
+      version='0.1',
+      description='Colours library package',
+      url='https://gitlab.bitcomp.intra/eryk_malczyk/colours_library.git',
+      author='ErykM',
+      author_email='eryk.malczyk@bitcomp.fi',
+      license='some_license',
+      packages=['colours_library'],
+      zip_safe=False)
