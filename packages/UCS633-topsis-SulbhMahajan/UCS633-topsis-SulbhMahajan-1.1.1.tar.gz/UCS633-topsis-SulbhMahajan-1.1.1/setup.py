3a18976ea492cbb4bac00a897a22c67f1303439a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:56 +0000

    Add package UCS633-topsis-SulbhMahajan/1.1.1/UCS633-topsis-SulbhMahajan-1.1.1.tar.gz

---
+++ packages/UCS633-topsis-SulbhMahajan/UCS633-topsis-SulbhMahajan-1.1.1.tar.gz/UCS633-topsis-SulbhMahajan-1.1.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        README=f.read()
+    return README
+
+setuptools.setup(
+    name="UCS633-topsis-SulbhMahajan",
+    version="1.1.1",
+    author="Sulbh Mahajan",
+    author_email="sulbh579@gmail.com",
+    description="A python package to implement topsis(MCDM)",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+	 "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    #packages=["mypackage"],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=["pandas","numpy","scipy"],
+    entry_points={
+        "console_scripts":[
+            "UCS633-topsis-Sulbh-101703569=mypackage.topsis:main",
+        ]
+    },
+)
