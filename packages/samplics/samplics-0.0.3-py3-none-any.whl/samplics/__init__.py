6ace46bab8dbdabf14964b559add9988b7ef3777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:27 +0000

    Add package samplics/0.0.3/samplics-0.0.3-py3-none-any.whl

---
+++ packages/samplics/samplics-0.0.3-py3-none-any.whl/samplics/__init__.py
@@ -0,0 +1,6 @@
+from samplics.sampling import *
+from samplics.weighting import *
+from samplics.estimation import *
+from samplics.utils.formats import array_to_dict
+
+# __all__ = ["sampling", "weighting", "estimation", "sae"]
