a06177e96d4ad58a8308580382242a52bd1e9a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:36 +0000

    Add package gaussian-binomial-distributions-vbcsja/7.781/gaussian_binomial_distributions_vbcsja-7.781.tar.gz

---
+++ packages/gaussian-binomial-distributions-vbcsja/gaussian_binomial_distributions_vbcsja-7.781.tar.gz/gaussian_binomial_distributions_vbcsja-7.781/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_distributions_vbcsja',
+      version='7.781',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_distributions'],
+      author="Maximilian",
+      author_email="max@something.com",
+      zip_safe=False)
