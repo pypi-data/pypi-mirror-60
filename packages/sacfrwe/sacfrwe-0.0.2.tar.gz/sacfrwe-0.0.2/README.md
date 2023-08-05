94ea4877a99d2e4e9d37aeb3857edac9b3da2dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:43 +0000

    Add package sacfrwe/0.0.2/sacfrwe-0.0.2.tar.gz

---
+++ packages/sacfrwe/sacfrwe-0.0.2.tar.gz/sacfrwe-0.0.2/README.md
@@ -0,0 +1,2 @@
+# pip_toudu
+pip toudu
---
+++ packages/sacfrwe/sacfrwe-0.0.2.tar.gz/sacfrwe-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sacfrwe',
+    version='0.0.2',
+    keywords=['simple', 'test'],
+    description='just a simple toudu',
+    license='MIT License',
+    author='jcc',
+    author_email='sacfrwe@126.com',
+    packages=find_packages(),
+    platforms=['any'],
+    url=['https://github.com/ewrfcas/pip_toudu/tree/master']
+)
