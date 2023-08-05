7355470d6900abf535a6c0bea826f33c2adb536b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:05 +0000

    Add package nmigen-boards/0.0/nmigen-boards-0.0.tar.gz

---
+++ packages/nmigen-boards/nmigen-boards-0.0.tar.gz/nmigen-boards-0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def scm_version():
+    def local_scheme(version):
+        return version.format_choice("+{node}", "+{node}.dirty")
+    return {
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme,
+    }
+
+
+setup(
+    name="nmigen-boards",
+#    use_scm_version=scm_version(),
+    version="0.0",
+    author="whitequark",
+    author_email="whitequark@whitequark.org",
+    description="Board and connector definitions for nMigen",
+    #long_description="""TODO""",
+    license="BSD",
+    setup_requires=["setuptools_scm"],
+    install_requires=["setuptools", "nmigen"],
+    packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/m-labs/nmigen-boards",
+        "Bug Tracker": "https://github.com/m-labs/nmigen-boards/issues",
+    },
+)
