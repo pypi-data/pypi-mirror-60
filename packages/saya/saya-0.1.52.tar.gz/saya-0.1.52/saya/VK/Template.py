aa585c2f64a24b035386517193a0d0d31ed73f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:54 +0000

    Add package saya/0.1.52/saya-0.1.52.tar.gz

---
+++ packages/saya/saya-0.1.52.tar.gz/saya-0.1.52/saya/VK/Template.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+import json
+
+
+class Template(dict):
+    def __init__(self, ttype="carousel"):
+        self["type"] = ttype
+        self["elements"] = []
+
+    def add(self, element):
+        """Adds new element in template.
+
+        Arguments:
+            element {TemplateElement}
+        """
+        if len(self["elements"]) < 10:
+            self["elements"].append(element)
+
+    def compile(self):
+        """Compiles Template for sendng in the message.
+
+        Returns:
+            dict -- compiled Template.
+        """
+        return json.dumps(self)
