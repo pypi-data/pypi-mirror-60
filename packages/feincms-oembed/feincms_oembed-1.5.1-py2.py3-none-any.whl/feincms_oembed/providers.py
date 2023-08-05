ada260f1643dc6b2f21769785a5424d7a68e8d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:44 +0000

    Add package feincms-oembed/1.5.1/feincms_oembed-1.5.1-py2.py3-none-any.whl

---
+++ packages/feincms-oembed/feincms_oembed-1.5.1-py2.py3-none-any.whl/feincms_oembed/providers.py
@@ -0,0 +1,20 @@
+from django.utils.http import urlencode
+from django.conf import settings
+
+
+def embedly_oembed_provider(url, kwargs):
+    """
+    Provider for the oEmbed service at http://embed.ly/
+    Embedly requires an API key.
+    """
+    kwargs["url"] = url
+    kwargs["key"] = settings.EMBEDLY_KEY
+    return "https://api.embed.ly/1/oembed?%s" % urlencode(kwargs)
+
+
+def noembed_oembed_provider(url, kwargs):
+    """
+    Provider for the oEmbed service at http://noembed.com/
+    """
+    kwargs["url"] = url
+    return "http://noembed.com/embed?%s" % urlencode(kwargs)
