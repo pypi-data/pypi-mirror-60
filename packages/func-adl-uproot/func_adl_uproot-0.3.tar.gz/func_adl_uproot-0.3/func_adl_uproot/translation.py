a9060fac35d93ff4b3984ba07ec424b2a78afa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:07 +0000

    Add package func-adl-uproot/0.3/func_adl_uproot-0.3.tar.gz

---
+++ packages/func-adl-uproot/func_adl_uproot-0.3.tar.gz/func_adl_uproot-0.3/func_adl_uproot/translation.py
@@ -0,0 +1,21 @@
+import qastle
+
+from .transformer import PythonSourceGeneratorTransformer
+
+
+def python_ast_to_python_source(python_ast):
+    return PythonSourceGeneratorTransformer().get_rep(python_ast)
+
+
+def generate_python_source(ast, array_name='output_array', array_pathname='output.awkd'):
+    if isinstance(ast, str):
+        ast = qastle.text_ast_to_python_ast(ast)
+    qastle.insert_linq_nodes(ast)
+    source = 'import awkward\n'
+    source += 'import uproot\n'
+    source += 'import sys\n'
+    source += 'if len(sys.argv) > 2:\n'
+    source += "    raise IndexError('Cannot handle multiple files at once in uproot backend')\n"
+    source += array_name + ' = ' + python_ast_to_python_source(ast) + '\n'
+    source += 'awkward.save(' + repr(array_pathname) + ', ' + array_name + ", mode='w')\n"
+    return source
