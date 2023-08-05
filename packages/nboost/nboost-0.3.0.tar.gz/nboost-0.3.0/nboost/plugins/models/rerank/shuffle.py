d64038f814d6b4d513110a27da783b1853954c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:02 +0000

    Add package nboost/0.3.0/nboost-0.3.0.tar.gz

---
+++ packages/nboost/nboost-0.3.0.tar.gz/nboost-0.3.0/nboost/plugins/models/rerank/shuffle.py
@@ -0,0 +1,13 @@
+"""Shuffle model"""
+
+from nboost.plugins.models.rerank.base import RerankModelPlugin
+import random
+
+
+class ShuffleRerankModelPlugin(RerankModelPlugin):
+    """Model that randomly shuffles choices. Useful for benchmarking/testing"""
+    def rank(self, query, choices, **kwargs):
+        """random shuffle with no regard for query"""
+        ranks = list(range(len(choices)))
+        random.shuffle(ranks)
+        return ranks
