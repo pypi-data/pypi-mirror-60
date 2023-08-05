d6ce31613359a9a7b4c58743b3e327e50862d4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package silkaj/0.7.5/silkaj-0.7.5-py3-none-any.whl

---
+++ packages/silkaj/silkaj-0.7.5-py3-none-any.whl/silkaj/constants.py
@@ -0,0 +1,25 @@
+"""
+Copyright  2016-2019 Maël Azimi <m.a@moul.re>
+
+Silkaj is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Silkaj is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Silkaj. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+SILKAJ_VERSION = "0.7.5"
+G1_SYMBOL = "Ğ1"
+GTEST_SYMBOL = "ĞTest"
+G1_DEFAULT_ENDPOINT = "g1.duniter.org", "443"
+G1_TEST_DEFAULT_ENDPOINT = "g1-test.duniter.org", "443"
+CONNECTION_TIMEOUT = 10
+ASYNC_SLEEP = 0.1
+SOURCES_PER_TX = 40
