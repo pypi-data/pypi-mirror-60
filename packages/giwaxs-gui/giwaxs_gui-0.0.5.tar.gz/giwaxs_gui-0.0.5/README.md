cc14f827189b821f68023e5b424bcad7e4be66d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package giwaxs-gui/0.0.5/giwaxs_gui-0.0.5.tar.gz

---
+++ packages/giwaxs-gui/giwaxs_gui-0.0.5.tar.gz/giwaxs_gui-0.0.5/README.md
@@ -0,0 +1,23 @@
+# giwaxs-gui
+### A graphical tool for GIWAXS data analysis.
+
+giwaxs-gui contains all the basic functionality for 
+the analysis of 2D diffraction images with circular symmetry. 
+In particular, it focuses on grazing-incidence wide-angle scattering
+data and its specific needs.
+
+## Install
+### Pip install 
+To install current release via pip, you should have python installed 
+on your computer. The minimum required version is 3.6.0. Install package via pip:
+
+```sh
+pip install giwaxs-gui
+```
+
+After installation you can launch the program:
+```sh
+python
+>>>from giwaxs_gui import run
+>>>run()
+```
---
+++ packages/giwaxs-gui/giwaxs_gui-0.0.5.tar.gz/giwaxs_gui-0.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from pathlib import Path
+from setuptools import find_packages
+
+
+def read(filename: str):
+    with open(Path(__file__).parent / filename, mode='r', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='giwaxs_gui',
+    packages=find_packages(),
+    version='0.0.5',
+    author='Vladimir Starostin',
+    author_email='vladimir.starostin@uni-tuebingen.de',
+    description='A graphical tool for basic analysis of GIWAXS images.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    license='GNU',
+    python_requires='>=3.6.*',
+    install_requires=[
+        'numpy>=1.18.1',
+        'matplotlib>=3.1.2',
+        'pillow',
+        'scipy>=1.4.1',
+        'h5py>=2.10.0',
+        'PyQt5',
+        'pyqtgraph'
+    ],
+    include_package_data=True,
+    keywords='xray python giwaxs scientific-analysis',
+    url='http://packages.python.org/giwaxs_gui',
+)
