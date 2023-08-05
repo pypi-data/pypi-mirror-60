cf34ad014cff8f138e20b069df079760f47b3b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:06 +0000

    Add package wandb/0.8.22/wandb-0.8.22.tar.gz

---
+++ packages/wandb/wandb-0.8.22.tar.gz/wandb-0.8.22/tests/test_summary.py
@@ -0,0 +1,203 @@
+import matplotlib
+# this needs to be called before importing wandb Graph
+matplotlib.use("Agg")
+
+
+from wandb import wandb_run
+from wandb.summary import FileSummary
+import pandas
+import tensorflow as tf
+import torch
+import json
+import glob
+import os
+import numpy as np
+import tempfile
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+from wandb import Histogram, Image, Graph, Table
+from click.testing import CliRunner
+import pytest
+
+
+@pytest.fixture
+def summary():
+    with CliRunner().isolated_filesystem():
+        run = wandb_run.Run()
+        run.summary.update({"foo": "init"})
+        yield run.summary
+
+
+def disk_summary(summary):
+    return json.load(open(summary._fname))
+
+
+def test_set_attrs(summary):
+    summary.foo = "bar"
+    assert disk_summary(summary) == {"foo": "bar"}
+
+
+def test_get_attr(summary):
+    assert summary.foo == "init"
+
+
+def test_update(summary):
+    summary.update({"foo": "bar"})
+    assert disk_summary(summary) == {"foo": "bar"}
+
+
+def test_update_histogram(summary):
+    summary.update({"hist": Histogram(np_histogram=([1, 2, 3], [1, 2, 3, 4]))})
+    assert disk_summary(summary) == {
+        'foo': 'init',
+        "hist": {"_type": "histogram", "values": [1, 2, 3], "bins": [1, 2, 3, 4]}}
+
+
+def test_set_histogram(summary):
+    summary["hist"] = Histogram(np_histogram=([1, 2, 3], [1, 2, 3, 4]))
+    assert disk_summary(summary) == {
+        'foo': 'init',
+        "hist": {"_type": "histogram", "values": [1, 2, 3], "bins": [1, 2, 3, 4]}}
+
+
+def test_set_item(summary):
+    summary["foo"] = "bar"
+    assert disk_summary(summary) == {"foo": "bar"}
+
+
+def test_get_item(summary):
+    assert summary["foo"] == "init"
+
+
+def test_delete(summary):
+    summary.update({"foo": "bar", "bad": True})
+    assert summary['foo'] == 'bar'
+    assert summary['bad'] is True
+    del summary["bad"]
+    assert disk_summary(summary) == {"foo": "bar"}
+
+
+def test_image(summary):
+    summary["image"] = Image(np.zeros((28, 28)))
+    ds = disk_summary(summary)
+    assert os.path.exists(os.path.join(summary._run.dir, ds['image']['path']))
+
+    expected = {
+        '_type': 'image-file',
+        'height': 28,
+        'width': 28,
+        'size': 73,
+    }
+    assert set(ds['image'].items()) >= set(expected.items())
+
+
+def test_matplot_image(summary):
+    img = plt.imshow(np.zeros((28, 28)), cmap='gray')
+    summary["fig"] = img
+    plt.close()
+    ds = disk_summary(summary)
+    assert os.path.exists(os.path.join(summary._run.dir, ds['fig']['path']))
+    assert set(ds["fig"].items()) >= set({
+        "_type": "image-file",
+        "height": 480,
+        "width": 640,
+    }.items())
+
+
+def test_matplot_plotly(summary):
+    plt.cla()
+    plt.plot([1, 2, 3])
+    summary["plot"] = plt
+    plt.close()
+    plot = disk_summary(summary)["plot"]
+    assert plot["_type"] == "plotly"
+
+
+def test_plotly_plot(summary):
+    summary["plot"] = go.Scatter(x=[0, 1, 2])
+    plot = disk_summary(summary)["plot"]
+    assert plot["_type"] == "plotly"
+    assert plot["plot"]['type'] == 'scatter'
+
+def test_plotly_big_numpy(summary):
+    N = 200
+    x = np.arange(N)
+    y = np.arange(0, N) / N
+    summary["plot"] = go.Figure(data=go.Scatter(x=x, y=y, mode='markers'))
+    plot = disk_summary(summary)["plot"]
+    assert len(plot["plot"]["data"][0]["x"]) == 200
+
+
+def test_newline(summary):
+    summary["rad \n"] = 1
+    summary.update({"bad \n ": 2})
+    summ = disk_summary(summary)
+    assert summ["rad"] == 1
+    assert summ["bad"] == 2
+
+
+def test_big_numpy(summary):
+    summary.update({"rad": np.random.rand(1000)})
+    assert disk_summary(summary)["rad"]["max"] > 0
+    assert os.path.exists(os.path.join(summary._run.dir, "wandb.h5"))
+
+
+def test_big_nested_numpy(summary):
+    summary.update({"rad": {"deep": np.random.rand(1000)}})
+    assert disk_summary(summary)["rad"]["deep"]["max"] > 0
+    summary["rad"]["deep2"] = np.random.rand(1000)
+    assert disk_summary(summary)["rad"]["deep2"]["max"] > 0
+    assert os.path.exists(os.path.join(summary._run.dir, "wandb.h5"))
+
+
+def test_torch_tensor(summary):
+    summary.update({"pytorch": torch.rand(1000, 1000)})
+    assert os.path.exists(os.path.join(summary._run.dir, "wandb.h5"))
+    assert disk_summary(summary)["pytorch"]["_type"] == "torch.Tensor"
+
+
+def test_tensorflow_tensor(summary):
+    with tf.Session().as_default():
+        summary.update({"tensorflow": tf.random_normal([1000])})
+    assert os.path.exists(os.path.join(summary._run.dir, "wandb.h5"))
+    assert disk_summary(summary)["tensorflow"]["_type"] == "tensorflow.Tensor"
+
+
+def test_pandas(summary):
+    summary.update({"pandas": pandas.DataFrame(
+        data=np.random.rand(1000), columns=['col'])})
+
+
+def test_read_numpy(summary):
+    summary.update({"rad": np.random.rand(1000)})
+    s = FileSummary(summary._run)
+    assert len(s["rad"]) == 1000
+
+
+def test_read_nested_numpy(summary):
+    summary.update({"rad": {"deep": np.random.rand(1000)}})
+    s = FileSummary(summary._run)
+    assert len(s["rad"]["deep"]) == 1000
+
+def test_read_nested_array(summary):
+    summary["rad"] = {"deep": "dish"}
+    s = FileSummary(summary._run)
+    assert summary["rad"]["deep"] == "dish"
+
+
+def test_read_very_nested_numpy(summary):
+    # Test that even deeply nested writes are written to disk.
+    summary.update(
+        {"a": {"b": {"c": {"d": {"e": {"f": {"g": {"h": {"i": {}}}}}}}}}})
+    summary['a']['b']['c']['d']['e']['f']['g']['h']['i']['j'] = True
+    assert disk_summary(summary)[
+        'a']['b']['c']['d']['e']['f']['g']['h']['i']['j'] is True
+
+
+def test_key_locking(summary):
+    summary.update({'a': 'a'})
+    assert summary['a'] == 'a'
+    summary.update({'a': 'b'})
+    assert summary['a'] == 'b'
+    summary.update({'a': 'c'}, overwrite=False)
+    assert summary['a'] == 'b'
---
+++ packages/wandb/wandb-0.8.22.tar.gz/wandb-0.8.22/wandb/meta.py
@@ -0,0 +1,151 @@
+import json
+import os
+import sys
+import platform
+import multiprocessing
+import pynvml
+import threading
+import time
+import socket
+import getpass
+from shutil import copyfile
+from datetime import datetime
+
+from wandb import util
+from wandb import env
+import wandb
+
+METADATA_FNAME = 'wandb-metadata.json'
+
+
+class Meta(object):
+    """Used to store metadata during and after a run."""
+
+    HEARTBEAT_INTERVAL_SECONDS = 15
+
+    def __init__(self, api, out_dir='.'):
+        self.out_dir = out_dir
+        self.fname = os.path.join(out_dir, METADATA_FNAME)
+        self._api = api
+        self._shutdown = False
+        try:
+            self.data = json.load(open(self.fname))
+        except (IOError, ValueError):
+            self.data = {}
+        self.lock = threading.Lock()
+        self.setup()
+        self._thread = threading.Thread(target=self._thread_body)
+        self._thread.daemon = True
+
+    def start(self):
+        self._thread.start()
+
+    def setup(self):
+        self.data["root"] = os.getcwd()
+        try:
+            import __main__
+            self.data["program"] = __main__.__file__
+        except (ImportError, AttributeError):
+            self.data["program"] = '<python with no main file>'
+            if wandb._get_python_type() != "python":
+                if os.getenv(env.NOTEBOOK_NAME):
+                    self.data["program"] = os.getenv(env.NOTEBOOK_NAME)
+                else:
+                    meta = wandb.jupyter.notebook_metadata()
+                    if meta.get("path"):
+                        if "fileId=" in meta["path"]:
+                            self.data["colab"] = "https://colab.research.google.com/drive/"+meta["path"].split("fileId=")[1]
+                            self.data["program"] = meta["name"]
+                        else:
+                            self.data["program"] = meta["path"]
+                            self.data["root"] = meta["root"]
+
+        program = os.path.join(self.data["root"], self.data["program"])
+        if not os.getenv(env.DISABLE_CODE):
+            if self._api.git.enabled:
+                self.data["git"] = {
+                    "remote": self._api.git.remote_url,
+                    "commit": self._api.git.last_commit
+                }
+
+                self.data["email"] = self._api.git.email
+                self.data["root"] = self._api.git.root or self.data["root"]
+
+            if os.path.exists(program) and self._api.git.is_untracked(self.data["program"]):
+                util.mkdir_exists_ok(os.path.join(self.out_dir, "code", os.path.dirname(self.data["program"])))
+                saved_program = os.path.join(self.out_dir, "code", self.data["program"])
+                if not os.path.exists(saved_program):
+                    self.data["codeSaved"] = True
+                    copyfile(program, saved_program)
+
+        self.data["startedAt"] = datetime.utcfromtimestamp(
+            wandb.START_TIME).isoformat()
+        try:
+            username = getpass.getuser()
+        except KeyError:
+            # getuser() could raise KeyError in restricted environments like
+            # chroot jails or docker containers.  Return user id in these cases.
+            username = str(os.getuid())
+
+        # Host names, usernames, emails, the root directory, and executable paths are sensitive for anonymous users.
+        if self._api.settings().get('anonymous') != 'true':
+            self.data["host"] = os.environ.get(env.HOST, socket.gethostname())
+            self.data["username"] = os.getenv(env.USERNAME, username)
+            self.data["executable"] = sys.executable
+        else:
+            self.data.pop("email", None)
+            self.data.pop("root", None)
+
+        self.data["os"] = platform.platform(aliased=True)
+        self.data["python"] = platform.python_version()
+
+        if env.get_docker():
+            self.data["docker"] = env.get_docker()
+        try:
+            pynvml.nvmlInit()
+            self.data["gpu"] = pynvml.nvmlDeviceGetName(
+                pynvml.nvmlDeviceGetHandleByIndex(0)).decode("utf8")
+            self.data["gpu_count"] = pynvml.nvmlDeviceGetCount()
+        except pynvml.NVMLError:
+            pass
+        try:
+            self.data["cpu_count"] = multiprocessing.cpu_count()
+        except NotImplementedError:
+            pass
+        # TODO: we should use the cuda library to collect this
+        if os.path.exists("/usr/local/cuda/version.txt"):
+            self.data["cuda"] = open(
+                "/usr/local/cuda/version.txt").read().split(" ")[-1].strip()
+        self.data["args"] = sys.argv[1:]
+        self.data["state"] = "running"
+
+    def write(self):
+        self.lock.acquire()
+        try:
+            self.data["heartbeatAt"] = datetime.utcnow().isoformat()
+            with open(self.fname, 'w') as f:
+                s = util.json_dumps_safer(self.data, indent=4)
+                f.write(s)
+                f.write('\n')
+        finally:
+            self.lock.release()
+
+    def shutdown(self):
+        self._shutdown = True
+        try:
+            self._thread.join()
+        # Incase we never start it
+        except RuntimeError:
+            pass
+
+    def _thread_body(self):
+        seconds = 0
+        while True:
+            if seconds > self.HEARTBEAT_INTERVAL_SECONDS or self._shutdown:
+                self.write()
+                seconds = 0
+            if self._shutdown:
+                break
+            else:
+                time.sleep(1)
+                seconds += 1
