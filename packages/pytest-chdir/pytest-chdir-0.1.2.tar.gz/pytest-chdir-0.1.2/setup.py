5ec61eaa0886e6c553e107eb5f42cd9b9479e1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:44 +0000

    Add package pytest-chdir/0.1.2/pytest-chdir-0.1.2.tar.gz

---
+++ packages/pytest-chdir/pytest-chdir-0.1.2.tar.gz/pytest-chdir-0.1.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pytest_chdir']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=5.0.0,<6.0.0']
+
+entry_points = \
+{'pytest11': ['pytest-chdir = pytest_chdir.plugin']}
+
+setup_kwargs = {
+    'name': 'pytest-chdir',
+    'version': '0.1.2',
+    'description': 'A pytest fixture for changing current working directory',
+    'long_description': None,
+    'author': 'Masahiro Wada',
+    'author_email': 'argon.argon.argon@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/pytest-chdir/pytest-chdir-0.1.2.tar.gz/pytest-chdir-0.1.2/src/pytest_chdir/plugin.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import os
+import sys
+
+import pytest
+
+
+def _get_datadir_path(request, fixture_name: str):
+    try:
+        dirpath = request.getfixturevalue("datadir")
+    except Exception as e:
+        if str(type(e)) == "<class '_pytest.fixtures.FixtureLookupError'>":
+            raise EnvironmentError("pytest-datadir is not found.")
+        raise
+    return dirpath
+
+
+def _chdir(dstdir: Path):
+    lwd = os.getcwd()
+    os.chdir(dstdir)
+    try:
+        yield dstdir
+    finally:
+        os.chdir(lwd)
+
+
+def define_chdir_fixture(name: str, dstdir: Path, module_name: str) -> None:
+    @pytest.fixture(scope="module")
+    def _f():
+        yield from _chdir(dstdir)
+
+    setattr(sys.modules[module_name], name, _f)
+
+
+@pytest.fixture
+def chtmpdir(request, tmpdir: Path):
+    yield from _chdir(tmpdir)
+
+
+@pytest.fixture
+def chdatadir(request):
+    datadir = _get_datadir_path(request, "datadir")
+    yield from _chdir(datadir)
+
+
+@pytest.fixture
+def chshared_datadir(request):
+    datadir = _get_datadir_path(request, "shared_datadir")
+    yield from _chdir(datadir)
