f215fc4271a05e494860f5cfda340de3efcda60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:35 +0000

    Add package wavefront-cli-dev-test/0.0.118/wavefront-cli-dev-test-0.0.118.tar.gz

---
+++ packages/wavefront-cli-dev-test/wavefront-cli-dev-test-0.0.118.tar.gz/wavefront-cli-dev-test-0.0.118/wavefront_cli/commands/__init__.py
@@ -0,0 +1,7 @@
+"""Manage wavefront CLI commands."""
+# pylint: disable=W0401, W0622
+
+from .configure import *
+from .hello import *
+from .install import *
+from .integration import *
---
+++ packages/wavefront-cli-dev-test/wavefront-cli-dev-test-0.0.118.tar.gz/wavefront-cli-dev-test-0.0.118/wavefront_cli/integrations/__init__.py
@@ -0,0 +1,7 @@
+"""This package will manage the integrations."""
+# pylint: disable=W0401
+
+from .base import *
+from .example import *
+from .statsd import *
+from .wavefront import *
