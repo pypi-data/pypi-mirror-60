ae94390b4dc7420ddb6aeb5171ec3a6f8e1192d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package allink-core/2.3.2/allink-core-2.3.2.tar.gz

---
+++ packages/allink-core/allink-core-2.3.2.tar.gz/allink-core-2.3.2/allink_core/core_apps/allink_pdf/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from cms.models.pluginmodel import CMSPlugin
+
+
+class AllinkPdfPageBreakPlugin(CMSPlugin):
+    """
+    A Plugin for telling the pdf export to do a page break
+    """
+
+    pass
