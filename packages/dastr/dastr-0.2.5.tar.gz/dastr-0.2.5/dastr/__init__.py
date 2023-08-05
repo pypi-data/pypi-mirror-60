c007f038e4b0ab9a5c5cd795842fd45643f79afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package dastr/0.2.5/dastr-0.2.5.tar.gz

---
+++ packages/dastr/dastr-0.2.5.tar.gz/dastr-0.2.5/dastr/__init__.py
@@ -0,0 +1,2 @@
+
+from dastr.dastr import json_to_params, read, translate, write
---
+++ packages/dastr/dastr-0.2.5.tar.gz/dastr-0.2.5/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+	name = 'dastr',
+	packages = ['dastr'],
+	version = '0.2.5',
+	license = 'MIT',
+	description = 'A tool for translating between scientific data structures',
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+	author = 'Isaac Kinley',
+	author_email = 'isaac.kinley@gmail.com',
+	url = 'https://github.com/kinleyid/dastr',
+	download_url = 'https://github.com/kinleyid/dastr/archive/v_025.tar.gz',
+	keywords = ['data', 'structure', 'translator'],
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python'
+	],
+)
