241a710af1d6697d6aa72acb77390352cff3c7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:06 +0000

    Add package geomatics-probability/0.1/geomatics_probability-0.1.tar.gz

---
+++ packages/geomatics-probability/geomatics_probability-0.1.tar.gz/geomatics_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='geomatics_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['geomatics_probability'], 
+      zip_safe=False)
