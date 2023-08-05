8aa5995479ca08a4f5f377bb981c39460c9698dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:23 +0000

    Add package threatbus/0.3.1/threatbus-0.3.1-py3-none-any.whl

---
+++ packages/threatbus/threatbus-0.3.1-py3-none-any.whl/threatbus/__init__.py
@@ -0,0 +1,11 @@
+import pluggy
+
+app = pluggy.HookimplMarker("threatbus.app")
+"""Marker to be imported and used in app-plugins"""
+
+backbone = pluggy.HookimplMarker("threatbus.backbone")
+"""Marker to be imported and used in backbone-plugins"""
+
+
+import threatbus.logger
+import threatbus.data
