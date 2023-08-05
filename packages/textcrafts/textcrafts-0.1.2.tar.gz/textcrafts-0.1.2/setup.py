1090b8a29286c7284df87411d4458fce164f73dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:22 +0000

    Add package textcrafts/0.1.2/textcrafts-0.1.2.tar.gz

---
+++ packages/textcrafts/textcrafts-0.1.2.tar.gz/textcrafts-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import subprocess
+from sys import platform
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+  required = f.read().splitlines()
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+version = "0.1.2"
+setup(name='textcrafts',
+      version=version,
+      description='textcrafts: Summary, keyphrase and relation extraction with dependecy graphs',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ptarau/TextGraphCrafts.git',
+      author='Paul Tarau, Andrea Cortis',
+      author_USER_EMAIL='<paul.tarau@gmail.com>; andrea.cortis@gmail.com>',
+      license='Apache',
+      packages=['textcrafts'],
+      #package_data={'deep_talk': ['*.pro']},
+      #include_package_data=True,
+      install_requires=required,
+      zip_safe=False
+      )
+
