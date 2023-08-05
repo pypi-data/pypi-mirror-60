7e952fb1237d18ce9f85d8219076ca926655e798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:29 +0000

    Add package dly-distributions/1.0/dly_distributions-1.0.tar.gz

---
+++ packages/dly-distributions/dly_distributions-1.0.tar.gz/dly_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dly_distributions',
+      version='1.0',
+      description='Gaussian/Binomial distributions',
+      packages=['dly_distributions'],
+      author= 'Derek Ly',
+      zip_safe=False)
