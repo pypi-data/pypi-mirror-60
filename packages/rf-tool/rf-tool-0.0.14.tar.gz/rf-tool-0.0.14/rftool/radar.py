d58089bd25a140974096acd9e9945c3e40595ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package rf-tool/0.0.14/rf-tool-0.0.14.tar.gz

---
+++ packages/rf-tool/rf-tool-0.0.14.tar.gz/rf-tool-0.0.14/rftool/radar.py
@@ -0,0 +1,138 @@
+from scipy.signal import hilbert
+import matplotlib.pyplot as plt
+import numpy as np
+from pyhht.visualization import plot_imfs   # Hilbert-Huang TF analysis
+from pyhht import EMD                       # Hilbert-Huang TF analysis
+
+def Albersheim( Pfa, Pd, N ):
+    """
+    Calculate required SNR for non-coherent integration over N pulses, by use of Albersheims equation.
+
+    Pd is the probability of detection (linear)
+    Pfa is the probability of false alarm (linear)
+    N is the number of non-coherently integrated pulses
+    Returns SNR in dB
+
+    Accurate within 0.2 dB for:
+    10^-7   <  Pfa  < 10^-3
+    0.1     <  Pd   < 0.9
+    1       <= N    < 8096
+    - M. A. Richards and J. A. Scheer and W. A. Holm, Principles of Modern Radar, SciTech Publishing, 2010 
+    """
+
+    A = np.log(np.divide(0.62, Pfa))
+    B = np.log(np.divide(Pd,1-Pd))
+    SNRdB = -5*np.log10(N)+(6.2+np.divide(4.54, np.sqrt(N+0.44)))*np.log10(A+(0.12*A*B)+(0.7*B))
+    return SNRdB
+
+def Shnidman( Pfa, Pd, N, SW ):
+    """
+    Calculate required SNR for non-coherent integration over N pulses for swerling cases, by use of Shnidman equation.
+
+    Pd is the probability of detection (linear)
+    Pfa is the probability of false alarm (linear)
+    N is the number of non-coherently integrated pulses
+    SW is the swerling model: 0-4 (zero is non-swerling)
+    Returns SNR in dB
+
+    Accurate within 1 dB for:
+    10^-7   <  Pfa  < 10^-3
+    0.1     <= Pd   =< 0.99*10^-9
+    1       <= N    < 100
+    - M. A. Richards and J. A. Scheer and W. A. Holm, Principles of Modern Radar, SciTech Publishing, 2010 
+    """
+    C = 0
+    alpha = 0
+
+    if N < 40:
+        alpha = 0
+    elif N > 40:
+        alpha = np.divide(1,4)
+    
+    eta = np.sqrt( -0.8*np.log(4*Pfa*(1-Pfa)) ) + np.sign(Pd-0.5)*np.sqrt( -0.8*np.log(4*Pd*(1-Pd)) )
+
+    X_inf = eta*( eta + 2*np.sqrt( np.divide(N,2)+(alpha-np.divide(1,4)) ) )
+
+    if SW==0:   # Non swerling case
+        C = 1
+    else:       # Swerling case
+        def K(SW):
+            if  SW==1:
+                K = 1
+            elif  SW==2:
+                K = N
+            elif  SW==3:
+                2
+            else:
+                K = 2*N
+
+        C1 = np.divide( ( (17.7006*Pd-18.4496)*Pd+14.5339 )*Pd-3.525, K(SW) )
+        C2 = np.divide(1,K(SW))*( np.exp( 27.31*Pd-25.14 ) + (Pd-0.8)*( 0.7*np.log( np.divide(10e-5, Pfa) ) + np.divide( 2*N-20, 80 ) ) )
+        CdB = 0
+        if Pd < 0.8872:
+            CdB = C1
+        elif Pd > 0.99:
+            CdB = C1 + C2
+        
+        C = np.power(10, np.divide(CdB,10) )
+    
+    X1= np.divide(C*X_inf, N)
+    
+    SNRdB = 10*np.log10(X1)
+    return SNRdB
+
+def upconvert( sig, f_c, Fs=1 ):
+    """
+    Upconvert baseband waveform to IF
+    f_c is the IF center frequency
+    Fs is the sample frequency
+    """
+    a = np.linspace(0, sig.shape[0]-1, sig.shape[0])
+    # Angular increments per sample times sample number
+    phi_j = 2*np.pi*np.divide(f_c,Fs)*a
+    # Complex IF carrier
+    sig = np.multiply(sig,np.exp(1j*phi_j))
+    return sig
+
+def hilbert_spectrum( sig, Fs=1 ):
+    """
+    Hilbert-Huang transform with Hilbert spectral plot.
+    The plot cuts off negative frequencies.
+    
+    sig is a time series
+    Fs is the sample frequency
+
+    based on:
+    S.E. Hamdi et. al, Hilbert-Huang Transform versus Fourier based analysis for diffused ultrasonic waves structural health monitoring in polymer based composite materials,Proceedings of the Acoustics 2012 Nantes Conference.
+    """
+
+    # Hilbert-Huang
+    decomposer = EMD(sig)
+    imfs = decomposer.decompose()
+    #plot_imfs(sig, imfs, t)
+
+    imfAngle = np.angle(hilbert(imfs))
+    dt = np.divide(1,Fs)
+    
+    t = np.linspace(0, (sig.shape[0]-1)*dt, sig.shape[0])
+
+    # Calculate instantaneous frequency
+    instFreq = np.divide(np.gradient(imfAngle,t,axis=1), 2*np.pi)
+    """
+    There is an image of the instantaneous frequency response occuring at -Fs/2. THis is currently not shown in the plot. 
+    """
+
+    # Calculate Hilbert spectrum
+    # Time, frequency, magnitude
+
+    intensity = np.absolute(hilbert(imfs))
+    plt.figure()
+    for i in range(np.size(instFreq,0)):
+        plt.scatter(t, instFreq[i], c=intensity[i], alpha=0.3)
+
+    plt.title("Hilbert Spectrum")
+    plt.xlabel('t [s]')
+    plt.ylabel('f [Hz]')
+    plt.ylim(0,np.divide(Fs,2))
+    plt.tight_layout()
+    plt.show()
