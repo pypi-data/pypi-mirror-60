84ccf7643b3afd449752d4f6087a4479582234dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:22 +0000

    Add package simpletransformers/0.19.1/simpletransformers-0.19.1-py3-none-any.whl

---
+++ packages/simpletransformers/simpletransformers-0.19.1-py3-none-any.whl/simpletransformers/classification/__init__.py
@@ -0,0 +1,4 @@
+from simpletransformers.classification.classification_model import ClassificationModel
+from simpletransformers.classification.multi_label_classification_model import (
+    MultiLabelClassificationModel,
+)
