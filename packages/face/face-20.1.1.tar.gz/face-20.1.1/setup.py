c5848b1d1e734ab726d3bc243d879f5a09600bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:38 +0000

    Add package face/20.1.1/face-20.1.1.tar.gz

---
+++ packages/face/face-20.1.1.tar.gz/face-20.1.1/setup.py
@@ -0,0 +1,56 @@
+"""A command-line interface parser and framework, friendly for users,
+full-featured for developers.
+"""
+
+from setuptools import setup
+
+
+__author__ = 'Mahmoud Hashemi'
+__version__ = '20.1.1'
+__contact__ = 'mahmoud@hatnote.com'
+__url__ = 'https://github.com/mahmoud/face'
+__license__ = 'BSD'
+
+
+setup(name='face',
+      version=__version__,
+      description="A command-line application framework (and CLI parser). Friendly for users, full-featured for developers.",
+      long_description=__doc__,
+      author=__author__,
+      author_email=__contact__,
+      url=__url__,
+      packages=['face', 'face.test'],
+      include_package_data=True,
+      zip_safe=False,
+      license=__license__,
+      platforms='any',
+      install_requires=['boltons>=20.0.0'],
+      classifiers=[
+          'Topic :: Utilities',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy', ]
+      )
+
+"""
+A brief checklist for release:
+
+* tox
+* git commit (if applicable)
+* Bump setup.py version off of -dev
+* git commit -a -m "bump version for vx.y.z release"
+* python setup.py sdist bdist_wheel upload
+* bump docs/conf.py version
+* git commit
+* git tag -a vx.y.z -m "brief summary"
+* write CHANGELOG
+* git commit
+* bump setup.py version onto n+1 dev
+* git commit
+* git push
+
+"""
