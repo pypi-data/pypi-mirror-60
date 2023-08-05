67ea4d8ae1246c7f364b0e2e5f19b88d91ae0ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:00 +0000

    Add package tjcsvreformatter/0.0.1/tjcsvreformatter-0.0.1.tar.gz

---
+++ packages/tjcsvreformatter/tjcsvreformatter-0.0.1.tar.gz/tjcsvreformatter-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name="tjcsvreformatter",
+    version="0.0.1",
+    description="reformat old csvs into new format specified here:https://wiki.mgcorp.co/pages/viewpage.action?spaceKey=ENGADS&title=DSP+CSV",
+    author='Maryia Hanina',
+    author_email="maryia.ganina@gmail.com",
+    url="https://stash.mgcorp.co/projects/ADS/repos/tj-reformat-csvs/browse",
+    packages=find_packages(where='src'),
+    python_requires='>=3.6.*',
+    install_requires=['pandas', 'datetime', 'argparse', 'csv', 'json', 'os'],
+    entry_points={  # Optional
+        'console_scripts': [
+            'reformat=reformat:main',
+        ],
+    },
+
+)
