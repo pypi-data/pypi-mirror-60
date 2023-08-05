1950c0b3c516c7ac32da60ade0915d85d2216ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:52 +0000

    Add package ziggeo/2.18/ziggeo-2.18-py2.py3-none-any.whl

---
+++ packages/ziggeo/ziggeo-2.18-py2.py3-none-any.whl/ZiggeoConfig.py
@@ -0,0 +1,11 @@
+
+class ZiggeoConfig:
+    def __init__(self):
+        self.server_api_url = "https://srvapi.ziggeo.com"
+
+        self.regions = {"r1":"https://srvapi-eu-west-1.ziggeo.com"}
+        self.api_url = "https://api-us-east-1.ziggeo.com"
+
+        self.api_regions = {"r1":"https://api-eu-west-1.ziggeo.com"}
+        self.resilience_factor = 5
+        self.request_timeout = 60
---
+++ packages/ziggeo/ziggeo-2.18-py2.py3-none-any.whl/ZiggeoStreams.py
@@ -0,0 +1,38 @@
+class ZiggeoStreams:
+
+    def __init__(self, application):
+        self.__application = application
+
+    def index(self, video_token_or_key, data = None):
+        return self.__application.connect.getJSON('/v1/videos/' + video_token_or_key + '/streams', data)
+
+    def get(self, video_token_or_key, token_or_key):
+        return self.__application.connect.getJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '')
+
+    def download_video(self, video_token_or_key, token_or_key):
+        return self.__application.connect.get('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video')
+
+    def download_image(self, video_token_or_key, token_or_key):
+        return self.__application.connect.get('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image')
+
+    def push_to_service(self, video_token_or_key, token_or_key, data = None):
+        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/push', data)
+
+    def delete(self, video_token_or_key, token_or_key):
+        return self.__application.connect.delete('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '')
+
+    def create(self, video_token_or_key, data = None, file = None):
+        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams', data, file)
+
+    def attach_image(self, video_token_or_key, token_or_key, data = None, file = None):
+        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image', data, file)
+
+    def attach_video(self, video_token_or_key, token_or_key, data = None, file = None):
+        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video', data, file)
+
+    def attach_subtitle(self, video_token_or_key, token_or_key, data = None):
+        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/subtitle', data)
+
+    def bind(self, video_token_or_key, token_or_key):
+        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/bind')
+
