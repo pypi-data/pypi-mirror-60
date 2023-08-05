70c31161e6e67e0775fdf2a572cdabf47ee4335f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:58 +0000

    Add package queryS3/0.0.20/queryS3-0.0.20.tar.gz

---
+++ packages/queryS3/queryS3-0.0.20.tar.gz/queryS3-0.0.20/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="queryS3", # Replace with your own username
+    version="0.0.20",
+    author="Qiao Liu, Everette Li",
+    author_email="qiao.liu01@sjsu.edu, everetteli12@gmail.com",
+    description="Minimum version of query s3 by label",
+    long_description=long_description,
+    long_description_content_type="text/markdown; charset=UTF-8; variant=CommonMark",
+    url="https://github.com/eugenechang2002/AITestResourceLibrary",
+    install_requires=[
+          'pandas',
+          'nltk'
+      ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.0',
+)
