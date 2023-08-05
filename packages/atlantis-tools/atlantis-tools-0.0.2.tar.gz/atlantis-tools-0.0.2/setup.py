bdf3d6041403668643bb576f505a73963110c9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:11 +0000

    Add package atlantis-tools/0.0.2/atlantis-tools-0.0.2.tar.gz

---
+++ packages/atlantis-tools/atlantis-tools-0.0.2.tar.gz/atlantis-tools-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='atlantis-tools',
+    version='0.0.2',
+    author='holynnchen',
+    author_email='holynnchen@foxmail.com',
+    url='https://github.com/HolynnChen/Atlantis',
+    description="Tools of Atlantis project",
+    packages=["atlantis_tools"],
+    install_requires=["requests"]
+    )
