6100a35d412cd9b33cda1e50c6fbdc82f068a6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package arjuna/0.8.5/arjuna-0.8.5-py3-none-any.whl

---
+++ packages/arjuna/arjuna-0.8.5-py3-none-any.whl/arjuna/core/helpers.py
@@ -0,0 +1,23 @@
+from .enums import TimeUnit
+
+class Time:
+
+    def __init__(self, time_unit, value):
+        self.__unit = time_unit
+        self.__value = value
+
+    @staticmethod
+    def seconds(self, secs):
+        return Time(TimeUnit.SECONDS, secs)
+
+    @staticmethod
+    def milli_seconds(self, ms):
+        return Time(TimeUnit.MILLI_SECONDS, ms)
+
+    @staticmethod
+    def minutes(self, mins):
+        return Time(TimeUnit.MINUTES, mins)
+
+logger = Arjuna.get_logger()
+console = Arjuna.get_console()
+
