899c92896000b9c99760506c32da2f43a8c54083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package pymap3d/2.3.0/pymap3d-2.3.0.tar.gz

---
+++ packages/pymap3d/pymap3d-2.3.0.tar.gz/pymap3d-2.3.0/examples/angle_distance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from pymap3d.haversine import anglesep, anglesep_meeus
+from argparse import ArgumentParser
+from pytest import approx
+
+
+def main():
+    p = ArgumentParser(description="angular distance between two sky points")
+    p.add_argument("r0", help="right ascension: first point [deg]", type=float)
+    p.add_argument("d0", help="declination: first point [deg]", type=float)
+    p.add_argument("r1", help="right ascension: 2nd point [deg]", type=float)
+    p.add_argument("d1", help="declination: 2nd point [degrees]", type=float)
+    a = p.parse_args()
+
+    dist_deg = anglesep_meeus(a.r0, a.d0, a.r1, a.d1)
+    dist_deg_astropy = anglesep(a.r0, a.d0, a.r1, a.d1)
+
+    print("{:.6f} deg sep".format(dist_deg))
+
+    assert dist_deg == approx(dist_deg_astropy)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pymap3d/pymap3d-2.3.0.tar.gz/pymap3d-2.3.0/examples/radec2azel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Example Kitt Peak
+
+./radec2azel.py 257.96295344 15.437854 31.9583 -111.5967 2014-12-25T22:00:00MST
+"""
+from pymap3d import radec2azel
+from argparse import ArgumentParser
+
+
+def main():
+    p = ArgumentParser(description="RightAscension,Declination =>" "Azimuth,Elevation")
+    p.add_argument("ra", help="right ascension [degrees]", type=float)
+    p.add_argument("dec", help="declination [degrees]", type=float)
+    p.add_argument("lat", help="WGS84 latitude of observer [degrees]", type=float)
+    p.add_argument("lon", help="WGS84 latitude of observer [degrees]", type=float)
+    p.add_argument("time", help="UTC time of observation YYYY-mm-ddTHH:MM:SSZ")
+    P = p.parse_args()
+
+    az_deg, el_deg = radec2azel(P.ra, P.dec, P.lat, P.lon, P.time)
+    print("azimuth: [deg]", az_deg)
+    print("elevation [deg]:", el_deg)
+
+
+if __name__ == "__main__":
+    main()
