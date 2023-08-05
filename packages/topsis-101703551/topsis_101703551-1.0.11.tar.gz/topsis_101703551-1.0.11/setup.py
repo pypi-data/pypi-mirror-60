9d21a0ae751df22c37da1dafca3c4bfea938d736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:27 +0000

    Add package topsis-101703551/1.0.11/topsis_101703551-1.0.11.tar.gz

---
+++ packages/topsis-101703551/topsis_101703551-1.0.11.tar.gz/topsis_101703551-1.0.11/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="topsis_101703551",
+    version="1.0.11",
+    description="A Python package to rank ML models/choices using topsis technique",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sirishbhu/topsis",
+    author="Sirish Bhudolia",
+    author_email="sirishbhudolia88@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis_101703551"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "topsis-package-1=topsis_101703551.__init__:main",
+        ]
+    },
+)
