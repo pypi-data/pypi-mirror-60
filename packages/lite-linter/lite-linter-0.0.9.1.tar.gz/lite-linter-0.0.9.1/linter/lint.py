f7db9c433da23c1d95034dbd6641102b30635690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:13 +0000

    Add package lite-linter/0.0.9.1/lite-linter-0.0.9.1.tar.gz

---
+++ packages/lite-linter/lite-linter-0.0.9.1.tar.gz/lite-linter-0.0.9.1/linter/lint.py
@@ -0,0 +1,30 @@
+import sys
+
+from linter.linters import headings
+from linter.linters.base import LintController
+
+
+def main():
+    LintController(
+        linters=[
+            headings.CheckHeadings,
+            headings.TagsSpacing,
+            headings.UnusedFiles,
+            headings.PreventInlineStyles,
+            headings.OneLoadBlock,
+            headings.AllTagsUsed,
+            headings.EnsureAllIDsAreUnique,
+            headings.BlockOrdering,
+            headings.BlocksSpacing,
+            headings.PreventTwoPaneOverwite,
+            headings.CheckFilenames,
+            # headings.EnforceBlockTitle,
+            # headings.FindHardcodedStrings,
+            # headings.EnforceSingleQuotesInBlocks,
+            # headings.TabsOverSpaces,
+        ]
+    ).output()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
