2d6d46fc37609ecca3853bb6514e1d01d70a55cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:19 +0000

    Add package iaqualink/0.3.1/iaqualink-0.3.1.tar.gz

---
+++ packages/iaqualink/iaqualink-0.3.1.tar.gz/iaqualink-0.3.1/src/iaqualink/__init__.py
@@ -0,0 +1,23 @@
+__author__ = "Florent Thoumie"
+__email__ = "florent@thoumie.net"
+__version__ = "0.3.1"
+
+from .client import AqualinkClient, AqualinkLoginException
+from .system import AqualinkSystem, AqualinkPoolSystem
+from .device import (
+    AqualinkAuxToggle,
+    AqualinkBinarySensor,
+    AqualinkColorLight,
+    AqualinkDevice,
+    AqualinkDimmableLight,
+    AqualinkHeater,
+    AqualinkLight,
+    AqualinkLightEffect,
+    AqualinkLightToggle,
+    AqualinkLightType,
+    AqualinkPump,
+    AqualinkSensor,
+    AqualinkState,
+    AqualinkThermostat,
+    AqualinkToggle,
+)
