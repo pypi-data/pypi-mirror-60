af82bbf1682a7222c6d21a0c8f1c694b81b9542a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:45 +0000

    Add package mulang/0.0.6/mulang-0.0.6.tar.gz

---
+++ packages/mulang/mulang-0.0.6.tar.gz/mulang-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", encoding="utf-8", mode="r") as readme:
+    longDescription = readme.read()
+
+setuptools.setup(
+    name="mulang",
+    version="0.0.6",
+    author="无名",
+    author_email="mulanrevive@gmail.com",
+    entry_points = {
+        "console_scripts": ['ulang = ulang.runtime.main:main']
+        },
+    description="木兰编程语言演示",
+    long_description=longDescription,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MulanRevive/mulan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
