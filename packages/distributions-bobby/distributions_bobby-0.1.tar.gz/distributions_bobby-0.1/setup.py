3818f262553225e9e62228e03ba4f6066284882c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:10 +0000

    Add package distributions-bobby/0.1/distributions_bobby-0.1.tar.gz

---
+++ packages/distributions-bobby/distributions_bobby-0.1.tar.gz/distributions_bobby-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_bobby',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_bobby'],
+      zip_safe=False)
