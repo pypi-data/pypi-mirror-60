06932b379d6fd886927606ae221bdf637d93f225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:22 +0000

    Add package canonical-phone-nitinsureshpp/0.0.1/canonical-phone-nitinsureshpp-0.0.1.tar.gz

---
+++ packages/canonical-phone-nitinsureshpp/canonical-phone-nitinsureshpp-0.0.1.tar.gz/canonical-phone-nitinsureshpp-0.0.1/README.md
@@ -0,0 +1,7 @@
+# Canonical Phone
+## Purpose
+Convert given phone number into a string with hiphenated phone number with country code.
+Default is assumed to be Indonesian number.
+
+## How to use?
+Use it the way you like.
---
+++ packages/canonical-phone-nitinsureshpp/canonical-phone-nitinsureshpp-0.0.1.tar.gz/canonical-phone-nitinsureshpp-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="canonical-phone-nitinsureshpp",
+    version="0.0.1",
+    author="Nitin Suresh",
+    author_email="nitin.kalal@pasarpolis.com",
+    description="Library to get canonical phone number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/nitinsureshp/canonical-phone.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
