9a44842dc35001f8e72493fe5bbac19adba7d7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package gym-fearbun/0.0.40/gym_fearbun-0.0.40.tar.gz

---
+++ packages/gym-fearbun/gym_fearbun-0.0.40.tar.gz/gym_fearbun-0.0.40/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import setuptools
+
+setup(name='gym_fearbun',
+      version='0.0.40',
+      install_requires=['gym', 'six', 'Cython', 'numpy'],
+      url='https://github.com/rolypolyvg295/gym_fearbun/tree/master/gym_fearbun',
+      include_package_data=True,
+      packages=setuptools.find_packages(),
+      license='MIT',
+      ext_modules=cythonize(['gym_fearbun/envs/raceboard/c_raceboard.pyx', 'chapter_5/cother_agent.pyx'],
+                            compiler_directives={'language_level': 3},
+                            annotate=True)
+      )
