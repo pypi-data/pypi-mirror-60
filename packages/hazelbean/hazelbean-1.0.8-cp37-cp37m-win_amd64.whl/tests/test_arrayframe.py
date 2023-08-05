64f1b89ff17cb423037d2f0b0bc9fdaeb7e6b948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:06 +0000

    Add package hazelbean/1.0.8/hazelbean-1.0.8-cp37-cp37m-win_amd64.whl

---
+++ packages/hazelbean/hazelbean-1.0.8-cp37-cp37m-win_amd64.whl/tests/test_arrayframe.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+
+import os, sys
+import hazelbean as hb
+import pandas as pd
+import numpy as np
+
+
+class ArrayFrameTester(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_arrayframe_load_and_save(self):
+        input_array = np.arange(0, 18, 1).reshape((3,6))
+        input_uri = hb.temp('.tif', remove_at_exit=True)
+        geotransform = hb.calc_cylindrical_geotransform_from_array(input_array)
+        # projection = hb.get_wkt_from_epsg_code(hb.common_epsg_codes_by_name['plate_carree'])
+        projection = 'wgs84'
+        ndv = 255
+        data_type = 1
+        hb.save_array_as_geotiff(input_array, input_uri, geotransform_override=geotransform, projection_override=projection, ndv=ndv, data_type=data_type)
+
+        hb.ArrayFrame(input_uri)
+
+        # "C:\Anaconda363\Library\share\gdal\gcs.csv"
+
+
+
+
+
+
