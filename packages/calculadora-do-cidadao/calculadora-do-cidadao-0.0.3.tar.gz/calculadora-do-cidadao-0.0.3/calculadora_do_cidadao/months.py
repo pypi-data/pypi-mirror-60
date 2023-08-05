490613bab06db85ea2096856bf54bcd54ae7723d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:25 +0000

    Add package calculadora-do-cidadao/0.0.3/calculadora-do-cidadao-0.0.3.tar.gz

---
+++ packages/calculadora-do-cidadao/calculadora-do-cidadao-0.0.3.tar.gz/calculadora-do-cidadao-0.0.3/calculadora_do_cidadao/months.py
@@ -0,0 +1,32 @@
+from itertools import chain, cycle
+
+
+SHORT = (
+    "JAN",
+    "FEV",
+    "MAR",
+    "ABR",
+    "MAI",
+    "JUN",
+    "JUL",
+    "AGO",
+    "SET",
+    "OUT",
+    "NOV",
+    "DEZ",
+)
+COMPLETE = (
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+)
+MONTHS = dict(zip(chain(SHORT, COMPLETE), cycle(range(1, 13))))
---
+++ packages/calculadora-do-cidadao/calculadora-do-cidadao-0.0.3.tar.gz/calculadora-do-cidadao-0.0.3/tests/conftest.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from zipfile import ZipFile
+
+from pytest import fixture
+
+
+@fixture(scope="session")
+def zip_file():
+    """Returns a path to a temporary zip file. Its content is a single file.
+    The contents of this single file is `42` in bytes."""
+    with TemporaryDirectory() as _tmp:
+        tmp = Path(_tmp)
+        fixture = tmp / "fixture"
+        fixture.write_bytes(b"42")
+
+        path = Path(tmp) / "fixture.zip"
+        with ZipFile(path, "w") as archive:
+            archive.write(fixture, arcname=fixture.name)
+
+        yield path
+
+
+def get_fixture(name):
+    return Path(__file__).parent / "fixtures" / name
+
+
+@fixture
+def ipca_fixture():
+    return get_fixture("ipca.xls")
+
+
+@fixture
+def selic_fixture():
+    return get_fixture("selic.html")
+
+
+@fixture
+def ipca15_fixture():
+    return get_fixture("ipca15.xls")
