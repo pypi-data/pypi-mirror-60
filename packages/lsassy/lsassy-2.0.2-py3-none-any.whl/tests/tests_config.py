c6653166d07b3285da69ef00a92bb460b3f184a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package lsassy/2.0.2/lsassy-2.0.2-py3-none-any.whl

---
+++ packages/lsassy/lsassy-2.0.2-py3-none-any.whl/tests/tests_config.py
@@ -0,0 +1,14 @@
+# Author:
+#  Romain Bentz (pixis - @hackanddo)
+# Website:
+#  https://beta.hackndo.com
+
+ip_address = "192.168.1.111"
+ip_address_protected = "192.168.1.112"
+domain = "lion.king"
+da_login = "simba"
+da_password = "Imtheking!"
+usr_login = "skar"
+usr_password = "YouDontWannaMessWithMe"
+procdump_path = "/home/pixis/Tools/Windows/Sysinternals/procdump.exe"
+dumpert_path = "/home/pixis/Tools/Windows/Dumpert/Outflank-Dumpert.exe"
