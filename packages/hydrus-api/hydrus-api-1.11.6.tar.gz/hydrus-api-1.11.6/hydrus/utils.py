5c2a52f26058e7f566dcd6980350339c0c682c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:12 +0000

    Add package hydrus-api/1.11.6/hydrus-api-1.11.6.tar.gz

---
+++ packages/hydrus-api/hydrus-api-1.11.6.tar.gz/hydrus-api-1.11.6/hydrus/utils.py
@@ -0,0 +1,69 @@
+from typing import Set, List, Generator, Any, Collection
+
+from . import Client, Permission, DEFAULT_API_URL
+
+# Encoding used by Hydrus to create metadata files
+HYDRUS_ENCODING = "UTF-8"
+
+
+def cli_request_api_key(
+    name: str,
+    permissions: List[Permission],
+    verify: bool = True,
+    api_url: str = DEFAULT_API_URL,
+):
+    while True:
+        input(
+            'Navigate to "services->review services->local->client api" in the Hydrus '
+            'client and click "add->from api request". Then press enter to continue...'
+        )
+        access_key = Client(url=api_url).request_new_permissions(name, permissions)
+        input(
+            "Press OK and then apply in the Hydrus client dialog. Then press enter to "
+            "continue..."
+        )
+
+        client = Client(access_key, api_url)
+        if verify and not verify_permissions(client, permissions):
+            granted_permissions = client.verify_access_key()["basic_permissions"]
+            print(
+                "The granted permissions ({}) differ from the requested permissions "
+                "({}), please grant all requested permissions.".format(
+                    granted_permissions, permissions
+                )
+            )
+            continue
+
+        return access_key
+
+
+# Useful in combination with search_files() and file_metadata(), since file_metadata()
+# seems to raise a ServerError after exceeding a certain number of requested items (in
+# my tests ~1344).
+def yield_chunks(
+    sequence: Collection, chunk_size, offset=0
+) -> Generator[Any, None, None]:
+    while offset < len(sequence):
+        yield sequence[offset : offset + chunk_size]
+        offset += chunk_size
+
+
+def read_hydrus_metadata_file(path: str) -> Set[str]:
+    tags = set()
+    with open(path, encoding=HYDRUS_ENCODING) as file:
+        for line in file:
+            if line.strip():
+                tags.add(line)
+
+    return tags
+
+
+def verify_permissions(
+    client: Client, permissions: List[Permission], exact: bool = False
+) -> bool:
+    granted_permissions = set(client.verify_access_key()["basic_permissions"])
+    return (
+        granted_permissions == set(permissions)
+        if exact
+        else granted_permissions.issuperset(permissions)
+    )
