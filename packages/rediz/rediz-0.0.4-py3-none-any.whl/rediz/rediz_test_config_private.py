08bc27091599c856417799cbfd6fa95ed028ba5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package rediz/0.0.4/rediz-0.0.4-py3-none-any.whl

---
+++ packages/rediz/rediz-0.0.4-py3-none-any.whl/rediz/rediz_test_config_private.py
@@ -0,0 +1,4 @@
+
+REDIZ_TEST_CONFIG = {"password":"5r70RHtd2m0x3BHdLE8mp7APxoIJS1aH",
+                     "host":"redis-19709.c11.us-east-1-3.ec2.cloud.redislabs.com",
+                     "port":"19709"}
