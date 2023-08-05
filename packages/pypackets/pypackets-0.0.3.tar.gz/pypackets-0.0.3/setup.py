0993187f82ec4f33fe44d66d2b2e85eb097ec299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:19 +0000

    Add package pypackets/0.0.3/pypackets-0.0.3.tar.gz

---
+++ packages/pypackets/pypackets-0.0.3.tar.gz/pypackets-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pypackets", # Replace with your own username
+    version="0.0.3",
+    author="RaidTheWeb",
+    author_email="therealraidtheweb@gmail.com",
+    description="A Python library for sending Packets",
+    url="https://github.com/RaidTheWeb/Black-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
