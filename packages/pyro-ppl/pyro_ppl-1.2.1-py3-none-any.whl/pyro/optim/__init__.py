858bf600283141bfa5bb84d55c6a3e6b25b02f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:28 +0000

    Add package pyro-ppl/1.2.1/pyro_ppl-1.2.1-py3-none-any.whl

---
+++ packages/pyro-ppl/pyro_ppl-1.2.1-py3-none-any.whl/pyro/optim/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2017-2019 Uber Technologies, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+from pyro.optim.lr_scheduler import PyroLRScheduler
+from pyro.optim.optim import AdagradRMSProp, ClippedAdam, PyroOptim
+from pyro.optim.pytorch_optimizers import __all__ as pytorch_optims
+from pyro.optim.pytorch_optimizers import *  # noqa F403
+import pyro.optim.multi  # noqa F403
+
+__all__ = [
+    "AdagradRMSProp",
+    "ClippedAdam",
+    "PyroOptim",
+    "PyroLRScheduler",
+]
+__all__.extend(pytorch_optims)
