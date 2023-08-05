5fd1533499ca886d66271acbfe147e546f5ecade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:49 +0000

    Add package pycuGMRES/1.1.3.1/pycuGMRES-1.1.3.1-py3-none-any.whl

---
+++ packages/pycuGMRES/pycuGMRES-1.1.3.1-py3-none-any.whl/pycuGMRES/Shared object generating/compile_cpp.sh
@@ -0,0 +1,7 @@
+FOLDER=$1
+
+echo importing
+echo creating shared object
+echo nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared CUDA\ C\ ++\ sources -o cuGMRES.so
+nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared ${FOLDER}/CUDA\ C\ ++\ sources/GMRES.cu -o ${FOLDER}/Shared\ object\ generating/cuGMRES.so
+
