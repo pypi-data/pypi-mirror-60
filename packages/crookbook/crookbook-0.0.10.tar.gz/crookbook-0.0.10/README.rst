fdec90b58e2caa66e857b872a63467b274373031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:30 +0000

    Add package crookbook/0.0.10/crookbook-0.0.10.tar.gz

---
+++ packages/crookbook/crookbook-0.0.10.tar.gz/crookbook-0.0.10/README.rst
@@ -0,0 +1,47 @@
+.. |name| replace:: crookbook
+.. |summary| replace:: Common utilities used in my projects.
+
+|name|
+======
+
+|summary|
+
+.. _repository: https://github.com/the-allanc/crookbook/
+.. _documentation: https://crookbook.readthedocs.io/en/stable/
+.. _pypi: https://pypi.python.org/pypi/crookbook
+.. _coveralls: https://coveralls.io/github/the-allanc/crookbook
+.. _license: https://github.com/the-allanc/crookbook/master/LICENSE.txt
+.. _travis: https://travis-ci.org/the-allanc/crookbook
+.. _codeclimate: https://codeclimate.com/github/the-allanc/crookbook
+
+.. |Build Status| image:: https://img.shields.io/travis/the-allanc/crookbook.svg
+    :target: travis_
+    :alt: Build Status
+.. |Coverage| image:: https://img.shields.io/coveralls/the-allanc/crookbook.svg
+    :target: coveralls_
+    :alt: Coverage
+.. |Docs| image:: https://readthedocs.org/projects/crookbook/badge/?version=stable&style=flat
+    :target: documentation_
+    :alt: Docs
+.. |Release Version| image:: https://img.shields.io/pypi/pyversions/crookbook.svg
+    :target: pypi_
+    :alt: Release Version
+.. |Python Version| image:: https://img.shields.io/pypi/v/crookbook.svg
+    :target: pypi_
+    :alt: Python Version
+.. |License| image:: https://img.shields.io/pypi/l/crookbook.svg
+    :target: license_
+    :alt: License
+.. |Code Climate| image:: https://img.shields.io/codeclimate/issues/github/the-allanc/crookbook.svg
+    :target: codeclimate_
+    :alt: Code Climate
+
+If there's something you find useful, feel free to request it gets put into a separate library.
+
+|Docs| |Release Version| |Python Version| |License| |Build Status| |Coverage| |Code Climate|
+
+.. all-content-above-will-be-included-in-sphinx-docs
+
+You can browse the source code and file bug reports at the project repository_. Full documentation can be found `here`__.
+
+__ documentation_
---
+++ packages/crookbook/crookbook-0.0.10.tar.gz/crookbook-0.0.10/setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+#
+# Extract the project identity from the README.
+#
+import io
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+    readme = readme.read()
+
+
+def get_definition(prefix):
+    for line in readme.split('\n'):
+        if line.startswith(prefix):
+            return line[len(prefix):].strip()
+    err = 'no line in README.rst with prefix {!r}'.format(prefix)
+    raise AssertionError(err)
+
+
+def get_description():
+    d_start = '|summary|\n'
+    i_start = readme.index(d_start) + len(d_start)
+    return readme[i_start:].strip()
+
+
+name = get_definition('.. |name| replace:: ')
+
+#
+# End extraction code.
+#
+
+params = dict(
+    name=name,
+    use_scm_version=True,
+    author="Allan Crooks",
+    author_email="allan@increment.one",
+    description=get_definition('.. |summary| replace:: '),
+    long_description=get_description(),
+    license='MIT',
+    url=get_definition('.. _repository: '),
+    keywords=[],
+    py_modules=['crookbook'],
+    include_package_data=True,
+    namespace_packages=name.split('.')[:-1],
+    python_requires='>=2.7',
+    install_requires=[
+        'six',
+    ],
+    extras_require={
+        'testing': [
+            'pytest>=3.5',
+            'pytest-sugar>=0.9.1',
+            'collective.checkdocs',
+            'pytest-cov',
+            'pylint',
+        ],
+        'docs': [
+            'sphinx',
+            'jaraco.packaging>=3.2',
+            'rst.linker>=1.9',
+            'allanc-sphinx[yeen]>=0.2',
+        ],
+        'lint': [
+            'flake8',
+            'radon',
+            'pylint',
+        ],
+        'manage': [
+            'bump2version>=0.5.6',
+            'tox>=2.4',
+            'pip-tools',
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+    },
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**params)
