6c5d72284d8f5033c73c31d0071ff71e53898548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:52 +0000

    Add package tarpan/0.2.2/tarpan-0.2.2.tar.gz

---
+++ packages/tarpan/tarpan-0.2.2.tar.gz/tarpan-0.2.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tarpan",
+    version="0.2.2",
+    author="Evgenii Neumerzhitckii",
+    author_email="sausageskin@gmail.com",
+    description="A collection of tools for analysing cmdspanpy output, written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/evgenyneu/tarpan",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'cmdstanpy',
+        'scipy',
+        'tabulate',
+        'pandas',
+        'numpy',
+        'arviz',
+        'matplotlib',
+        'seaborn'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    license='Public Domain',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
