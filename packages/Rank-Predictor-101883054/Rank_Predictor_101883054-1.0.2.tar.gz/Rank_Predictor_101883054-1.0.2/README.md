0e2108ffcc5f21b01fb2cfcd537c21432178c40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:38 +0000

    Add package Rank-Predictor-101883054/1.0.2/Rank_Predictor_101883054-1.0.2.tar.gz

---
+++ packages/Rank-Predictor-101883054/Rank_Predictor_101883054-1.0.2.tar.gz/Rank_Predictor_101883054-1.0.2/README.md
@@ -0,0 +1,31 @@
+
+# ABOUT RANK PREDICTOR
+
+
+It is a method of compensatory aggregation that compares a set of alternatives 
+by identifying weights for each criterion, normalising scores for each criterion and 
+calculating the geometric distance between each alternative and the ideal alternative, 
+which is the best score in each criterion.
+
+
+**NAME**    :- Mehak Garg
+
+**ROLL NO** :- 101883054
+
+**GROUP**   :- 3CO23
+
+## INSTALLATION
+
+The package can be installed  using the following command in anaconda/command prompt.
+
+```bash
+pip install Rank_Predictor_101883054
+```
+
+## USAGE
+
+The package can be used for topsis analysis by providing dataset,weights,impacts.
+
+```python
+topsis <dataset.csv> <[weights]> <[impacts]>
+```
---
+++ packages/Rank-Predictor-101883054/Rank_Predictor_101883054-1.0.2.tar.gz/Rank_Predictor_101883054-1.0.2/setup.py
@@ -0,0 +1,34 @@
+
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="Rank_Predictor_101883054",
+    version="1.0.2",
+    description="A python package to predict the rank of a MCDMP using topsis",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    #url="https://github.com/nikhilkumarsingh/weather-reporter",
+    author="Mehak Garg",
+    #author_email="nikhilksingh97@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["top"],
+    include_package_data=True,
+    install_requires=["pandas","numpy"],
+    entry_points={
+        "console_scripts": [
+            "Rank_Predictor_101883054=top.topsis:main",
+        ]
+    },
+)
