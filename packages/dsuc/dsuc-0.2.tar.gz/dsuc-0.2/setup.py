288faae92390cf860c6b574b9c46bfc60e0d41cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:41 +0000

    Add package dsuc/0.2/dsuc-0.2.tar.gz

---
+++ packages/dsuc/dsuc-0.2.tar.gz/dsuc-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsuc',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsuc'],
+      author = 'Ieva Dirdaite',
+      author_email = 'ieva.dirdaite@gmail.com',
+      zip_safe=False)
