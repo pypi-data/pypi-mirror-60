fbe9c4e40bd284f1654cbd2dd6e9013794983ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:44 +0000

    Add package splitio-client/8.1.7/splitio_client-8.1.7.tar.gz

---
+++ packages/splitio-client/splitio_client-8.1.7.tar.gz/splitio_client-8.1.7/setup.py
@@ -0,0 +1,56 @@
+"""Setup module."""
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+tests_require = [
+    'flake8',
+    'pytest',
+    'pytest-mock',
+    'coverage',
+    'pytest-cov',
+    'mock;python_version<"3"'
+]
+install_requires = [
+    'requests>=2.9.1',
+    'pyyaml>=5.1',
+    'future>=0.15.2',
+    'docopt>=0.6.2',
+    'enum34;python_version<"3.4"',
+    'six>=1.10.0;python_version<"3"',
+    'futures>=3.0.5;python_version<"3"'
+]
+
+with open(path.join(path.abspath(path.dirname(__file__)),
+                    'splitio', 'version.py')) as f:
+    exec(f.read())
+
+setup(name='splitio_client',
+      version=__version__,  # noqa
+      description='Split.io Python Client',
+      author='Patricio Echague, Sebastian Arrubia',
+      author_email='pato@split.io, sebastian@split.io',
+      url='https://github.com/splitio/python-client',
+      download_url=('https://github.com/splitio/python-client/tarball/' +
+                    __version__),
+      license='Apache License 2.0',
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={
+          'test': tests_require,
+          'redis': ['redis>=2.10.5'],
+          'uwsgi': ['uwsgi>=2.0.0'],
+          'cpphash': ['mmh3cffi>=0.1.4']
+      },
+      setup_requires=['pytest-runner'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries'
+      ],
+      packages=find_packages())
