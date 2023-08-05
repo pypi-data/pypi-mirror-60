822a35c4886373f73b6dd4ebd488d99a57b5fdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package engi1020/0.1.7/engi1020-0.1.7.tar.gz

---
+++ packages/engi1020/engi1020-0.1.7.tar.gz/engi1020-0.1.7/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="engi1020",
+    version="0.1.7",
+    description="Software library for Engineering 1020: Introduction to Programming at Memorial University.",
+    license="MIT",
+    author="Jonathan Anderson, Lori Hogan",
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        arduino=engi1020.arduino.cli:cli
+    ''',
+    install_requires=[
+        'click',
+        'matplotlib',
+        'nanpy',
+    ],
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
