ac2c01b862707d73b74b919171a994f47b4b70ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:03 +0000

    Add package maruti/1.0.1/maruti-1.0.1.tar.gz

---
+++ packages/maruti/maruti-1.0.1.tar.gz/maruti-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maruti",
+    version="1.0.1",
+    author="Ankit Saini",
+    author_email="ankitsaini100205@gmail.com",
+    description="Maruti Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ankitsainidev/maruti",
+    packages=['maruti','maruti.vision'],
+    package_dir={'maruti': 'maruti'},
+    package_data={'maruti': ['data/*/*','vision/data/*/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=['tqdm==4.40.2','opencv-python']
+)
