78b80665c796fe8eeef7b161e26b2f0a58e9a38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package miniirc-extras/0.3.4/miniirc_extras-0.3.4.tar.gz

---
+++ packages/miniirc-extras/miniirc_extras-0.3.4.tar.gz/miniirc_extras-0.3.4/miniirc_extras/__init__.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+#
+# miniirc_extras: Adds extendability to miniirc.
+#
+# © 2019 by luk3yx.
+#
+
+import miniirc, os, sys
+assert hasattr(miniirc, 'ver') and miniirc.ver >= (1,5,0), \
+    'miniirc_extras requires miniirc >= 1.5.0.'
+
+from miniirc import CmdHandler, Handler, IRC
+from typing import Any, List
+from ._classes import *
+
+# Version info
+__version_info__ = ver = VersionInfo(0,3,4)
+__version__ = '0.3.4'
+version = 'miniirc v{}.{}.{} / miniirc_extras v{}'.format(miniirc.ver[0],
+    miniirc.ver[1], miniirc.ver[2], __version__)
+
+# The base exception class
+class error(Exception):
+    pass
+
+# The require() code may eventually move into miniirc.
+if hasattr(miniirc, 'Feature'):
+    from miniirc import Feature # type: ignore
+else:
+    from ._require import Feature
+
+AbstractIRC.require = miniirc.IRC.require # type: ignore
+AbstractIRC.require.__qualname__ = 'AbstractIRC.require'
+AbstractIRC.require.__module__ = __name__
+
+# Load features on-the-fly when required
+def _core_feature(name: str) -> None:
+    module = __name__ + '.features.' + name # type: str
+
+    @Feature(name)
+    def _feature(irc: miniirc.IRC) -> Any:
+        if name.startswith('_'):
+            print('WARNING: WIP feature loaded!', file=sys.stderr)
+
+        __import__(module)
+        return irc.require(name)
+
+# Create __all__
+__all__ = ['CmdHandler', 'Feature', 'Handler', 'IRC', 'error'] # type: List[str]
+
+# Load the base classes
+from . import _classes
+__all__.extend(_classes.__all__)
+del _classes
+
+# Add core features
+for f in os.listdir(os.path.dirname(__file__) + os.sep + 'features'):
+    if not f.startswith('__') and f.endswith('.py'):
+        _core_feature(f[:-3])
+
+del f
+
+# Set a few docstrings.
+miniirc.ircv3_message_parser.__doc__ = """
+    The default IRCv2/IRCv3 message parser, returns a 4-tuple:
+    (`command`, `hostmask`, `tags`, `args`). Do not use this directly, if you
+    want to parse IRCv3 messages in your own code use
+    `miniirc_extras.utils.ircv3_message_parser`, and if you want to reset the
+    message parser on an IRC object, call `irc.change_parser()` without any
+    parameters.
+"""
+
+miniirc.Handler.__doc__ = """
+    Adds `Handler`s to every IRC object that will ever exist (inside the
+    current process). For more information on handlers, see
+    https://github.com/luk3yx/miniirc/#handlers
+    or https://gitlab.com/luk3yx/miniirc/#handlers.
+"""
+
+miniirc.CmdHandler.__doc__ = """
+    Adds `CmdHandler`s to every IRC object that will ever exist (inside the
+    current process). For more information on handlers, see
+    https://github.com/luk3yx/miniirc/#handlers
+    or https://gitlab.com/luk3yx/miniirc/#handlers.
+"""
---
+++ packages/miniirc-extras/miniirc_extras-0.3.4.tar.gz/miniirc_extras-0.3.4/miniirc_extras/features/ensure_connection.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+#
+# irc.ensure_connection: Decreases the ping interval temporarily
+#
+
+import threading
+from .. import AbstractIRC, error, Feature, Hostmask
+from ..utils import get_raw_socket
+from typing import List, Optional, Tuple
+
+class NotConnectedError(error):
+    pass
+
+_ping_arg = 'miniirc_extras-connection_ensurer' # type: str
+
+# The ensure_connection feature
+@Feature('ensure_connection')
+class ConnectionEnsurer:
+    _new_interval = 2     # type: int
+    _old_interval = None  # type: Optional[int]
+    _managers     = 0     # type: int
+
+    # Handle new contexts
+    def __enter__(self) -> None:
+        irc, interval = self._irc, self._new_interval
+
+        # Make sure irc.connected is True.
+        if not irc.connected:
+            raise NotConnectedError('You are not connected to IRC!')
+
+        # Back up the current ping interval
+        if self._old_interval is None:
+            self._old_interval = irc.ping_interval
+
+        # Change the ping interval
+        get_raw_socket(irc).settimeout(interval)
+
+        # Wait for a PONG - This also makes the new ping interval work
+        self._managers += 1
+        try:
+            self._event.clear()
+            irc.quote('PING', ':' + _ping_arg)
+
+            # Raise an error
+            if not self._event.wait(interval):
+                raise NotConnectedError('PING timeout!')
+
+            self._event.clear()
+        except:
+            self.__exit__(1, 1, 1)
+            raise
+
+    # Handle leaving a context
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        if self._managers > 0:
+            self._managers -= 1
+
+        if not self._managers:
+            try:
+                get_raw_socket(self._irc).settimeout(self._old_interval)
+            except error:
+                pass
+
+    # Set the new interval
+    def __call__(self, interval: int = 2):
+        if not isinstance(interval, (int, float)) or interval < 1:
+            raise ValueError(
+                'irc.ensure_connection() interval must be at least 1.'
+            )
+
+        self._new_interval = interval
+        return self
+
+    # Handle PONGs
+    def _handle_pong(self, irc: AbstractIRC, hostmask: Hostmask,
+            args: List[str]) -> None:
+        if args and args[-1] == _ping_arg and self._managers:
+            self._event.set()
+
+    # Create new irc.ensure_connection() instances for IRC objects.
+    def __init__(self, irc: AbstractIRC) -> None:
+        self._event = threading.Event()
+        self._irc   = irc
+        irc.Handler('PONG', colon=False)(self._handle_pong)
