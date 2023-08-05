8a63245ffa4ab60d98e0c93e4875a376f169dcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:44 +0000

    Add package nwdiag/2.0.0/nwdiag-2.0.0.tar.gz

---
+++ packages/nwdiag/nwdiag-2.0.0.tar.gz/nwdiag-2.0.0/README.rst
@@ -0,0 +1,88 @@
+`nwdiag` generate network-diagram image file from spec-text file.
+
+.. image:: https://drone.io/bitbucket.org/blockdiag/nwdiag/status.png
+   :target: https://drone.io/bitbucket.org/blockdiag/nwdiag
+   :alt: drone.io CI build status
+
+.. image:: https://pypip.in/v/nwdiag/badge.png
+   :target: https://pypi.python.org/pypi/nwdiag/
+   :alt: Latest PyPI version
+
+.. image:: https://pypip.in/d/nwdiag/badge.png
+   :target: https://pypi.python.org/pypi/nwdiag/
+   :alt: Number of PyPI downloads
+
+
+Features
+========
+* Generate network-diagram from dot like text (basic feature).
+* Multilingualization for node-label (utf-8 only).
+
+You can get some examples and generated images on 
+`blockdiag.com <http://blockdiag.com/en/nwdiag/nwdiag-examples.html>`_ .
+
+Setup
+=====
+
+Use easy_install or pip::
+
+   $ sudo easy_install nwdiag
+
+   Or
+
+   $ sudo pip nwdiag
+
+
+spec-text setting sample
+========================
+
+Few examples are available.
+You can get more examples at
+`blockdiag.com`_ .
+
+simple.diag
+------------
+
+simple.diag is simply define nodes and transitions by dot-like text format::
+
+    nwdiag {
+      network dmz {
+          address = "210.x.x.x/24"
+
+          web01 [address = "210.x.x.1"];
+          web02 [address = "210.x.x.2"];
+      }
+      network internal {
+          address = "172.x.x.x/24";
+
+          web01 [address = "172.x.x.1"];
+          web02 [address = "172.x.x.2"];
+          db01;
+          db02;
+      }
+    }
+
+
+Usage
+=====
+
+Execute nwdiag command::
+
+   $ nwdiag simple.diag
+   $ ls simple.png
+   simple.png
+
+
+Requirements
+============
+* Python 3.5 or later
+* blockdiag 1.5.0 or later
+* funcparserlib 0.3.6 or later
+* reportlab (optional)
+* wand and imagemagick (optional)
+* setuptools
+
+
+License
+=======
+Apache License 2.0
---
+++ packages/nwdiag/nwdiag-2.0.0.tar.gz/nwdiag-2.0.0/setup.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import find_packages, setup
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Text Processing :: Markup",
+]
+
+
+def get_version():
+    """Get version number of the package from version.py without importing core module."""
+    package_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(package_dir, 'src/nwdiag/__init__.py')
+
+    namespace = {}
+    with open(version_file, 'r') as f:
+        exec(f.read(), namespace)
+
+    return namespace['__version__']
+
+
+setup(
+    name='nwdiag',
+    version=get_version(),
+    description='nwdiag generates network-diagram image from text',
+    long_description=open("README.rst").read(),
+    long_description_content_type='text/x-rst',
+    classifiers=classifiers,
+    keywords=['diagram', 'generator'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya@gmail.com',
+    url='http://blockdiag.com/',
+    download_url='http://pypi.python.org/pypi/nwdiag',
+    project_urls={
+        "Code": "https://github.com/blockdiag/nwdiag",
+        "Issue tracker": "https://github.com/blockdiag/nwdiag/issues",
+    },
+    license='Apache License 2.0',
+    py_modules=[
+        'nwdiag_sphinxhelper',
+        'rackdiag_sphinxhelper',
+        'packetdiag_sphinxhelper',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=['blockdiag>=1.5.0'],
+    extras_require=dict(
+        testing=[
+            'nose',
+            'pep8>=1.3',
+            'flake8',
+            'flake8-coding',
+            'flake8-copyright',
+            'flake8-isort',
+            'reportlab',
+            'docutils'
+        ],
+        pdf=[
+            'reportlab',
+        ],
+        rst=[
+            'docutils',
+        ],
+    ),
+    test_suite='nose.collector',
+    entry_points="""
+       [console_scripts]
+       nwdiag = nwdiag.command:main
+       rackdiag = rackdiag.command:main
+       packetdiag = packetdiag.command:main
+
+       [blockdiag_noderenderer]
+       _packet_node = packetdiag.noderenderers
+    """,
+)
