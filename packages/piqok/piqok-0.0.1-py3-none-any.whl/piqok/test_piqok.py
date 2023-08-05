2277a2b30633e00f012a39a15ea8a8a70b694f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:53 +0000

    Add package piqok/0.0.1/piqok-0.0.1-py3-none-any.whl

---
+++ packages/piqok/piqok-0.0.1-py3-none-any.whl/piqok/test_piqok.py
@@ -0,0 +1,31 @@
+import json
+from typing import List
+from piqok.piqok import Json
+
+
+class Person(Json):
+    name: str
+    age: float
+    friends: List['Person']
+
+
+def test_a_person_with_a_bag():
+    p = Person(json.loads("""
+        {
+            "name": "Gilad",
+            "age": 40,
+            "friends": [
+                {
+                    "name": "Uri",
+                    "age": 40
+                },
+                {
+                    "name": "Chen",
+                    "age": 40
+                }
+            ]
+        }
+        """))
+    assert p.age == 40
+    assert p.friends[0].name == 'Uri'
+    assert p.friends[1].age == 40
