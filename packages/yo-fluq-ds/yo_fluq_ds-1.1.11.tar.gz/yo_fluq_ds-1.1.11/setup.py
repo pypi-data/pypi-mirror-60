a9cb1d1bb40954e707156805e26b2cbc4587ac15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package yo-fluq-ds/1.1.11/yo_fluq_ds-1.1.11.tar.gz

---
+++ packages/yo-fluq-ds/yo_fluq_ds-1.1.11.tar.gz/yo_fluq_ds-1.1.11/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+def readme():
+      with open('README.md') as file:
+            return file.read()
+
+setup(name='yo_fluq_ds',
+      version='1.1.11',
+      description='Fluent interface for data processing, advanced toolkit for data science',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers = [
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      url='http://github.com/okulovsky/yo_ds',
+      author='Yuri Okulovsky',
+      author_email='yuri.okulovsky@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'pandas',
+            'matplotlib',
+            'numpy',
+            'tqdm',
+            'seaborn',
+            'pyaml',
+            'jsonpickle',
+            'ipython',
+            'ipywidgets',
+            'sklearn',
+            'yo_fluq==1.1.11'
+
+      ],
+      include_package_data = True,
+      zip_safe=False
+      )
