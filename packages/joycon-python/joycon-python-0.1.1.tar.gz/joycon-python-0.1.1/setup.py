7427f86e141ed7c7f166ff3350ccdd616df888e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:37 +0000

    Add package joycon-python/0.1.1/joycon-python-0.1.1.tar.gz

---
+++ packages/joycon-python/joycon-python-0.1.1.tar.gz/joycon-python-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='joycon-python',
+    version='0.1.1',
+    description='Python driver for Nintendo Switch Joy-Con',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='tokoroten-lab, atsukoba',
+    author_email='tokoroten.lab@gmail.com, atsuya_kobayashi@yahoo.co.jp',
+    url='https://github.com/tokoroten-lab/joycon-python',
+    license=license,
+    packages=find_packages()
+)
