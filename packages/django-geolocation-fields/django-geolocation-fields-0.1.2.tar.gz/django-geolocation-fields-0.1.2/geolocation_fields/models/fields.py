8e6b1fa88af57c4491d169f545b764591ea64bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package django-geolocation-fields/0.1.2/django-geolocation-fields-0.1.2.tar.gz

---
+++ packages/django-geolocation-fields/django-geolocation-fields-0.1.2.tar.gz/django-geolocation-fields-0.1.2/geolocation_fields/models/fields.py
@@ -0,0 +1,34 @@
+"""
+This file holds all our Geometric fields
+"""
+from django.db import models
+from geolocation_fields.models import base
+from geolocation_fields.forms import fields
+
+
+class PointField(models.Field):
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 104
+        super().__init__(*args, **kwargs)
+        
+    
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
+
+    
+    def from_db_value(self, value, expression, connection):
+        return value
+
+
+    def get_internal_type(self):
+        return "CharField"
+
+    
+    def formfield(self, **kwargs):
+        # This is a fairly standard way to set up some defaults
+        # while letting the caller override them.
+        defaults = {'form_class': fields.PointField}
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
