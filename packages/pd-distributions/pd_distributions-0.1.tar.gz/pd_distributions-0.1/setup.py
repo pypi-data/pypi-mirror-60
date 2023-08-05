7e92d84db05e5a2c61414a9dcdb9a0c815e9bc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:57 +0000

    Add package pd-distributions/0.1/pd_distributions-0.1.tar.gz

---
+++ packages/pd-distributions/pd_distributions-0.1.tar.gz/pd_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pd_distributions'],
+      zip_safe=False)
