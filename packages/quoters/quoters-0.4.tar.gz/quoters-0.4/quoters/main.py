062a5e516b7bb565249523cc84c768d730bcecb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:01 +0000

    Add package quoters/0.4/quoters-0.4.tar.gz

---
+++ packages/quoters/quoters-0.4.tar.gz/quoters-0.4/quoters/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from quoters.extract_data import check_connection_and_generate_quote
+
+class Quote:
+    def print():
+        return check_connection_and_generate_quote()
+
