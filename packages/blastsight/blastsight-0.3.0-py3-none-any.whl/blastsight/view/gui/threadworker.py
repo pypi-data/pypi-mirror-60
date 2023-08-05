b6cdfb2c456dae1d2011fa421258c5b29e199008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package blastsight/0.3.0/blastsight-0.3.0-py3-none-any.whl

---
+++ packages/blastsight/blastsight-0.3.0-py3-none-any.whl/blastsight/view/gui/threadworker.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#  Copyright (c) 2019-2020 Gabriel Sanhueza.
+#
+#  Distributed under the MIT License.
+#  See LICENSE for more info.
+
+from qtpy.QtCore import QRunnable
+
+
+class ThreadWorker(QRunnable):
+    def __init__(self, method, *args, **kwargs):
+        super().__init__()
+        self.method = method
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self) -> None:
+        self.method(*self.args, **self.kwargs)
