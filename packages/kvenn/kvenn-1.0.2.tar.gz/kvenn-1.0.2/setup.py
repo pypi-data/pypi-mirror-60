f1ccf85adfda1f15f9af42f60815e96fee1febf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:54 +0000

    Add package kvenn/1.0.2/kvenn-1.0.2.tar.gz

---
+++ packages/kvenn/kvenn-1.0.2.tar.gz/kvenn-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = 'kvenn',
+    scripts=['bin/kvenn'],
+    version = '1.0.2',
+    description = 'CLI tool for waiting on ports to open',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'KJ',
+    author_email = 'jdotpy@users.noreply.github.com',
+    url = 'https://github.com/jdotpy/kvenn',
+    download_url = 'https://github.com/jdotpy/kvenn/tarball/master',
+    keywords = ['tools'],
+    classifiers = [],
+)
