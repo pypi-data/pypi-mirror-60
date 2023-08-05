0c1a688a798776e71c329ab7bda71c8062b87fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:17 +0000

    Add package toot/0.25.2/toot-0.25.2-py3-none-any.whl

---
+++ packages/toot/toot-0.25.2-py3-none-any.whl/toot/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+
+__version__ = '0.25.2'
+
+App = namedtuple('App', ['instance', 'base_url', 'client_id', 'client_secret'])
+User = namedtuple('User', ['instance', 'username', 'access_token'])
+
+DEFAULT_INSTANCE = 'mastodon.social'
+
+CLIENT_NAME = 'toot - a Mastodon CLI client'
+CLIENT_WEBSITE = 'https://github.com/ihabunek/toot'
