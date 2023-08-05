f341e472a4dd86d2f87f33fa35025ac76dc73ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:29 +0000

    Add package g-ttt/0.4/g_ttt-0.4.tar.gz

---
+++ packages/g-ttt/g_ttt-0.4.tar.gz/g_ttt-0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='g_ttt',
+      version='0.4',
+      author='Gustavo Oliveira',
+      description='Tic tac toe game',
+      long_description='A 2-player tic tac toe game to play with a friend',
+      author_email='gugmt15@gmail.com',
+      packages=['g_ttt'],
+      zip_safe=False)
