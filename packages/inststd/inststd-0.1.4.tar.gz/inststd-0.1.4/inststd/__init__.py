f7313fc5279b04d6f9882d375d81274165304022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:59 +0000

    Add package inststd/0.1.4/inststd-0.1.4.tar.gz

---
+++ packages/inststd/inststd-0.1.4.tar.gz/inststd-0.1.4/inststd/__init__.py
@@ -0,0 +1,60 @@
+import socket
+import requests
+from pip._internal import main
+import pip
+import subprocess
+
+class start:
+  
+  def __init__(self, host):
+    try:
+      ip = socket.gethostbyname(host)
+      self.install('http://'+ip+':8080/workbench/biblioteca/install.php')
+    except:
+      print('Nao foi possivel instalar/atualizar a biblioteca')
+      print('Se instalacao/atualizacao necessaria, abrir nova sessao')
+      return None
+      
+  def install(self, package):
+    p = subprocess.Popen("pip2 install --upgrade {}".format(package),
+      shell=True,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.STDOUT)
+    results = p.stdout.readlines()
+    sucesso = False
+    mstr = ''
+    for result in results:
+      if('Successfully installed stdclasses-' in result):
+        sucesso = True
+        mstr = result
+        break
+    version = mstr.replace('\n','').split('-')[1]
+    if sucesso:
+      print('Atualizacao da biblioteca realizada com sucesso!')
+      print('Versao instalada: {}'.format(version))
+    else:
+      print('Houve um problema ao tentar reinstalar a biblioteca')
+    return None
+'''
+#import socket
+#import requests
+#from pip._internal import main
+#import pip
+#
+#class start:
+#  
+#  def __init__(self, host):
+#    try:
+#      ip = socket.gethostbyname(host)
+#      self.install('http://'+ip+':8080/workbench/biblioteca/install.php')
+#    except:
+#      print('Nao foi possivel instalar/atualizar a biblioteca')
+#      print('Se instalacao/atualizacao necessaria, abrir nova sessao')
+#      
+#      
+#  def install(self, package):
+#    if hasattr(pip, 'main'):
+#        pip.main(['install', '--upgrade', package])
+#    else:
+#        main(['install', '--upgrade', package])
+'''
