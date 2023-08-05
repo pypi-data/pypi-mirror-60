c83868d2349430d6462b29e4bb3ab002d6db75e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package tensornet/0.0.0/tensornet-0.0.0.tar.gz

---
+++ packages/tensornet/tensornet-0.0.0.tar.gz/tensornet-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="tensornet",
+    version="0.0.0",
+    author="Arvin Singh Kushwaha",
+    author_email="arvin.singhk@gmail.com",
+    packages=find_packages(),
+    install_requires=["numpy"]
+)
