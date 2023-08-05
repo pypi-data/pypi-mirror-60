1a7376aebed31d56d79dce94efcef1ca58431428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package pybetter/0.3.1/pybetter-0.3.1-py3-none-any.whl

---
+++ packages/pybetter/pybetter-0.3.1-py3-none-any.whl/pybetter/transformers/equals_none.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+import libcst as cst
+from libcst import matchers as m
+
+from pybetter.transformers.base import NoqaAwareTransformer
+
+
+class EqualsNoneIsNoneTransformer(NoqaAwareTransformer):
+    def leave_ComparisonTarget(
+        self, original_node: cst.ComparisonTarget, updated_node: cst.ComparisonTarget
+    ) -> Union[cst.ComparisonTarget, cst.RemovalSentinel]:
+        if not m.matches(
+            updated_node,
+            m.ComparisonTarget(comparator=m.Name(value="None"), operator=m.Equal()),
+        ):
+            return original_node
+
+        return updated_node.with_changes(
+            operator=cst.Is(
+                # For some reason, mypy cannot understand that operator is cst.Equal,
+                # even with explicit direction by cst.ensure_type :/
+                whitespace_after=updated_node.operator.whitespace_after,  # type: ignore
+                whitespace_before=updated_node.operator.whitespace_before,  # type: ignore
+            )
+        )
