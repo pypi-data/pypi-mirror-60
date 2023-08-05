434f0d89d7da496526578ec8e3054fc8642bb6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:51 +0000

    Add package memory-monitor/0.1.1/memory_monitor-0.1.1.tar.gz

---
+++ packages/memory-monitor/memory_monitor-0.1.1.tar.gz/memory_monitor-0.1.1/memory_monitor/setup.py
@@ -0,0 +1,31 @@
+from glob import glob
+import setuptools
+
+setuptools.setup(
+    name="memory_monitor",
+    packages=['memory_monitor'],
+    version='0.1.1',
+    license='MIT',
+    description="Simple Jupyter extension to show how much resources (RAM) your notebook is using",
+    url="https://github.com/steelersd/memory_monitor",
+    download_url="https://github.com/steelersd/memory_monitor/archive/0.1.1.tar.gz",
+    keywords=['Jupyter', 'Notebook', 'Extension'],
+    # install_requires=[
+    #     'psutil',
+    #     'notebook',
+    # ],
+    include_package_data=True,
+    data_files=[
+        # like `jupyter nbextension install --sys-prefix`
+        ('share/jupyter/nbextensions/memory_monitor',
+         glob('memory_monitor/static/*')),
+        # like `jupyter nbextension enable --sys-prefix`
+        ('etc/jupyter/nbconfig/notebook.d',
+         ['memory_monitor/etc/nbextension.json']),
+        # like `jupyter serverextension enable --sys-prefix`
+        ('etc/jupyter/jupyter_notebook_config.d',
+         ['memory_monitor/etc/serverextension.json']),
+    ],
+    python_requires='>=3.6'
+
+)
