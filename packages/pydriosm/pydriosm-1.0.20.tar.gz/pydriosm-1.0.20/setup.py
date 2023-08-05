212fad28d64e96271e7847b1d37bb9e7e3a066a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:01 +0000

    Add package pydriosm/1.0.20/pydriosm-1.0.20.tar.gz

---
+++ packages/pydriosm/pydriosm-1.0.20.tar.gz/pydriosm-1.0.20/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+import pydriosm.settings
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+requirements_ = [r.strip() for r in requirements]
+
+setuptools.setup(
+
+    name='pydriosm',
+    version='1.0.20',
+
+    author='Qian Fu',
+    author_email='qian.fu@outlook.com',
+
+    description="Download, read/parse and import/export OpenStreetMap data extracts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/mikeqfu/pydriosm',
+
+    install_requires=requirements_,
+
+    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
+
+    package_data={"pydriosm": ["dat/*"]},
+    include_package_data=True,
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux'
+    ],
+)
+
+pydriosm.settings.gdal_configurations(reset=False)
