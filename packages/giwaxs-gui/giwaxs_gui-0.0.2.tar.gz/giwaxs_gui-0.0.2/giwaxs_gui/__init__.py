35e45c21ed18a7005d0c3608f569e07a44137c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:10 +0000

    Add package giwaxs-gui/0.0.2/giwaxs_gui-0.0.2.tar.gz

---
+++ packages/giwaxs-gui/giwaxs_gui-0.0.2.tar.gz/giwaxs_gui-0.0.2/giwaxs_gui/__init__.py
@@ -0,0 +1,14 @@
+import sys
+import logging
+from PyQt5.QtWidgets import QApplication
+
+from .gui import GiwaxsProgram
+
+__all__ = ['GiwaxsProgram', 'run']
+
+
+def run():
+    logging.basicConfig(level=logging.ERROR)
+    app = QApplication(sys.argv)
+    window = GiwaxsProgram()
+    sys.exit(app.exec_())
