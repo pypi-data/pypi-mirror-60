79c412eda02913c9968ad4c5d1432f805ae4126e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package quantumgraphs/0.1.4/QuantumGraphs-0.1.4.tar.gz

---
+++ packages/quantumgraphs/QuantumGraphs-0.1.4.tar.gz/QuantumGraphs-0.1.4/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quantumgraphs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ipython>=7.11,<8.0',
+ 'matplotlib>=3.1,<4.0',
+ 'networkx>=1.10,<2.0',
+ 'numpy>=1.18,<2.0',
+ 'p-tqdm>=1.3,<2.0',
+ 'pandas>=0.25,<0.26',
+ 'scipy>=1.4,<2.0',
+ 'seaborn>=0.10,<0.11']
+
+setup_kwargs = {
+    'name': 'quantumgraphs',
+    'version': '0.1.4',
+    'description': 'Grow random graphs using quantum random walks',
+    'long_description': None,
+    'author': 'ziofil',
+    'author_email': 'miatto@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
