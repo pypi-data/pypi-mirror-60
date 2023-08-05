d638098b9cfb38cba5aebe071293fc53c69feedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:57 +0000

    Add package dagster-spark/0.6.8rc2/dagster_spark-0.6.8rc2-py3-none-any.whl

---
+++ packages/dagster-spark/dagster_spark-0.6.8rc2-py3-none-any.whl/dagster_spark/types.py
@@ -0,0 +1,13 @@
+'''Type definitions for the event pipeline demo'''
+
+from dagster import Enum, EnumValue
+
+SparkDeployModeCluster = EnumValue('cluster')
+SparkDeployModeClient = EnumValue('client')
+SparkDeployMode = Enum(
+    name='SparkDeployMode', enum_values=[SparkDeployModeCluster, SparkDeployModeClient]
+)
+
+
+class SparkSolidError(Exception):
+    pass
