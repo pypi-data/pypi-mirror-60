5e90b5c10a964856e412e172454ec2c4dce61b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:14 +0000

    Add package testpoetrypypi/0.1.20/testpoetrypypi-0.1.20.tar.gz

---
+++ packages/testpoetrypypi/testpoetrypypi-0.1.20.tar.gz/testpoetrypypi-0.1.20/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testpoetrypypi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry-version>=0.1.5,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['runtest = runtest:main',
+                     'testpoetrypypi = testpoetrypypi:main']}
+
+setup_kwargs = {
+    'name': 'testpoetrypypi',
+    'version': '0.1.20',
+    'description': '',
+    'long_description': '# testpoetrypypi\n\n|           | master | develop |\n|:----------|:-------|:--------|\n| CI Status | [![CircleCI](https://circleci.com/gh/jeff-snyk/testpoetrypypi/tree/master.svg?style=svg)](https://circleci.com/gh/jeff-snyk/testpoetrypypi/tree/master)|[![CircleCI](https://circleci.com/gh/jeff-snyk/testpoetrypypi/tree/develop.svg?style=svg)](https://circleci.com/gh/jeff-snyk/testpoetrypypi/tree/develop)|\n\nA reference-ish implementation of a Python project that uses:\n+ Poetry (and pyproject.toml)\n+ pytest and Black for testing and linting\n+ CircleCI\n+ Git Flow (i.e. `master` + `develop` + feature branches)\n+ [python-semantic-release](https://pypi.org/project/python-semantic-release/) to compute the next version and publish to PyPI upon merging `develop` into `master`.\n',
+    'author': 'Jeff',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
