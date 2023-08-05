3343242ebe745f15ea020122f2a65932bacb903a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package vega/3.0.0/vega-3.0.0.tar.gz

---
+++ packages/vega/vega-3.0.0.tar.gz/vega-3.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vega', 'vega.tests']
+
+package_data = \
+{'': ['*'], 'vega': ['static/*']}
+
+install_requires = \
+['jupyter>=1.0.0,<2.0.0', 'pandas>=0.25.3,<0.26.0']
+
+entry_points = \
+{'console_scripts': ['test = pytest:main']}
+
+setup_kwargs = {
+    'name': 'vega',
+    'version': '3.0.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Dominik Moritz',
+    'author_email': 'domoritz@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
