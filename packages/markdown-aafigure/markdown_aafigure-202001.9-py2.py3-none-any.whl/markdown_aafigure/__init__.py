c15e5ba6a5b6f0eb88e9b7c4ddbf2ad5fd88e9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:39 +0000

    Add package markdown-aafigure/202001.9/markdown_aafigure-202001.9-py2.py3-none-any.whl

---
+++ packages/markdown-aafigure/markdown_aafigure-202001.9-py2.py3-none-any.whl/markdown_aafigure/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# This file is part of the markdown_aafigure project
+# https://gitlab.com/mbarkhau/markdown_aafigure
+#
+# Copyright (c) 2019 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+__version__ = 'v202001.0009'
+from markdown_aafigure.extension import AafigureExtension
+
+
+def makeExtension(**kwargs):
+    return AafigureExtension(**kwargs)
