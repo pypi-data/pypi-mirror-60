21600bbb7b3e5827efdff79aebf14672d0639f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package django-setty/3.2.0/django_setty-3.2.0-py3-none-any.whl

---
+++ packages/django-setty/django_setty-3.2.0-py3-none-any.whl/setty/admin.py
@@ -0,0 +1,71 @@
+import json
+from distutils.util import strtobool
+
+from django import forms
+from django.conf import settings
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from .models import TypeChoices, SettySettings
+
+SERIALIZERS = {
+    TypeChoices.BOOL: lambda x: strtobool(x),
+    TypeChoices.DICT: lambda x: json.loads(x),
+    TypeChoices.FLOAT: lambda x: float(x),
+    TypeChoices.INTEGER: lambda x: int(x),
+    TypeChoices.LIST: lambda x: json.loads(x),
+    TypeChoices.STRING: lambda x: str(x),
+}
+
+
+class SettingsForm(forms.ModelForm):
+
+    def __init__(self, *args, instance=None, **kwargs):
+        # Loading the stringified value does not work without manually passing this in as initial data.
+        # This is due to it being a model property. We can safely ignore the provided inital kwarg as it is merged later
+        kwargs.pop('initial', None)
+        initial_data = {'value_unpacked': getattr(instance, 'value_unpacked', None)}
+        super().__init__(*args, initial=initial_data, instance=instance, **kwargs)
+
+    value_unpacked = forms.CharField(label='Value',
+                                     help_text='The value to store. '
+                                               'List and Dict data types should be defined as JSON strings.')
+
+    def clean_value_unpacked(self):
+        serializer = SERIALIZERS[self.cleaned_data['type']]
+        try:
+            serialized_value = serializer(self.cleaned_data['value_unpacked'].replace("\'", "\""))
+        except Exception as e:
+            raise ValidationError('An error occurred storing the value: {}'.format(e))
+
+        return serialized_value
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.value = self.cleaned_data['value_unpacked']
+        instance.save()
+
+        # Reset item in cache if changed in the admin
+        if settings.SETTY_BACKEND == 'CacheBackend':
+            from setty.backend import CacheBackend
+            CacheBackend().set_in_cache(instance.name, instance.value)
+
+        return instance
+
+    class Meta:
+        model = SettySettings
+        fields = ['name', 'app_name', 'type', 'value_unpacked']
+        read_only_fields = ['created_time', 'updated_time']
+
+        help_texts = {
+            'name': _('The setting name.'),
+            'type': _('The data type of the setting being stored.'),
+        }
+
+
+@admin.register(SettySettings)
+class SettyAdmin(admin.ModelAdmin):
+    form = SettingsForm
+    list_display = ['name', 'app_name', 'type', 'value_unpacked', 'created_time', 'updated_time']
+    readonly_fields = ['created_time', 'updated_time']
---
+++ packages/django-setty/django_setty-3.2.0-py3-none-any.whl/setty/backend.py
@@ -0,0 +1,69 @@
+import logging
+
+from django.conf import settings
+from django.core.cache import cache
+from setty.exceptions import SettingDoesNotExistError
+
+from .models import SettySettings
+
+logger = logging.getLogger(__name__)
+
+
+class DatabaseBackend:
+    """
+    The simple DatabaseBackend is backed by the Django model storing these settings
+    """
+    def get_all(self, app_name=None):
+        if app_name:
+            return SettySettings.objects.filter(app_name=app_name).all()
+        return SettySettings.objects.all()
+
+    def get(self, name):
+        try:
+            setting = SettySettings.objects.get(name=name).value
+        except SettySettings.DoesNotExist:
+            setting = getattr(settings, 'SETTY_NOT_FOUND_VALUE', None)
+
+        return setting
+
+    def set(self, name, value):
+        updated_count = SettySettings.objects.filter(name=name).update(value=value)
+        if not updated_count:
+            raise SettingDoesNotExistError(f'Error setting value for {name} - '
+                                           f'this setting does not exist in the database!')
+        return value
+
+
+class CacheBackend(DatabaseBackend):
+    """
+    CacheBackend uses the Django cache setup to cache values instead of accessing the
+    database on each get call.
+    """
+    def get(self, name):
+        cache_key = self._make_cache_key(name)
+        setting_value = cache.get(cache_key, '__expired__')
+        if setting_value != '__expired__':
+            logger.debug('From Cache:', setting_value)
+            return setting_value
+        logger.debug('From Database:', setting_value)
+        return self._retrieve_and_cache_setting(name)
+
+    def _retrieve_and_cache_setting(self, name):
+        value = super().get(name)
+        return self.set(name, value)
+
+    def set(self, name, value):
+        super().set(name, value)
+        self.set_in_cache(name, value)
+        return value
+
+    def set_in_cache(self, name, value):
+        cache.set(self._make_cache_key(name), value, getattr(settings, 'SETTY_CACHE_TTL', 3600))
+
+    def load_all_settings_into_cache(self):
+        for setting in self.get_all():
+            self.set_in_cache(setting.name, setting.value)
+
+    @staticmethod
+    def _make_cache_key(name):
+        return ':'.join([getattr(settings, 'SETTY_CACHE_PREFIX', '_dyn_settings_'), name])
