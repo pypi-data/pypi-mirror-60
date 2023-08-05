ad117e5f7c3611d2bd9fc01dba56e3f8b4c2dad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:04 +0000

    Add package segmentation-evaluation/1.0.2/segmentation-evaluation-1.0.2.tar.gz

---
+++ packages/segmentation-evaluation/segmentation-evaluation-1.0.2.tar.gz/segmentation-evaluation-1.0.2/README.md
@@ -0,0 +1,2 @@
+# Segmentation Evaluation
+The tool allows to manually segment an image and perform analysis between two segmentation
---
+++ packages/segmentation-evaluation/segmentation-evaluation-1.0.2.tar.gz/segmentation-evaluation-1.0.2/performance_tool/__init__.py
@@ -0,0 +1,2 @@
+from performance_tool.evaluate_segmentation import evaluate_segmentation
+from performance_tool.manually_segmentation import manually_segmentation
