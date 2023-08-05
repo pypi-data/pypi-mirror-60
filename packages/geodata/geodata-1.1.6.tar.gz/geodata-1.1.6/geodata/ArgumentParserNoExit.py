4943cfdff519f097a0f9a34384e194407ebc33b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package geodata/1.1.6/geodata-1.1.6.tar.gz

---
+++ packages/geodata/geodata-1.1.6.tar.gz/geodata-1.1.6/geodata/ArgumentParserNoExit.py
@@ -0,0 +1,12 @@
+""" Command argument parser that throws exception rather than exitting """
+
+import argparse
+
+# Variant of ArgParse which will not exit when there is a parser error, instead raises exception
+class ArgumentParserError(Exception):
+    pass
+
+
+class ArgumentParserNoExit(argparse.ArgumentParser):
+    def error(self, message):
+        raise ArgumentParserError(message)
