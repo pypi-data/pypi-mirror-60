7667d7a63c7e41102b6098484b7de42f40aec68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:03 +0000

    Add package seiran/1.5.0/seiran-1.5.0.tar.gz

---
+++ packages/seiran/seiran-1.5.0.tar.gz/seiran-1.5.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='seiran',
+      version='1.5.0',
+      description='Local bookmarks manager',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Internet',
+      ],
+      keywords='bookmarks',
+      url='https://www.twinkle-night.net/Code/seiran.html',
+      author='Garrick',
+      author_email='earthisthering@posteo.de',
+      license='GPLv3+',
+      packages=['seiran'],
+      install_requires=[
+          'appdirs'
+      ],
+      entry_points={'console_scripts':['seiran=seiran.seiran:main']},
+      include_package_data=True,
+      zip_safe=False)
