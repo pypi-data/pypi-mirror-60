d453b866f5da1906c0674e8cc1d1572add8ccd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:05 +0000

    Add package pm4py/1.2.9/pm4py-1.2.9.tar.gz

---
+++ packages/pm4py/pm4py-1.2.9.tar.gz/pm4py-1.2.9/pm4py/objects/process_tree/util.py
@@ -0,0 +1,199 @@
+from pm4py.objects.process_tree import process_tree as pt
+from pm4py.objects.process_tree import pt_operator as pt_op
+from pm4py.objects.process_tree import state as pt_st
+
+
+def fold(tree):
+    '''
+    This method reduces a process tree by merging nodes of the form N(N(a,b),c) into N(a,b,c), i.e., where
+    N = || or X. For example X(X(a,b),c) == X(a,b,c).
+    Furthermore, meaningless parts, e.g., internal nodes without children, or, operators with one child are removed
+    as well.
+
+    :param tree:
+    :return:
+    '''
+    if len(tree.children) > 0:
+        for c in tree.children:
+            fold(c)
+        cc = tree.children
+        for c in cc:
+            if c.operator is not None:
+                if len(c.children) == 0:
+                    tree.children.remove(c)
+                    c.parent = None
+                elif len(c.children) == 1:
+                    i = tree.children.index(c)
+                    tree.children[i:i] = c.children
+                    # tree.children.extend(c.children)
+                    for cc in c.children:
+                        cc.parent = tree
+                    tree.children.remove(c)
+                    c.children.clear()
+                    c.parent = None
+        if tree.operator in [pt_op.Operator.SEQUENCE, pt_op.Operator.XOR, pt_op.Operator.PARALLEL]:
+            chlds = [c for c in tree.children]
+            for c in chlds:
+                if c.operator == tree.operator:
+                    i = tree.children.index(c)
+                    tree.children[i:i] = c.children
+                    # tree.children.extend(c.children)
+                    for cc in c.children:
+                        cc.parent = tree
+                    tree.children.remove(c)
+                    c.children.clear()
+                    c.parent = None
+    if tree.parent is None and len(tree.children) == 1:
+        root = tree.children[0]
+        root.parent = None
+        tree.children.clear()
+        return root
+    return tree
+
+
+def reduce_tau_leafs(pt):
+    '''
+    This method reduces tau leaves that are not meaningful. For example tree ->(a,\tau,b) is reduced to ->(a,b).
+    In some cases this results in constructs such as ->(a), i.e., a sequence with a single child. Such constructs
+    are not further reduced.
+
+    :param pt:
+    :return:
+    '''
+    if len(pt.children) > 0:
+        for c in pt.children:
+            reduce_tau_leafs(c)
+        if pt.operator in [pt_op.Operator.SEQUENCE]:
+            chlds = [c for c in pt.children]
+            for c in chlds:
+                if (len(c.children) == 0 or c.children is None) and c.label is None and c.operator is None:
+                    c.parent = None
+                    pt.children.remove(c)
+    return pt
+
+
+def project_execution_sequence_to_leafs(execution_sequence):
+    """
+    Project an execution sequence to the set of leafs
+    of the tree.
+
+    Parameters
+    ------------
+    execution_sequence
+        Execution sequence on the process tree
+
+    Returns
+    ------------
+    list_leafs
+        Leafs nodes of the process tree
+    """
+    return list(map(lambda x: x[0],
+                    filter(lambda x: (x[1] is pt_st.State.OPEN and len(x[0].children) == 0), execution_sequence)))
+
+
+def project_execution_sequence_to_labels(execution_sequence):
+    """
+    Project an execution sequence to a set of labels
+
+    Parameters
+    ------------
+    execution_sequence
+        Execution sequence on the process tree
+
+    Returns
+    ------------
+    list_labels
+        List of labels contained in the process tree
+    """
+    return list(map(lambda x: x.label,
+                    filter(lambda x: x.label is not None, project_execution_sequence_to_leafs(execution_sequence))))
+
+
+def parse(string_rep):
+    """
+    Parse a string provided by the user to a process tree
+    (initialization method)
+
+    Parameters
+    ------------
+    string_rep
+        String representation of the process tree
+
+    Returns
+    ------------
+    node
+        Process tree object
+    """
+    depth_cache = dict()
+    depth = 0
+    return parse_recursive(string_rep, depth_cache, depth)
+
+
+def parse_recursive(string_rep, depth_cache, depth):
+    """
+    Parse a string provided by the user to a process tree
+    (recursive method)
+
+    Parameters
+    ------------
+    string_rep
+        String representation of the process tree
+    depth_cache
+        Depth cache of the algorithm
+    depth
+        Current step depth
+
+    Returns
+    -----------
+    node
+        Process tree object
+    """
+    string_rep = string_rep.strip()
+    node = None
+    operator = None
+    if string_rep.startswith(pt_op.Operator.LOOP.value):
+        operator = pt_op.Operator.LOOP
+        string_rep = string_rep[1:]
+    elif string_rep.startswith(pt_op.Operator.PARALLEL.value):
+        operator = pt_op.Operator.PARALLEL
+        string_rep = string_rep[1:]
+    elif string_rep.startswith(pt_op.Operator.XOR.value):
+        operator = pt_op.Operator.XOR
+        string_rep = string_rep[1:]
+    elif string_rep.startswith(pt_op.Operator.OR.value):
+        operator = pt_op.Operator.OR
+        string_rep = string_rep[1:]
+    elif string_rep.startswith(pt_op.Operator.SEQUENCE.value):
+        operator = pt_op.Operator.SEQUENCE
+        string_rep = string_rep[2:]
+    if operator is not None:
+        parent = None if depth == 0 else depth_cache[depth - 1]
+        node = pt.ProcessTree(operator=operator, parent=parent)
+        depth_cache[depth] = node
+        if parent is not None:
+            parent.children.append(node)
+        depth += 1
+        string_rep = string_rep.strip()
+        assert (string_rep[0] == '(')
+        parse_recursive(string_rep[1:], depth_cache, depth)
+    else:
+        label = None
+        if string_rep.startswith('\''):
+            string_rep = string_rep[1:]
+            escape_ext = string_rep.find('\'')
+            label = string_rep[0:escape_ext]
+            string_rep = string_rep[escape_ext + 1:]
+        else:
+            assert (string_rep.startswith('tau'))
+            string_rep = string_rep[3:]
+        parent = None if depth == 0 else depth_cache[depth - 1]
+        node = pt.ProcessTree(operator=operator, parent=parent, label=label)
+        if parent is not None:
+            parent.children.append(node)
+
+        while string_rep.strip().startswith(')'):
+            depth -= 1
+            string_rep = (string_rep.strip())[1:]
+        if len(string_rep.strip()) > 0:
+            parse_recursive((string_rep.strip())[1:], depth_cache, depth)
+    return node
---
+++ packages/pm4py/pm4py-1.2.9.tar.gz/pm4py-1.2.9/tests/execute_tests.py
@@ -0,0 +1,70 @@
+import inspect
+import os
+import sys
+import unittest
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.insert(0, parent_dir)
+    from tests.role_detection import RoleDetectionTest
+    from tests.passed_time import PassedTimeTest
+    from tests.ltl_checking_logs import LtlCheckingLogTest
+    from tests.ltl_checking_pandas import LtlCheckingPandasTest
+    from tests.imp_everything import Pm4pyImportPackageTest
+    from tests.xes_impexp_test import XesImportExportTest
+    from tests.csv_impexp_test import CsvImportExportTest
+    from tests.alpha_test import AlphaMinerTest
+    from tests.imdf_test import InductiveMinerDFTest
+    from tests.imdf_tree_test import InductiveMinerDFTreeTest
+    from tests.alignment_test import AlignmentTest
+    from tests.sna_test import SnaTests
+    from tests.petri_imp_exp_test import PetriImportExportTest
+    from tests.documentation_tests.test_doc_alpha1 import AlphaMinerDocumentationTest
+    from tests.documentation_tests.test_doc_inductive1 import InductiveMinerDocumentationTest
+    from tests.documentation_tests.test_doc_dfgraph1 import DfGraphDocumentationTest
+    from tests.documentation_tests.test_doc_xes1 import XES1DocumentationTest
+    from tests.documentation_tests.test_doc_csv1 import CSV1DocumentationTest
+    from tests.documentation_tests.test_doc_classifiers1 import Classifiers1DocumentationTest
+    from tests.documentation_tests.test_doc_measures import DocMeasuresDocumentationTest
+    from tests.etc_tests import ETCTest
+    from tests.evaluation_tests import ProcessModelEvaluationTests
+    from tests.dec_tree_test import DecisionTreeTest
+    from tests.filtering_test import LogFilteringTest
+    from tests.dataframe_prefilter import DataframePrefilteringTest
+    from tests.serialization_test import SerializationTest
+    from tests.graphs_forming import GraphsForming
+    from tests.heuminer_test import HeuMinerTest
+    from tests.matrix_rep_test import MatrixRepTest
+
+    test_roles = RoleDetectionTest()
+    test_pttime = PassedTimeTest()
+    test_ltl_logs = LtlCheckingLogTest()
+    test_ltl_pandas = LtlCheckingPandasTest()
+    test1_object = Pm4pyImportPackageTest()
+    test2_object = XesImportExportTest()
+    test3_object = CsvImportExportTest()
+    test4_object = AlphaMinerTest()
+    test5_object = InductiveMinerDFTest()
+    test55_object = InductiveMinerDFTreeTest()
+    test6_object = AlignmentTest()
+    test7_object = PetriImportExportTest()
+    test8_object = AlphaMinerDocumentationTest()
+    test9_object = InductiveMinerDocumentationTest()
+    test10_object = DfGraphDocumentationTest()
+    test11_object = XES1DocumentationTest()
+    test12_object = CSV1DocumentationTest()
+    test13_object = Classifiers1DocumentationTest()
+    test14_object = DocMeasuresDocumentationTest()
+    test17_object = ETCTest()
+    test20_object = ProcessModelEvaluationTests()
+    test21_object = DecisionTreeTest()
+    sna_tests = SnaTests()
+    filtering_test = LogFilteringTest()
+    prefiltering_test = DataframePrefilteringTest()
+    serialization_test = SerializationTest()
+    graphforming_test = GraphsForming()
+    heuminer_test = HeuMinerTest()
+    matrixrep_test = MatrixRepTest()
+
+    unittest.main()
