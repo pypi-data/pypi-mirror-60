fd04ec9b17bde5a90055057195acb946d0022d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:21 +0000

    Add package ga-capstone-hakngrow/0.0.18/ga-capstone-hakngrow-0.0.18.tar.gz

---
+++ packages/ga-capstone-hakngrow/ga-capstone-hakngrow-0.0.18.tar.gz/ga-capstone-hakngrow-0.0.18/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ga-capstone-hakngrow", # Replace with your own username
+    version="0.0.18",
+    author="Howie Ng",
+    author_email="hakngrow@gmail.com",
+    description="GA Capstone project",
+    long_description="GA Capstone project on timeseries stock price predicition.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/hakngrow/capstone.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
