202e0e95d194e0e90480b9e932b49653a64ba020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:38 +0000

    Add package domdf-wxpython-tools/0.1.32/domdf_wxpython_tools-0.1.32.tar.gz

---
+++ packages/domdf-wxpython-tools/domdf_wxpython_tools-0.1.32.tar.gz/domdf_wxpython_tools-0.1.32/domdf_wxpython_tools/__init__.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  __init__.py
+#
+#  Copyright 2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+__all__ = [
+	"dialogs",
+	"picker",
+	"events",
+	"icons",
+	"keyboard",
+	"list_dialog",
+	"LogCtrl",
+	"style_picker",
+	"timer_thread",
+	"utils",
+	"panel_listctrl",
+	"__author__",
+	"__version__",
+	"__copyright__"
+]
+
+__author__ = "Dominic Davis-Foster"
+__copyright__ = "Copyright 2019 Dominic Davis-Foster"
+
+__license__ = "LGPL"
+__version__ = "0.1.32"
+__email__ = "dominic@davis-foster.co.uk"
+
+from . import dialogs
+from . import picker
+from . import events
+from . import icons
+from . import keyboard
+from . import list_dialog
+from . import LogCtrl
+from . import style_picker
+from . import timer_thread
+from . import utils
+
+
+
+
+
