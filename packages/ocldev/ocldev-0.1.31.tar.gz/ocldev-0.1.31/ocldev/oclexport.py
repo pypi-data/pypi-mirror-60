1dded215ca1edcf5dd8c9ea212933f99fbf7c71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:08 +0000

    Add package ocldev/0.1.31/ocldev-0.1.31.tar.gz

---
+++ packages/ocldev/ocldev-0.1.31.tar.gz/ocldev-0.1.31/ocldev/oclexport.py
@@ -0,0 +1,152 @@
+"""
+Objects to work with OCL's export API
+"""
+import requests
+import json
+import zipfile
+from pprint import pprint
+from StringIO import StringIO
+
+
+class OclError(Exception):
+    """ Base class for exceptions in this module """
+    pass
+
+
+class OclUnknownResourceError(OclError):
+    """ Error thrown when an unknown resource type is encountered """
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+
+class OclExportFactory(object):
+    """ Factory class to create OclExport factory objects """
+
+    @staticmethod
+    def load_export(repo_version_url='', oclapitoken=''):
+        """
+        Retrieve a cached repository export from OCL, decompress, parse the JSON, and return as a python dictionary.
+        NOTE: The export is decompressed and parsed in memory. It may be necessary in the future to handle very large
+        exports using the filesystem rather than processing in memory.
+        """
+
+        # Prepare the request headers
+        oclapiheaders = {'Content-Type': 'application/json'}
+        if oclapitoken:
+            oclapiheaders['Authorization'] = 'Token ' + oclapitoken
+
+        # Fetch the zipped export from OCL
+        repo_export_url = '%sexport/' % (repo_version_url)
+        r = requests.get(repo_export_url, allow_redirects=True, headers=oclapiheaders)
+        r.raise_for_status()
+
+        # Decompress "export.json" from the zipfile in memory and return as a python dictionary
+        repo_export = None
+        export_string_handle = StringIO(r.content)
+        zipref = zipfile.ZipFile(export_string_handle, "r")
+        if 'export.json' in zipref.namelist():
+            repo_export = json.loads(zipref.read('export.json'))
+            zipref.close()
+        else:
+            zipref.close()
+            errmsg = 'ERROR: Invalid repository export for "%s": export.json not found in the export response.\n%s' % (
+                original_export_url, r.content)
+            self.vlog(1, errmsg)
+            raise Exception(errmsg)
+        return OclExport(repo_export)
+
+    @staticmethod
+    def load_latest_export(repo_url, oclapitoken=''):
+        """
+        Load latest export of the specified repository. Repo URL should be of the format:
+        https://api.openconceptlab.org/orgs/MyOrg/sources/MySource/
+        """
+        repo_id = OclExportFactory.get_latest_version_id(repo_url, oclapitoken=oclapitoken)
+        if repo_id:
+            repo_version_url = '%s%s/' % (repo_url, repo_id)
+            return OclExportFactory.load_export(repo_version_url, oclapitoken=oclapitoken)
+        else:
+            return None
+
+    @staticmethod
+    def get_latest_version_id(repo_url, oclapitoken=''):
+        """
+        Get the ID of the most recent released version of the specified repository
+        """
+
+        # Prepare the request headers
+        oclapiheaders = {'Content-Type': 'application/json'}
+        if oclapitoken:
+            oclapiheaders['Authorization'] = 'Token ' + oclapitoken
+
+        # Get the latest version ID
+        repo_latest_url = '%slatest/' % repo_url
+        r = requests.get(repo_latest_url, headers=oclapiheaders)
+        repo_version = r.json()
+        if repo_version and 'id' in repo_version:
+            return repo_version['id']
+        else:
+            raise OclUnknownResourceError(repo_url, 'Repository "%s" does not exist' % repo_url)
+
+    @staticmethod
+    def load_from_export_json_file(filename):
+        with open(filename) as input_file:
+            export_json = json.loads(input_file.read())
+            return OclExport(export_json)
+
+
+class OclExport(object):
+    """ Object to represent an OCL export """
+
+    def __init__(self, export_json=None, ocl_export=None):
+        self.set_export(export_json=export_json, ocl_export=ocl_export)
+
+    def set_export(self, export_json=None, ocl_export=None):
+        if export_json and ocl_export:
+            raise Exception('uhoh')
+        elif export_json:
+            self._export = export_json
+            self._concepts = {}
+            self._mappings = []
+        elif ocl_export:
+            if not isinstance(ocl_export, OclExport):
+                raise Exception('oh no!')
+            self._export = ocl_export._export
+            self._concepts = ocl_export._concepts
+            self._mappings = ocl_export._mappings
+        return None
+
+        for concept in self._export['concepts']:
+            self._concepts[concept['id']] = concept
+        self._mappings = self._export['mappings']
+
+    def get_concept_by_id(self, concept_id):
+        if concept_id in self._concepts:
+            return self._concepts[concept_id]
+        return None
+
+    def get_concept_by_uri(self, concept_uri):
+        for concept in self._concepts:
+            if concept['url'] == concept_url:
+                return concept
+        return None
+
+    def get_concepts(self, concept_class='', datatype='', concept_id='', concept_uri=''):
+        concepts = []
+        for concept in self._export['concepts']:
+            if (concept_id == concept['id'] or not concept_id) and \
+                (concept_class == concept['concept_class'] or not concept_class) and \
+                (datatype == concept['datatype'] or not datatype) and \
+                (concept_uri == concept['url'] or not concept_uri):
+                concepts.append(concept)
+        return concepts
+
+    def get_mappings(self, from_concept_uri='', to_concept_uri='', map_type=''):
+        mappings = []
+        for mapping in self._export['mappings']:
+            if (mapping['from_concept_url'] == from_concept_uri or not from_concept_uri) and \
+                (mapping['to_concept_url'] == to_concept_uri or not to_concept_uri) and \
+                (mapping['map_type'] == map_type or not map_type):
+                mappings.append(mapping)
+        return mappings
