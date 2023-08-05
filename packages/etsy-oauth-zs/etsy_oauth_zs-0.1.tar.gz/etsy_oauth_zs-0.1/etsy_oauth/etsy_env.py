b7eed0ede7fd81f4948a6ea23a9371fc54b34172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:31 +0000

    Add package etsy-oauth-zs/0.1/etsy_oauth_zs-0.1.tar.gz

---
+++ packages/etsy-oauth-zs/etsy_oauth_zs-0.1.tar.gz/etsy_oauth_zs-0.1/etsy_oauth/etsy_env.py
@@ -0,0 +1,12 @@
+class EtsyEnvSandbox:
+    request_token_url = "https://sandbox.openapi.etsy.com/v2/oauth/request_token"
+    access_token_url = "https://sandbox.openapi.etsy.com/v2/oauth/access_token"
+    signin_url = "https://www.etsy.com/oauth/signin"
+    api_url = "http://sandbox.openapi.etsy.com/v2"
+
+
+class EtsyEnvProduction:
+    request_token_url = "https://openapi.etsy.com/v2/oauth/request_token"
+    access_token_url = "https://openapi.etsy.com/v2/oauth/access_token"
+    signin_url = "https://www.etsy.com/oauth/signin"
+    api_url = "https://openapi.etsy.com/v2"
