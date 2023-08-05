c3b4fce35814d4b7dd91ffe9b8f4a7522435e2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package rpa/1.23.0/rpa-1.23.0.tar.gz

---
+++ packages/rpa/rpa-1.23.0.tar.gz/rpa-1.23.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='rpa',
+    version='1.23.0',
+    py_modules=['rpa'], install_requires=['tagui>=1.23.0'],
+    author='Ken Soh',
+    author_email='opensource@tebel.org',
+    license='Apache License 2.0',
+    url='https://github.com/tebelorg/RPA-Python',
+    description='RPA for Python is a Python package for RPA (robotic process automation)',
+    long_description='RPA for Python homepage - https://github.com/tebelorg/RPA-Python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
