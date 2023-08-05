4366bfb4e68d4bef3afa174d97c2e8e2620fe76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:56 +0000

    Add package ohmysportsfeedspy/2.1.0/ohmysportsfeedspy-2.1.0.zip

---
+++ packages/ohmysportsfeedspy/ohmysportsfeedspy-2.1.0.zip/ohmysportsfeedspy-2.1.0/ohmysportsfeedspy/__init__.py
@@ -0,0 +1,10 @@
+### Import MySportsFeeds class(es) upon initiation of the program
+from .MySportsFeeds_API import MySportsFeeds
+from .v1_0 import API_v1_0
+from .v1_1 import API_v1_1
+from .v1_2 import API_v1_2
+from .v2_0 import API_v2_0
+from .v2_1 import API_v2_1
+from pkg_resources import get_distribution
+
+__version__ = "2.1.0" #get_distribution('ohmysportsfeedspy').version
---
+++ packages/ohmysportsfeedspy/ohmysportsfeedspy-2.1.0.zip/ohmysportsfeedspy-2.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ohmysportsfeedspy',
+    packages=['ohmysportsfeedspy'],
+    version='2.1.0',
+    author = ['Brad Barkhouse', 'MySportsFeeds'],
+    author_email='brad.barkhouse@mysportsfeeds.com',
+    url='https://github.com/MySportsFeeds/mysportsfeeds-python',
+    license='MIT',
+    description='A Python wrapper for the MySportsFeeds Sports Data API'
+)
