6ce07ee1df13441f31340ebe6bd510ab2c5249bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package ccodegen/0.1/ccodegen-0.1.tar.gz

---
+++ packages/ccodegen/ccodegen-0.1.tar.gz/ccodegen-0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='ccodegen',
+    version='0.1',
+    description="C code generator",
+    author="Seungha Son",
+    author_email="linuxias@gmail.com",
+    license="MIT",
+    url="https://github.com/linuxias/C_CodeGen",
+    packages=setuptools.find_packages(),
+    keywords=["C", "Generator"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
