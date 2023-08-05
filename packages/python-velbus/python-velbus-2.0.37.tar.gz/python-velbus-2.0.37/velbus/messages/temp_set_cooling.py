34b470a6e0ef6660f072c026d89b95fc16dd1ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:43 +0000

    Add package python-velbus/2.0.37/python-velbus-2.0.37.tar.gz

---
+++ packages/python-velbus/python-velbus-2.0.37.tar.gz/python-velbus-2.0.37/velbus/messages/temp_set_cooling.py
@@ -0,0 +1,45 @@
+"""
+:author: Thomas Delaet <thomas@delaet.org>
+"""
+import json
+import logging
+from velbus.message import Message
+from velbus.command_registry import register_command
+
+COMMAND_CODE = 0xdf
+
+
+class TempSetCoolingMessage(Message):
+    """
+    send by:
+    received by: VMB4RYLD
+    """
+
+    def __init__(self, address=None):
+        Message.__init__(self)
+        self.set_defaults(address)
+
+    def populate(self, priority, address, rtr, data):
+        """
+        :return: None
+        """
+        self.needs_no_rtr(rtr)
+        self.set_attributes(priority, address, rtr)
+
+    def to_json(self):
+        """
+        :return: str
+        """
+        json_dict = self.to_json_basic()
+        return json.dumps(json_dict)
+
+    def data_to_binary(self):
+        """
+        :return: bytes
+        """
+        return bytes([
+            COMMAND_CODE,
+            0xaa
+        ])
+
+register_command(COMMAND_CODE, TempSetCoolingMessage)
