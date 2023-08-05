97b54582522fdf155fa9ba6df5712cfe71b39988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:15 +0000

    Add package cvtlib/0.8.2/cvtlib-0.8.2-py2.py3-none-any.whl

---
+++ packages/cvtlib/cvtlib-0.8.2-py2.py3-none-any.whl/cvtlib/files.py
@@ -0,0 +1,24 @@
+from os import path
+from glob import iglob
+from typing import Tuple
+
+
+def list_files(
+    root_path: str,
+    extensions: Tuple[str, ...],
+    recursive: bool = True
+):
+    file_paths = []
+    for ext in extensions:
+        root_path_ext = path.join(root_path, f'*{ext}')
+        file_paths.extend(iglob(root_path_ext, recursive=False))
+        if recursive:
+            root_path_ext = path.join(root_path, f'**/*{ext}')
+            file_paths.extend(iglob(root_path_ext, recursive=True))
+
+    return file_paths
+
+
+def relpath(file_path, child_path):
+    return path.relpath(path.join(path.dirname(file_path), child_path))
+
