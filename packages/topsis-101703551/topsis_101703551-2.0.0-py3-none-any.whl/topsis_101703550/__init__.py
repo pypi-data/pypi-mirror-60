edd5089639b7cd7752f1e98c35a18f084797313b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:55 +0000

    Add package topsis-101703551/2.0.0/topsis_101703551-2.0.0-py3-none-any.whl

---
+++ packages/topsis-101703551/topsis_101703551-2.0.0-py3-none-any.whl/topsis_101703550/__init__.py
@@ -0,0 +1,5 @@
+import sys
+import pandas as pd
+from .topsis_101703550 import *
+if __name__=="__main__":
+    main()
---
+++ packages/topsis-101703551/topsis_101703551-2.0.0-py3-none-any.whl/topsis_101703551/__init__.py
@@ -0,0 +1,5 @@
+import sys
+import pandas as pd
+from .topsis_101703551 import *
+if __name__=="__main__":
+    main()
