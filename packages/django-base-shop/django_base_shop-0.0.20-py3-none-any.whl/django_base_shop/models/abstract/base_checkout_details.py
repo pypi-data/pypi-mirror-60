b827c37ae489ee630b303f94050c69f93f6abee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:37 +0000

    Add package django-base-shop/0.0.20/django_base_shop-0.0.20-py3-none-any.whl

---
+++ packages/django-base-shop/django_base_shop-0.0.20-py3-none-any.whl/django_base_shop/models/abstract/base_checkout_details.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from django.db import models
+
+from ..concrete.address import Address
+from ..concrete.shipping_option import ShippingOption
+
+
+class BaseCheckoutDetails(models.Model):
+    class Meta:
+        abstract = True
+
+    customer_name = models.CharField(max_length=100)
+    customer_company = models.CharField(max_length=100, blank=True)
+    customer_email = models.EmailField()
+    customer_phone = models.CharField(max_length=100)
+    customer_coupon = models.CharField(max_length=100, blank=True)
+
+    shipping_selection = models.ForeignKey(
+        ShippingOption, null=True, on_delete=models.PROTECT
+    )
+
+    shipping_address = models.ForeignKey(
+        Address, related_name="+", on_delete=models.PROTECT
+    )
+
+    billing_address_same_as_shipping = models.BooleanField(
+        default=True, verbose_name="Billing same as shipping"
+    )
+
+    billing_name = models.CharField(max_length=100, blank=True)
+    billing_company = models.CharField(max_length=100, blank=True)
+    billing_address = models.ForeignKey(
+        Address, null=True, blank=True, related_name="+", on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        if self.customer_name:
+            return f"CheckoutDetails ({self.customer_name})"
+
+        return super().__str__()
