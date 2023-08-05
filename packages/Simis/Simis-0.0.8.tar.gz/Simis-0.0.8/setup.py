eb383841c9882925fae3cf5c3781ce1de5cd1ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:52 +0000

    Add package Simis/0.0.8/Simis-0.0.8.tar.gz

---
+++ packages/Simis/Simis-0.0.8.tar.gz/Simis-0.0.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="Simis",
+  version="0.0.8",
+  author="Simis AS",
+  author_email="support@simis.io",
+  description="A package for communicating with the Ashes wind turbine simulation software in order to run custom simulation code.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://www.simis.io",
+  packages=setuptools.find_packages(include=['PySide2', 'PySide2.*']),
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Programming Language :: Python :: 3",
+    "License :: Other/Proprietary License",
+    "Operating System :: Microsoft :: Windows"
+  ]
+)
