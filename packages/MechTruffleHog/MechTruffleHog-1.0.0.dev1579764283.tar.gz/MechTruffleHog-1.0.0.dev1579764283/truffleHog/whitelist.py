4f42eb8d659260fb141c314fe1e887c2b828768c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:20 +0000

    Add package MechTruffleHog/1.0.0.dev1579764283/MechTruffleHog-1.0.0.dev1579764283.tar.gz

---
+++ packages/MechTruffleHog/MechTruffleHog-1.0.0.dev1579764283.tar.gz/MechTruffleHog-1.0.0.dev1579764283/truffleHog/whitelist.py
@@ -0,0 +1,243 @@
+import json
+import hashlib
+import jsons
+import datetime
+from itertools import groupby
+from collections import Counter
+from termcolor import colored
+import colorama
+
+colorama.init()
+
+
+class WhitelistEntry:
+    def __init__(
+        self,
+        commit,
+        commitAuthor,
+        commitHash,
+        date,
+        path,
+        reason,
+        stringDetected,
+        acknowledged=False,
+        secretGuid=None,
+        confidence="High"
+    ):
+        self.commit = commit
+        self.commitAuthor = commitAuthor
+        self.commitHash = commitHash
+        self.date = date
+        self.path = path
+        self.reason = reason
+        self.stringDetected = stringDetected.lstrip("+-")
+        self.acknowledged = acknowledged
+
+        self.secretGuid = secretGuid
+        if secretGuid == None:
+            self.secretGuid = str(
+                hashlib.md5(
+                    (commitHash + str(path) + stringDetected).encode("utf-8")
+                ).hexdigest()
+            )
+        self.confidence = confidence
+        if self.reason == "High Entropy":
+            self.confidence = "Low"
+
+
+    def __repr__(self):
+        return f"Secret Instance GUID: {self.secretGuid}, String Detected:{self.stringDetected}"
+
+    def __eq__(self, other):
+        return self.secretGuid == other.secretGuid
+
+    def __hash__(self):
+        return int(self.secretGuid, 16)
+
+
+class WhitelistStatistics:
+    def __init__(self, whitelist_object, pipeline_mode):
+        self.whitelist_object = {
+            entry for entry in whitelist_object if entry.acknowledged == False
+        }
+
+        self.pipeline_mode = pipeline_mode
+
+    def top_secrets(self):
+        if self.pipeline_mode == True:
+            return "Secrets unavailable in pipeline mode."
+        counter = Counter([entry.stringDetected for entry in self.whitelist_object])
+        top_secrets = ""
+        for key, val in counter.most_common(10):
+            top_secrets += f"{'  '+key:<35}{val:>7}\n"
+        return top_secrets
+
+    def largest_files(self):
+        largest_files = ""
+        counter = Counter([entry.path for entry in self.whitelist_object])
+
+        for key, val in counter.most_common(10):
+            largest_files += f"{'  '+key:<35}{val:>7}\n"
+        return largest_files
+
+    def unique(self, attr):
+        return set([getattr(entry, attr) for entry in self.whitelist_object])
+
+    def count(self, attr):
+        return len([getattr(entry, attr) for entry in self.whitelist_object])
+
+    def breakdown(self):
+        reason_breakdown = ""
+        for given_reason in self.unique("reason"):
+            reason_breakdown += f"{'  '+given_reason:<35}{len([x for x in self.whitelist_object if x.reason == given_reason]):>7}\n"
+        return reason_breakdown
+
+    def to_dict(self):
+        return {
+            "Files": len(self.unique("path")),
+            "Total Strings": self.count("stringDetected"),
+            "Unique Strings": len(self.unique("stringDetected")),
+            "Categories": " ".join(self.breakdown().replace("\n", ",").split()),
+            "Top Files": " ".join(self.largest_files().replace("\n", ",").split()),
+        }
+
+    def to_dict_per_commit(self, repo, commit):
+        now = datetime.datetime.utcnow()
+        commit = repo.commit(commit)
+        commit_timestamp = datetime.datetime.utcfromtimestamp(commit.authored_date)
+        return {
+                    "detectionTimestamp": now.strftime('%Y-%m-%dT%H:%M:%S') + now.strftime('.%f')[:4] + 'Z',
+                    "repository": commit.repo.git_dir,
+                    "commit": commit.hexsha,
+                    "commitMessage": commit.message.strip('\n'),
+                    "commitTimestamp": commit_timestamp.strftime('%Y-%m-%dT%H:%M:%S') + commit_timestamp.strftime('.%f')[:4] + 'Z',
+                    "totalStrings": self.count("stringDetected"),
+                    "uniqueStrings": len(self.unique("stringDetected")),
+                    "findings": jsons.dump(self.whitelist_object)
+                } 
+
+    def __repr__(self):
+        message = (
+            f"Whitelist Statistics:\n"
+            f"{'  Files: ':<35}{len(self.unique('path')):>7}\n"
+            f"{'  Total Strings: ':<35}{self.count('stringDetected'):>7}\n"
+            f"{'  Unique Strings: ':<35}{len(self.unique('stringDetected')):>7}\n"
+            f"\nCategories:\n"
+            f"{self.breakdown()}"
+            f"\nTop Files:\n"
+            f"{self.largest_files()}\n"
+            f"\nMost Common Secrets:\n"
+            f"{self.top_secrets()}\n"
+        )
+        return message
+
+
+def curate_whitelist(outstanding_secrets):
+    whitelist_in_memory = read_whitelist_to_memory()
+    if not whitelist_in_memory:
+        print(f"Creating a new whitelist.json...")
+        write_whitelist_to_disk(outstanding_secrets)
+    else:
+        outstanding_secrets, whitelist_in_memory = reconcile_secrets(
+            outstanding_secrets, whitelist_in_memory
+        )
+
+        for entry in outstanding_secrets:
+            whitelist = add_to_whitelist(entry, whitelist_in_memory)
+
+        write_whitelist_to_disk(whitelist_in_memory)
+
+    return outstanding_secrets
+
+
+def write_whitelist_to_disk(whitelist_object):
+    try:
+        with open("whitelist.json", "w+") as whitelist:
+            whitelist_object = jsons.dump(
+                sorted(
+                    whitelist_object,
+                    key=lambda whitelist: whitelist.acknowledged,
+                    reverse=False,
+                )
+            )
+            json.dump(whitelist_object, whitelist, indent=4)
+    except Exception as e:
+        print(f"Unable to write to whitelist: {e}")
+
+
+def read_whitelist_to_memory():
+    try:
+        with open("whitelist.json", "r") as whitelist:
+            file_contents = json.load(whitelist)
+            in_memory_whitelist = set()
+            for entry in file_contents:
+                in_memory_whitelist.add(WhitelistEntry(**entry))
+        return in_memory_whitelist
+    except Exception as e:
+        print("Whitelist not found")
+        return False
+
+
+def add_to_whitelist(entry, whitelist):
+    if entry not in whitelist:
+        whitelist.add(entry)
+    return whitelist
+
+
+def reconcile_secrets(matches, whitelist):
+    for entry in whitelist.copy():
+        if entry not in matches:
+            # print(f"Can no longer find {entry.secretGuid}")
+            whitelist.remove(entry)
+            continue
+        if entry.acknowledged == True:
+            # print(f"Already acknowledged:\n{entry}. No action required.")
+            matches.remove(entry)
+    return matches, whitelist
+
+
+def remediate_secrets():
+    in_memory_whitelist = read_whitelist_to_memory()
+    if in_memory_whitelist:
+        counter = Counter(
+            [
+                entry.stringDetected
+                for entry in in_memory_whitelist
+                if entry.acknowledged == False
+            ]
+        )
+        for secret in counter:
+            # if "PRIVATE KEY" in secret:
+            #     # We can't automatically reconcile private keys
+            #     continue
+            classification = user_classify_secrets(secret)
+            update_secret(secret, classification, in_memory_whitelist)
+        write_whitelist_to_disk(in_memory_whitelist)
+    else:
+        return False
+
+
+def user_classify_secrets(secret):
+    secret = colored(secret, "yellow")
+    while True:
+        print()
+        prompt = input(f"{secret}: False Positive? (y/n)> ")
+        if prompt == "y":
+            return True
+        if prompt == "n":
+            return False
+
+
+def update_secret(secret, classification, whitelist):
+    for entry in whitelist:
+        if entry.stringDetected == secret:
+            entry.acknowledged = classification
+
+
+def whitelist_statistics(pipeline_mode=False):
+    in_memory_whitelist = read_whitelist_to_memory()
+    if in_memory_whitelist:
+        unique_secrets = WhitelistStatistics(in_memory_whitelist, pipeline_mode)
+        return unique_secrets
+    else:
+        return False
