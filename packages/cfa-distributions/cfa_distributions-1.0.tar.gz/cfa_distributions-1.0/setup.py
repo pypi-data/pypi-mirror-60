321ddc552bdd50a2b6501cce6c788e0dc816dd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:20 +0000

    Add package cfa-distributions/1.0/cfa_distributions-1.0.tar.gz

---
+++ packages/cfa-distributions/cfa_distributions-1.0.tar.gz/cfa_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cfa_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['cfa_distributions'],
+      author='Deric Atienza',
+      author_email='dericatienza@live.com.ph',
+      zip_safe=False)
