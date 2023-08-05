ad468abd7bb937ae34249e77d64b15a139233243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:57 +0000

    Add package teslacam-py/20.1.32/teslacam_py-20.1.32-py3-none-any.whl

---
+++ packages/teslacam-py/teslacam_py-20.1.32-py3-none-any.whl/teslacam/services/notification.py
@@ -0,0 +1,13 @@
+from teslacam.config import Configuration
+from teslacam.consts import NOTIFIERS
+
+class NotificationService:
+    def __init__(self, cfg: Configuration):
+        if cfg.notifier:
+            self.__notifier = NOTIFIERS[cfg.notifier](cfg)
+
+    def notify(self, msg: str):
+        if not self.__notifier:
+            return
+
+        self.__notifier.notify(msg)
