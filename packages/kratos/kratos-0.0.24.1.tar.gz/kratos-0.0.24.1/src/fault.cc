a39770aa392e2bd61c16164aa8e28d785e66cf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:22 +0000

    Add package kratos/0.0.24.1/kratos-0.0.24.1.tar.gz

---
+++ packages/kratos/kratos-0.0.24.1.tar.gz/kratos-0.0.24.1/src/fault.cc
@@ -0,0 +1,362 @@
+#include "fault.hh"
+
+#include <fstream>
+
+#include "eval.hh"
+#include "except.hh"
+#include "fmt/format.h"
+#include "graph.hh"
+#include "stmt.hh"
+#include "util.hh"
+
+using fmt::format;
+
+namespace kratos {
+
+void SimulationRun::add_simulation_state(const std::map<std::string, int64_t> &values) {
+    // need to parse the inputs and outputs
+    states_.emplace_back(std::make_unique<Simulator>(top_));
+    auto state = get_state(states_.size() - 1);
+    for (auto const &[name, value] : values) {
+        // we need to use dot notation to select from the hierarchy
+        // notice these names do not contain the "top" name, e.g. TOP for verilator
+        auto var = select(name);
+        if (!var) {
+            throw UserException(::format("Unable to parse {0}", name));
+        }
+        state->set(var, value, false);
+    }
+}
+
+void SimulationRun::mark_wrong_value(const std::string &name) {
+    if (states_.empty()) throw UserException("Simulation run is empty");
+    auto v = select(name);
+    auto v_base = const_cast<Var *>(v->get_var_root_parent());
+    auto index = states_.size() - 1;
+    wrong_value_[index].emplace(v_base);
+}
+
+void SimulationRun::add_simulation_coverage(const std::unordered_map<Stmt *, uint32_t> &coverage) {
+    // for now we are not interested in stmt counts yet
+    for (auto const &iter : coverage) {
+        coverage_.emplace(iter.first);
+    }
+}
+
+std::pair<Generator *, uint64_t> SimulationRun::select_gen(const std::vector<std::string> &tokens) {
+    Generator *gen = top_;
+    if (tokens[0] != gen->instance_name) return {nullptr, 1};
+    for (uint64_t index = 1; index < tokens.size(); index++) {
+        auto const &name = tokens[index];
+        if (!gen->has_child_generator(name)) {
+            return {gen, index};
+        } else {
+            gen = gen->get_child_generator(name);
+        }
+    }
+    return {gen, tokens.size()};
+}
+
+Var *SimulationRun::select(const std::string &name) {
+    auto tokens = string::get_tokens(name, ".");
+    auto [gen, index] = select_gen(tokens);
+    if (index >= tokens.size()) return nullptr;
+    if (!gen) return nullptr;
+    auto var_tokens = std::vector<std::string>(tokens.begin() + index, tokens.end());
+    // get var name, which has to be the first one
+    auto const &var_name = var_tokens[0];
+    auto var = gen->get_var(var_name).get();
+    if (!var) return nullptr;
+
+    if (var->is_packed()) {
+        throw InternalException("Packed struct not supported yet");
+    }
+    // get index, if any
+    std::vector<uint32_t> indices;
+    for (uint64_t i = 1; i < var_tokens.size(); i++) {
+        int v;
+        try {
+            v = std::stoi(var_tokens[i]);
+        } catch (std::invalid_argument &) {
+            return nullptr;
+        }
+        if (v < 0) throw UserException(::format("Unable to parse {0}", name));
+        // slice it
+        var = &(*var)[v];
+    }
+    return var;
+}
+
+Simulator *SimulationRun::get_state(uint32_t index) {
+    if (index < states_.size()) {
+        return states_[index].get();
+    }
+    return nullptr;
+}
+
+FaultAnalyzer::FaultAnalyzer(kratos::Generator *generator) : generator_(generator) {}
+
+void FaultAnalyzer::add_simulation_run(const std::shared_ptr<SimulationRun> &run) {
+    runs_.emplace_back(run);
+}
+
+template <typename T>
+T *cast(Stmt *stmt) {
+    auto r = dynamic_cast<T *>(stmt);
+    if (!r) throw InternalException("Unable to cast stmt type");
+    return r;
+}
+
+void compute_hit_stmts(Simulator *state, std::unordered_set<Stmt *> &result, Stmt *stmt) {
+    if (stmt->type() == StatementType::If) {
+        auto if_ = cast<IfStmt>(stmt);
+        auto cond = if_->predicate();
+        auto val = state->get(cond.get());
+        if (val && *val) {
+            compute_hit_stmts(state, result, if_->then_body().get());
+        } else {
+            compute_hit_stmts(state, result, if_->else_body().get());
+        }
+    } else if (stmt->type() == StatementType::Block) {
+        auto block = cast<StmtBlock>(stmt);
+        // normal sequential block and combinational block always gets executed
+        // as a result, we're only interested in the conditional statement block, i.e. scoped block
+        if (block->block_type() == StatementBlockType::Scope)
+            result.emplace(stmt);
+        for (auto const &s : *block) {
+            compute_hit_stmts(state, result, s.get());
+        }
+    } else if (stmt->type() == StatementType::FunctionalCall) {
+        auto func = cast<FunctionCallStmt>(stmt);
+        if (func->var()->func()->is_dpi()) {
+            // nothing
+        } else {
+            compute_hit_stmts(state, result, func->var()->func());
+        }
+    }
+}
+
+std::unordered_set<Stmt *> FaultAnalyzer::compute_coverage(uint32_t index) {
+    auto run = runs_[index].get();
+    std::unordered_set<Stmt *> result;
+    if (run->has_coverage()) {
+        auto const &cov = run->coverage();
+        for (auto const &stmt : cov) result.emplace(stmt);
+    } else {
+        auto num_states = run->num_states();
+        for (uint64_t i = 0; i < num_states; i++) {
+            auto state = run->get_state(i);
+            // given the state, we need to go through each generators
+            GeneratorGraph g(generator_);
+            auto generators = g.get_sorted_generators();
+            for (auto const &gen : generators) {
+                // need to calculate the sequential or combination block
+                auto stmts = gen->get_all_stmts();
+                for (auto const &stmt : stmts) {
+                    compute_hit_stmts(state, result, stmt.get());
+                }
+            }
+        }
+    }
+    coverage_maps_.emplace(index, result);
+    return result;
+}
+
+std::unordered_set<Stmt *> FaultAnalyzer::compute_fault_stmts_from_coverage() {
+    // compute coverage for each run
+    auto const num_runs_ = num_runs();
+    for (uint64_t i = 0; i < num_runs_; i++) {
+        if (coverage_maps_.find(i) == coverage_maps_.end()) {
+            compute_coverage(i);
+        }
+    }
+    std::map<Stmt *, uint32_t> correct_stmt_count;
+    std::map<Stmt *, uint32_t> wrong_stmt_count;
+    for (auto const &[run_index, coverage] : coverage_maps_) {
+        auto const &run = runs_[run_index];
+        bool has_wrong_value = run->has_wrong_value();
+        for (auto const &stmt : coverage) {
+            if (has_wrong_value) {
+                if (wrong_stmt_count.find(stmt) == wrong_stmt_count.end())
+                    wrong_stmt_count[stmt] = 0;
+                wrong_stmt_count[stmt] += 1;
+            } else {
+                if (correct_stmt_count.find(stmt) == correct_stmt_count.end())
+                    correct_stmt_count[stmt] = 0;
+                correct_stmt_count[stmt] += 1;
+            }
+        }
+    }
+    std::unordered_set<Stmt *> result;
+    // compute the sum
+    for (auto const &iter : wrong_stmt_count) {
+        auto const &stmt = iter.first;
+        if (correct_stmt_count.find(stmt) == correct_stmt_count.end()) result.emplace(stmt);
+    }
+    return result;
+}
+
+class CollectScopeStmtVisitor : public IRVisitor {
+public:
+
+    void visit(ScopedStmtBlock *stmt) override { add_stmt(stmt); }
+
+    [[nodiscard]] const std::map<std::pair<std::string, uint32_t>, Stmt *> &stmt_map() const {
+        return stmt_map_;
+    }
+
+private:
+    void add_stmt(Stmt *stmt) {
+        auto gen = stmt->generator_parent();
+        if (!gen->verilog_fn.empty()) {
+            auto filename = fs::basename(gen->verilog_fn);
+            stmt_map_.emplace(std::make_pair(filename, stmt->verilog_ln), stmt);
+        }
+    }
+
+    std::map<std::pair<std::string, uint32_t>, Stmt *> stmt_map_;
+};
+
+std::unordered_map<Stmt *, uint32_t> reverse_map_stmt(
+    Generator *top, const std::set<std::tuple<std::string, uint32_t, uint32_t>> &hit_counts) {
+    CollectScopeStmtVisitor visitor;
+    visitor.visit_root(top);
+    auto const &map = visitor.stmt_map();
+    std::unordered_map<Stmt *, uint32_t> stmts;
+    for (auto const &[fn, ln, count] : hit_counts) {
+        if (count == 0) continue;
+        auto filename = fs::basename(fn);
+        auto entry = std::make_pair(filename, ln);
+        if (map.find(entry) != map.end()) {
+            stmts.emplace(map.at(entry), count);
+        }
+    }
+    return stmts;
+}
+
+std::unordered_map<Stmt *, uint32_t> parse_verilator_coverage(Generator *top,
+                                                              const std::string &filename) {
+    if (!fs::exists(filename)) throw UserException(::format("{0} does not exist"));
+    std::ifstream file(filename);
+    std::set<std::tuple<std::string, uint32_t, uint32_t>> parse_result;
+    for (std::string line; std::getline(file, line);) {
+        if (line[0] != 'C') continue;
+        // parse the line based on key value pair
+        std::unordered_map<std::string, std::string> data;
+        std::string key;
+        std::string buffer;
+        // a tiny FSM to decode
+        // 0 -> nothing
+        // 1 -> key
+        // 2 -> value
+        uint32_t state = 0;
+        uint64_t index;
+        for (index = 2; index < line.size(); index++) {
+            auto c = line[index];
+            if (state == 0) {
+                if (c == 1) {
+                    // this is key
+                    state = 1;
+                }
+            } else if (state == 1) {
+                if (c == 2) {
+                    // key ends here
+                    key = buffer;
+                    buffer = "";
+                    state = 2;
+                } else {
+                    buffer += c;
+                }
+            } else {
+                if (c == '\'' || c == 1) {
+                    // end of value
+                    if (key.empty()) throw InternalException("Failed to parse" + line);
+                    data.emplace(key, buffer);
+                    key = "";
+                    buffer = "";
+                    if (c == 1) state = 1;
+                    if (c == '\'') break;
+                } else {
+                    buffer += c;
+                }
+            }
+        }
+        // parse the page type
+        if (data.find("page") == data.end()) throw UserException("Unable to parse " + line);
+        auto page_type = data.at("page");
+        const std::string line_cov_prefix = "v_line";
+        if (page_type.substr(0, line_cov_prefix.size()) != line_cov_prefix) continue;
+        if (index >= line.size() - 1) throw UserException("Unable to parse " + line);
+        // need to parse the count
+        std::string count_s = line.substr(index + 1);
+        string::trim(count_s);
+        auto count = static_cast<uint64_t>(std::stol(count_s));
+        // check on the filename and line number
+        if (data.find("f") == data.end() || data.find("l") == data.end())
+            throw UserException("Unable to parse" + line);
+        auto const &fn = data.at("f");
+        auto const ln = static_cast<uint32_t>(std::stoi(data.at("l")));
+        parse_result.emplace(std::make_tuple(fn, ln, count));
+    }
+
+    auto reverse_map = reverse_map_stmt(top, parse_result);
+    return reverse_map;
+}
+
+std::unordered_map<Stmt *, uint32_t> parse_icc_coverage(Generator *top,
+                                                        const std::string &filename) {
+    std::set<std::tuple<std::string, uint32_t, uint32_t>> parse_result;
+    if (!fs::exists(filename)) throw UserException(::format("{0} does not exist"));
+    std::ifstream file(filename);
+    // state 0: nothing
+    // state 1: searching for block header
+    // state 2: reading block coverage
+    uint32_t state = 0;
+    uint32_t line_count = 0;
+    std::string current_filename;
+    for (std::string line; std::getline(file, line); line_count++) {
+        // scan file by file
+        string::trim(line);
+        if (line[0] == '-') continue;  // skip the line section
+        if (state == 0) {
+            const std::string filename_tag = "File name:";
+            auto pos = line.find(filename_tag);
+            if (pos != std::string::npos) {
+                state = 1;
+                // extract out filename
+                current_filename = line.substr(pos + filename_tag.size());
+                string::trim(current_filename);
+            }
+        } else if (state == 1) {
+            const std::string count_tag = "Count  Block";
+            auto pos = line.find(count_tag);
+            if (pos != std::string::npos) {
+                state = 2;
+            }
+        } else {
+            if (line.empty()) {
+                // reset everything
+                current_filename = "";
+                state = 0;
+                continue;
+            }
+            auto tokens = string::get_tokens(line, " ");
+            if (tokens.size() < 6)
+                throw UserException(::format("Unable to parse line {0} at file {1}:{2}", line,
+                                             filename, line_count));
+            auto count_s = tokens[0];
+            auto line_num = tokens[2];
+            auto count = static_cast<uint32_t>(std::stoi(count_s));
+            auto fn = static_cast<uint32_t>(std::stoi(line_num));
+            if (current_filename.empty())
+                throw UserException(
+                    ::format("Filename is empty, Unable to parse line {0} at file {1}:{2}", line,
+                             filename, line_count));
+            parse_result.emplace(std::make_tuple(current_filename, fn, count));
+        }
+    }
+    auto reverse_map = reverse_map_stmt(top, parse_result);
+    return reverse_map;
+}
+
+}  // namespace kratos
---
+++ packages/kratos/kratos-0.0.24.1.tar.gz/kratos-0.0.24.1/src/tb.cc
@@ -0,0 +1,250 @@
+#include "tb.hh"
+
+#include <fmt/format.h>
+
+#include "codegen.hh"
+#include "except.hh"
+#include "pass.hh"
+#include "util.hh"
+
+using fmt::format;
+
+namespace kratos {
+
+AssertValueStmt::AssertValueStmt(const std::shared_ptr<Var> &expr) : assert_var_(expr.get()) {
+    // making sure that the expression has to be width 1
+    if (expr->width() != 1) throw VarException("Assert variable has to be width 1", {expr.get()});
+}
+
+AssertValueStmt::AssertValueStmt() : AssertValueStmt(constant(0, 1).as<Const>()) {}
+
+AssertPropertyStmt::AssertPropertyStmt(const std::shared_ptr<Property> &property)
+    : property_(property.get()) {}
+
+Sequence *Sequence::imply(const std::shared_ptr<Var> &var) {
+    next_ = std::make_shared<Sequence>(var);
+    next_->parent_ = this;
+    return next_.get();
+}
+
+std::string Sequence::wait_to_str() const {
+    if (wait_low_ == 0 && wait_high_ == 0)
+        return "";
+    else if (wait_low_ == wait_high_)
+        return ::format("##{0}", wait_low_);
+    else
+        return ::format("##[{0}:{1}]", wait_low_, wait_high_);
+}
+
+std::string Sequence::to_string() const {
+    auto wait_str = wait_to_str();
+    std::string seq;
+    if (wait_str.empty())
+        seq = var_->handle_name(true);
+    else
+        seq = ::format("{0} {1}", wait_str, var_->handle_name(true));
+    if (next_) {
+        auto next = next_->to_string();
+        seq = ::format("{0} |-> {1}", seq, next);
+    }
+    return seq;
+}
+
+Sequence *Sequence::wait(uint32_t from_num_clk, uint32_t to_num_clk) {
+    wait_low_ = from_num_clk;
+    wait_high_ = to_num_clk;
+    return this;
+}
+
+Property::Property(std::string property_name, std::shared_ptr<Sequence> sequence)
+    : property_name_(std::move(property_name)), sequence_(std::move(sequence)) {}
+
+void Property::edge(kratos::BlockEdgeType type, const std::shared_ptr<Var> &var) {
+    if (var->width() != 1) throw VarException("{0} should be width 1", {var.get()});
+    edge_ = {var.get(), type};
+}
+
+TestBench::TestBench(Context *context, const std::string &top_name) {
+    auto &gen = context->generator(top_name);
+    top_ = &gen;
+}
+
+std::shared_ptr<Property> TestBench::property(const std::string &property_name,
+                                              const std::shared_ptr<Sequence> &sequence) {
+    if (properties_.find(property_name) != properties_.end())
+        throw UserException(
+            ::format("Property {0} already exists in {1}", property_name, top_->name));
+    auto prop = std::make_shared<Property>(property_name, sequence);
+    properties_.emplace(property_name, prop);
+    return prop;
+}
+
+void TestBench::wire(const std::shared_ptr<Var> &var, const std::shared_ptr<Port> &port) {
+    top_->add_stmt(var->assign(port));
+}
+
+void TestBench::wire(std::shared_ptr<Port> &port1, std::shared_ptr<Port> &port2) {
+    top_->wire_ports(port1, port2);
+}
+
+void TestBench::wire(const std::shared_ptr<Var> &src, const std::shared_ptr<Var> &sink) {
+    top_->add_stmt(src->assign(sink));
+}
+
+class TestBenchCodeGen : public SystemVerilogCodeGen {
+public:
+    explicit TestBenchCodeGen(Generator *top) : SystemVerilogCodeGen(top), top_(top) {}
+
+private:
+    Generator *top_;
+    // override the default behavior
+    void dispatch_node(IRNode *node) override {
+        if (node->ir_node_kind() != IRNodeKind::StmtKind)
+            throw StmtException(::format("Cannot codegen non-statement node. Got {0}",
+                                         ast_type_to_string(node->ir_node_kind())),
+                                {node});
+        auto stmt_ptr = reinterpret_cast<Stmt *>(node);
+        if (stmt_ptr->type() == StatementType::Assert) {
+            auto assert_base = reinterpret_cast<AssertBase *>(stmt_ptr);
+            switch (assert_base->assert_type()) {
+                case AssertType::AssertValue:
+                    stmt_code(reinterpret_cast<AssertValueStmt *>(stmt_ptr));
+                    break;
+                case AssertType::AssertProperty:
+                    stmt_code(reinterpret_cast<AssertPropertyStmt *>(stmt_ptr));
+                    break;
+            }
+        } else {
+            SystemVerilogCodeGen::dispatch_node(node);
+        }
+    }
+
+    std::string var_name(Var *var) {
+        if (var->parent() == top_ || var->parent() == Const::const_gen())
+            return var->to_string();
+        else
+            return var->handle_name(true);
+    }
+
+protected:
+    void stmt_code(AssertValueStmt *stmt) {
+        stream_ << indent() << "assert (" << stmt->value()->handle_name(true) << ");"
+                << stream_.endl();
+    }
+
+    void stmt_code(AssertPropertyStmt *stmt) {
+        auto property = stmt->property();
+        stream_ << indent() << "property " << property->property_name() << ";" << stream_.endl();
+        increase_indent();
+        auto edge = property->edge();
+        auto seq = property->sequence();
+        // automatically determine the clock, only if it's safe to do so (only one clock in the
+        // design
+        if (!edge.first && seq->next()) {
+            // try to determine the clock
+            // it's concurrent property, we have to have a clock
+            auto generator = stmt->generator_parent();
+            // because it's a test bench, the variable doesn't have type
+            // we need to source for connected modules to see what they are connected to
+            auto children = generator->get_child_generators();
+            std::vector<Var *> clk_vars;
+            for (auto const &gen : children) {
+                auto clks = gen->get_ports(PortType::Clock);
+                for (auto const &clk_name : clks) {
+                    auto clk = gen->get_port(clk_name);
+                    auto source = clk->sources();
+                    for (auto const &assign : source) {
+                        auto src_var = assign->right();
+                        if (src_var->generator == generator) {
+                            if (src_var->type() == VarType::BaseCasted) {
+                                // only casted to clock
+                                auto casted = src_var->as<VarCasted>();
+                                if (casted->cast_type() == VarCastType::Clock)
+                                    clk_vars.emplace_back(src_var);
+                            }
+                        }
+                    }
+                }
+            }
+            if (clk_vars.size() == 1) {
+                edge.first = clk_vars[0];
+                edge.second = BlockEdgeType::Posedge;
+            } else {
+                // next is not null but edge is not set
+                throw StmtException(
+                    ::format("Clock edge not set for sequence {0}", seq->to_string()), {stmt});
+            }
+        }
+        if (edge.first) {
+            auto const &[var, type] = edge;
+            stream_ << indent()
+                    << ::format("@({0} {1}) ",
+                                type == BlockEdgeType::Posedge ? "posedge" : "negedge",
+                                var->handle_name(true));
+        }
+        stream_ << seq->to_string() << ";" << stream_.endl();
+        decrease_indent();
+        stream_ << indent() << "endproperty" << stream_.endl();
+
+        // put assert here
+        stream_ << indent() << "assert property (" << property->property_name() << ");"
+                << stream_.endl();
+    }
+
+    void stmt_code(AssignStmt *stmt) override {
+        if (stmt->assign_type() != AssignmentType::Blocking) {
+            throw StmtException("Test bench assignment as to be blocking", {stmt});
+        }
+        if ((stmt->left()->type() == VarType::PortIO && stmt->left()->generator != top_) ||
+            (stmt->right()->type() == VarType::PortIO && stmt->right()->generator != top_))
+            return;
+        std::string delay_str;
+        if (stmt->get_delay() > 0) {
+            int delay = stmt->get_delay();
+            delay_str = ::format("#{0} ", delay);
+        }
+        stream_ << indent() << delay_str << var_name(stmt->left()) << " = "
+                << var_name(stmt->right()) << ";" << stream_.endl();
+    }
+};
+
+uint32_t get_order(const std::shared_ptr<Stmt> &stmt) {
+    if (stmt->type() != StatementType::Block) return 0;
+    auto block = stmt->as<StmtBlock>();
+    if (block->block_type() != StatementBlockType::Initial) return 0;
+    return 1;
+}
+
+void sort_initials(Generator *top) {
+    auto const &stmts = top->get_all_stmts();
+    auto new_stmts = std::vector<std::shared_ptr<Stmt>>(stmts.begin(), stmts.end());
+
+    // move initialize to the last
+    std::sort(new_stmts.begin(), new_stmts.end(), [](const auto &left, const auto &right) {
+        return get_order(left) < get_order(right);
+    });
+
+    top->set_stmts(new_stmts);
+}
+
+std::string TestBench::codegen() {
+    // fix connections
+    fix_assignment_type(top_);
+    create_module_instantiation(top_);
+    verify_generator_connectivity(top_);
+
+    // sort initials in case we need to access internal signals
+    sort_initials(top_);
+
+    // need to convert properties into properties statement
+    for (auto const &iter : properties_) {
+        auto stmt = std::make_shared<AssertPropertyStmt>(iter.second);
+        top_->add_stmt(stmt);
+    }
+
+    // code gen the module top
+    TestBenchCodeGen code_gen(top_);
+    return code_gen.str();
+}
+
+}  // namespace kratos
