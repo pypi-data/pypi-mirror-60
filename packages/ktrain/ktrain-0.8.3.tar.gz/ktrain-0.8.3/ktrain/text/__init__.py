bf91702492fdc26d36da11fa720a58fdd6daad56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package ktrain/0.8.3/ktrain-0.8.3.tar.gz

---
+++ packages/ktrain/ktrain-0.8.3.tar.gz/ktrain-0.8.3/ktrain/text/__init__.py
@@ -0,0 +1,45 @@
+from .models import *
+from .data import *
+from .ner.data import entities_from_gmb, entities_from_conll2003, entities_from_txt
+from .ner.models import sequence_tagger, print_sequence_taggers
+from .eda import get_topic_model
+from .textutils import extract_filenames, load_text_files, filter_by_id
+from .preprocessor import Transformer
+
+__all__ = [
+           'text_classifier', 
+           'print_text_classifiers'
+           'texts_from_folder', 'texts_from_csv',
+           'entities_from_gmb',
+           'entities_from_conll2003',
+           'entities_from_txt',
+           'sequence_tagger',
+           'print_sequence_taggers',
+           'get_topic_model',
+           'extract_filenames', 
+           'load_text_files',
+           'Transformer'
+           ]
+
+
+def load_topic_model(fname):
+    """
+    Load saved TopicModel object
+    Args:
+        fname(str): base filename for all saved files
+    """
+    with open(fname+'.tm_vect', 'rb') as f:
+        vectorizer = pickle.load(f)
+    with open(fname+'.tm_model', 'rb') as f:
+        model = pickle.load(f)
+    with open(fname+'.tm_params', 'rb') as f:
+        params = pickle.load(f)
+    tm = get_topic_model(n_topics=params['n_topics'],
+                         n_features = params['n_features'],
+                         verbose = params['verbose'])
+    tm.model = model
+    tm.vectorizer = vectorizer
+    return tm
+
+
+
