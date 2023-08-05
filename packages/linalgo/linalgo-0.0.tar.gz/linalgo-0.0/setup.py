fc20a75113191f27a286cddc37dcec6b65eb1fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package linalgo/0.0/linalgo-0.0.tar.gz

---
+++ packages/linalgo/linalgo-0.0.tar.gz/linalgo-0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='linalgo',
+    packages=['linalgo'],
+    description=('Python library for Natural Language Processing supporting '
+                 'the open annotation standard'),
+    version='0.0',
+    author='Arnaud Rachez',
+    author_email='arnaud@linalgo.com',
+    download_url = 'https://github.com/DiscoverSDK/pysimplib/tarball/0.1',
+    requires=['numpy', 'scipy'],
+)
