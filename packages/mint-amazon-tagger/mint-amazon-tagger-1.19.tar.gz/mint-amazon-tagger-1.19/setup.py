1c75601e52b9df69c998b84442cf6e61d0b1f5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package mint-amazon-tagger/1.19/mint-amazon-tagger-1.19.tar.gz

---
+++ packages/mint-amazon-tagger/mint-amazon-tagger-1.19.tar.gz/mint-amazon-tagger-1.19/setup.py
@@ -0,0 +1,63 @@
+import os
+import setuptools
+from mintamazontagger import VERSION
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+class CleanCommand(setuptools.Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+
+setuptools.setup(
+    name="mint-amazon-tagger",
+    version=VERSION,
+    author="Jeff Prouty",
+    author_email="jeff.prouty@gmail.com",
+    description=("Fetches your Amazon order history and matching/tags your "
+                 "Mint transactions"),
+    keywords='amazon mint tagger transactions order history',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jprouty/mint-amazon-tagger",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Office/Business :: Financial",
+    ],
+    install_requires=[
+        'PyQt5',
+        'keyring',
+        'mock',
+        'mintapi>=1.40',
+        'outdated',
+        'progress',
+        'requests',
+        'readchar',
+        'selenium',
+        'selenium-requests',
+    ],
+    entry_points=dict(
+        console_scripts=[
+            'mint-amazon-tagger=mintamazontagger.gui:main',
+            'mint-amazon-tagger-cli=mintamazontagger.cli:main',
+        ],
+    ),
+    cmdclass={
+        'clean': CleanCommand,
+    },
+)
