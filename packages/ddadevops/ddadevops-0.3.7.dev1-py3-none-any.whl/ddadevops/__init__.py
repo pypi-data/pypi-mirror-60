eb5fdb97d44b7c4a64e3b40e10739b8dacfe24f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:29 +0000

    Add package ddadevops/0.3.7.dev1/ddadevops-0.3.7.dev1-py3-none-any.whl

---
+++ packages/ddadevops/ddadevops-0.3.7.dev1-py3-none-any.whl/ddadevops/__init__.py
@@ -0,0 +1,16 @@
+"""
+ddadevops provide tools to support builds combining gopass, 
+terraform, dda-pallet, aws & hetzner-cloud.
+
+"""
+
+from .build import init_project, stage, name, module, \
+    project_root_path, build_commons_path, build_target_path, \
+    initialize_target, tf_import_name, tf_import_resource
+from .meissa_build import meissa_init_project, hetzner_api_key
+from .credential import gopass_credential_from_env_path, gopass_credential_from_path
+from .dda_pallet import dda_write_target, dda_write_domain, dda_install, dda_configure
+from .terraform import tf_copy_common, tf_plan, tf_import, tf_apply, tf_output, tf_destroy, \
+    tf_read_output_json
+
+__version__ = "0.3.7.dev1"
