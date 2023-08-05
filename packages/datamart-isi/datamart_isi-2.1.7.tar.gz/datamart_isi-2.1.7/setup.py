48a347684aa4037948d899d5fb37c5b58fdec30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:41 +0000

    Add package datamart-isi/2.1.7/datamart_isi-2.1.7.tar.gz

---
+++ packages/datamart-isi/datamart_isi-2.1.7.tar.gz/datamart_isi-2.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    dependency_links = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            install_requires.append(re)
+
+setup(name='datamart_isi',
+      version='2.1.7',
+      description='USC ISI implementation of D3M Datamart API',
+      author='USC ISI',
+      url='https://github.com/usc-isi-i2/datamart-userend/tree/d3m',
+      packages=find_packages(),
+
+      install_requires=install_requires,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
