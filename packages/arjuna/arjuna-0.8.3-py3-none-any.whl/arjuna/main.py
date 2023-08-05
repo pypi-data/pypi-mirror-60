4bce73a3a2ab3854afa8d5d2bef0914c01c375e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package arjuna/0.8.3/arjuna-0.8.3-py3-none-any.whl

---
+++ packages/arjuna/arjuna-0.8.3-py3-none-any.whl/arjuna/main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+
+
+def main(args=None):
+    try:
+        import signal
+        import sys
+        def signal_handler(sig, frame):
+                print('Exiting...')
+                sys.exit(0)
+        signal.signal(signal.SIGINT, signal_handler)
+        from arjuna import Arjuna
+        Arjuna.launch(args and args or sys.argv)
+    except Exception as e:
+        # The following sleep is to accommodate a common IDE issue of
+        # interspersing main exception with console output.
+        time.sleep(0.5)
+        msg = '''
+    {0}
+    Sorry. Looks like this is an error Arjuna couldn't handle.
+    Create a bug report on GitHub: https://github.com/rahul-verma/arjuna
+    {0}
+
+    Message: {1}
+        '''.format("-" * 70, str(e))
+        print(msg)
+        import traceback
+        print(traceback.format_exc())
