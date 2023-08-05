66b23fecba9fc3d4d6ee3b06bcbb1638b8e35f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:26 +0000

    Add package pybliotecario/1.3/pybliotecario-1.3.tar.gz

---
+++ packages/pybliotecario/pybliotecario-1.3.tar.gz/pybliotecario-1.3/setup.py
@@ -0,0 +1,52 @@
+import pathlib
+from setuptools import setup, find_packages
+from sys import argv
+
+if len(argv) > 1 and argv[1] in ('develop', 'install'):
+    import socket
+# When installing with setup.py in develop/install mode
+# use hostname as the name of the script
+# with the first letter uppercased
+    hostname = socket.gethostname()
+    pybliotecario_name = hostname.capitalize()
+else:
+    pybliotecario_name = "pybliotecario"
+
+# Readup the readme
+README = (pathlib.Path(__file__).parent / "readme.md").read_text()
+setup(
+    name=pybliotecario_name,
+    version="1.3",
+
+    author="Scarlehoff",
+    author_email="juacrumar@lairen.eu",
+    url="https://github.com/scarlehoff/pybliotecario",
+
+    description="Personal telegram bot to interact between your Telegram account and your computer",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="GNU GPLv3",
+
+    package_dir = {'':'src'},
+    packages=find_packages('src'),
+
+    install_requires=[
+        'arxiv',
+        'pyowm',
+        'psutil',
+        'wikipedia',
+    ],
+
+    entry_points = {'console_scripts':
+                    ['{0} = pybliotecario.pybliotecario:main'.format(pybliotecario_name),]
+                    },
+)
+
+print("""
+
+##############
+Installed pybliotecario as {0}
+
+IMPORTANT: Don't forget to run
+      ~$ {0} --init
+for proper configuration of the pybliotecario""".format(pybliotecario_name))
