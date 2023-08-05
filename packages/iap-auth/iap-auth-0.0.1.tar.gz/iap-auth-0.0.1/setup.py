fb332bd6d1a313eafc853708b748b284a0b0b0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package iap-auth/0.0.1/iap-auth-0.0.1.tar.gz

---
+++ packages/iap-auth/iap-auth-0.0.1.tar.gz/iap-auth-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='iap-auth',
+    version='0.0.1',
+    description='Perform authentication for Google Cloud Identity Aware Proxy from a service account',
+    url='https://github.com/lmtani/iap-auth',
+    author='Lucas Taniguti',
+    author_email='ltaniguti@gmail.com',
+    license='unlicense',
+    packages=['iap_auth'],
+    install_requires=['requests_toolbelt', 'google-auth-oauthlib'],
+    zip_safe=False
+)
