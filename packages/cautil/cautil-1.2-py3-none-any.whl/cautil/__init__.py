0bc277a7928021cc5de3bfba8391120d97cf9958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:00 +0000

    Add package cautil/1.2/cautil-1.2-py3-none-any.whl

---
+++ packages/cautil/cautil-1.2-py3-none-any.whl/cautil/__init__.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta, abstractmethod
+import importlib
+config = {}
+
+class CAUtil:
+    def __init__(self):
+        self.config = config
+
+    def init(self):
+        print(config)
+        return "a"
+
+class EmailUtilInterface(CAUtil):
+    def send(self):
+        return None
+
+def get_resource(resource_name):
+    cls_name = config[resource_name]['vendor']
+    cls = importlib.import_module('cautil.utils.{}.{}'.format(resource_name,cls_name))
+    cls = getattr(cls, cls_name)
+    obj = cls()
+    return obj
+
+def cautil_init(config_file_path):
+    print(config_file_path)
+    config['Email'] = {'vendor':'SendInBlue'}
+    return None
