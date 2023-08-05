50ff642b2ef0449ca8a421f69336a62d3a426594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:28 +0000

    Add package adcom/1.2/adcom-1.2.tar.gz

---
+++ packages/adcom/adcom-1.2.tar.gz/adcom-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='adcom',
+    version='1.2',
+    packages=[''],
+    url='https://github.com/KvaksMan/adcom',
+    license='Apache 2.0',
+    author='KvaksManYT',
+    author_email='kvaksman225@gmail.com',
+    description='The additional commands to Python!'
+)
