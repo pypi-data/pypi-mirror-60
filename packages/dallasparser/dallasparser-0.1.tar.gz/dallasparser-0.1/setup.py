7dc988bf58665b64eeefb711a1e347bd3b323418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:39 +0000

    Add package dallasparser/0.1/dallasparser-0.1.tar.gz

---
+++ packages/dallasparser/dallasparser-0.1.tar.gz/dallasparser-0.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import Command, find_packages, setup
+import os
+
+_VERSION = '0.1'
+
+_CLASSIFIERS = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+]
+
+setup(
+    name='dallasparser',
+    version=_VERSION,
+    install_requires=[
+        r for r in open('requirements.txt', 'r').read().split('\n') if r
+    ],
+    description='TX Dallas Criminal Case Parser',
+    keywords='texas dallas criminal court parser',
+    url='https://github.com/jaycatsby/tx_dallas_court_parser',
+    author='Jay Choi',
+    author_email='nobeedee@gmail.com',
+    license='MIT',
+    packages=find_packages(include=['dallasparser', 'dallasparser.*']),
+    entry_points={
+        'console_scripts': ['dallasparser=dallasparser.cli:main'],
+    },
+    python_requires='>=3.6',
+    classifiers=_CLASSIFIERS,
+)
