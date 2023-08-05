5be197957303073dadaad377a363b5452292b712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:36 +0000

    Add package lite-linter/0.0.4/lite-linter-0.0.4.tar.gz

---
+++ packages/lite-linter/lite-linter-0.0.4.tar.gz/lite-linter-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import os
+
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setuptools.setup(
+    name='lite-linter',
+    version='0.0.4',
+    author="Jan Faracik",
+    author_email="",
+    description="Linting for LITE",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uktrade/lite-linter",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'lint = linter.lint:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
