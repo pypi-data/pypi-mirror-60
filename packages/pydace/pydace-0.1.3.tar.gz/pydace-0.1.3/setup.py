ccb4e71b5e0072019db31305c3e16c61277e7d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:24 +0000

    Add package pydace/0.1.3/pydace-0.1.3.tar.gz

---
+++ packages/pydace/pydace-0.1.3.tar.gz/pydace-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+PACKAGE_NAME = 'pydace'
+VERSION = '0.1.3'
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description='Design and Analysis of Computational Experiments as python toolbox.',
+      author='Felipe Souza Lima',
+      author_email='felipe.lima@eq.ufcg.edu.br',
+      url='https://github.com/feslima/pydace',
+      license='Apache License 2.0',
+      packages=find_packages(where='src', exclude=['tests']),
+      package_dir={'': 'src'},
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords="surrogate, metamodel",
+      install_requires=['numpy>=1.16', 'scipy>=1.2.0', 'pyDOE2'],
+      python_requires='>=3.5',
+      classifiers=[
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering",
+      ]
+      )
