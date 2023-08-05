6702e6f789127e77f803e6af15a3f5187ae4ad77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:23 +0000

    Add package pyoidc-redis-session-backend/1.0.1/pyoidc-redis-session-backend-1.0.1.tar.gz

---
+++ packages/pyoidc-redis-session-backend/pyoidc-redis-session-backend-1.0.1.tar.gz/pyoidc-redis-session-backend-1.0.1/pyoidc_redis_session_backend/__init__.py
@@ -0,0 +1,69 @@
+import redis
+import jsonpickle
+import base64
+from jsonpickle.handlers import BaseHandler
+from oic.utils.session_backend import AuthnEvent, SessionBackend
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
+from typing import cast
+from Cryptodome.PublicKey.RSA import RsaKey, import_key
+
+class RSAKeyHandler(BaseHandler):
+    def flatten(self, obj: RsaKey, data):
+        data["rsa_key"] = base64.b64encode(obj.export_key()).decode("utf-8")
+        return data
+
+    def restore(self, obj):
+        return import_key(base64.b64decode(obj["rsa_key"]))
+
+jsonpickle.register(RsaKey, RSAKeyHandler)
+
+class RedisSessionBackend(SessionBackend):
+    """
+    Redis session backend
+    """
+
+    def __init__(self, redis_instance: redis.Redis, **args):
+        """Create the storage."""
+        self.r = redis_instance
+
+    def __setitem__(self, key: str, value: Dict[str, Union[str, bool]]) -> None:
+        """Store the session info in the storage."""
+        self.r.set(key, jsonpickle.encode(value))
+
+    def __getitem__(self, key: str) -> Dict[str, Union[str, bool]]:
+        """Retrieve session information based on session id."""
+        data = self.r.get(key)
+        if(data == None):
+            raise KeyError # Makes __getItem__ handle like Python dict
+        return jsonpickle.decode(data.decode("utf-8"))
+
+    def __delitem__(self, key: str) -> None:
+        """Delete the session info."""
+        self.r.delete(key)
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.r.keys()
+
+    def get_by_sub(self, sub: str) -> List[str]:
+        """Return session ids based on sub."""
+        return [
+            key for key in self.r.keys() if jsonpickle.loads(self.r.get(key).decode("utf-8")).get("sub") == sub
+        ]
+
+    def get_by_uid(self, uid: str) -> List[str]:
+        """Return session ids based on uid."""
+        return [
+            key
+            for key in self.r.keys()
+            if AuthnEvent.from_json(self.r.get(key).decode("utf-8"))["authn_event"].uid == uid
+        ]
+
+    def get(self, attr: str, val: str) -> List[str]:
+        """Return session ids based on attribute name and value."""
+        return [
+            key for key in self.r.keys() if jsonpickle.loads(self.r.get(key).decode("utf-8")).get(attr) == val
+        ]
---
+++ packages/pyoidc-redis-session-backend/pyoidc-redis-session-backend-1.0.1.tar.gz/pyoidc-redis-session-backend-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyoidc-redis-session-backend",
+    version="1.0.1",
+    author="alehuo",
+    author_email="aleksi.huotala@helsinki.fi",
+    description="Redis-based session storage for oic library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alehuo/pyoidc-redis-session-backend",
+    packages=['pyoidc_redis_session_backend'],
+    py_modules=['pyoidc_redis_session_backend.RedisSessionBackend'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    install_requires=['oic', 'jsonpickle', 'redis', 'pycryptodome', 'pycryptodomex'],
+    python_requires='>=3.6',
+)
