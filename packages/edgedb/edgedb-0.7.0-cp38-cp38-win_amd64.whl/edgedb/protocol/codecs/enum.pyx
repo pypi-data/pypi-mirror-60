ce70cb94cd4d373f29c942520746d706db461517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:27 +0000

    Add package edgedb/0.7.0/edgedb-0.7.0-cp38-cp38-win_amd64.whl

---
+++ packages/edgedb/edgedb-0.7.0-cp38-cp38-win_amd64.whl/edgedb/protocol/codecs/enum.pyx
@@ -0,0 +1,39 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2016-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+@cython.final
+cdef class EnumCodec(BaseCodec):
+
+    cdef decode(self, FRBuffer *buf):
+        label = pgproto.text_decode(DEFAULT_CODEC_CONTEXT, buf)
+        return datatypes.EnumValue(self.descriptor, label)
+
+    @staticmethod
+    cdef BaseCodec new(bytes tid, tuple enum_labels):
+        cdef:
+            EnumCodec codec
+
+        codec = EnumCodec.__new__(EnumCodec)
+
+        codec.tid = tid
+        codec.name = 'Enum'
+        codec.descriptor = datatypes.EnumDescriptor(
+            pgproto.UUID(tid), enum_labels)
+
+        return codec
