3e1816b4abdf200dccd2d6719706ea0331f1b47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:28 +0000

    Add package tarjinja/0.0.5/tarjinja-0.0.5-py3-none-any.whl

---
+++ packages/tarjinja/tarjinja-0.0.5-py3-none-any.whl/tarjinja/jinjafilter.py
@@ -0,0 +1,29 @@
+from typing import List
+from jinja2 import Environment
+from .iface import Filter
+
+
+class JinjaFilter(Filter):
+    def __init__(self, **kwargs):
+        self.env = Environment(keep_trailing_newline=True, **kwargs)
+        self.addfilter("brace", self.brace)
+
+    def addfilter(self, name, fn):
+        self.env.filters[name] = fn
+
+    def brace(self, vals):
+        if isinstance(vals, (tuple, list)):
+            vals = [self.brace(x) for x in vals]
+            return "{" + ",".join(vals) + "}"
+        return str(vals)
+
+    def render(self, s: str, vals: dict) -> str:
+        tmpl = self.env.from_string(s)
+        return tmpl.render(**vals)
+
+    def var_names(self, s: str) -> List[str]:
+        res = set()
+        for b in self.env.parse(source=s).body:
+            res.update([x.name for x in filter(
+                lambda f: f.__class__.__name__ == "Name", b.nodes)])
+        return res
