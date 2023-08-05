75e98d26a745e75f417a637828fb7a26bd863360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:06 +0000

    Add package django-twilio/0.12.0/django_twilio-0.12.0-py3-none-any.whl

---
+++ packages/django-twilio/django_twilio-0.12.0-py3-none-any.whl/test_project/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^test_app/', include('test_project.test_app.urls')),
+]
+
