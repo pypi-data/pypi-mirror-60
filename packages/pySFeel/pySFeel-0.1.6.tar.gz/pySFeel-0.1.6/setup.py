712cfd6d5fb0208ae8072c1c4d5328b45a7a5f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:08 +0000

    Add package pySFeel/0.1.6/pySFeel-0.1.6.tar.gz

---
+++ packages/pySFeel/pySFeel-0.1.6.tar.gz/pySFeel-0.1.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pySFeel',
+    version='0.1.6',
+    author='Russell McDonell',
+    author_email='russell.mcdonell@c-cost.com',
+    description='An implementation of S-FEEL using SLY (lex and yacc for Python3)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/russell.mcdonell/pySFeel',
+    package_dir={'': '.'},
+    py_modules=['pySFeel'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['sly','datetime', 'python-dateutil', 'statistics'],
+)
+
