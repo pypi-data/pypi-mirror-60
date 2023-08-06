cd5b995bfa92aa10f204effeab7d7890619253f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:20 +0000

    Add package blinkparse/0.0.1/blinkparse-0.0.1-py3-none-any.whl

---
+++ packages/blinkparse/blinkparse-0.0.1-py3-none-any.whl/blinkparse/command.py
@@ -0,0 +1,24 @@
+class Command:
+    def __init__(self, name, args=[]):
+        self.name = name
+        self.args = args
+
+    def check(self, inputCommand, inputArgs):
+        if inputCommand == self.name:
+            inputArgs.pop(0)
+            commandArgs = {}
+            for i, commandArg in enumerate(self.args):
+                try:
+                    value = inputArgs[0]
+                except IndexError:
+                    if commandArg.required:
+                        raise ValueError(f'Argument {commandArg.name} is required')
+                    else:
+                        continue
+                if commandArg.required or value[0] != '-':
+                    if commandArg.options is not None and value not in commandArg.options:
+                        raise ValueError(f'{value} is not a valid argument option. The valid options are {commandArg.options}')
+                    inputArgs.pop(0)
+                    commandArgs[commandArg.name] = value
+            return commandArgs
+        return None
