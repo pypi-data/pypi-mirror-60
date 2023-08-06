d91c1c5c3509a888dbc8f14406184e12acf972bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:11 +0000

    Add package Airport-Monitor/0.2/Airport-Monitor-0.2.tar.gz

---
+++ packages/Airport-Monitor/Airport-Monitor-0.2.tar.gz/Airport-Monitor-0.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'Airport-Monitor',         # How you named your package folder (MyLib)
+  packages = ['Airport-Monitor'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'monitor the weather condition of airports in US',   # Give a short description about your library
+  author = 'kZ',                   # Type in your name
+  author_email = 'robertmorrislike@gmail.com',      # Type in your E-Mail
+  ##download_url = 'https://github.com/h4x0rMadness/BUEC500_api-design-private/archive/0.3.zip',    # I explain this later on
+  keywords = ['intime', 'airports', 'weather'],   # Keywords that define your package best
+  install_requires=[
+            'get_data',
+            'requests',
+            'json',
+            'requests',
+            'io',
+            'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
