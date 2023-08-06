a84d3e27681d261c2019c9e737610208108990e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:50 +0000

    Add package image-croquis/0.0.5/image_croquis-0.0.5.tar.gz

---
+++ packages/image-croquis/image_croquis-0.0.5.tar.gz/image_croquis-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='image_croquis',
+    url='https://github.com/nagavenkateshgavini/image_croquis',
+    author='Naga Venkatesh Gavini',
+    author_email='',
+    # Needed to actually package something
+    packages=['image_croquis'],
+    # Needed for dependencies
+    install_requires=['opencv-python'],
+    # *strongly* suggested for sharing
+    version='0.0.5',
+    # The license can be anything you like
+    license='MIT',
+    description='An example of a python package with a basic use case',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+    entry_points = {
+        'console_scripts':[
+            'image_croquis = image_croquis.image_croquis:main',
+        ]
+    }
+)
