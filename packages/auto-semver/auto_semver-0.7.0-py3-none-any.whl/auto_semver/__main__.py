7b68bd5cb54a19df21b576641a94818034f3bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:16 +0000

    Add package auto-semver/0.7.0/auto_semver-0.7.0-py3-none-any.whl

---
+++ packages/auto-semver/auto_semver-0.7.0-py3-none-any.whl/auto_semver/__main__.py
@@ -0,0 +1,19 @@
+import sys
+
+from auto_semver.cli import parse_cli_arguments
+from auto_semver.git import GitTagSource
+from auto_semver.auto_semver import AutoSemver
+
+
+def main():
+    args = parse_cli_arguments()
+
+    # TODO: Do some checking to determine which source to use.
+    # For now, the only one we support is the GitTagSource
+    git_semver_list = GitTagSource(args.use_local).get_semver_list()
+    a = AutoSemver(git_semver_list, args.value, args.print_highest)
+    a.auto_increment_semver()
+
+
+if __name__ == "__main__":
+    main()
