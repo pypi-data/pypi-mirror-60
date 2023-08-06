38a1ffc2ce39082c468af6802feea0625f30dd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:51 +0000

    Add package sightseer/1.1.0/sightseer-1.1.0.tar.gz

---
+++ packages/sightseer/sightseer-1.1.0.tar.gz/sightseer-1.1.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+	long_description = f.read()
+
+setup(
+  name="sightseer",
+  version="1.1.0",
+  description="State-of-the-art Computer Vision and Object Detection for TensorFlow.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/rish-16/sight',
+  download_url="https://github.com/rish-16/sight/archive/1.0.0.tar.gz",
+  author="Rishabh Anand",
+  author_email="mail.rishabh.anand@gmail.com",
+  license="ASF",
+  packages=["sightseer"],
+  zip_safe=False
+)
