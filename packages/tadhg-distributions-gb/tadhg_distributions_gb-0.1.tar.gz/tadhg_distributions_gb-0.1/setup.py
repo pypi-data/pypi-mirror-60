29923088be8a7ebf6b55fa1fc7b4def35cb212e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:26 +0000

    Add package tadhg-distributions-gb/0.1/tadhg_distributions_gb-0.1.tar.gz

---
+++ packages/tadhg-distributions-gb/tadhg_distributions_gb-0.1.tar.gz/tadhg_distributions_gb-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tadhg_distributions_gb',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tadhg_distributions_gb'],
+      zip_safe=False)
