7094a83d97bf8415ff3842066fbe330efaeac575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:07 +0000

    Add package climateserv/0.0.11/climateserv-0.0.11.tar.gz

---
+++ packages/climateserv/climateserv-0.0.11.tar.gz/climateserv-0.0.11/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="climateserv", # Replace with your own username
+    version="0.0.11",
+    author="Billy Ashmall",
+    author_email="billy.ashmall@nasa.gov",
+    description="This is a package to access the ClimateSERV API](https://climateserv.servirglobal.net/) ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/servir/ClimateSERVpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
