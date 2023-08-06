f247c02ec255184b3a51ebd4e8a322a4047bb1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:15 +0000

    Add package deepracer-vehicle-api/0.0.9/deepracer_vehicle_api-0.0.9-py3-none-any.whl

---
+++ packages/deepracer-vehicle-api/deepracer_vehicle_api-0.0.9-py3-none-any.whl/tests/test_1.py
@@ -0,0 +1,18 @@
+
+import unittest
+import os
+import requests
+
+class TestPipreqs(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
