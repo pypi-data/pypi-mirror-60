c5f56a450c4a92da32c4d970afb81305f4072011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:15 +0000

    Add package vim-kernel/0.0.2/vim_kernel-0.0.2.tar.gz

---
+++ packages/vim-kernel/vim_kernel-0.0.2.tar.gz/vim_kernel-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vim_kernel']
+
+package_data = \
+{'': ['*'], 'vim_kernel': ['autoload/*']}
+
+install_requires = \
+['notebook>=6.0.3,<7.0.0']
+
+setup_kwargs = {
+    'name': 'vim-kernel',
+    'version': '0.0.2',
+    'description': 'A Jupyter kernel for Vim script',
+    'long_description': None,
+    'author': 'mattn',
+    'author_email': 'mattn.jp@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
