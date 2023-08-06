46e3bfb995648092a4c3a8a5c1459b4a8365001a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:46 +0000

    Add package tlacli/0.0.1/tlacli-0.0.1-py3-none-any.whl

---
+++ packages/tlacli/tlacli-0.0.1-py3-none-any.whl/tlacli/tlacli.py
@@ -0,0 +1,23 @@
+import argparse
+
+from tlacli.tools import tlc, pluscal
+
+
+
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(help="Which TLA+ tool to run")
+
+tlc.setup(subparsers)
+pluscal.setup(subparsers)
+
+def main():
+    args = parser.parse_args()
+    args.run(args)
+
+if __name__ == "__main__":
+    
+    """Each TLA+ tool has its own parser, which also handles running the command.
+    If you're looking for the model checker, you want tools/tlc.py"""
+    args = parser.parse_args()
+    args.run(args)
