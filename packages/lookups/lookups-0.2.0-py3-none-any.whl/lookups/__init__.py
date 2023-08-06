eda1e4e19fde3b6d426c98efcf0d3b250a072e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:26 +0000

    Add package lookups/0.2.0/lookups-0.2.0-py3-none-any.whl

---
+++ packages/lookups/lookups-0.2.0-py3-none-any.whl/lookups/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Contributors as noted in the AUTHORS file
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# System imports
+
+# Third-party imports
+
+# Local imports
+from .lookup import (
+    Lookup, Result, Item,
+    LookupProvider,
+)
+from .lookups import singleton, fixed
+from .generic_lookup import GenericLookup, AbstractLookup
+from .instance_content import InstanceContent, Convertor
+from .__about__ import (
+    __title__, __summary__, __uri__, __version__,
+    __author__, __email__, __license__, __copyright__,
+)
+
+__all__ = [
+    'Lookup', 'Result', 'Item',
+    'LookupProvider',
+    'GenericLookup', 'AbstractLookup', 'InstanceContent', 'Convertor',
+    'singleton', 'fixed',
+
+    '__title__', '__summary__', '__uri__', '__version__',
+    '__author__', '__email__', '__license__', '__copyright__',
+]
