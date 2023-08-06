080b793575d1351f8e1900daaff8ab348fa472e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:11 +0000

    Add package avatar2/1.3.1/avatar2-1.3.1.tar.gz

---
+++ packages/avatar2/avatar2-1.3.1.tar.gz/avatar2-1.3.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from sys import version_info
+
+
+setup(
+    name='avatar2',
+    version='1.3.1',
+    packages=['avatar2',
+              'avatar2/archs',
+              'avatar2/targets',
+              'avatar2/protocols',
+              'avatar2/peripherals',
+              'avatar2/plugins',
+              'avatar2/plugins/arm',
+              'avatar2/installer'
+              ],
+    install_requires=[
+        'pygdbmi==0.9.0.2;python_version<"3.5"',
+        'pygdbmi;python_version>="3.5"',
+        'intervaltree',
+        'posix_ipc>=1.0.0',
+        'capstone>=3.0.4',
+        'keystone-engine',
+        'parse',
+        'configparser',
+        'npyscreen',
+        'enum34;python_version<"3.4"',
+        'unicorn',
+        'bitstring',
+	'pylink-square',
+    ],
+    url='https://github.com/avatartwo/avatar2',
+    description='A Dynamic Multi-Target Orchestration Framework',
+    maintainer='Marius Muench',
+    maintainer_email='marius.muench@eurecom.fr'
+)
