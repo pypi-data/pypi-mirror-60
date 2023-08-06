3277a300c162fd7b15ce43ab60069bbc68b5f58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:23 +0000

    Add package nester-another/1.3.0/nester-another-1.3.0.tar.gz

---
+++ packages/nester-another/nester-another-1.3.0.tar.gz/nester-another-1.3.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+#from setuptools import setup
+
+
+setup(
+    name = 'nester-another',
+    version = '1.3.0',
+    py_modules = ['nester-another'],
+    author =    'Leoncio',
+    author_email= 'leonciocast@gmail.com',
+    url    =  'http://www.headfirstlabs.com'
+    
+    
+)
