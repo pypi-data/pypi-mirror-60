8c84dcb4a2491967a5781263a8aff2c34bfc9c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:24 +0000

    Add package players-amp/2.33/players_amp-2.33.tar.gz

---
+++ packages/players-amp/players_amp-2.33.tar.gz/players_amp-2.33/README.md
@@ -0,0 +1,9 @@
+# player_amp
+### written in Phyton 3.8.1 by Strolch
+a little tool written in phyton to count all the active players on your AMP managed game server
+
+1. use player_amp.setserver to save the login data 
+
+2. use player_amp or player_amp.debug to count the active users
+
+3. have FUN or not ;)
---
+++ packages/players-amp/players_amp-2.33.tar.gz/players_amp-2.33/setup.py
@@ -0,0 +1,33 @@
+# players_amp
+### written in Phyton 3.8.1 by Strolch
+
+from setuptools import setup
+
+setup(
+    name='players_amp',
+    version='2.33',
+    license='MIT',
+    description='a little tool written in phyton to count all the active players on your AMP managed game server',
+    author='Strolch',
+    author_email='hello.circles@gmail.com',
+    url="https://github.com/R0b95/player_amp",
+    download_url='https://github.com/R0b95/players_amp/archive/2.0.tar.gz',
+    keywords=['AMP', 'UserCount', 'ActiveUser', 'ActivePlayer'],
+    packages=['players_amp', 'players_amp.api_functions'],
+    entry_points="""
+        [console_scripts] 
+            players_amp = players_amp.main:main
+            players_amp.debug = players_amp.debug:debug
+            players_amp.setserver = players_amp.setserver:setserver
+        """,
+    install_requires="""
+        appdirs
+    """,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+)
