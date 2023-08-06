1beb3eccee133788dab568767f35e8b37d29b1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:51 +0000

    Add package cluster-vcf-records/0.11.1/cluster_vcf_records-0.11.1.tar.gz

---
+++ packages/cluster-vcf-records/cluster_vcf_records-0.11.1.tar.gz/cluster_vcf_records-0.11.1/setup.py
@@ -0,0 +1,23 @@
+import glob
+from setuptools import setup, find_packages
+
+
+setup(
+    name="cluster_vcf_records",
+    version="0.11.1",
+    description="Package to cluster VCF records. For use by gramtools and minos",
+    packages=find_packages(),
+    author="Martin Hunt",
+    author_email="mhunt@ebi.ac.uk",
+    url="https://github.com/iqbal-lab-org/cluster_vcf_records",
+    test_suite="nose.collector",
+    tests_require=["nose >= 1.3"],
+    install_requires=["pyfastaq >= 3.14.0"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
