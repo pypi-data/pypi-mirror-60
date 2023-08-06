17233637f98c05901c720b1f63fa70cda2b7f879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:55 +0000

    Add package textaugH/0.2.2/textaugH-0.2.2.tar.gz

---
+++ packages/textaugH/textaugH-0.2.2.tar.gz/textaugH-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+with open('requirements.txt', 'r') as file:
+      requirements = file.readlines()[0].split()
+
+setup(name='textaugH',
+      version='0.2.2',
+      description='Little package for text augmentation',
+      author_email='comptechaugteam@gmail.com',
+      url='https://github.com/comptechaug/textaug',
+      author='TextAugmentationTeam',
+      license='MIT',
+      packages=['textaugH'],
+      include_package_data=True,
+      install_requires= requirements,
+      zip_safe=False)
