2c975b70362fda1638266e1e5b1ff9cd0ae3029e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:45 +0000

    Add package gg69-super-calc-engine/1.3/gg69-super-calc-engine-1.3.tar.gz

---
+++ packages/gg69-super-calc-engine/gg69-super-calc-engine-1.3.tar.gz/gg69-super-calc-engine-1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gg69-super-calc-engine",
+    description="One line calculator engine with example of wrapping his functional",
+    long_description="",
+    keywords=["cool", "calc", "expeval", "calculator", "69"],
+    author="Andreew Gregory",
+    author_email="grinadand@gmail.com",
+    url="https://github.com/1Gregory/gg69-calc-engine.git",
+    download_url="https://github.com/1Gregory/gg69-calc-engine/archive/1.0.tar.gz",
+    version="1.3",
+    install_requires=[
+        "colorama"
+    ],
+    packages=find_packages(),
+    license="MIT",
+)
