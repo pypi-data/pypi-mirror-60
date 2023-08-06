473c828d10aac98d515bae12c3c4b6b44e606153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:59 +0000

    Add package soulknight/0.1/soulknight-0.1.tar.gz

---
+++ packages/soulknight/soulknight-0.1.tar.gz/soulknight-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='soulknight',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description='Really, the funniest around.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='https://github.com/9kin/soul-knight',
+      author='9kin',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'pygame', 'pygame-menu',
+      ],
+      dependency_links=['https://github.com/9kin/pytmxloader'],
+      entry_points={
+        'console_scripts':
+            ['soulknight = soulknight:main']
+      },
+
+	  include_package_data=True,
+      zip_safe=False
+)
