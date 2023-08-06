83f0a7f68023a3d3aeeab0111811aec0b36ddd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package pyrror/0.0.2/pyrror-0.0.2.tar.gz

---
+++ packages/pyrror/pyrror-0.0.2.tar.gz/pyrror-0.0.2/pyrror/Error.py
@@ -0,0 +1,23 @@
+import datetime
+import json
+import os
+import traceback
+import requests
+
+
+class Error:
+
+    def __init__(self, client_id, monitoring_error_url):
+        self.client_id = client_id
+        self.monitoring_error_url = monitoring_error_url
+
+    def log(self, function_name):
+        body = {
+            "client_id": self.client_id,
+            "timestamp": str(datetime.datetime.now()),
+            "local_absolute_path": os.getcwd(),
+            "function_name": function_name,
+            "error": traceback.format_exc()
+        }
+        r = requests.post(url=self.monitoring_error_url, data=json.dumps(body))
+        return r.status_code
