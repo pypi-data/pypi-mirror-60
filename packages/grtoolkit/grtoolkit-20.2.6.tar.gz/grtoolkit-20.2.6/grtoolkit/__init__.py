26021bde22da6e70197335d81ca5f20c504404d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:44 +0000

    Add package grtoolkit/20.2.6/grtoolkit-20.2.6.tar.gz

---
+++ packages/grtoolkit/grtoolkit-20.2.6.tar.gz/grtoolkit-20.2.6/grtoolkit/__init__.py
@@ -0,0 +1,19 @@
+import grtoolkit.Decorators
+import grtoolkit.File
+import grtoolkit.Format
+import grtoolkit.Hardcode
+import grtoolkit.Heat
+import grtoolkit.Math
+import grtoolkit.Mechanics
+import grtoolkit.MSAccess
+import grtoolkit.PDF
+import grtoolkit.Python
+import grtoolkit.Storage
+import grtoolkit.Windows
+
+# import grtoolkit.Subclass.SubItem
+
+import os, sys
+
+def cwd():
+    return os.path.dirname(sys.argv[0])
