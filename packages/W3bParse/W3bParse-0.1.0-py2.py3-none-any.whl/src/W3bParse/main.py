7221444594aaa15677cc3ea0248bb807d64ce73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:40 +0000

    Add package W3bParse/0.1.0/W3bParse-0.1.0-py2.py3-none-any.whl

---
+++ packages/W3bParse/W3bParse-0.1.0-py2.py3-none-any.whl/src/W3bParse/main.py
@@ -0,0 +1,10 @@
+# This file is a demonstration of my replit module fix. To use it in your own python repls, just add a file called replit.py and copy in the code from the replit.py file here. You then shouldn't need to change the rest of your code at all.
+
+import time, replit
+
+print('Not cleared')
+
+time.sleep(1)
+
+replit.clear()
+print('Cleared')
---
+++ packages/W3bParse/W3bParse-0.1.0-py2.py3-none-any.whl/src/__init__.py
@@ -0,0 +1,6 @@
+def main():
+    import urllib.request
+    import urllib.parse
+    import re
+    import webParse
+    print("Call your main application code here")
