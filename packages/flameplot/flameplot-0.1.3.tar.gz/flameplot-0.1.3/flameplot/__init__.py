195cfef70238dd8e716dbafaff60125a21495b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:49 +0000

    Add package flameplot/0.1.3/flameplot-0.1.3.tar.gz

---
+++ packages/flameplot/flameplot-0.1.3.tar.gz/flameplot-0.1.3/flameplot/__init__.py
@@ -0,0 +1,20 @@
+from flameplot.flameplot import(
+    compare,
+    plot,
+    import_example,
+    scatter,
+    )
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.3'
+
+# module level doc-string
+__doc__ = """
+flameplot - Quantification of local similarity across two maps/embeddings.
+=====================================================================
+
+**flameplot** 
+See README.md file for more information.
+
+"""
---
+++ packages/flameplot/flameplot-0.1.3.tar.gz/flameplot-0.1.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import re
+
+VERSIONFILE="flameplot/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     install_requires=['matplotlib','numpy','tqdm','scipy','imagesc','scatterd'],
+     python_requires='>=3',
+     name='flameplot',
+     version=new_version,
+     author="Erdogan Taskesen",
+     author_email="erdogant@gmail.com",
+     description="Quantification of local similarity across two maps/embeddings.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/erdogant/flameplot",
+	 download_url = 'https://github.com/erdogant/flameplot/archive/'+new_version+'.tar.gz',
+     packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
+     include_package_data=True, # Must be true to include files depicted in MANIFEST.in
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
