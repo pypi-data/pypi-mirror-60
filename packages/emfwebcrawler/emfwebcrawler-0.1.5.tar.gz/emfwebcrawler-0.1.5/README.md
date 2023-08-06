6c3b1119dede30dc4f97ea288d1b137078ebf1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:19 +0000

    Add package emfwebcrawler/0.1.5/emfwebcrawler-0.1.5.tar.gz

---
+++ packages/emfwebcrawler/emfwebcrawler-0.1.5.tar.gz/emfwebcrawler-0.1.5/README.md
@@ -0,0 +1,3 @@
+# EMF Web Crawler #
+Part of school diploma thesis project
+Get links from url and analyze them
---
+++ packages/emfwebcrawler/emfwebcrawler-0.1.5.tar.gz/emfwebcrawler-0.1.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='emfwebcrawler',
+    version='0.1.5',
+    author="Erik Fabry",
+    author_email="erikfabry@gmail.com",
+    license="MIT",
+    description="Basic web crawler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Max1mum4444/emf-web-crawler",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'beautifulsoup4',
+        'requests',
+        'lxml',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
