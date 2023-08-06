36bb5a4084215df12afbe1b13b5496060234e214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:49 +0000

    Add package saya/0.2.3/saya-0.2.3-py3-none-any.whl

---
+++ packages/saya/saya-0.2.3-py3-none-any.whl/saya/AVK/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .AVk import AVk
+from .ALongPoll import ALongPoll
+from .AUploader import AUploader
+
+
+if __name__ == '__main__':
+    print(AVk, ALongPoll, AUploader)
