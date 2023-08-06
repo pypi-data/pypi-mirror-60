13f2621d685237829d04516c63bc53e47bc99d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:58 +0000

    Add package kix/0.1.3/kix-0.1.3-py3-none-any.whl

---
+++ packages/kix/kix-0.1.3-py3-none-any.whl/kix/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.3"
+
+
+from .color import red, green, blue, yellow, purple, black
+from .log import info, debug, warning, error, critical
+from .dict_expander import expand
+
+import kix.prompt
+
