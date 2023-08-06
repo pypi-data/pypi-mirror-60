fbfbd9f5ccb1d5f00067bd50f67b292a5a6f0453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:07 +0000

    Add package pyrsm/0.1.4/pyrsm-0.1.4.tar.gz

---
+++ packages/pyrsm/pyrsm-0.1.4.tar.gz/pyrsm-0.1.4/pyrsm/__init__.py
@@ -0,0 +1,6 @@
+from .bins import *
+from .perf import *
+from .stats import *
+from .utils import *
+from .props import *
+from .logit import *
---
+++ packages/pyrsm/pyrsm-0.1.4.tar.gz/pyrsm-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyrsm",
+    version="0.1.4",
+    description="Python functions for Customer Analytics at the Rady School of Management (RSM)",
+    long_description="Python functions for Customer Analytics at the Rady School of Management (RSM)",
+    long_description_content_type="text/markdown",
+    license="AGPL",
+    author="Vincent Nijs",
+    author_email="vnijs@ucsd.edu",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "numpy>=1.17.3",
+        "pandas>=0.25.2",
+        "seaborn>=0.9.0",
+        "matplotlib>=3.1.1",
+        "statsmodels>=0.10.1",
+        "scipy>=1.4.1"
+    ],
+    project_urls={
+        "Bug Reports": "https://github.com/vnijs/pyrsm/issues",
+        "Source": "https://github.com/vnijs/pyrsm",
+    },
+)
