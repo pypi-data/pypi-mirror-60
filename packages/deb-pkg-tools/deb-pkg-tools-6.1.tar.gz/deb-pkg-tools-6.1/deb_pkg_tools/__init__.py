4f4c2196e576695baaff6bf3f9c6205d31e0b3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:58 +0000

    Add package deb-pkg-tools/6.1/deb-pkg-tools-6.1.tar.gz

---
+++ packages/deb-pkg-tools/deb-pkg-tools-6.1.tar.gz/deb-pkg-tools-6.1/deb_pkg_tools/__init__.py
@@ -0,0 +1,43 @@
+# Debian packaging tools.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: February 5, 2020
+# URL: https://github.com/xolox/python-deb-pkg-tools
+
+"""
+The top-level :mod:`deb_pkg_tools` module.
+
+The :mod:`deb_pkg_tools` module defines the `deb-pkg-tools` version number and
+the Debian packages that are required to use all of the features provided by
+the `deb-pkg-tools` package.
+"""
+
+# Semi-standard module versioning.
+__version__ = '6.1'
+
+debian_package_dependencies = (
+    'apt',        # apt-get
+    'apt-utils',  # apt-ftparchive
+    'dpkg-dev',   # dpkg-architecture
+    'fakeroot',   # fakeroot
+    'gnupg',      # gpg
+    'lintian',    # lintian
+    'python-apt',
+)
+"""A tuple of strings with required Debian packages."""
+
+
+def generate_stdeb_cfg():
+    """
+    Generate the contents of the ``stdeb.cfg`` file used by stdeb_ and py2deb_.
+
+    The Debian package dependencies and minimal Python version are included in
+    the output.
+
+    .. _stdeb: https://pypi.python.org/pypi/stdeb
+    .. _py2deb: https://pypi.python.org/pypi/py2deb
+    """
+    print('[deb-pkg-tools]')
+    print('Depends: python-apt')
+    print('Recommends: %s' % ', '.join(pkg for pkg in debian_package_dependencies if pkg != 'python-apt'))
+    print('Suggests: memcached')
---
+++ packages/deb-pkg-tools/deb-pkg-tools-6.1.tar.gz/deb-pkg-tools-6.1/deb_pkg_tools/config.py
@@ -0,0 +1,55 @@
+# Debian packaging tools: Configuration defaults.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: November 22, 2016
+# URL: https://github.com/xolox/python-deb-pkg-tools
+
+"""Configuration defaults for the `deb-pkg-tools` package."""
+
+# Standard library modules.
+import os
+
+# External dependencies.
+from humanfriendly import parse_path
+
+system_config_directory = '/etc/deb-pkg-tools'
+"""The pathname of the global (system wide) configuration directory used by `deb-pkg-tools` (a string)."""
+
+system_cache_directory = '/var/cache/deb-pkg-tools'
+"""The pathname of the global (system wide) package cache directory (a string)."""
+
+user_config_directory = parse_path('~/.deb-pkg-tools')
+"""
+The pathname of the current user's configuration directory used by `deb-pkg-tools` (a string).
+
+:default: The expanded value of ``~/.deb-pkg-tools``.
+"""
+
+user_cache_directory = parse_path('~/.cache/deb-pkg-tools')
+"""
+The pathname of the current user's package cache directory (a string).
+
+:default: The expanded value of ``~/.cache/deb-pkg-tools``.
+"""
+
+# The location of the package cache. If we're running as root we have write
+# access to the system wide package cache so we'll pick that; the more users
+# sharing this cache the more effective it is.
+
+package_cache_directory = system_cache_directory if os.getuid() == 0 else user_cache_directory
+"""
+The pathname of the selected package cache directory (a string).
+
+:default: The value of :data:`system_cache_directory` when running as ``root``,
+          the value of :data:`user_cache_directory` otherwise.
+"""
+
+repo_config_file = 'repos.ini'
+"""
+The base name of the configuration file with user-defined Debian package repositories (a string).
+
+This configuration file is loaded from :data:`system_config_directory` and/or
+:data:`user_config_directory`.
+
+:default: The string ``repos.ini``.
+"""
