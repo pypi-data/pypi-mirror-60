c38060edcde989feb7291b898286eb908b35318f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:11 +0000

    Add package topmind-s3/0.0.4/topmind-s3-0.0.4.tar.gz

---
+++ packages/topmind-s3/topmind-s3-0.0.4.tar.gz/topmind-s3-0.0.4/setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import setup, Command
+
+package_name = 'topmind-s3'
+version = '0.0.4'
+
+
+class CleanCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./.pytest_cache ./.eggs')
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=package_name,
+    packages=[package_name],
+    package_dir={
+        package_name: 's3'
+    },
+    version=version,
+    license='MIT',
+    description='TopMind S3 Helper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Pavel Popov',
+    author_email='p.popov@talenttech.ru',
+    url='https://github.com/severgroup-tt/topmind-commons',
+    install_requires=[
+        'boto3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    cmdclass={
+        'clean': CleanCommand
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
