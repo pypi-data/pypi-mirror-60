00fb1293db85984a5c4cf64aa87f6c3f26b832e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:06 +0000

    Add package abxnester/1.1.0/abxnester-1.1.0.tar.gz

---
+++ packages/abxnester/abxnester-1.1.0.tar.gz/abxnester-1.1.0/abxnester.py
@@ -0,0 +1,18 @@
+
+"""dfasdfasdfasdfasdasdasdasdfasdfasdfsd
+sadfsdafasdfasdf
+sadfasdf"""
+
+
+def print_l(abc):
+	for each in abc:
+		if isinstance (each,list):
+			print_l(each)
+		else: print(each)
+
+
+
+"""dfefewfasef
+safaseegeafae
+
+efaeaesfe"""
---
+++ packages/abxnester/abxnester-1.1.0.tar.gz/abxnester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='abxnester',
+    version='1.1.0',
+    py_modules=['abxnester'],
+    author='1821562166',
+    author_email='453692941@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='my first',
+    )
