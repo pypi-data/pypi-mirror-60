42fd2a23d301dd762ca5d162a5259a0be5f64d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:17 +0000

    Add package i3-battery-block-vgg/0.1.0/i3-battery-block-vgg-0.1.0.tar.gz

---
+++ packages/i3-battery-block-vgg/i3-battery-block-vgg-0.1.0.tar.gz/i3-battery-block-vgg-0.1.0/tests/test_cli.py
@@ -0,0 +1,41 @@
+import logging
+
+import pytest
+
+from i3_battery_block_vgg import cli
+
+
+def test_compact():
+    sut = cli.CLI()
+    sut.parse(args=['-c'])
+    assert sut.args.compact is True, "Compact flag should be true"
+
+
+def test_compact_negated():
+    sut = cli.CLI()
+    sut.parse(args=[])
+    assert sut.args.show_bug is False, "show bug flag should be false"
+
+
+def test_show_bug():
+    sut = cli.CLI()
+    sut.parse(args=['--show_bug'])
+    assert sut.args.show_bug is True, "show bug flag should be true"
+
+
+def test_show_bug_negated():
+    sut = cli.CLI()
+    sut.parse(args=[])
+    assert sut.args.compact is False, "Compact flag should be false"
+
+
+def test_log_level_set():
+    sut = cli.CLI()
+    sut.parse(args=['-l=warning'])
+    assert sut.get_log_level == logging.WARNING, "log level should be warning"
+
+
+def test_log_level_wrong():
+    sut = cli.CLI()
+    with pytest.raises(SystemExit):
+        sut.parse(args=['--loglevel=fubar'])
---
+++ packages/i3-battery-block-vgg/i3-battery-block-vgg-0.1.0.tar.gz/i3-battery-block-vgg-0.1.0/tests/test_html_formatter.py
@@ -0,0 +1,70 @@
+import pytest
+
+from i3_battery_block_vgg.font_awesome_glyphs import FA_BATTERY_LIST
+from i3_battery_block_vgg.html_formatter import color
+from i3_battery_block_vgg.html_formatter import discern_loading_state
+from i3_battery_block_vgg.html_formatter import wrap_span
+from i3_battery_block_vgg.html_formatter import wrap_span_fa
+
+
+def test_wrap():
+    assert wrap_span("a") == "<span>a</span>", "String should be wrapped into span element!"
+
+
+def test_wrap_fa():
+    assert wrap_span_fa("a") == "<span font='FontAwesome'>a</span>", "String should be wrapped into span element!"
+
+
+def test_wrap_with_color():
+    assert wrap_span("a", "b") == "<span col='b'>a</span>", "String should be wrapped into span element!"
+
+
+def test_wrap_fa_with_color():
+    assert wrap_span_fa("a",
+                        "b") == "<span font='FontAwesome' col='b'>a</span>", "String should be wrapped into span " \
+                                                                             "element! "
+
+
+@pytest.mark.parametrize(
+    "smaller_then, expected",
+    [
+        (10, "#FFFFFF"),
+        (20, "#FF3300"),
+        (30, "#FF6600"),
+        (40, "#FF9900"),
+        (50, "#FFCC00"),
+        (60, "#FFFF00"),
+        (70, "#FFFF33"),
+        (80, "#FFFF66"),
+        (100, "#FFFFFF")
+    ]
+)
+def test_color(smaller_then: int, expected: str):
+    assert color(smaller_then - 1) == expected, "below threshold %s color %s is expected" % (smaller_then, expected)
+
+
+@pytest.mark.parametrize(
+    "not_percentage",
+    [
+        -50, 150,
+    ]
+)
+def test_color_percentage_over_stepped(not_percentage):
+    with pytest.raises(AttributeError, match=r'Threshold.*'):
+        color(not_percentage)
+
+
+@pytest.mark.parametrize(
+    "percentage, icon",
+    [
+        (0, FA_BATTERY_LIST[0]),
+        (9, FA_BATTERY_LIST[0]),
+        (25, FA_BATTERY_LIST[1]),
+        (50, FA_BATTERY_LIST[2]),
+        (75, FA_BATTERY_LIST[3]),
+        (95, FA_BATTERY_LIST[4]),
+        (100, FA_BATTERY_LIST[4])
+    ]
+)
+def test_correct_icon(percentage, icon):
+    assert discern_loading_state(percentage).find(icon) != -1, "glyph should be in string"
