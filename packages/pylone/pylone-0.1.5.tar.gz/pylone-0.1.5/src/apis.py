e0f683d42666e530acb92e59210c38dd0095a2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:35 +0000

    Add package pylone/0.1.5/pylone-0.1.5.tar.gz

---
+++ packages/pylone/pylone-0.1.5.tar.gz/pylone-0.1.5/src/apis.py
@@ -0,0 +1,19 @@
+from .config import load_config
+
+
+class PyloneApi():
+    def __init__(self, config, global_config):
+        self.cfg = config
+        self.gcf = global_config
+
+    def check_for_update(self, stage):
+        pass
+
+    def update(self, stage):
+        pass
+
+    def create(self):
+        pass
+
+    def remove(self):
+        pass
