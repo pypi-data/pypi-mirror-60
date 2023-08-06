47604790a7a5c0b81fd582dfe6b8c7f21c5ff912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:40 +0000

    Add package slackers/0.4.0a1/slackers-0.4.0a1-py3-none-any.whl

---
+++ packages/slackers/slackers-0.4.0a1-py3-none-any.whl/slackers/hooks.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+
+from pyee import AsyncIOEventEmitter
+from fastapi.encoders import jsonable_encoder
+
+from slackers.registry import R
+
+
+class NamedEventEmitter(AsyncIOEventEmitter):
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
+
+        AsyncIOEventEmitter.__init__(self, *args, **kwargs)
+
+
+events = NamedEventEmitter(name="events")
+actions = NamedEventEmitter(name="actions")
+commands = NamedEventEmitter(name="commands")
+
+
+def emit(emitter: NamedEventEmitter, event, payload):
+    listeners = emitter.listeners(event)
+    print(listeners)
+
+    async def _emit_async():
+        emitter.emit(event, jsonable_payload)
+
+    jsonable_payload = jsonable_encoder(payload)
+    log = logging.getLogger(__name__)
+    log.info(f"Emitting '{event}' using emitter '{emitter.name}'")
+    log.debug(jsonable_payload)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(_emit_async())
