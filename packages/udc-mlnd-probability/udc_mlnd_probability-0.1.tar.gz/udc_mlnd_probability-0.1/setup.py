8795c8e2af2b2d31ca01c60835ad9a8de2aeaf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:39 +0000

    Add package udc-mlnd-probability/0.1/udc_mlnd_probability-0.1.tar.gz

---
+++ packages/udc-mlnd-probability/udc_mlnd_probability-0.1.tar.gz/udc_mlnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udc_mlnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udc_mlnd_probability'],
+      author='Chukwuemeka Ezumezu',
+      author_email='emyfrancis2012@gmail.com',
+      zip_safe=False)
