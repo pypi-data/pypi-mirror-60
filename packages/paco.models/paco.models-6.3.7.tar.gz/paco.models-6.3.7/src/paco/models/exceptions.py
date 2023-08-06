f13895b94bae33df9ddd8f4becc5891e490d77d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:13 +0000

    Add package paco.models/6.3.7/paco.models-6.3.7.tar.gz

---
+++ packages/paco.models/paco.models-6.3.7.tar.gz/paco.models-6.3.7/src/paco/models/exceptions.py
@@ -0,0 +1,26 @@
+class InvalidPacoFieldType(Exception):
+    pass
+
+class InvalidPacoSchema(Exception):
+    pass
+
+class InvalidPacoProjectFile(Exception):
+    pass
+
+class UnusedPacoProjectField(Exception):
+    pass
+
+class InvalidPacoReference(Exception):
+    pass
+
+class InvalidCFNMapping(Exception):
+    pass
+
+class InvalidPacoBucket(Exception):
+    pass
+
+class InvalidAWSResourceName(Exception):
+    pass
+
+class TroposphereConversionError(Exception):
+    pass
