22cf6d6d43cc7b763c0a0b2872f546aa08d370d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:23 +0000

    Add package referenceseeker/1.6/referenceseeker-1.6.tar.gz

---
+++ packages/referenceseeker/referenceseeker-1.6.tar.gz/referenceseeker-1.6/referenceseeker/constants.py
@@ -0,0 +1,12 @@
+
+# Mash constants
+MAX_MASH_DIST = '0.1'
+UNFILTERED_MASH_DIST = '0.3'
+
+# DNA fragmentation constants
+FRAGMENT_SIZE = 1020
+MIN_FRAGMENT_SIZE = 100
+
+CITATION = '''Schwengers O., Hain T., Chakraborty T., Goesmann A. (2019)
+ReferenceSeeker: rapid determination of appropriate reference genomes.
+bioRxiv 863621; doi: https://doi.org/10.1101/863621'''
