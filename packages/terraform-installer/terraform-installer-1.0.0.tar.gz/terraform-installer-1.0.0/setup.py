bd10207e8227d97461c6e17ff10901ed34b49c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:13 +0000

    Add package terraform-installer/1.0.0/terraform-installer-1.0.0.tar.gz

---
+++ packages/terraform-installer/terraform-installer-1.0.0.tar.gz/terraform-installer-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="terraform-installer",
+    version="1.0.0",
+    author="Andy Klier",
+    author_email="andyklier@gmail.com",
+    description="install or upgrade the open source version of terraform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/shindagger/terraform-installer",
+    packages=['terraforminstaller'],
+    python_requires='>=3.6',
+    install_requires= ['setuptools', 'string-color', 'beautifulsoup4', 'requests', 'tqdm', 'pywin32 ; platform_system=="Windows"'],
+    entry_points={
+        'console_scripts': ['terraform-installer=terraforminstaller.main:main', 'ti=terraforminstaller.main:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
