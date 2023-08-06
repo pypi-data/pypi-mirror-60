d50815c88d9f9bbb8cd765dbc8ff003e0f5594e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:03 +0000

    Add package exonum-client-cis/0.4.0.dev5/exonum-client-cis-0.4.0.dev5.tar.gz

---
+++ packages/exonum-client-cis/exonum-client-cis-0.4.0.dev5.tar.gz/exonum-client-cis-0.4.0.dev5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Setup Script for the Exonum CIS Python Light Client."""
+import setuptools
+
+INSTALL_REQUIRES = ["protobuf", "pysodium", "requests", "websocket-client-py3"]
+
+PYTHON_REQUIRES = ">=3.4"
+
+with open("README.md", "r") as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setuptools.setup(
+    name="exonum-client-cis",
+    version="0.4.0.dev5",
+    author="The Exonum team",
+    author_email="contact@exonum.com",
+    description="Exonum Python Light Client",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/exonum/exonum-python-client",
+    packages=[
+        "exonum_client_cis",
+        "exonum_client_cis.proofs",
+        "exonum_client_cis.proofs.map_proof",
+        "exonum_client_cis.proofs.list_proof",
+    ],
+    install_requires=INSTALL_REQUIRES,
+    python_requires=PYTHON_REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Security :: Cryptography",
+    ],
+)
