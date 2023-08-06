06e6ddec6c7c619dd970e7e8bffe63a538287223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:24 +0000

    Add package hiveqlKernel/1.0.19/hiveqlKernel-1.0.19.tar.gz

---
+++ packages/hiveqlKernel/hiveqlKernel-1.0.19.tar.gz/hiveqlKernel-1.0.19/hiveql/constants.py
@@ -0,0 +1,10 @@
+import os
+__version__ = '1.0.19'
+
+KERNEL_NAME = 'hiveql'
+LANGUAGE = 'hiveql'
+DISPLAY_NAME = 'HiveQL'
+
+DEFAULT_TEXT_LANG = ['en']
+
+CONFIG_FILE = os.environ.get("HIVE_KERNEL_CONF_FILE", "~/.hiveql_kernel.json")
---
+++ packages/hiveqlKernel/hiveqlKernel-1.0.19.tar.gz/hiveqlKernel-1.0.19/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+
+from hiveql.constants import __version__, KERNEL_NAME, DISPLAY_NAME
+
+setup(
+    name=KERNEL_NAME + "Kernel",
+    version=__version__,
+    description=DISPLAY_NAME + ' Kernel',
+    long_description=long_description,
+    url='https://github.com/EDS-APHP/HiveQLKernel',
+    author='APHP - EDS',
+    license='MIT',
+    keywords='Hive HiveQL PyHive Kernel Ipykernel',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts':
+            ['jupyter-hiveql = {}.__main__:main'.format(KERNEL_NAME)],
+    },
+    include_package_data=False,  # otherwise package_data is not used
+    package_data={
+        KERNEL_NAME: ['resources/logo-*.png', 'resources/*.css'],
+    },
+)
