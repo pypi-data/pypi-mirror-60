1276d69c86c7cf56a7e22ebc188dc8b830f8ab51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:47 +0000

    Add package generators/2020.2.4/generators-2020.2.4.tar.gz

---
+++ packages/generators/generators-2020.2.4.tar.gz/generators-2020.2.4/generators/chunk_on.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2018-01-24 11:52:06
+# @Last Modified 2018-01-24
+# @Last Modified time: 2018-01-24 11:57:27
+
+from collections import deque
+from strict_functions import strict_globals
+from .iterable import iterable
+
+@strict_globals(deque=deque, iterable=iterable)
+def chunk_on(pipeline, new_chunk_signal, output_type=tuple):
+    ''' split the stream into seperate chunks based on a new chunk signal '''
+    assert iterable(pipeline), 'chunks needs pipeline to be iterable'
+    assert callable(new_chunk_signal), 'chunks needs new_chunk_signal to be callable'
+    assert callable(output_type), 'chunks needs output_type to be callable'
+    
+    out = deque()
+    for i in pipeline:
+        if new_chunk_signal(i) and len(out): # if new chunk start detected
+            yield output_type(out)
+            out.clear()
+        out.append(i)
+    # after looping, if there is anything in out, yield that too
+    if len(out):
+        yield output_type(out)
---
+++ packages/generators/generators-2020.2.4.tar.gz/generators-2020.2.4/generators/chunks.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-09-09
+# @Last Modified 2017-10-17
+
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest, imap as map
+else:
+    map = map
+
+from strict_functions import strict_globals
+from .chunk_on import chunk_on
+from .apply_to_last import apply_to_last
+from .iterable import iterable
+
+@strict_globals(zip_longest=zip_longest, apply_to_last=apply_to_last, chunk_on=chunk_on, iterable=iterable, map=map)
+def chunks(stream, chunk_size, output_type=tuple):
+    ''' returns chunks of a stream '''
+    assert iterable(stream), 'chunks needs stream to be iterable'
+    assert (isinstance(chunk_size, int) and chunk_size > 0) or callable(chunk_size), 'chunks needs chunk_size to be a positive int or callable'
+    assert callable(output_type), 'chunks needs output_type to be callable'
+    if callable(chunk_size):
+        ''' chunk_size is acting as a separator function '''
+        for chunk in chunk_on(stream, chunk_size, output_type):
+            yield chunk
+    else:
+        it = iter(stream)
+        marker = object()
+        iters = [it] * chunk_size
+        pipeline = apply_to_last(
+           zip_longest(*iters, fillvalue=marker),
+           lambda last_chunk: tuple(i for i in last_chunk if i is not marker)
+        )
+        if output_type is not tuple:
+            pipeline = map(output_type, pipeline)
+        for chunk in pipeline:
+            yield chunk
+
+del zip_longest
+del apply_to_last
+del chunk_on
+del map
+del strict_globals
