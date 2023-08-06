c25d79e0bb31e0b14cfa0905e155655c69b3cfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:02 +0000

    Add package s3-talenttech-oss/0.0.1/s3-talenttech-oss-0.0.1.tar.gz

---
+++ packages/s3-talenttech-oss/s3-talenttech-oss-0.0.1.tar.gz/s3-talenttech-oss-0.0.1/setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import setup, Command, find_packages
+
+package_name = 's3-talenttech-oss'
+version = '0.0.1'
+
+
+class CleanCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./.pytest_cache ./.eggs')
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=package_name,
+    packages=find_packages(),
+    version=version,
+    license='MIT',
+    description='TopMind S3 Helper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Pavel Popov',
+    author_email='p.popov@talenttech.ru',
+    url='https://github.com/severgroup-tt/topmind-commons',
+    install_requires=[
+        'boto3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    cmdclass={
+        'clean': CleanCommand
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
