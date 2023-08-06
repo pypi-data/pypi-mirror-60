baf2dffc2b25fe2083c03db92abc2f671ab4efef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package afmformats/0.5.2/afmformats-0.5.2.tar.gz

---
+++ packages/afmformats/afmformats-0.5.2.tar.gz/afmformats-0.5.2/afmformats/afm_fdist.py
@@ -0,0 +1,161 @@
+import pathlib
+
+import numpy as np
+
+from .afm_data import AFMData
+from .meta import MetaData
+
+
+class AFMForceDistance(AFMData):
+    """Base class for AFM force-distance data
+
+    A force-distance dataset consists of an approach and
+    a retract curve.
+    """
+
+    def __init__(self, data, metadata, diskcache=False):
+        """Initialization
+
+        Parameters
+        ----------
+        data: dict-like
+
+        """
+        # convert meta data
+        metadata_i = MetaData(metadata)
+        # check data keys
+        for cc in data:
+            if cc not in known_columns:
+                raise ValueError("Unknown column name '{}'!".format(cc))
+        self._path = pathlib.Path(metadata_i["path"])
+        self._metadata = metadata_i
+        self._enum = metadata_i["enum"]
+        # raw data will not be touched
+        self._raw_data = data
+        self._data = {"segment": data["segment"]}
+        if "index" not in self._raw_data:
+            self._data["index"] = np.arange(len(data["segment"]))
+        # TODO:
+        # - implement caching (hdf5?)
+
+    def __contains__(self, key):
+        return self._data.__contains__(key) or self._raw_data.__contains__(key)
+
+    def __getitem__(self, key):
+        if key in self._data:
+            data = self._data[key]
+        elif key in self._raw_data:
+            data = self._raw_data[key].copy()
+        else:
+            raise KeyError("Column '{}' not defined!".format(key))
+        return data
+
+    def __len__(self):
+        return len(self._data["segment"])
+
+    def __setitem__(self, key, values):
+        if len(values) != len(self):
+            raise ValueError(
+                "Cannot set data of length '{}' ".format(len(values))
+                + "for AFMForceDistance of length '{}'!".format(len(self)))
+        # do not touch raw data
+        self._data[key] = values
+
+    @property
+    def appr(self):
+        """Dictionary-like interface to the approach segment"""
+        return Segment(self._raw_data, self._data, which="approach")
+
+    @property
+    def columns(self):
+        """Available data columns"""
+        raw = self._raw_data.keys()
+        new = self._data.keys()
+        return sorted(set(raw) | set(new))
+
+    @property
+    def enum(self):
+        """Unique index of `self` in `self.path`"""
+        return self._enum
+
+    @property
+    def metadata(self):
+        """Unique index of `self` in `self.path`"""
+        return MetaData(self._metadata.copy())
+
+    @property
+    def mode(self):
+        """Imaging modality"""
+        return "force-distance"
+
+    @property
+    def path(self):
+        """Path to the measurement file"""
+        return self._path
+
+    @property
+    def retr(self):
+        """Dictionary-like interface to the retract segment"""
+        return Segment(self._raw_data, self._data, which="retract")
+
+    def export(self, path):
+        """Export all columns to a tab-separated value file"""
+        path = pathlib.Path(path)
+        cols = self.columns
+        end = "\r\n"
+        # get all data (faster than doing it every time for every row)
+        data = {}
+        for cc in cols:
+            data[cc] = self[cc]
+
+        with path.open("w") as fd:
+            # header
+            fd.write("#" + "\t".join(cols) + end)
+            # rows
+            for ii in range(len(self)):
+                items = []
+                for cc in cols:
+                    if cc in column_dtypes and column_dtypes[cc] == bool:
+                        items.append("{}".format(data[cc][ii]))
+                    else:
+                        items.append("{:.8e}".format(data[cc][ii]))
+                fd.write("\t".join(items) + end)
+
+
+class Segment(object):
+    """Simple wrapper around dict-like `data` to expose a single segment"""
+
+    def __init__(self, raw_data, data, which="approach"):
+        if which not in ["approach", "retract"]:
+            raise ValueError("`which` must be 'approach' or 'retract', "
+                             + "got '{}'!".format(which))
+        #: The segment type (approach or retract)
+        self.which = which
+        if which == "approach":
+            self.idx = False
+        else:
+            self.idx = True
+        self.raw_data = raw_data
+        self.data = data
+
+    def __getitem__(self, key):
+        used = self.data["segment"] == self.idx
+        if key in self.data:
+            return self.data[key][used]
+        elif key in self.raw_data:
+            return self.raw_data[key][used].copy()
+        else:
+            raise KeyError("Undefined feature '{}'!".format(key))
+
+
+#: Data types of all known columns (all other columns are assumed to be float)
+column_dtypes = {
+    "force": float,
+    "height (measured)": float,
+    "segment": bool,
+    "time": float,
+    "tip position": float,
+}
+
+#: Known data columns
+known_columns = sorted(column_dtypes.keys())
---
+++ packages/afmformats/afmformats-0.5.2.tar.gz/afmformats-0.5.2/afmformats/meta.py
@@ -0,0 +1,135 @@
+import pathlib
+
+import numpy as np
+
+from .parse_funcs import fint, vd_str_in
+
+
+#: Definition of metadata related to the data generated
+#: (dictionary with metadata keys, descriptions, units, and validators)
+DEF_DATASET = {
+    "enum": ["Datapoint index within the dataset", "", fint],
+    "identifier": ["Measurement identifier", "", str],
+    "path": ["Measurement path", "", pathlib.Path],
+    "session": ["Recording session identifier", "", str],
+}
+
+#: Definition of metadata related to the AFM experiment
+#: (dictionary with metadata keys, descriptions, units, and validators)
+DEF_EXPERIMENT = {
+    "duration": ["Duration of experiment", "s", float],
+    "feedback mode": ["Feedback mode", "", vd_str_in([
+        # JPK contact mode
+        "contact",
+        # From the NanoWizard User Manual (v. 4.2) sec. 5.7:
+        # Force modulation mode is a mixture between contact mode and
+        # intermittend mode and "can be thought of as a kind of contact
+        # mode with an added vibration on the cantilever".
+        "force-modulation",
+        ])],
+    "imaging mode": ["Imaging modality", "", vd_str_in(["force-distance"])],
+    "point count": ["Size of the dataset in points", "", fint],
+    "rate": ["Data recording rate", "Hz", float],
+    "sensitivity": ["Sensitivity", "m/V", float],
+    "setpoint": ["Active feedback loop setpoint", "N", float],
+    "speed": ["Piezo speed", "m/s", float],
+    "spring constant": ["Cantilever spring constant", "N/m", float],
+    "z range": ["Axial piezo range covered", "m", float],
+}
+
+#: Definition of metadata related to quantitative maps
+#: (dictionary with metadata keys, descriptions, units, and validators)
+DEF_QMAP = {
+    "grid center x": ["Horizontal center of grid", "m", float],
+    "grid center y": ["Vertical center of grid", "m", float],
+    "grid index x": ["Horizontal grid position index", "", fint],
+    "grid index y": ["Vertical grid position index", "", fint],
+    "grid shape x": ["Horizontal grid shape", "px", fint],
+    "grid shape y": ["Vertical grid shape", "px", fint],
+    "grid size x": ["Horizontal grid size", "m", float],
+    "grid size y": ["Vertical grid size", "m", float],
+    "position x": ["Horizontal position", "m", float],
+    "position y": ["Vertical position", "m", float],
+}
+
+#: Definition of metadata related to data analysis
+#: (dictionary with metadata keys, descriptions, units, and validators)
+DEF_ANALYSIS = {
+}
+
+#: A dictionary for all metadata definitions
+DEF_ALL = {}
+DEF_ALL.update(DEF_DATASET)
+DEF_ALL.update(DEF_EXPERIMENT)
+DEF_ALL.update(DEF_QMAP)
+DEF_ALL.update(DEF_ANALYSIS)
+
+#: List of all valid meta data keys
+KEYS_VALID = sorted(DEF_ALL.keys())
+
+
+class MetaDataMissingError(BaseException):
+    """Raised when meta data is missing"""
+    pass
+
+
+class MetaData(dict):
+    """Management of meta data variables
+
+    Valid key names are defined in :const:`afmformats.meta.KEYS_VALID`.
+    """
+    valid_keys = KEYS_VALID
+
+    def __init__(self, *args, **kwargs):
+        super(MetaData, self).__init__(*args, **kwargs)
+        # check for invalid keys
+        for key in self:
+            if key not in self.valid_keys:
+                raise KeyError("Unknown metadata key: '{}'".format(key))
+
+    def __setitem__(self, key, value):
+        """Set a metadata key
+
+        The key must be a valid key defined in `self.valid_keys`
+        (defaults to :const:`afmformats.meta.KEYS_VALID`).
+        """
+        if key not in self.valid_keys:
+            raise KeyError("Unknown metdataa key: '{}'".format(key))
+        super(MetaData, self).__setitem__(key, value)
+
+    def __getitem__(self, *args, **kwargs):
+        if args[0] not in self and args[0] in self.valid_keys:
+            msg = "No meta data was defined for '{}'!".format(args[0]) \
+                  + "Please make sure you passed the dictionary `metadata` " \
+                  + "when you loaded your data."
+            raise MetaDataMissingError(msg)
+        elif args[0] not in self:
+            msg = "Unknown meta key: '{}'!".format(args[0])
+            raise KeyError(msg)
+        return super(MetaData, self).__getitem__(*args, **kwargs)
+
+    def get_summary(self):
+        """Convenience function returning the meta data summary
+
+        Returns a dict of dicts with keys matching the DEF_* dicts.
+        Unset values are returned as `np.nan`.
+        """
+        dataset = {}
+        for key in DEF_DATASET:
+            dataset[key] = self.get(key, np.nan)
+        experiment = {}
+        for key in DEF_EXPERIMENT:
+            experiment[key] = self.get(key, np.nan)
+        qmap = {}
+        for key in DEF_QMAP:
+            qmap[key] = self.get(key, np.nan)
+        analysis = {}
+        for key in DEF_ANALYSIS:
+            analysis[key] = self.get(key, np.nan)
+
+        summary = {"dataset": dataset,
+                   "experiment": experiment,
+                   "qmap": qmap,
+                   "analysis": analysis,
+                   }
+        return summary
