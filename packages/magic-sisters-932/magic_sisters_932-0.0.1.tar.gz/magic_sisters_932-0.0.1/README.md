6e565075d523165e113d061f0eecad2269c27e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:07 +0000

    Add package magic-sisters-932/0.0.1/magic_sisters_932-0.0.1.tar.gz

---
+++ packages/magic-sisters-932/magic_sisters_932-0.0.1.tar.gz/magic_sisters_932-0.0.1/README.md
@@ -0,0 +1,1 @@
+## This is a package created specially for the magic sisters.
---
+++ packages/magic-sisters-932/magic_sisters_932-0.0.1.tar.gz/magic_sisters_932-0.0.1/magic_sisters_932/roomie_names.py
@@ -0,0 +1,8 @@
+def cara(a, b, c):
+    text = "Cara is awesome!"
+    return text
+
+def JJ(a, b, c):
+    text = "Jing is awesome!"
+    return text
+
