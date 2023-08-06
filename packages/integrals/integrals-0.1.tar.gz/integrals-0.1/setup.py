2ca13e5dd7075541508d7daeb4b3c69312f18188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:15 +0000

    Add package integrals/0.1/integrals-0.1.tar.gz

---
+++ packages/integrals/integrals-0.1.tar.gz/integrals-0.1/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(name='integrals',
+     version='0.1',
+     description='Numerical approximators to calculate the definit integral',
+     packages=find_packages(),
+     author='Osama Maharmeh',
+     author_email='maharmeh.osama98@gmail.com',
+     classifiers=["Programming Language :: Python :: 3",
+                  "License :: OSI Approved :: MIT License",
+                  "Operating System :: OS Independent"
+    ],
+     zip_safe=False)
