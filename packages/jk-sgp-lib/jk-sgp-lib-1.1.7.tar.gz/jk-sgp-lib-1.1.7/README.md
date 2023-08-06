2758b781b6509537a30646196b589ad5f761e3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:20 +0000

    Add package jk-sgp-lib/1.1.7/jk-sgp-lib-1.1.7.tar.gz

---
+++ packages/jk-sgp-lib/jk-sgp-lib-1.1.7.tar.gz/jk-sgp-lib-1.1.7/README.md
@@ -0,0 +1,11 @@
+# jk_sgp_lib 
+
+Make Scrapy easier and more versatile.
+
+## v1.1.7 修正早起版本部分BUG
+
+这组API提供，Items、Pipelines、Spiders 三组类库，用来辅助Scrapy 的上层功能实现，帮助爬虫完成页面的区域校对、帮助Pipeline 对Mysql 的直接输出
+
+## Install 
+
+安装方法
---
+++ packages/jk-sgp-lib/jk-sgp-lib-1.1.7.tar.gz/jk-sgp-lib-1.1.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jk-sgp-lib", # Replace with your own username
+    version="1.1.7",
+    author="kami1983",
+    author_email="kami@cancanyou.com",
+    description="Make Scrapy easier and more versatile.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kami1983/jk_sgp_lib",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    install_requires=['scrapy', 'simplejson', 'pytz', 'pymysql', 'datetime'],
+    python_requires='>=2.7',
+)
