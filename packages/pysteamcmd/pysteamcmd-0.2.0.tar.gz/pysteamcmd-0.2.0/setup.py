b70814f2ed34abb3b657ceb2062344bb8cea4482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:25 +0000

    Add package pysteamcmd/0.2.0/pysteamcmd-0.2.0.tar.gz

---
+++ packages/pysteamcmd/pysteamcmd-0.2.0.tar.gz/pysteamcmd-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pysteamcmd',
+      version='0.2.0',
+      description='Python package to install and utilize steamcmd',
+      url='http://github.com/f0rkz/pysteamcmd',
+      author='f0rkz',
+      author_email='f0rkz@f0rkznet.net',
+      license='MIT',
+      packages=['pysteamcmd'],
+      install_requires=[],
+      zip_safe=False)
