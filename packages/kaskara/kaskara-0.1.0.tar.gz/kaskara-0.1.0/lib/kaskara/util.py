55c64a713ea32421d6ed2f8b23be656eb8939fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:52 +0000

    Add package kaskara/0.1.0/kaskara-0.1.0.tar.gz

---
+++ packages/kaskara/kaskara-0.1.0.tar.gz/kaskara-0.1.0/lib/kaskara/util.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os
+
+from .core import FileLocationRange, FileLocation
+
+
+def abs_to_rel_filename(prefix: str, filename: str) -> str:
+    if prefix[-1] != '/':
+        prefix = prefix + '/'
+    assert filename.startswith(prefix)
+    return filename[len(prefix):]
+
+
+def abs_to_rel_floc(prefix: str, l: FileLocation) -> FileLocation:
+    return FileLocation(abs_to_rel_filename(prefix, l.filename),
+                        l.location)
+
+
+def rel_to_abs_floc(prefix: str, l: FileLocation) -> FileLocation:
+    return FileLocation(os.path.join(prefix, l.filename), l.location)
+
+
+def abs_to_rel_flocrange(prefix: str,
+                         l: FileLocationRange
+                         ) -> FileLocationRange:
+    return FileLocationRange(abs_to_rel_filename(prefix, l.filename),
+                             l.location_range)
+
+
+def rel_to_abs_flocrange(prefix: str,
+                         l: FileLocationRange
+                         ) -> FileLocationRange:
+    return FileLocationRange(os.path.join(prefix, l.filename),
+                             l.location_range)
