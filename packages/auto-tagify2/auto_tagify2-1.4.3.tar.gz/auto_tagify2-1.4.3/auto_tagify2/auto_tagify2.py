1d7838e050d4ac8289a054709cc364b290b9f23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:49 +0000

    Add package auto-tagify2/1.4.3/auto_tagify2-1.4.3.tar.gz

---
+++ packages/auto-tagify2/auto_tagify2-1.4.3.tar.gz/auto_tagify2-1.4.3/auto_tagify2/auto_tagify2.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import re
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.parse import quote
+
+import nltk
+
+from nltk.stem.wordnet import WordNetLemmatizer
+
+CLEAN_LINK = re.compile('(?<=^\/)\/+|\/+$')
+CLEAN_WORD = re.compile('[\[\],().:;"\/\'?*%!*+=@$;#%{}`~\r\n\t]')
+LONG_DASH = re.compile('(\&#8212;)')
+MIN_TAG_LENGTH = 2
+SMART_QUOTES_D = re.compile('(\xe2\x80\x9c)|(\xe2\x80\x9d)|(\&#8220;)|(\&#8221;)')
+SMART_QUOTES_S = re.compile('(\xe2\x80\x98)|(\xe2\x80\x99)|(\&#8216;)|(\&#8217;)')
+STOP_WORDS = ['DT', 'IN', 'TO', 'VBD', 'VBD', 'VBG', 'VBN', 'VBZ', 'MD', 'RB',
+              'CC', 'WDT', 'PRO', 'PRP', 'PRP$']
+
+
+class AutoTagify():
+    lemma = WordNetLemmatizer()
+
+    def __init__(self):
+        self.css = ''
+        self.link = ''
+        self.text = ''
+
+    def _tokenize(self):
+        """Tag words from the string."""
+        return nltk.pos_tag(nltk.word_tokenize(self._clean_text()))
+
+    def _cleaned(self, word, strict):
+        lemmatized = self.lemma.lemmatize(self._clean_text(word))
+        if strict:
+            return lemmatized
+        else:
+            return quote(self._clean_text(word))
+
+    def _clean_text(self, word=''):
+        if len(word) > MIN_TAG_LENGTH:
+            return CLEAN_WORD.sub('', self._replace_special_chars(word.lower()))
+        else:
+            return CLEAN_WORD.sub('', self._replace_special_chars(self.text))
+
+    def _replace_special_chars(self, text):
+        return SMART_QUOTES_S.sub('\'', SMART_QUOTES_D.sub('"', LONG_DASH.sub('-',text)))
+
+    def generate(self, strict=True):
+        """Return the HTML version of tags for the string."""
+        tag_words = []
+        for (word, word_type) in self._tokenize():
+            tag_word = self._cleaned(word, strict)
+            if len(tag_word) > MIN_TAG_LENGTH and word_type not in STOP_WORDS:
+                tag_words.append('<a href="%s/%s" class="%s">%s</a> ' % (CLEAN_LINK.sub('', self.link),
+                                                                         quote(tag_word),
+                                                                         CLEAN_WORD.sub('', self.css),
+                                                                         self._replace_special_chars(word)))
+            else:
+                tag_words.append(word + ' ')
+        return ''.join(tag_words)
+
+    def tag_list(self, strict=True):
+        """Return the tags from string as a list. If strict is set
+        to True, then only return the stemmed version. Otherwise, return the
+        full string - therefore, `cat` will be considered different from `cats`.
+        """
+        tag_words = []
+        for (word, word_type) in self._tokenize():
+            tag_word = self._cleaned(word, strict)
+            if len(tag_word) > MIN_TAG_LENGTH and word_type not in STOP_WORDS:
+                tag_words.append(tag_word)
+        return tag_words
---
+++ packages/auto-tagify2/auto_tagify2-1.4.3.tar.gz/auto_tagify2-1.4.3/setup.py
@@ -0,0 +1,66 @@
+from distutils.core import setup
+
+setup(name='auto_tagify2',
+      version='1.4.3',
+      author='operatorjen',
+      author_email='python@0112358.io',
+      url='https://github.com/operatorjen/auto-tagify',
+      description='Auto-tags a selection of text and generates links to the tagified versions of the words',
+      license='BSD',
+      long_description="""
+
+Auto Tagify2 is a simple auto tagging module that uses NLTK to generate tags out of a selection of text. Any
+text that is less than 3 characters long or matches a particular POS (part-of-speech) will be ignored.
+
+There are two operations Auto Tagify performs - one returns the selection of text with links embedded in the
+string and the other returns a list of all the taggable words as the stem word (using lemmatization).
+
+For the first operation, everything is optional, but it is most effective to enter some text. Optional
+parameters you can set are the paths for tag links and the css classes for link. For instance, if you set
+your tag routing to a relative path such as /tags/<tagged_word> and want to use the css class named "tagged":
+
+from auto_tagify2 import AutoTagify
+
+t = AutoTagify()
+
+t.text = "This is the text to display!"
+
+t.link = "/tags"
+
+t.css = "tagged"
+
+t.generate()
+
+The result will be: This is the <a href="/tags/text" class="tagged">text</a> to <a href="/tags/display" class="tagged">display!</a>
+
+If no link is set, the default path is "/<tagged word>", such as "/text".
+
+For the second operation, you will only receive a list of all your taggable words from the text. You can call it like so:
+
+t.text = "This text is tagged kittens"
+
+t.tag_list()
+
+The result will be a list: ['text', 'tag', 'kitten']
+
+By default, generate() and tag_list() will be in strict mode, which means all special characters will be stripped and
+lemmatization will be enforced. If generate(strict=False) or tag_list(strict=False) is set, then special characters will be url encoded
+and lemmatization will be ignored.
+
+These two operations are sufficient for you to maintain tag counts and tag references to text in your application.
+        """,
+        packages=['auto_tagify2'],
+        keywords='tagging tags html nltk english',
+        classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        ],
+        )
