0312d9a8565fe1539c3f9baebca6df8823029770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:04 +0000

    Add package synbiochem-py/0.6.29/synbiochem-py-0.6.29.tar.gz

---
+++ packages/synbiochem-py/synbiochem-py-0.6.29.tar.gz/synbiochem-py-0.6.29/synbiochem/utils/taxonomy_utils.py
@@ -0,0 +1,39 @@
+'''
+synbiochem (c) University of Manchester 2016
+
+synbiochem is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+import json
+from urllib import parse, request
+
+
+def get_taxonomy_id(name):
+    '''Gets the NCBI Taxonomy id from supplied name.'''
+    url = 'http://www.ebi.ac.uk/ols/api/search?ontology=ncbitaxon' + \
+        '&exact=true&queryFields=label&q=' + parse.quote(name)
+
+    response = request.urlopen(url)
+    data = json.loads(response.read())
+
+    if data['response']['numFound'] == 1:
+        term = data['response']['docs'][0]['obo_id']
+        return term[term.find(':') + 1:]
+
+    raise RuntimeError(name + ' not found in NCBI Taxonomy')
+
+
+def search(term, exact=False):
+    '''Gets the NCBI Taxonomy id from supplied name.'''
+    url = 'http://www.ebi.ac.uk/ols/api/search?ontology=ncbitaxon' + \
+        '&exact=' + str(exact) + '&queryFields=label&q=' + parse.quote(term)
+
+    response = request.urlopen(url)
+    data = json.loads(response.read())
+
+    return [{'id': doc['obo_id'].replace('NCBITaxon:', ''),
+             'name': doc['label']}
+            for doc in data['response']['docs']]
