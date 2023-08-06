3d8e77f3a02a96688931eb916eb34c908cd48576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:44 +0000

    Add package ravencoinlib/0.0/ravencoinlib-0.0.tar.gz

---
+++ packages/ravencoinlib/ravencoinlib-0.0.tar.gz/ravencoinlib-0.0/README.md
@@ -0,0 +1,6 @@
+# ravencoinlib
+
+Ravencoin fork of python-bitcoinlib intended to provide access to Ravencoin data 
+structures and protocol.
+
+This is an alias for python-ravencoinlib
---
+++ packages/ravencoinlib/ravencoinlib-0.0.tar.gz/ravencoinlib-0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    author='standard-error@github',
+    author_email='ravencoinlib@gmail.com',
+    description='Ravencoin fork of python-bitcoinlib',
+    install_requires=['python-ravencoinlib'],
+    long_description='Use python-ravencoinlib <https://github.com/standard-error/python-ravencoinlib> instead.',
+    name='ravencoinlib',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://github.com/standard-error/python-ravencoinlib',
+    version="0.0",
+    zip_safe=False,
+)
