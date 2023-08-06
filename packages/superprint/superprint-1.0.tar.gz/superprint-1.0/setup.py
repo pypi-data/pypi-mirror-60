fe9251f7c0e50392dedd7eae5944ae04fb72b1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package superprint/1.0/superprint-1.0.tar.gz

---
+++ packages/superprint/superprint-1.0.tar.gz/superprint-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='superprint',#对外名称
+      version='1.0',#版本
+      description='超级打印',#描述
+      author='满小鱼',#作者
+      py_modules=['superprint.myfile'],
+      )
---
+++ packages/superprint/superprint-1.0.tar.gz/superprint-1.0/superprint/myfile.py
@@ -0,0 +1,3 @@
+def superprint():
+    print('满小鱼真帅！')
+
