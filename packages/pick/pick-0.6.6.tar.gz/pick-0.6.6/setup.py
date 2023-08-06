4888b96d90748a70f0dcd7f7e59b0206d100b1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:54 +0000

    Add package pick/0.6.6/pick-0.6.6.tar.gz

---
+++ packages/pick/pick-0.6.6.tar.gz/pick-0.6.6/setup.py
@@ -0,0 +1,34 @@
+#-*-coding:utf-8-*-
+
+import os
+from setuptools import setup
+
+def fread(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath, 'r') as fp:
+        return fp.read()
+
+setup(
+    name='pick',
+    version='0.6.6',
+    description='pick an option in the terminal with a simple GUI',
+    long_description=fread('README.rst'),
+    keywords='terminal gui',
+    url='https://github.com/wong2/pick',
+    author='wong2',
+    author_email='wonderfuly@gmail.com',
+    license='MIT',
+    packages=['pick'],
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
