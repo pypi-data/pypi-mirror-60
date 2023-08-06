5ce460322d8bfe60d92cefc5cee0dbc198edaff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:56 +0000

    Add package Krypton-GD/1.0.0/Krypton-GD-1.0.0.tar.gz

---
+++ packages/Krypton-GD/Krypton-GD-1.0.0.tar.gz/Krypton-GD-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Krypton-GD", 
+    version="1.0.0",
+    author="Imran S M",
+    author_email="imran.malik01@icloud.com",
+    description="A small package to add up two gaussian distributions together",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/immu0001/Krypton",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
