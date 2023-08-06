a5839d3b4737a575762a07c907641fd4174d2cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:17 +0000

    Add package wikipedia-histories/0.0.1/wikipedia_histories-0.0.1.tar.gz

---
+++ packages/wikipedia-histories/wikipedia_histories-0.0.1.tar.gz/wikipedia_histories-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wikipedia_histories",
+    version="0.0.1",
+    author="Nathan Drezner",
+    author_email="nathan@drezner.com",
+    description="A simple package designed to collect the edit histories of Wikipedia pages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ndrezn/wiki-revisions",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
