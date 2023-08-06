829238dd5030814cad82e24049bbe5cb28ffa313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:06 +0000

    Add package esc1pe/0.3/esc1pe-0.3.tar.gz

---
+++ packages/esc1pe/esc1pe-0.3.tar.gz/esc1pe-0.3/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'esc1pe',         # How you named your package folder (MyLib)
+  packages = ['esc1pe'],   # Chose the same as "name"
+  version = '0.3',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'esc1pe description',   # Give a short description about your library
+  author = 'robertmorris',                   # Type in your name
+  author_email = 'robertmorrislike@gmail.com',      # Type in your E-Mail
+  download_url = 'https://github.com/h4x0rMadness/BUEC500_api-design-private/archive/0.3.zip',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
