57fc32951d2a27337874f441e37c498f204f13b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:17 +0000

    Add package JSONSchema2PoPo2/2.0.22/JSONSchema2PoPo2-2.0.22.tar.gz

---
+++ packages/JSONSchema2PoPo2/JSONSchema2PoPo2-2.0.22.tar.gz/JSONSchema2PoPo2-2.0.22/test/valid/test_root_string_enum.py
@@ -0,0 +1,18 @@
+import enum
+
+
+class Abcd(enum.Enum):
+
+    A = "A"
+    B = "B"
+    C = "C"
+
+    @staticmethod
+    def from_dict(d):
+        return Abcd(d)
+
+    def as_dict(self):
+        return self.value
+
+    def __repr__(self):
+        return "<Enum Abcd. {}: {}>".format(self.name, self.value)
