19701a7163d1ab6c6829b8fd67dd703a89fd9239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package tf-geometric/0.0.17/tf_geometric-0.0.17.tar.gz

---
+++ packages/tf-geometric/tf_geometric-0.0.17.tar.gz/tf_geometric-0.0.17/tf_geometric/layers/conv/gcn.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+import tensorflow as tf
+from tensorflow.python.keras.layers import Dense
+
+from tf_geometric.data.graph import Graph
+from tf_geometric.nn.conv.gcn import gcn_norm_edge, gcn
+from tf_geometric.layers.kernel.map_reduce import MapReduceGNN
+
+
+class GCN(MapReduceGNN):
+
+    def build(self, input_shapes):
+        x_shape = input_shapes[0]
+        num_features = x_shape[-1]
+
+        self.kernel = self.add_weight("kernel", shape=[num_features, self.units], initializer="glorot_uniform")
+        self.bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
+
+    def __init__(self, units, activation=None, improved=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.units = units
+
+        self.acvitation = activation
+        self.kernel = None
+        self.bias = None
+
+        self.improved = improved
+
+    def call(self, inputs, training=None, mask=None, cache=None):
+        """
+
+        :param inputs:
+        :param training:
+        :param mask:
+        :param graph: If graph is provided, it is used for caching normed edge info
+        :return:
+        """
+
+        if len(inputs) == 3:
+            x, edge_index, edge_weight = inputs
+        else:
+            x, edge_index = inputs
+            edge_weight = None
+
+        return gcn(x, edge_index, edge_weight, self.kernel, self.bias,
+                   activation=self.acvitation, improved=self.improved, cache=cache)
