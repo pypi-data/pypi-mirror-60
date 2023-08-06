445cae117c3abff648c14bd8fcfb738e48610dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:56 +0000

    Add package TestProject1830/1.0.0/TestProject1830-1.0.0.tar.gz

---
+++ packages/TestProject1830/TestProject1830-1.0.0.tar.gz/TestProject1830-1.0.0/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 20:42:27 2020
+
+@author: chaitanyam
+"""
+
+from setuptools import setup
+
+
+setup(
+    name="TestProject1830",
+    version="1.0.0",
+    description="A Python package to add numbers.",
+    long_description_content_type="text/markdown",
+    author="chaitanya sudhakar",
+    author_email="chaitanya.chowdary25@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["TestProject1830"],
+    include_package_data=True,
+    
+)
