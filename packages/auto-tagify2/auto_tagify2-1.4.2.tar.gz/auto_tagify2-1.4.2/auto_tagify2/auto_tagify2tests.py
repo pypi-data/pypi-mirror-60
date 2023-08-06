c2b9e374f5acc1d70a5dcd1785d371c14ceeb93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:21 +0000

    Add package auto-tagify2/1.4.2/auto_tagify2-1.4.2.tar.gz

---
+++ packages/auto-tagify2/auto_tagify2-1.4.2.tar.gz/auto_tagify2-1.4.2/auto_tagify2/auto_tagify2tests.py
@@ -0,0 +1,40 @@
+import unittest
+from auto_tagify2 import AutoTagify
+
+class AutoTagifyTestCase(unittest.TestCase):
+
+    def testTextNotEmptyStrict(self):
+        """Verify strict text returns content, if text is provided and
+        not null.
+        """
+        a = AutoTagify()
+        a.text = 'This is a test'
+        a.css = 'taggable'
+        self.assertEqual(a.tag_list(), ['test'])
+        self.assertEqual(a.generate(), 'This is a <a href="/test" class="taggable">test</a> ')
+
+    def testTextNotEmptyNotStrict(self):
+        """Verify non-strict text returns content, if text is provided and
+        not null.
+        """
+        a = AutoTagify()
+        a.text = 'These are my tests'
+        a.css = 'taggable'
+        self.assertEqual(a.tag_list(), ['are', 'test'])
+        self.assertEqual(a.generate(strict=False),
+                         'These <a href="/are" class="taggable">are</a> my <a href="/tests" class="taggable">tests</a> ')
+
+    def testTextEmpty(self):
+        """Verify sending no text returns nothing."""
+        a = AutoTagify()
+        self.assertEqual(a.generate(), '')
+
+    def testTagsNotEmpty(self):
+        """Verify that tags are returned."""
+        a = AutoTagify()
+        a.text = 'This is a test with other valid tags'
+        test_array = ['test', 'other', 'valid', 'tag']
+        self.assertEqual(a.tag_list(), test_array)
+
+if __name__ == "__main__":
+        unittest.main()
