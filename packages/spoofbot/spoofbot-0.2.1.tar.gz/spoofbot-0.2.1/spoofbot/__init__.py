a019c2721b730203a67b0cc4f25f722556a724e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:03 +0000

    Add package spoofbot/0.2.1/spoofbot-0.2.1.tar.gz

---
+++ packages/spoofbot/spoofbot-0.2.1.tar.gz/spoofbot-0.2.1/spoofbot/__init__.py
@@ -0,0 +1,6 @@
+"""Core modules for tht web bot"""
+
+from browser import Browser, Firefox, Chrome, MimeTypeTag, LanguageTag
+from operating_system import OS, Windows, WindowsVersion, MacOSX, MacOSXVersion, Linux, LinuxDerivatives
+
+__version__ = "0.2.1"
