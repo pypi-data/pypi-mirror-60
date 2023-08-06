669223455e621bcb047d0bedc80ee9250024064b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:20 +0000

    Add package datastep/0.1.1/datastep-0.1.1.tar.gz

---
+++ packages/datastep/datastep-0.1.1.tar.gz/datastep-0.1.1/datastep/constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+
+
+CONFIG_ENV_VAR_NAME = "WORKFLOW_CONFIG"
+CWD_CONFIG_FILE_NAME = "workflow_config.json"
+
+DEFAULT_QUILT_STORAGE = "s3://allencell-internal-quilt"
+DEFAULT_QUILT_PACKAGE_OWNER = "aics"
+DEFAULT_PROJECT_LOCAL_STAGING_DIR = "{cwd}/local_staging"
+DEFAULT_STEP_LOCAL_STAGING_DIR = "/".join(
+    [DEFAULT_PROJECT_LOCAL_STAGING_DIR, "{module_name}"]
+)
