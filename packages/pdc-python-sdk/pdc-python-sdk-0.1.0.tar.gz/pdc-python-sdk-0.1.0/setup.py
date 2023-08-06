5ab1d3e93307ee3ed4e0f5a1469b653ed08da86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:28 +0000

    Add package pdc-python-sdk/0.1.0/pdc-python-sdk-0.1.0.tar.gz

---
+++ packages/pdc-python-sdk/pdc-python-sdk-0.1.0.tar.gz/pdc-python-sdk-0.1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2020 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+Setup script.
+
+Authors: jiaqianjing(jiaqianjing@baidu.com)
+Date:    2020/02/04 16:06:56
+"""
+
+import setuptools
+
+setuptools.setup()
+
