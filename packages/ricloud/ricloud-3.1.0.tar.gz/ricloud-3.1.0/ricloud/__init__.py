41329b2cbef60674bff8478dd310b967d72b47b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:10 +0000

    Add package ricloud/3.1.0/ricloud-3.1.0.tar.gz

---
+++ packages/ricloud/ricloud-3.1.0.tar.gz/ricloud-3.1.0/ricloud/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+__version__ = "3.1.0"
+
+from ricloud import conf
+
+token = conf.get("api", "token")
+url = conf.get("api", "url")
+
+from ricloud.resources import *
+
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
