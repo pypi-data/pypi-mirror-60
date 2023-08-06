4aabd623cb8da1181369c4b3a6f775ef95a7757d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:28 +0000

    Add package fycharts/2.0.0/fycharts-2.0.0.tar.gz

---
+++ packages/fycharts/fycharts-2.0.0.tar.gz/fycharts-2.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fycharts",
+    version="2.0.0",
+    author="Kelvin Gakuo",
+    author_email="kelvingakuo@gmail.com",
+    description="A fully-fledged installable python package for extracting top 200 and viral 50 charts off of spotifycharts.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kelvingakuo/fycharts",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pandas",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
