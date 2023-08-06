6f7fa421cce36faa28931f3ad2c30404fc146d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:34 +0000

    Add package pytest-xprocess/0.13.1/pytest-xprocess-0.13.1.tar.gz

---
+++ packages/pytest-xprocess/pytest-xprocess-0.13.1.tar.gz/pytest-xprocess-0.13.1/example/server.py
@@ -0,0 +1,35 @@
+import os
+try:
+    import SocketServer as socketserver
+except ImportError:
+    import socketserver
+
+import sys
+
+
+response = os.environ.get('RESPONSE', "1").encode('utf8')
+
+
+class MainHandler(socketserver.StreamRequestHandler):
+    count = 0
+
+    def handle(self):
+        while 1:
+            line = self.rfile.readline()
+            if not line:
+                break
+            sys.stderr.write("[%d] %r\n" % (self.count, line))
+            sys.stderr.flush()
+            self.request.sendall(response)
+            MainHandler.count += 1
+
+if __name__ == "__main__":
+    class MyServer(socketserver.TCPServer):
+        allow_reuse_address = True
+    mainserver = MyServer(("localhost", int(sys.argv[1])), MainHandler)
+    # write a bunch of empty lines first, which should be ignored by `xprocess` (see #13)
+    for _ in range(100):
+        sys.stderr.write("\n")
+    sys.stderr.write("started\n")
+    sys.stderr.flush()
+    mainserver.serve_forever()
