2b1ee0f025334668cd604516835ab0b5c7df7ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:59 +0000

    Add package nestersru/1.1.2/nestersru-1.1.2.tar.gz

---
+++ packages/nestersru/nestersru-1.1.2.tar.gz/nestersru-1.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+       name    = 'nestersru',
+       version = '1.1.2',
+       py_modules =['nestersru'],
+       author = 'hfpython',
+       author_email ='hfpython@headfirstlabs.com',
+       url          = 'http://www.headfirstlabs.com',
+       description   = 'A simple printer of nested lists',
+
+       )
