63d7e78dacfef8de7b65c16e5c056eba0f522164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:00 +0000

    Add package authentication/1.0.1/authentication-1.0.1.tar.gz

---
+++ packages/authentication/authentication-1.0.1.tar.gz/authentication-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r+") as fh:
+    long_description = fh.read()
+
+setup(
+    name='authentication',
+    version='1.0.1',
+    packages=find_packages(),
+    package_data={'authentication': ['templates/*']},
+    install_requires=['Flask >= 1.0', 'pycryptodome', 'wtforms', 'passlib', 'redis'],
+    url='',
+    license='MIT',
+    author='Kateryna Melnykova',
+    author_email='forkatemelnikova@gmail.com',
+    description='Learning authentication',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
