e45b32cff9c4e5cbc0268c3a880f97e2e602e285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package phigaro/2.1.10/phigaro-2.1.10.tar.gz

---
+++ packages/phigaro/phigaro-2.1.10.tar.gz/phigaro-2.1.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='phigaro',
+      description='Phigaro is a scalable command-line tool for predictions phages and prophages '
+                  'from nucleid acid sequences (including metagenomes) and '
+                  'is based on phage genes HMMs and a smoothing window algorithm.',
+      version="2.1.10",
+      license='MIT',
+      author='E.Starikova, N.Pryanichnikov, P.Tikhonova',
+      author_email='hed.robin@gmail.com',
+      url='https://github.com/bobeobibo/phigaro',
+      packages=['phigaro',
+                'phigaro.finder',
+                'phigaro.cli',
+                'phigaro.batch',
+                'phigaro.batch.task',
+                'phigaro.misc',
+    'phigaro.to_html',
+                ],
+      package_data = {
+        '': ['*.pickle', 'README.md']
+      },
+      entry_points={
+          'console_scripts': [
+              'phigaro = phigaro.cli.batch:main',
+              'phigaro-setup = phigaro.cli.helper:main',
+          ]
+      }, install_requires=['numpy', 'six>=1.7.0', 'pandas>=0.23.4','sh', 'singleton', 'PyYAML>=5.1', 'future', 'argparse', 'numpy', 'plotly', 'bs4', 'beautifulsoup4>=4.4.0', 'lxml','biopython']
+)
