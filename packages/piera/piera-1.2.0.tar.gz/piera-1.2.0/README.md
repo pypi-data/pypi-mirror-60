e92ceb47c588853523a94b3ace5e2834e3539641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package piera/1.2.0/piera-1.2.0.tar.gz

---
+++ packages/piera/piera-1.2.0.tar.gz/piera-1.2.0/README.md
@@ -0,0 +1,42 @@
+# Piera
+Piera is a lightweight, pure-Python [Hiera](http://docs.puppetlabs.com/hiera/) parser. It was built to help bridge the gap between Puppet/Hiera and Python system scripts. Piera is currently not feature complete, lacking some less-used interoplation and loading features (feel free to contribute!)
+
+## Why?
+Piera was built at [Braintree](http://github.com/braintree) to help us bridge a gap of emerging Python system scripts, and a historical storage of Puppet/Hiera data.
+
+## Install It
+
+### PyPi
+`pip install piera`
+
+### Manual
+```bash
+git clone git@github.com:b1naryth1ef/piera.git
+cd piera
+python setup.py install
+```
+
+## Usage
+```python
+import piera
+
+h = piera.Hiera("my_hiera.yaml")
+
+# You can use piera to simply interact with your structured Hiera data
+
+# key: 'value'
+assert h.get("key") == "value"
+
+# key_alias: '%{alias('key')}'
+assert h.get("key_alias") == "value"
+
+# key_hiera: 'OHAI %{hiera('key_alias')}'
+assert h.get("key_hiera") == "OHAI value"
+
+# Give piera context
+assert h.get("my_context_based_key", name='test01', environment='qa') == "context is great!"
+```
+
+### Requirements
+
+* Python 2.7+
---
+++ packages/piera/piera-1.2.0.tar.gz/piera-1.2.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'piera',
+]
+
+requires = [
+    'pyyaml'
+]
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='piera',
+    version='1.2.0',
+    description='a python hiera parser',
+    long_description=readme + '\n\n',
+    long_description_content_type="text/markdown",
+    author='Andrei Zbikowski',
+    author_email='andrei.zbikowski@gmail.com',
+    url='http://github.com/b1naryth1ef/piera',
+    packages=packages,
+    package_data={"": ["README.md"]},
+    package_dir={'piera': 'piera'},
+    include_package_data=True,
+    install_requires=requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
