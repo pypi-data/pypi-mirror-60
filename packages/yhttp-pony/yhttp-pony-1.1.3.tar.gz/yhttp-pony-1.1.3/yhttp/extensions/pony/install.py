98ea77e878934698863d1fda5bf53d8a9a387e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:13 +0000

    Add package yhttp-pony/1.1.3/yhttp-pony-1.1.3.tar.gz

---
+++ packages/yhttp-pony/yhttp-pony-1.1.3.tar.gz/yhttp-pony-1.1.3/yhttp/extensions/pony/install.py
@@ -0,0 +1,37 @@
+from pony.orm import Database
+
+from .cli import DatabaseCLI
+from . import uri
+
+
+def install(app, db=None):
+    app.cliarguments.append(DatabaseCLI)
+    if db is None:
+        db = Database()
+
+    app.db = db
+
+    @app.when
+    def ready(app):
+        settings = app.settings
+
+        if 'db' not in settings:
+            raise ValueError(
+                'Please provide db.url configuration entry, for example: ' \
+                'postgres://:@/dbname'
+            )
+
+        url = uri.parse(settings.db.url)
+        db.bind(**url)
+        db.generate_mapping(create_tables=True)
+
+    @app.when
+    def shutdown(app):
+        app.db.disconnect()
+        if app.db.provider is not None:
+            app.db.provider.disconnect()
+            app.db.provider = None
+
+        app.db.schema = None
+    return db
+
