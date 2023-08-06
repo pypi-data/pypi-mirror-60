b7f90193c67d457ced7a38c10e16afbcf9b37712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:26 +0000

    Add package TEtranscripts/2.1.4/TEtranscripts-2.1.4.tar.gz

---
+++ packages/TEtranscripts/TEtranscripts-2.1.4.tar.gz/TEtranscripts-2.1.4/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+def readme():
+      with open('README.rst') as f:
+               return f.read()
+
+setup(name='TEtranscripts',
+      version='2.1.4',
+      description='Tools for estimating differential enrichment of Transposable Elements and other highly repetitive regions',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'Environment :: Console',
+          'Natural Language :: English',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: MacOS',
+          'Operating System :: Unix'
+      ],
+      keywords='TE transposable element differential enrichment',
+      url='http://hammelllab.labsites.cshl.edu/software#TEtranscripts',
+      author='Ying Jin, Eric Paniagua, Oliver Tam, Molly Hammell',
+      author_email='yjin@cshl.edu',
+      license='GPLv3',
+      packages=[
+          'TEToolkit',
+          'TEToolkit.IO',
+          'TEToolkit.ShortRead'
+      ],
+      platforms=[
+          'Linux',
+          'MacOS'
+      ],
+      install_requires=[
+          'argparse',
+          'pysam>=0.9'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      scripts=[
+          'bin/TEtranscripts',
+          'bin/TEcount'
+      ]
+)
