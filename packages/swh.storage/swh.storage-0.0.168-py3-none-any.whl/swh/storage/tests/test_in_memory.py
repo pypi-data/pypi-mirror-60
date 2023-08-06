af3bd7ea1192aac0ee37c2511de28c560fd515f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:37 +0000

    Add package swh.storage/0.0.168/swh.storage-0.0.168-py3-none-any.whl

---
+++ packages/swh.storage/swh.storage-0.0.168-py3-none-any.whl/swh/storage/tests/test_in_memory.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import pytest
+
+from swh.storage import get_storage
+from swh.storage.tests.test_storage import (  # noqa
+    TestStorage, TestStorageGeneratedData)
+
+
+# tests are executed using imported classes (TestStorage and
+# TestStorageGeneratedData) using overloaded swh_storage fixture
+# below
+
+@pytest.fixture
+def swh_storage():
+    storage_config = {
+        'cls': 'memory',
+        'journal_writer': {
+            'cls': 'memory',
+        },
+    }
+    storage = get_storage(**storage_config)
+    return storage
