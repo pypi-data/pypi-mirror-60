decdd278d8f084975d4c7d545474803147dddea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package dsnd-gb-distributions/1.0/dsnd_gb_distributions-1.0.tar.gz

---
+++ packages/dsnd-gb-distributions/dsnd_gb_distributions-1.0.tar.gz/dsnd_gb_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_gb_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_gb_distributions'],
+      author='Bruno Guimarães de Oliveira',
+      author_email='oliveira.ufrj11@gmail.com',
+      zip_safe=False)
