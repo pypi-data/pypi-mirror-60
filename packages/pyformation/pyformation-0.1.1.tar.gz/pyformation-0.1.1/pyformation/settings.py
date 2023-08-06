6d21a9889170c7a35b133ac8edb0ae15e212960b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:08 +0000

    Add package pyformation/0.1.1/pyformation-0.1.1.tar.gz

---
+++ packages/pyformation/pyformation-0.1.1.tar.gz/pyformation-0.1.1/pyformation/settings.py
@@ -0,0 +1,11 @@
+from pyformation.utils.get_aws_data import get_regions
+
+DEFAULT_SPEC_LIB = "specifications/"
+PROJECT_DIR = "projects/"
+
+PROJECT_SUB_DIRS = [
+    'build',
+    'custom',
+    'stacks',
+    'tests'
+]
---
+++ packages/pyformation/pyformation-0.1.1.tar.gz/pyformation-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='pyformation',
+      version='0.1.1',
+      description='CloudFormation eloquently expressed as Python',
+      url='http://github.com/dennisvink/pyformation',
+      author='Dennis Vink',
+      author_email='dennis@drvink.com',
+      license='MIT',
+      packages=['pyformation'],
+      install_requires=[
+            'requests',
+            'beautifulsoup4',
+            'pyfiglet',
+            'PyInquirer',
+            'lxml',
+            'jinja2',
+            'GitPython',
+            'python-dotenv',
+            'moto'
+      ],
+      entry_points={
+            'console_scripts': ['pyformation-admin=pyformation.bin.pyformation_admin:controller'],
+      },
+      zip_safe=False)
