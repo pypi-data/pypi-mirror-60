00f44d62e2d1d7ea38e5b6aee8c8741d28f58773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:28 +0000

    Add package spotifypy/0.0.2/spotifypy-0.0.2.tar.gz

---
+++ packages/spotifypy/spotifypy-0.0.2.tar.gz/spotifypy-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import setuptools
+
+
+setup(
+    name='spotifypy',
+    version='0.0.2',
+    author='MrWalrus',
+    description=('No', 'description'),
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
