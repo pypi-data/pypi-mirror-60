cac707c0d0760bd1b8e46ada7d4f21cd3519de8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:35 +0000

    Add package xrandroll/0.1.2/xrandroll-0.1.2.tar.gz

---
+++ packages/xrandroll/xrandroll-0.1.2.tar.gz/xrandroll-0.1.2/README.md
@@ -0,0 +1,23 @@
+# XRandRoll
+
+None of the existing display configuration tools does what I think is "the right thing".
+So I went and wrote one.
+
+## The Right Thing
+
+* Don't start from a stored config, use xrandr to read the systems' current state
+* Allow creating "profiles" that will get applied smartly (not there yet)
+* Generate a xrandr invocation to reflect the desired configuration
+* Allow per-monitor scaling
+* Allow arbitrary monitor positioning
+* Implement "scale everything so all the pixels are the same size" (not done yet)
+
+## To try:
+
+If you have PySide2: `python -m xrandroll` in the folder where you cloned it.
+
+## TODO:
+
+* Implement other things
+* Make it a proper app, with installation and whatnot
+* Forget about it forever
---
+++ packages/xrandroll/xrandroll-0.1.2.tar.gz/xrandroll-0.1.2/xrandroll/monitor_item.py
@@ -0,0 +1,71 @@
+from PySide2.QtCore import Qt, QObject
+from PySide2.QtWidgets import QGraphicsRectItem, QGraphicsTextItem
+from PySide2.QtGui import QBrush
+
+
+class MonitorItem(QGraphicsRectItem, QObject):
+    z = 0
+
+    def __init__(self, *a, **kw):
+        data = kw.pop("data")
+        self.name = kw.pop("name")
+        self.window = kw.pop("window")
+        super().__init__(0, 0, 0, 0)
+        self.setAcceptedMouseButtons(Qt.LeftButton)
+        self.label = QGraphicsTextItem("", self)
+        self.bottom_edge = QGraphicsRectItem(0, 0, 0, 0, self)
+        self.bottom_edge.setBrush(QBrush("red", Qt.SolidPattern))
+        self.update_visuals(data)
+
+    def update_visuals(self, data):
+        self.setRect(0, 0, data["res_x"], data["res_y"])
+        self.setPos(data["pos_x"], data["pos_y"])
+        if data["orientation"] == 0:
+            self.bottom_edge.setRect(0, data["res_y"] - 50, data["res_x"], 50)
+        elif data["orientation"] == 1:
+            self.bottom_edge.setRect(data["res_x"] - 50, 0, 50, data["res_y"])
+        elif data["orientation"] == 2:
+            self.bottom_edge.setRect(0, 0, data["res_x"], 50)
+        elif data["orientation"] == 3:
+            self.bottom_edge.setRect(0, 0, 50, data["res_y"])
+        if data["replica_of"]:
+            label_text = f"{self.name} [{','.join(data['replica_of'])}]"
+        else:
+            label_text = self.name
+        self.label.setPlainText(label_text)
+        label_scale = min(
+            self.rect().width() / self.label.boundingRect().width(),
+            self.rect().height() / self.label.boundingRect().height(),
+        )
+        self.label.setScale(label_scale)
+        if data["enabled"]:
+            if data["primary"]:
+                self.setBrush(QBrush("#eee8d5", Qt.SolidPattern))
+                self.setZValue(1000)
+            else:
+                self.setBrush(QBrush("#ffffff", Qt.SolidPattern))
+                self.setZValue(self.z)
+                self.z -= 1
+        else:
+            self.setBrush(QBrush("#f1f1f1", Qt.SolidPattern))
+            self.setZValue(-1000)
+
+    def mousePressEvent(self, event):
+        self.window.pos_label.show()
+        self.setCursor(Qt.ClosedHandCursor)
+        self.orig_pos = self.pos()
+        self.window.ui.screenCombo.setCurrentText(self.name)
+
+    def mouseReleaseEvent(self, event):
+        self.setCursor(Qt.OpenHandCursor)
+        self.window.monitor_moved()
+        self.window.pos_label.hide()
+
+    def mouseMoveEvent(self, event):
+        view = event.widget().parent()
+        click_pos = event.buttonDownScreenPos(Qt.LeftButton)
+        current_pos = event.screenPos()
+        self.setPos(
+            view.mapToScene(view.mapFromScene(self.orig_pos) + current_pos - click_pos)
+        )
+        self.window.show_pos(int(self.pos().x()), int(self.pos().y()))
