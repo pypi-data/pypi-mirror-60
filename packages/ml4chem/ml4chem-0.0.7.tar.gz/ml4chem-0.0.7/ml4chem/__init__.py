ae3655162b3abb63dfc9f93389c2d7fd57eb14db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:42 +0000

    Add package ml4chem/0.0.7/ml4chem-0.0.7.tar.gz

---
+++ packages/ml4chem/ml4chem-0.0.7.tar.gz/ml4chem-0.0.7/ml4chem/__init__.py
@@ -0,0 +1,5 @@
+from ml4chem.potentials import Potentials
+
+
+__all__ = ["Potentials"]
+__version__ = "0.0.7"
---
+++ packages/ml4chem/ml4chem-0.0.7.tar.gz/ml4chem-0.0.7/ml4chem/features/__init__.py
@@ -0,0 +1,6 @@
+from ml4chem.features.autoencoders import LatentFeatures
+from ml4chem.features.cartesian import Cartesian
+from ml4chem.features.coulombmatrix import CoulombMatrix
+from ml4chem.features.gaussian import Gaussian
+
+__all__ = ["LatentFeatures", "Cartesian", "CoulombMatrix", "Gaussian"]
