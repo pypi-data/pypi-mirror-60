043a79a223250ed88829e4d02ec9b23f7590fe59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:40 +0000

    Add package GPro/1.0.1/GPro-1.0.1.tar.gz

---
+++ packages/GPro/GPro-1.0.1.tar.gz/GPro-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GPro',
+    version='1.0.1',
+    url='https://github.com/chariff/GPro',
+    packages=find_packages(),
+    author='Chariff Alkhassim',
+    author_email='chariff.alkhassim@gmail.com',
+    description='Preference Learning with Gaussian Processes.',
+    long_description='Python implementation of a probabilistic kernel approach '
+                     'to preference learning based on Gaussian processes.',
+    install_requires=[
+        "numpy >= 1.9.0",
+        "scipy >= 0.14.0",
+        "pandas >= 0.24.0"
+    ],
+    license='MIT',
+)
+
+
