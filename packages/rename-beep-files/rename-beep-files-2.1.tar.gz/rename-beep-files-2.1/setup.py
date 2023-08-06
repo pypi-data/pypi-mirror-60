c1060e98ff67a202a5df549463370883997afb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:02 +0000

    Add package rename-beep-files/2.1/rename-beep-files-2.1.tar.gz

---
+++ packages/rename-beep-files/rename-beep-files-2.1.tar.gz/rename-beep-files-2.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+CLIENT_VERSION = "2.1"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rename-beep-files",
+    version=CLIENT_VERSION,
+    scripts=['rename-beep-files'],
+    author="Mattia Sanchioni",
+    author_email="mattia.sanchioni.dev@gmail.com",
+    description="Rename Beep files",
+    keywords="rename files beep Beep",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mett96/rename-beep-files",
+    packages=setuptools.find_packages(),
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
