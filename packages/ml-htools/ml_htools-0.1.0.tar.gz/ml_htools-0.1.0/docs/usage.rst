fc94b8cafefca29129d147ce358169d99e5b91e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:41 +0000

    Add package ml-htools/0.1.0/ml_htools-0.1.0.tar.gz

---
+++ packages/ml-htools/ml_htools-0.1.0.tar.gz/ml_htools-0.1.0/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use ML Tools in a project::
+
+    import ml_htools
---
+++ packages/ml-htools/ml_htools-0.1.0.tar.gz/ml_htools-0.1.0/ml_htools/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ML Tools."""
+
+__author__ = """Harrison Mamin"""
+__email__ = 'Hmamin2@dons.usfca.edu'
+__version__ = '0.1.0'
