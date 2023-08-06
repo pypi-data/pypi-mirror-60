6e0e9e2af5ef8b4c582bebeefdb7da0a9341c397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:50 +0000

    Add package pydeconz/69/pydeconz-69-py3-none-any.whl

---
+++ packages/pydeconz/pydeconz-69-py3-none-any.whl/pydeconz/api.py
@@ -0,0 +1,42 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class APIItems:
+    """Base class for a map of API Items."""
+
+    def __init__(self, raw, request, path, item_cls) -> None:
+        self._request = request
+        self._path = path
+        self._item_cls = item_cls
+        self._items = {}
+        self.process_raw(raw)
+
+    def update(self) -> None:
+        raw = self._request("get", self._path)
+        self.process_raw(raw)
+
+    def process_raw(self, raw: dict, **kwargs) -> None:
+        for id, raw_item in raw.items():
+            obj = self._items.get(id)
+
+            if obj is not None:
+                obj.async_update(raw_item, **kwargs)
+            else:
+                self._items[id] = self._item_cls(id, raw_item, self._request)
+
+    def items(self):
+        return self._items.items()
+
+    def keys(self):
+        return self._items.keys()
+
+    def values(self):
+        return self._items.values()
+
+    def __getitem__(self, obj_id: str):
+        return self._items[obj_id]
+
+    def __iter__(self):
+        return iter(self._items)
