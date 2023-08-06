e37e7b36951940a7ec0290ab44d84e7e7b5ed89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:08 +0000

    Add package UsingPy/0.1/UsingPy-0.1.tar.gz

---
+++ packages/UsingPy/UsingPy-0.1.tar.gz/UsingPy-0.1/UsingPy/__init__.py
@@ -0,0 +1,9 @@
+# Beginning Code
+
+def using(imports,everything="0"):
+	if everything = 1:
+		import imports 
+		from imports import *
+	else:
+		import imports
+		
---
+++ packages/UsingPy/UsingPy-0.1.tar.gz/UsingPy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='UsingPy',
+      version='0.1',
+      description='C# Using in Python',
+      url='http://github.com/BatDev0/UsingPy',
+      author='BatDev0',
+      author_email='Brody@tntsplash.net',
+      license='MIT',
+      packages=['UsingPy'],
+      zip_safe=False)
