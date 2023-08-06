d1c452ef2a4937c6337d208a7a742e4005d75deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:27 +0000

    Add package bonifacio-probability/0.1/bonifacio_probability-0.1.tar.gz

---
+++ packages/bonifacio-probability/bonifacio_probability-0.1.tar.gz/bonifacio_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bonifacio_probability',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['bonifacio_probability'],
+      author='Luis Bonifacio',
+      author_email='luis.g.bonifacio@gmail.com',
+      zip_safe=False)
