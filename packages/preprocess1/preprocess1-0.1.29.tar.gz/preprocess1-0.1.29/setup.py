b23434c1cadac6dda8358b43053802706bb2284b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:24 +0000

    Add package preprocess1/0.1.29/preprocess1-0.1.29.tar.gz

---
+++ packages/preprocess1/preprocess1-0.1.29.tar.gz/preprocess1-0.1.29/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+requirements = ["scikit-learn","datetime","datefinder"] # add plotly later on
+
+# this is where you mention dependencies for your package
+# only add libraries that are not by default installed by python , so if your package only
+# used say pandas or numpy , you dont need to put them here as they are already installed 
+
+setup(
+    name="preprocess1",
+    version="0.1.29",
+    author="Fahad Akbar",
+    author_email="fahadakbar@gmail.com",
+    description="A package to provide preprocessing steps for Machine Learning in an super easy way !",
+    #long_description=readme,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/mfahadakbar",
+    packages=find_packages(),
+    install_requires=requirements,
+    #classifiers=[
+    #    "Programming Language :: Python :: 3.7",
+    #    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    #],
+)
