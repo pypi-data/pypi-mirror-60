7b4606a1888cc04770f7f3a822f60b3cd4a15e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:52 +0000

    Add package swh.journal/0.0.25/swh.journal-0.0.25-py3-none-any.whl

---
+++ packages/swh.journal/swh.journal-0.0.25-py3-none-any.whl/swh/journal/tests/test_cli.py
@@ -0,0 +1,217 @@
+# Copyright (C) 2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import functools
+import logging
+import re
+import tempfile
+from subprocess import Popen
+from typing import Any, Dict, Tuple
+from unittest.mock import patch
+
+from click.testing import CliRunner
+from confluent_kafka import Producer
+import pytest
+
+from swh.objstorage.backends.in_memory import InMemoryObjStorage
+from swh.storage.in_memory import InMemoryStorage
+
+from swh.journal.cli import cli
+from swh.journal.serializers import key_to_kafka, value_to_kafka
+
+
+logger = logging.getLogger(__name__)
+
+
+CLI_CONFIG = '''
+storage:
+    cls: memory
+    args: {}
+objstorage_src:
+    cls: mocked
+    args:
+        name: src
+objstorage_dst:
+    cls: mocked
+    args:
+        name: dst
+'''
+
+
+@pytest.fixture
+def storage():
+    """An instance of swh.storage.in_memory.InMemoryStorage that gets injected
+    into the CLI functions."""
+    storage = InMemoryStorage()
+    with patch('swh.journal.cli.get_storage') as get_storage_mock:
+        get_storage_mock.return_value = storage
+        yield storage
+
+
+def invoke(catch_exceptions, args):
+    runner = CliRunner()
+    with tempfile.NamedTemporaryFile('a', suffix='.yml') as config_fd:
+        config_fd.write(CLI_CONFIG)
+        config_fd.seek(0)
+        args = ['-C' + config_fd.name] + args
+        result = runner.invoke(cli, args, obj={'log_level': logging.DEBUG})
+    if not catch_exceptions and result.exception:
+        print(result.output)
+        raise result.exception
+    return result
+
+
+def test_replay(
+        storage: InMemoryStorage,
+        kafka_prefix: str,
+        kafka_server: Tuple[Popen, int]):
+    (_, port) = kafka_server
+    kafka_prefix += '.swh.journal.objects'
+
+    producer = Producer({
+        'bootstrap.servers': 'localhost:{}'.format(port),
+        'client.id': 'test-producer',
+        'enable.idempotence': 'true',
+    })
+
+    snapshot = {'id': b'foo', 'branches': {
+        b'HEAD': {
+            'target_type': 'revision',
+            'target': b'\x01'*20,
+        }
+    }}  # type: Dict[str, Any]
+    producer.produce(
+        topic=kafka_prefix+'.snapshot',
+        key=key_to_kafka(snapshot['id']),
+        value=value_to_kafka(snapshot),
+    )
+    producer.flush()
+
+    logger.debug('Flushed producer')
+
+    result = invoke(False, [
+        'replay',
+        '--broker', '127.0.0.1:%d' % port,
+        '--group-id', 'test-cli-consumer',
+        '--prefix', kafka_prefix,
+        '--max-messages', '1',
+    ])
+    expected = r'Done.\n'
+    assert result.exit_code == 0, result.output
+    assert re.fullmatch(expected, result.output, re.MULTILINE), result.output
+
+    assert storage.snapshot_get(snapshot['id']) == {
+        **snapshot, 'next_branch': None}
+
+
+def _patch_objstorages(names):
+    objstorages = {name: InMemoryObjStorage() for name in names}
+
+    def get_mock_objstorage(cls, args):
+        assert cls == 'mocked', cls
+        return objstorages[args['name']]
+
+    def decorator(f):
+        @functools.wraps(f)
+        @patch('swh.journal.cli.get_objstorage')
+        def newf(get_objstorage_mock, *args, **kwargs):
+            get_objstorage_mock.side_effect = get_mock_objstorage
+            f(*args, objstorages=objstorages, **kwargs)
+
+        return newf
+
+    return decorator
+
+
+def _fill_objstorage_and_kafka(kafka_port, kafka_prefix, objstorages):
+    producer = Producer({
+        'bootstrap.servers': '127.0.0.1:{}'.format(kafka_port),
+        'client.id': 'test-producer',
+        'enable.idempotence': 'true',
+    })
+
+    contents = {}
+    for i in range(10):
+        content = b'\x00'*19 + bytes([i])
+        sha1 = objstorages['src'].add(content)
+        contents[sha1] = content
+        producer.produce(
+            topic=kafka_prefix+'.content',
+            key=key_to_kafka(sha1),
+            value=key_to_kafka({
+                'sha1': sha1,
+                'status': 'visible',
+            }),
+        )
+
+    producer.flush()
+
+    return contents
+
+
+@_patch_objstorages(['src', 'dst'])
+def test_replay_content(
+        objstorages,
+        storage: InMemoryStorage,
+        kafka_prefix: str,
+        kafka_server: Tuple[Popen, int]):
+    (_, kafka_port) = kafka_server
+    kafka_prefix += '.swh.journal.objects'
+
+    contents = _fill_objstorage_and_kafka(
+        kafka_port, kafka_prefix, objstorages)
+
+    result = invoke(False, [
+        'content-replay',
+        '--broker', '127.0.0.1:%d' % kafka_port,
+        '--group-id', 'test-cli-consumer',
+        '--prefix', kafka_prefix,
+        '--max-messages', '10',
+    ])
+    expected = r'Done.\n'
+    assert result.exit_code == 0, result.output
+    assert re.fullmatch(expected, result.output, re.MULTILINE), result.output
+
+    for (sha1, content) in contents.items():
+        assert sha1 in objstorages['dst'], sha1
+        assert objstorages['dst'].get(sha1) == content
+
+
+@_patch_objstorages(['src', 'dst'])
+def test_replay_content_exclude(
+        objstorages,
+        storage: InMemoryStorage,
+        kafka_prefix: str,
+        kafka_server: Tuple[Popen, int]):
+    (_, kafka_port) = kafka_server
+    kafka_prefix += '.swh.journal.objects'
+
+    contents = _fill_objstorage_and_kafka(
+        kafka_port, kafka_prefix, objstorages)
+
+    excluded_contents = list(contents)[0::2]  # picking half of them
+    with tempfile.NamedTemporaryFile(mode='w+b') as fd:
+        fd.write(b''.join(sorted(excluded_contents)))
+
+        fd.seek(0)
+
+        result = invoke(False, [
+            'content-replay',
+            '--broker', '127.0.0.1:%d' % kafka_port,
+            '--group-id', 'test-cli-consumer',
+            '--prefix', kafka_prefix,
+            '--max-messages', '10',
+            '--exclude-sha1-file', fd.name,
+        ])
+    expected = r'Done.\n'
+    assert result.exit_code == 0, result.output
+    assert re.fullmatch(expected, result.output, re.MULTILINE), result.output
+
+    for (sha1, content) in contents.items():
+        if sha1 in excluded_contents:
+            assert sha1 not in objstorages['dst'], sha1
+        else:
+            assert sha1 in objstorages['dst'], sha1
+            assert objstorages['dst'].get(sha1) == content
---
+++ packages/swh.journal/swh.journal-0.0.25-py3-none-any.whl/swh/journal/tests/test_write_replay.py
@@ -0,0 +1,140 @@
+# Copyright (C) 2019 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import functools
+
+import attr
+from hypothesis import given, settings, HealthCheck
+from hypothesis.strategies import lists
+
+from swh.model.hypothesis_strategies import object_dicts
+from swh.storage.in_memory import InMemoryStorage
+from swh.storage import HashCollision
+
+from swh.journal.replay import process_replay_objects
+from swh.journal.replay import process_replay_objects_content
+
+from .utils import MockedJournalClient, MockedKafkaWriter
+
+
+def empty_person_name_email(rev_or_rel):
+    """Empties the 'name' and 'email' fields of the author/committer fields
+    of a revision or release; leaving only the fullname."""
+    if getattr(rev_or_rel, 'author', None):
+        rev_or_rel = attr.evolve(
+            rev_or_rel,
+            author=attr.evolve(
+                rev_or_rel.author,
+                name=b'',
+                email=b'',
+            )
+        )
+
+    if getattr(rev_or_rel, 'committer', None):
+        rev_or_rel = attr.evolve(
+            rev_or_rel,
+            committer=attr.evolve(
+                rev_or_rel.committer,
+                name=b'',
+                email=b'',
+            )
+        )
+
+    return rev_or_rel
+
+
+@given(lists(object_dicts(), min_size=1))
+@settings(suppress_health_check=[HealthCheck.too_slow])
+def test_write_replay_same_order_batches(objects):
+    queue = []
+    replayer = MockedJournalClient(queue)
+
+    storage1 = InMemoryStorage()
+    storage1.journal_writer = MockedKafkaWriter(queue)
+
+    for (obj_type, obj) in objects:
+        obj = obj.copy()
+        if obj_type == 'origin_visit':
+            storage1.origin_add_one({'url': obj['origin']})
+            storage1.origin_visit_upsert([obj])
+        else:
+            method = getattr(storage1, obj_type + '_add')
+            try:
+                method([obj])
+            except HashCollision:
+                pass
+
+    queue_size = len(queue)
+    assert replayer.max_messages == 0
+    replayer.max_messages = queue_size
+
+    storage2 = InMemoryStorage()
+    worker_fn = functools.partial(process_replay_objects, storage=storage2)
+    nb_messages = 0
+    while nb_messages < queue_size:
+        nb_messages += replayer.process(worker_fn)
+
+    assert replayer.consumer.committed
+
+    for attr_name in ('_contents', '_directories',
+                      '_snapshots', '_origin_visits', '_origins'):
+        assert getattr(storage1, attr_name) == getattr(storage2, attr_name), \
+            attr_name
+
+    # When hypothesis generates a revision and a release with same
+    # author (or committer) fullname but different name or email, then
+    # the storage will use the first name/email it sees.
+    # This first one will be either the one from the revision or the release,
+    # and since there is no order guarantees, storage2 has 1/2 chance of
+    # not seeing the same order as storage1, therefore we need to strip
+    # them out before comparing.
+    for attr_name in ('_revisions', '_releases'):
+        items1 = {k: empty_person_name_email(v)
+                  for (k, v) in getattr(storage1, attr_name).items()}
+        items2 = {k: empty_person_name_email(v)
+                  for (k, v) in getattr(storage2, attr_name).items()}
+        assert items1 == items2, attr_name
+
+
+# TODO: add test for hash collision
+
+
+@given(lists(object_dicts(), min_size=1))
+@settings(suppress_health_check=[HealthCheck.too_slow])
+def test_write_replay_content(objects):
+
+    queue = []
+    replayer = MockedJournalClient(queue)
+
+    storage1 = InMemoryStorage()
+    storage1.journal_writer = MockedKafkaWriter(queue)
+
+    contents = []
+    for (obj_type, obj) in objects:
+        obj = obj.copy()
+        if obj_type == 'content':
+            # avoid hash collision
+            if not storage1.content_find(obj):
+                storage1.content_add([obj])
+                contents.append(obj)
+
+    queue_size = len(queue)
+    assert replayer.max_messages == 0
+    replayer.max_messages = queue_size
+
+    storage2 = InMemoryStorage()
+    worker_fn = functools.partial(process_replay_objects_content,
+                                  src=storage1.objstorage,
+                                  dst=storage2.objstorage)
+    nb_messages = 0
+    while nb_messages < queue_size:
+        nb_messages += replayer.process(worker_fn)
+
+    # only content with status visible will be copied in storage2
+    expected_objstorage_state = {
+        c['sha1']: c['data'] for c in contents if c['status'] == 'visible'
+    }
+
+    assert expected_objstorage_state == storage2.objstorage.state
