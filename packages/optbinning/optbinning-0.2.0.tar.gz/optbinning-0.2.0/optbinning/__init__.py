31baeb5b4279550c9685bb4ff3d2d3632e75916d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:31 +0000

    Add package optbinning/0.2.0/optbinning-0.2.0.tar.gz

---
+++ packages/optbinning/optbinning-0.2.0.tar.gz/optbinning-0.2.0/optbinning/__init__.py
@@ -0,0 +1,11 @@
+from .binning import OptimalBinning
+from .continuous_binning import ContinuousOptimalBinning
+from .multiclass_binning import MulticlassOptimalBinning
+
+from .binning_process import BinningProcess
+
+
+__all__ = ['OptimalBinning',
+           'ContinuousOptimalBinning',
+           'MulticlassOptimalBinning',
+           'BinningProcess']
