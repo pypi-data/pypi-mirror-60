c3249eb4498354b9dee357d37e847fa7dfb396f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:23 +0000

    Add package oelint-adv/1.9.0/oelint_adv-1.9.0.tar.gz

---
+++ packages/oelint-adv/oelint_adv-1.9.0.tar.gz/oelint_adv-1.9.0/oelint_adv/cls_rule.py
@@ -0,0 +1,55 @@
+import importlib
+import inspect
+import os
+import pkgutil
+
+
+class Rule():
+    def __init__(self, id="", severity="", message=""):
+        self.ID = id
+        self.Severity = severity
+        self.Msg = message
+
+    def check(self, _file, stash):
+        return []
+
+    def fix(self, _file, stash):
+        return []
+
+    def finding(self, _file, _line, override_msg=None):
+        return ["{}:{}:{}:{}:{}".format(os.path.abspath(_file), _line, self.Severity, self.ID, override_msg or self.Msg)]
+
+    def __repr__(self):
+        return "{}".format(self.ID)
+
+    def FormatMsg(self, *args):
+        return self.Msg.format(*args)
+
+    def OverrideMsg(self, newmsg):
+        self.Msg = newmsg
+
+
+def load_rules(add_rules=[]):
+    res = []
+    _path_list = {
+        "base": os.path.dirname(os.path.abspath(__file__))
+    }
+    for ar in add_rules:
+        _path_list[ar] = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "rule_{}".format(ar))
+    for k, v in _path_list.items():
+        packages = pkgutil.walk_packages(path=[v])
+        for importer, name, is_package in packages:
+            if k != "base":
+                name = os.path.basename(v) + "." + name
+            mod = importlib.import_module(name)
+            for m in inspect.getmembers(mod, inspect.isclass):
+                try:
+                    if issubclass(m[1], Rule):
+                        inst = m[1]()
+                        if inst.ID:
+                            res.append(inst)
+                except Exception as ex:
+                    pass
+                    # print("{} -> {}".format(name, ex))
+    return res
