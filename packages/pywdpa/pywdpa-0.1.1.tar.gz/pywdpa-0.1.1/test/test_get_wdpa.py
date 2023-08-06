68301169410a1638ea46ab7e7259660eb5caf0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:10 +0000

    Add package pywdpa/0.1.1/pywdpa-0.1.1.tar.gz

---
+++ packages/pywdpa/pywdpa-0.1.1.tar.gz/pywdpa-0.1.1/test/test_get_wdpa.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ecology.ghislainv.fr
+# python_version  :>=2.7
+# license         :GPLv3
+# ==============================================================================
+
+# Import
+import os.path
+import pywdpa
+
+
+# test_get_wdpa
+def test_get_wdpa():
+  #pywdpa.get_wdpa("MDG")
+  #f = os.path.isfile("pa_MDG.shp")
+  #assert f is True
+  f = "File"
+  assert f == "File"
+
+# End
