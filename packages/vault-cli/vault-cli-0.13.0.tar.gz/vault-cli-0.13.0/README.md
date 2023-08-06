3864991a28a13143903a0b46db70167b7b39be33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package vault-cli/0.13.0/vault-cli-0.13.0.tar.gz

---
+++ packages/vault-cli/vault-cli-0.13.0.tar.gz/vault-cli-0.13.0/README.md
@@ -0,0 +1,426 @@
+# CLI tool for Hashicorp Vault
+
+This tools allows simple interactions with the vault API, allowing
+configuration to be done in a separate step using a YAML configuration file.
+
+This is especially interesting if you interact with Hashicorp Vault from
+automated deployment tools.
+
+## Installation
+
+`pip install vault-cli`
+
+Vault-cli only works with python 3.6 and over.
+
+## Usage
+
+```
+Usage: vault [OPTIONS] COMMAND [ARGS]...
+
+  Interact with a Vault. See subcommands for details.
+
+  All arguments can be passed by environment variables:
+  VAULT_CLI_UPPERCASE_NAME (including VAULT_CLI_PASSWORD and
+  VAULT_CLI_TOKEN).
+
+Options:
+  -U, --url TEXT                  URL of the vault instance
+  --verify / --no-verify          Verify HTTPS certificate
+  --ca-bundle PATH                Location of the bundle containing the server
+                                  certificate to check against.
+  --login-cert PATH               Path to a public client certificate to use
+                                  for connecting to vault.
+  --login-cert-key PATH           Path to a private client certificate to use
+                                  for connecting to vault.
+  -T, --token-file PATH           File which contains the token to connect to
+                                  Vault. Configuration file can also contain a
+                                  "token" key.
+  -u, --username TEXT             Username used for userpass authentication
+  -w, --password-file PATH        Can read from stdin if "-" is used as
+                                  parameter. Configuration file can also
+                                  contain a "password" key.
+  -b, --base-path TEXT            Base path for requests
+  -s, --safe-write / --unsafe-write
+                                  When activated, you can't overwrite a secret
+                                  without passing "--force" (in commands
+                                  "set", "mv", etc)
+  --render / --no-render          Render templated values
+  -v, --verbose                   Use multiple times to increase verbosity
+  --config-file PATH              Config file to use. Use 'no' to disable
+                                  config file. Default value: first of
+                                  ./vault.yml, ~/.vault.yml, /etc/vault.yml
+  -V, --version
+  -h, --help                      Show this message and exit.
+
+Commands:
+  delete        Delete a single secret.
+  delete-all    Delete multiple secrets.
+  dump-config   Display settings in the format of a config file.
+  env           Launch a command, loading secrets in environment.
+  get           Return a single secret value.
+  get-all       Return multiple secrets.
+  list          List all the secrets at the given path.
+  lookup-token  Return information regarding the current token
+  mv            Recursively move secrets from source to destination path.
+  set           Set a single secret to the given value(s).
+  template      Render the given Jinja2 template and insert secrets in it.
+```
+
+## Authentication
+
+There are three ways to authenticate against the vault:
+- Username and password file: provide a username and a file to read the
+  password from. The file may be `-` for stdin.
+- Client certificate: provide the path to a certificate file.
+- Token: Bypass authentication step if you already have a valid token.
+
+## Showcase
+
+### Connect to https://vault.mydomain:8200/project and list the secrets
+```console
+$ vault --url=https://vault.mydomain:8200 --certificate=/etc/vault/certificate.key --base-path=project/ list
+['my_secret']
+```
+
+On the following examples, we'll be considering that we have a complete configuration file.
+
+### Read a secret in plain text (default)
+```console
+$ vault get my_secret
+qwerty
+```
+
+### Read a secret in yaml format
+```console
+$ vault get --yaml my_secret
+--- qwerty
+...
+```
+
+### Write a secret
+```console
+$ vault set my_other_secret supersecret
+Done
+```
+
+### Read/write a secret outside the base path
+```console
+$ export VAULT_CLI_BASE_PATH=myapp/
+$ vault set /global_secret sharedsecret
+Done
+$ vault get /global_secret
+sharedsecret
+$ vault get global_secret
+Error: Secret not found
+$ unset VAULT_CLI_BASE_PATH
+```
+
+
+### Write a secret via stdin.
+You can use this when the secret has multiple lines or starts with a "-"
+
+```console
+$ vault set third_secret --stdin
+----BEGIN SECRET KEY----
+...
+<hit ctrl+d to end stdin>
+Done
+
+vault get third_secret
+----BEGIN SECRET KEY----
+...
+```
+
+Identically, piping allows you to write the content of a file into the vault:
+
+```console
+$ cat my_certificate.key | vault set third_secret --stdin
+Done
+```
+
+### Write a secret using an invisible input prompt
+
+This will avoid your secrets to be displayed in plain text in your shell history.
+
+ ```console
+ $ vault set mykey --prompt
+ Please enter value for `mykey`:
+ Done
+ ```
+
+### Anything following "--" will not be seen as a flag even if it starts with a "-"
+```console
+$ vault set -- -secret-name -oh-so-secret
+Done
+
+$ vault get -- -secret-name
+-oh-so-secret
+```
+
+### Write a secret complex object
+```console
+$ vault set --yaml blob_secret "{code: supercode}"
+Done
+```
+
+### Write a secret list
+```console
+$ vault set list_secret secret1 secret2 secret3
+Done
+
+# (For complex types, yaml format is selected)
+$ vault get list_secret
+---
+- secret1
+- secret2
+- secret3
+```
+
+### Protect yourself from overwriting a secret by mistake
+
+```console
+vault set a b
+Done
+$ vault --safe-write set a c
+Error: Secret already exists at a. Use -f to force overwriting.
+$ vault --safe-write set -f a c
+Done
+```
+(`safe-write` can be set in your configuration file, see details below)
+
+### Get all values from the vault in a single command (yaml format)
+```console
+$ vault get-all
+---
+-secret-name: -oh-so-secret
+blob_secret:
+  code: supercode
+list_secret:
+- secret1
+- secret2
+- secret3
+my_other_secret: supersecret
+my_secret: qwerty
+third_secret: '----BEGIN SECRET KEY----
+
+  ...'
+```
+
+### Get a nested secret based on a path
+```console
+$ vault set test/my_folder_secret yaysecret
+Done
+
+$ vault get-all test/my_folder_secret
+---
+test:
+  my_folder_secret: yaysecret
+```
+
+### Get all values recursively from several folders in a single command (yaml format)
+```console
+$ vault get-all test my_secret
+---
+my_secret: qwerty
+test:
+  my_folder_secret: yaysecret
+```
+
+### Delete a secret
+```console
+$ vault delete my_other_secret
+Done
+```
+
+### Move secrets and folders
+```console
+$ vault mv my_secret test/my_secret
+Move 'my_secret' to 'test/my_secret'
+
+$ vault mv blob_secret test/blob_secret
+Move 'blob_secret' to 'test/blob_secret'
+
+$ vault get-all
+---
+-secret-name: -oh-so-secret
+list_secret:
+- secret1
+- secret2
+- secret3
+test:
+  blob_secret:
+    code: supercode
+  my_folder_secret: yaysecret
+  my_secret: qwerty
+third_secret: '----BEGIN SECRET KEY----
+
+  ...'
+```
+
+### Launch a process loading secrets through environment variables
+```console
+$ vault env --path blob_secret -- env
+...
+BLOB_SECRET={"code": "supercode"}
+...
+$ vault set foo/bar/service/instance/dsn value
+$ vault env --path blob_secret=blob --path foo/bar/service/instance=my -- env
+...
+BLOB={"code": "supercode"}
+MY_DSN=value
+...
+```
+
+
+### Render a template file with values from the vault
+```console
+$ vault template mytemplate.j2 > /etc/conf
+
+# mytemplate.j2:
+Hello={{ vault("my_secret") }}
+
+# /etc/conf:
+Hello=querty
+```
+(Use `-` for stdin and `-o <file or ->` to specify the file to write to, or stdout).
+
+### (Re)create a configuration file based on the current settings
+```console
+$ vault --url https://something --token mytoken dump-config > .vault.yaml
+```
+
+### Delete everything under blob-secret
+```console
+$ vault delete-all blob-secret
+```
+
+### Delete everything, no confirmation
+```console
+$ vault delete-all --force
+```
+
+### Create a templated value
+```console
+$ vault set password foo
+$ vault set dsn '!template!proto://username:{{ vault("password") }}@host/'
+$ vault get dsn
+proto://username:foo@host/
+$ vault --no-render get --text dsn
+!template!proto://username:{{ vault("password") }}@host/
+```
+The `vault` function does not render variables recursively.
+
+### Get information on your current token
+```
+$ vault lookup-token
+```
+
+### Use the testing client in your tests
+
+```console
+$ pip install vault-cli[testing]
+```
+
+```python
+# conftest.py (for pytest)
+from vault_cli.testing import vault
+
+__all__ = ["vault"]
+```
+```python
+# test_something.py
+
+def test_bla(vault):
+    vault.db = {"a/b": "c"}
+
+    assert vault.get_secret("a/b") == "c"
+
+```
+
+## Configuration
+
+The first file found in the following location is read, parsed and used:
+1. `/etc/vault.yml`
+2. `~/.vault.yml`
+3. `./vault.yml`
+
+Any option passed as command line flag will be used over the corresponding
+option in the documentation (use either `-` or `_`).
+
+The expected format of the configuration is a mapping, with option names and
+their corresponding values:
+
+```yaml
+---
+username: my_username
+password-file: ~/.vault-password
+# or
+token-file: ~/.vault-token
+url: https://vault.mydomain:8200
+verify: no
+base-path: project/
+...
+```
+
+Make sure the secret files have their permissions set accordingly.
+
+For simple cases, you can directly define your `token` or `password` in the
+file:
+
+```yaml
+---
+username: my_username
+password: secret-password
+# or
+token: secret-token
+url: https://vault.mydomain:8200
+verify: no
+base-path: project/
+...
+```
+
+If you do so, make sure the permissions of the configuration file itself are
+not too broad.
+
+Just note that the `--verify / --no-verify` flag become `verify: yes` or
+`verify: no`
+
+All parameters can be defined from environment variables:
+
+```console
+$ VAULT_CLI_URL=https://myvault.com vault list
+```
+The name is always the uppercase underscored name of the equivalent command
+line option. Token and password can also be passed as environment variables as
+VAULT_CLI_TOKEN and VAULT_CLI_PASSWORD.
+
+## Troubleshooting
+
+### `SyntaxError: invalid syntax`
+
+You're most probably using Python 3.5 or below (including Python 2)
+
+## State
+
+The tool is currently in beta mode. It's missing docs and other things.
+Be warned.
+
+## Contributing
+
+We welcome any help :) See [CONTRIBUTING.md](CONTRIBUTING.md) for details.
+
+## License
+
+Copyright 2018-2019 PeopleDoc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ packages/vault-cli/vault-cli-0.13.0.tar.gz/vault-cli-0.13.0/vault_cli/cli.py
@@ -0,0 +1,538 @@
+import contextlib
+import logging
+import os
+import pathlib
+import sys
+from typing import Any, Dict, Mapping, NoReturn, Optional, Sequence, TextIO
+
+import click
+import yaml
+
+import vault_cli
+from vault_cli import client, environment, exceptions, settings, types
+
+logger = logging.getLogger(__name__)
+
+CONTEXT_SETTINGS = {
+    "help_option_names": ["-h", "--help"],
+    "auto_envvar_prefix": settings.ENV_PREFIX,
+}
+
+
+def load_config(ctx: click.Context, param: click.Parameter, value: str) -> None:
+    if value == "no":
+        ctx.default_map = {}
+        return
+
+    if value is None:
+        config_files = settings.CONFIG_FILES
+    else:
+        config_files = [value]
+
+    config = settings.build_config_from_files(*config_files)
+    ctx.default_map = config
+
+
+def set_verbosity(ctx: click.Context, param: click.Parameter, value: int) -> int:
+    level = settings.get_log_level(verbosity=value)
+    logging.basicConfig(level=level)
+    logger.info(f"Log level set to {logging.getLevelName(level)}")
+    return value
+
+
+@contextlib.contextmanager
+def handle_errors():
+    try:
+        yield
+    except exceptions.VaultException as exc:
+        raise click.ClickException(str(exc))
+
+
+def print_version(ctx, __, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(f"vault-cli {vault_cli.__version__}")
+    click.echo(f"License: {vault_cli.__license__}")
+    ctx.exit()
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+@click.option(
+    "--url", "-U", help="URL of the vault instance", default=settings.DEFAULTS.url
+)
+@click.option(
+    "--verify/--no-verify",
+    default=settings.DEFAULTS.verify,
+    help="Verify HTTPS certificate",
+)
+@click.option(
+    "--ca-bundle",
+    type=click.Path(),
+    help="Location of the bundle containing the server certificate "
+    "to check against.",
+)
+@click.option(
+    "--login-cert",
+    type=click.Path(),
+    help="Path to a public client certificate to use for connecting to vault.",
+)
+@click.option(
+    "--login-cert-key",
+    type=click.Path(),
+    help="Path to a private client certificate to use for connecting to vault.",
+)
+@click.option(
+    "--token-file",
+    "-T",
+    type=click.Path(),
+    help="File which contains the token to connect to Vault. "
+    'Configuration file can also contain a "token" key.',
+)
+@click.option("--username", "-u", help="Username used for userpass authentication")
+@click.option(
+    "--password-file",
+    "-w",
+    type=click.Path(),
+    help='Can read from stdin if "-" is used as parameter. '
+    'Configuration file can also contain a "password" key.',
+)
+@click.option("--base-path", "-b", help="Base path for requests")
+@click.option(
+    "-s",
+    "--safe-write/--unsafe-write",
+    default=settings.DEFAULTS.safe_write,
+    help="When activated, you can't overwrite a secret without "
+    'passing "--force" (in commands "set", "mv", etc)',
+)
+@click.option(
+    "--render/--no-render",
+    default=settings.DEFAULTS.render,
+    help="Render templated values",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    is_eager=True,
+    callback=set_verbosity,
+    count=True,
+    help="Use multiple times to increase verbosity",
+)
+@click.option(
+    "--config-file",
+    is_eager=True,
+    callback=load_config,
+    help="Config file to use. Use 'no' to disable config file. "
+    "Default value: first of " + ", ".join(settings.CONFIG_FILES),
+    type=click.Path(),
+)
+@click.option(
+    "-V",
+    "--version",
+    is_flag=True,
+    callback=print_version,
+    expose_value=False,
+    is_eager=True,
+)
+@handle_errors()
+def cli(ctx: click.Context, **kwargs) -> None:
+    """
+    Interact with a Vault. See subcommands for details.
+
+    All arguments can be passed by environment variables: VAULT_CLI_UPPERCASE_NAME
+    (including VAULT_CLI_PASSWORD and VAULT_CLI_TOKEN).
+
+    """
+    kwargs.pop("config_file")
+    verbose = kwargs.pop("verbose")
+
+    assert ctx.default_map  # make mypy happy
+    kwargs.update(extract_special_args(ctx.default_map, os.environ))
+
+    # There might still be files to read, so let's do it now
+    kwargs = settings.read_all_files(kwargs)
+    saved_settings = kwargs.copy()
+    saved_settings.update({"verbose": verbose})
+
+    ctx.obj = client.get_client_class()(**kwargs)  # type: ignore
+    ctx.obj.auth()
+    ctx.obj.saved_settings = saved_settings
+
+
+def extract_special_args(
+    config: Mapping[str, Any], environ: Mapping[str, str]
+) -> Dict[str, Any]:
+    result = {}
+    for key in ["password", "token"]:
+        result[key] = config.get(key)
+        env_var_key = "VAULT_CLI_{}".format(key.upper())
+        if env_var_key in environ:
+            result[key] = environ.get(env_var_key)
+
+    return result
+
+
+@cli.command("list")
+@click.argument("path", required=False, default="")
+@click.pass_obj
+@handle_errors()
+def list_(client_obj: client.VaultClientBase, path: str):
+    """
+    List all the secrets at the given path. Folders are listed too. If no path
+    is given, list the objects at the root.
+    """
+    result = client_obj.list_secrets(path=path)
+    click.echo("\n".join(result))
+
+
+@cli.command(name="get-all")
+@click.option(
+    "--flat",
+    is_flag=True,
+    help=("Returns the full path as keys instead of merging paths into a tree"),
+)
+@click.argument("path", required=False, nargs=-1)
+@click.pass_obj
+@handle_errors()
+def get_all(client_obj: client.VaultClientBase, path: Sequence[str], flat: bool):
+    """
+    Return multiple secrets. Return a single yaml with all the secrets located
+    at the given paths. Folders are recursively explored. Without a path,
+    explores all the vault.
+    """
+    paths = list(path) or [""]
+
+    result = client_obj.get_all_secrets(*paths, flat=flat)
+
+    click.echo(
+        yaml.safe_dump(result, default_flow_style=False, explicit_start=True), nl=False
+    )
+
+
+@cli.command()
+@click.pass_obj
+@click.option(
+    "--text/--yaml",
+    default=True,
+    help=(
+        "Returns the value in yaml format instead of plain text."
+        "If the secret is not a string, it will always be yaml."
+    ),
+)
+@click.argument("name")
+@handle_errors()
+def get(client_obj: client.VaultClientBase, text: bool, name: str):
+    """
+    Return a single secret value.
+    """
+    secret = client_obj.get_secret(path=name)
+    force_yaml = isinstance(secret, list) or isinstance(secret, dict)
+    if text and not force_yaml:
+        if secret is None:
+            secret = "null"
+        click.echo(secret)
+        return
+
+    click.echo(
+        yaml.safe_dump(secret, default_flow_style=False, explicit_start=True), nl=False
+    )
+
+
+@cli.command("set")
+@click.pass_obj
+@click.option("--yaml", "format_yaml", is_flag=True)
+@click.option(
+    "--prompt",
+    is_flag=True,
+    default=False,
+    help="Prompt user for value using a hidden input.",
+)
+@click.option("--stdin/--no-stdin", default=False)
+@click.option(
+    "--force/--no-force",
+    "-f",
+    is_flag=True,
+    default=None,
+    help="In case the path already holds a secret, allow overwriting it "
+    "(this is necessary only if --safe-write is set).",
+)
+@click.option(
+    "--strip/--no-strip",
+    default=True,
+    help="If --no-strip is set, value will not be stripped of surrounding whitespaces",
+)
+@click.argument("name")
+@click.argument("value", nargs=-1)
+@handle_errors()
+def set_(
+    client_obj: client.VaultClientBase,
+    format_yaml: bool,
+    prompt: bool,
+    stdin: bool,
+    strip: bool,
+    name: str,
+    value: Sequence[str],
+    force: Optional[bool],
+):
+    """
+    Set a single secret to the given value(s).
+
+    Value can be either passed as argument (several arguments will be
+    interpreted as a list) or via stdin with the --stdin flag.
+    """
+    if prompt + bool(value) + stdin > 1:
+        raise click.UsageError(
+            "Conflicting input methods: use only one of --prompt, --stdin and "
+            "positional argument"
+        )
+
+    json_value: types.JSONValue
+    if stdin:
+        json_value = click.get_text_stream("stdin").read()
+
+    elif prompt:
+        json_value = click.prompt(f"Please enter value for `{name}`", hide_input=True)
+
+    elif len(value) == 1:
+        json_value = value[0]
+
+    else:
+        json_value = list(value)
+
+    if format_yaml:
+        if not isinstance(json_value, str):
+            raise click.UsageError(
+                "Cannot pass several values when using yaml format. "
+                "Please use a yaml list instead."
+            )
+        json_value = yaml.safe_load(json_value)
+
+    if strip and isinstance(json_value, str):
+        json_value = fix_whitespaces(string=json_value)
+
+    try:
+        client_obj.set_secret(path=name, value=json_value, force=force)
+    except exceptions.VaultOverwriteSecretError as exc:
+        raise click.ClickException(
+            f"Secret already exists at {exc.path}. Use -f to force overwriting."
+        )
+    except exceptions.VaultMixSecretAndFolder as exc:
+        raise click.ClickException(str(exc))
+    click.echo("Done")
+
+
+def fix_whitespaces(string: str) -> str:
+    """
+    Single line secrets are stripped of all surrounding whitespace, which most probably
+    got here by accident. Multiline secrets are stripped, but a single trailing new line
+    is enforced.
+    """
+    stripped = string.strip()
+    if "\n" in stripped:
+        stripped += "\n"
+    return stripped
+
+
+@cli.command()
+@click.pass_obj
+@click.argument("name")
+@handle_errors()
+def delete(client_obj: client.VaultClientBase, name: str) -> None:
+    """
+    Delete a single secret.
+    """
+    client_obj.delete_secret(path=name)
+    click.echo("Done")
+
+
+@cli.command("env")
+@click.option(
+    "-p",
+    "--path",
+    multiple=True,
+    required=True,
+    help="Folder or single item. Pass several times to load multiple values. You can use --path mypath=prefix if you want to change the generated names of the environment variables",
+)
+@click.argument("command", nargs=-1)
+@click.pass_obj
+@handle_errors()
+def env(
+    client_obj: client.VaultClientBase, path: Sequence[str], command: Sequence[str]
+) -> NoReturn:
+    """
+    Launch a command, loading secrets in environment.
+
+    Strings are exported as-is, other types (including booleans, nulls, dicts, lists)
+    are exported as yaml (more specifically as json).
+
+    VARIABLE NAMES
+
+    If the path is not a folder the prefix value is used as the name of the
+    variable.
+    e.g.: for a/b/key, `--path a/b/c=foo` gives `FOO=...`
+
+    If the path is a folder, then the variable names will be generated as:
+    PREFIX + _ + relative path
+    e.g.: for a/b/key, `--path a/b=my` gives `MY_KEY=...`
+
+    The standard behavior when no prefix is set will use the relative path to
+    the parent of the given path as variable name
+    e.g.: for a/b/key, --path a/b gives `B_KEY=...`
+    e.g.: for a/b/key, --path a/b/key gives `KEY=...`
+    """
+    paths = list(path) or [""]
+
+    env_secrets = {}
+
+    for path in paths:
+        path, _, prefix = path.partition("=")
+        secrets = client_obj.get_secrets(path)
+        env_secrets.update(
+            {
+                environment.make_env_key(
+                    path=path, prefix=prefix, key=key
+                ): environment.make_env_value(value)
+                for key, value in secrets.items()
+            }
+        )
+
+    environ = os.environ.copy()
+    environ.update(env_secrets)
+
+    environment.exec_command(command=command, environ=environ)
+
+
+@cli.command("dump-config")
+@click.pass_obj
+@handle_errors()
+def dump_config(client_obj: client.VaultClientBase,) -> None:
+    """
+    Display settings in the format of a config file.
+    """
+    assert client_obj.saved_settings
+    click.echo(
+        yaml.safe_dump(
+            client_obj.saved_settings, default_flow_style=False, explicit_start=True
+        ),
+        nl=False,
+    )
+
+
+@cli.command("delete-all")
+@click.option(
+    "-f",
+    "--force",
+    is_flag=True,
+    help="If not force, prompt for confirmation before each deletion.",
+)
+@click.argument("path", required=False, nargs=-1)
+@click.pass_obj
+@handle_errors()
+def delete_all(
+    client_obj: client.VaultClientBase, path: Sequence[str], force: bool
+) -> None:
+    """
+    Delete multiple secrets.
+    """
+    paths = list(path) or [""]
+
+    for secret in client_obj.delete_all_secrets(*paths, generator=True):
+        if not force and not click.confirm(text=f"Delete '{secret}'?", default=False):
+            raise click.Abort()
+        click.echo(f"Deleted '{secret}'")
+
+
+@cli.command()
+@click.argument("source", required=True)
+@click.argument("dest", required=True)
+@click.option(
+    "--force/--no-force",
+    "-f",
+    is_flag=True,
+    default=None,
+    help="In case the path already holds a secret, allow overwriting it "
+    "(this is necessary only if --safe-write is set).",
+)
+@click.pass_obj
+@handle_errors()
+def mv(
+    client_obj: client.VaultClientBase, source: str, dest: str, force: Optional[bool]
+) -> None:
+    """
+    Recursively move secrets from source to destination path.
+    """
+    try:
+        for old_path, new_path in client_obj.move_secrets(
+            source=source, dest=dest, force=force, generator=True
+        ):
+            click.echo(f"Move '{old_path}' to '{new_path}'")
+    except exceptions.VaultOverwriteSecretError as exc:
+        raise click.ClickException(
+            f"Secret already exists at {exc.path}. Use -f to force overwriting."
+        )
+    except exceptions.VaultMixSecretAndFolder as exc:
+        raise click.ClickException(str(exc))
+
+
+@cli.command()
+@click.argument(
+    "template",
+    type=click.Path(exists=True, allow_dash=True, file_okay=True),
+    required=True,
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.File("w"),
+    default="-",
+    help="File in which to write the rendered template. "
+    "If ommited (or -), write in standard output",
+)
+@click.pass_obj
+@handle_errors()
+def template(client_obj: client.VaultClientBase, template: str, output: TextIO) -> None:
+    """
+    Render the given template and insert secrets in it.
+
+    Rendering is done with Jinja2. A vault() function is exposed that
+    receives a path and outputs the secret at this path.
+
+    Search path (see https://jinja.palletsprojects.com/en/2.10.x/api/#jinja2.FileSystemLoader)
+    for possible Jinja2 `{% include() %}` statement is set to the template's directory.
+
+    If template is -, standard input will be read and the current working directory becomes the search path.
+
+    """
+    if template == "-":
+        template_text = sys.stdin.read()
+        search_path = pathlib.Path.cwd()
+    else:
+        with open(template, mode="r") as ftemplate:
+            template_text = ftemplate.read()
+        search_path = pathlib.Path(template).parent
+
+    result = client_obj.render_template(template_text, search_path=search_path)
+    output.write(result)
+
+
+@cli.command()
+@click.pass_obj
+@handle_errors()
+def lookup_token(client_obj: client.VaultClientBase) -> None:
+    """
+    Return information regarding the current token
+    """
+    click.echo(
+        yaml.safe_dump(
+            client_obj.lookup_token(), default_flow_style=False, explicit_start=True
+        ),
+        nl=False,
+    )
+
+
+def main():
+    # https://click.palletsprojects.com/en/7.x/python3/
+    os.environ.setdefault("LC_ALL", "C.UTF-8")
+    os.environ.setdefault("LANG", "C.UTF-8")
+
+    return cli()
