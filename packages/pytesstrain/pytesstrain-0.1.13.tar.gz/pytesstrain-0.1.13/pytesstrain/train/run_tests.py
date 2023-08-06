55fd09bfaf3bf78506ddd92a78d8eb02b6a314f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:43 +0000

    Add package pytesstrain/0.1.13/pytesstrain-0.1.13.tar.gz

---
+++ packages/pytesstrain/pytesstrain-0.1.13.tar.gz/pytesstrain-0.1.13/pytesstrain/train/run_tests.py
@@ -0,0 +1,18 @@
+"""Multiple tests (parallel) runner."""
+
+from itertools import product
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
+
+from .run_test import run_test
+
+
+def run_tests(lang: str, ref: str, wrap: int, fonts_dir: str, fonts: list, exposures: list, config='') -> List[tuple]:
+    """
+    Run run_test function on multiple processors simultaneously.
+    Returns tuples (reference, hypothesis, font, exposure).
+    """
+    with ThreadPoolExecutor(max_workers=None) as executor:
+        futures = [executor.submit(run_test, lang, ref, wrap, fonts_dir, font, exposure, config)
+                   for font, exposure in product(fonts, exposures)]
+    return [future.result() for future in futures]
