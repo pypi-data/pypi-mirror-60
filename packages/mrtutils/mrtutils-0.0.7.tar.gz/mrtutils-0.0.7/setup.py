1ba47e06faa2ecec8e8128358bf5af56b5b7bff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package mrtutils/0.0.7/mrtutils-0.0.7.tar.gz

---
+++ packages/mrtutils/mrtutils-0.0.7.tar.gz/mrtutils-0.0.7/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='mrtutils',
+     version='0.0.07',
+     author="Jason Berger",
+     author_email="JBerger@up-rev.com",
+     description="Utilities for MrT",
+     long_description=long_description,
+     scripts=['mrtutils/mrt-config','mrtutils/mrt-device'],
+     long_description_content_type="text/markdown",
+     url="http://www.up-rev.com/",
+     packages=setuptools.find_packages(),
+     package_data={
+     'mrtutils':['templates/*'],
+     },
+     install_requires=[
+        'markdown',
+        'mako',
+        'pyyaml'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
