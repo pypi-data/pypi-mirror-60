87b2324749c9bc5973e4e37ad2338d41f04dd3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:01 +0000

    Add package distribuicao-gaussiana/0.1/distribuicao_gaussiana-0.1.tar.gz

---
+++ packages/distribuicao-gaussiana/distribuicao_gaussiana-0.1.tar.gz/distribuicao_gaussiana-0.1/setup.py
@@ -0,0 +1,14 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='distribuicao_gaussiana',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribuicao_gaussiana'],
+      zip_safe=False)
