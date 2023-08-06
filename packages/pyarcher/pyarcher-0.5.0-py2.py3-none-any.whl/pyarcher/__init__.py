1ef2e642e98e09cd9b607ff7eff5277aa4ed34f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:19 +0000

    Add package pyarcher/0.5.0/pyarcher-0.5.0-py2.py3-none-any.whl

---
+++ packages/pyarcher/pyarcher-0.5.0-py2.py3-none-any.whl/pyarcher/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for RSA Archer Client Library."""
+from pyarcher.archer import Archer
+from pyarcher.archer_db import ArcherDB
+
+
+__author__ = """Kyle Cribbs"""
+__email__ = 'kylecribbs@outlook.com'
+__version__ = '0.5.0'
+__all__ = ["Archer"]
+
+name = "pyarcher"
