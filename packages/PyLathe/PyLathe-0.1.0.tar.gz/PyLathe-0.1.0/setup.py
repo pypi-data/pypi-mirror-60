9b7bc3286dfa8a762756b0d0da9e3d05f26ceb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:32 +0000

    Add package PyLathe/0.1.0/PyLathe-0.1.0.tar.gz

---
+++ packages/PyLathe/PyLathe-0.1.0.tar.gz/PyLathe-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# !/usr/env/python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyLathe",
+    version="0.1.0",
+    author="Emmett Boudreau",
+    author_email="emmettgb@gmail.com",
+    description="A Python Wrapper for Lathe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/emmettgb/PyLathe",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={'': ['Lathe/JL/*']},
+    include_package_data=True,
+)
