8b6240b5015aab3d9fd141fa70aca9e290421ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:54 +0000

    Add package handright/5.4.0/handright-5.4.0-py3-none-any.whl

---
+++ packages/handright/handright-5.4.0-py3-none-any.whl/handright/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+"""A lightweight Python library for simulating Chinese handwriting
+
+Created by Chenghui Li (Gsllchb).
+
+Licensed under BSD 3-Clause.
+"""
+from handright._core import (
+    handwrite, Template, Error, LayoutError, BackgroundTooLargeError
+)
+
+__version__ = "5.4.0"
