e144ecd2daef4016155fb226c40c6208a9478bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:05 +0000

    Add package scimschema/0.1.55/scimschema-0.1.55.tar.gz

---
+++ packages/scimschema/scimschema-0.1.55.tar.gz/scimschema-0.1.55/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("./README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scimschema",
+    version="0.1.55",
+    author="Gordon So",
+    author_email="gordonkwso@gmail.com",
+    description="A validator for System for Cross-domain Identity Management (SCIM) responses given predefine schemas",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/GordonSo/pyscim",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
