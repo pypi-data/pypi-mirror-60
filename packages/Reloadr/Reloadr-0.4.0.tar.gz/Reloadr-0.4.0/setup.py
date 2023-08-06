5ebe8c7dc1586b4f8564a10677eefd30ca889e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:10 +0000

    Add package Reloadr/0.4.0/Reloadr-0.4.0.tar.gz

---
+++ packages/Reloadr/Reloadr-0.4.0.tar.gz/Reloadr-0.4.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Reloadr - Python library for hot code reloading
+(c) 2015-2020 Hugo Herter
+"""
+
+import os
+
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='Reloadr',
+      version='0.4.0',
+      description='Hot code reloading tool for Python',
+      long_description=long_description,
+      author='Hugo Herter',
+      author_email='git@hugoherter.com',
+      url='https://github.com/hoh/reloadr',
+      py_modules=['reloadr'],
+      scripts=['reloadr.py'],
+      data_files=[],
+      install_requires=[
+          'watchdog',
+          ],
+      license='LGPLv3',
+      platform='any',
+      keywords="reload hot code reloading",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 3',
+                   'Operating System :: POSIX :: Linux',
+                   'Intended Audience :: Developers',
+                   ],
+      )
