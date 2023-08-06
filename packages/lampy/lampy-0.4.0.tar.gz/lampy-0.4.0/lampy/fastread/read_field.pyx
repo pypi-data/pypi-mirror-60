f76273cb780fab1207446391dbc19f7d37825112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package lampy/0.4.0/lampy-0.4.0.tar.gz

---
+++ packages/lampy/lampy-0.4.0.tar.gz/lampy-0.4.0/lampy/fastread/read_field.pyx
@@ -0,0 +1,66 @@
+#!python
+#cython: language_level=3
+#setuptools: sources = clibs/lib_read_binary.c
+
+cdef extern from "clibs/lib_read_binary.c":
+    pass
+cdef extern from 'clibs/lib_read_binary.h':
+    void read_binary(float* field, float* x, float* y, float* z, char* file_name)
+
+from cython.view cimport array as cvarray
+import struct
+import numpy as np
+cimport numpy as np
+
+def read_ALaDyn_bin(file_path,params):
+    
+    # - #
+    cdef int nx
+    cdef int ny
+    cdef int nz
+    
+    path = file_path+'.bin'
+    f = open(path, 'rb')
+    uni_path = path.encode('UTF-8')
+    cdef char* c_path = uni_path
+
+    # Here it reads the header via a standard struct unpack procedure
+    f.seek(4)
+    Nparam = struct.unpack('=i', f.read(4))[0]
+    f.seek(16)
+    integerdata_temp = struct.unpack('='+Nparam*'i', f.read(Nparam*4))
+    f.seek(104)
+    realdata_temp = struct.unpack('='+Nparam*'f', f.read(Nparam*4))
+    f.seek(4)
+
+    ndimension = integerdata_temp[14]
+    nx = params['nx']
+    ny = params['ny']
+    nz = params['nz']
+    # End of the header read
+
+    totlen = nx*ny*nz
+    f = cvarray(shape=(totlen,), itemsize=sizeof(float), format="f")  
+    x = cvarray(shape=(nx,), itemsize=sizeof(float), format="f")    
+    y = cvarray(shape=(ny,), itemsize=sizeof(float), format="f")   
+    z = cvarray(shape=(nz,), itemsize=sizeof(float), format="f") 
+
+    read_file(f, x, y, z, c_path)
+
+    f = np.reshape(f, (nx,ny,nz), order='F')
+    x = np.asarray(x)
+    y = np.asarray(y)
+    z = np.asarray(z)
+
+    return (f, x, y, z)
+
+def read_file(f, x, y, z, f_path):
+
+    cdef float [::1] f_view = f
+    cdef float [::1] x_view = x
+    cdef float [::1] y_view = y
+    cdef float [::1] z_view = z
+
+    read_binary(&f_view[0], &x_view[0], &y_view[0], &z_view[0], f_path)
+
+    return (f, x, y, z)
---
+++ packages/lampy/lampy-0.4.0.tar.gz/lampy-0.4.0/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy
+import os
+
+about = dict()
+with open("lampy/__about__.py") as fp:
+    exec(fp.read(), about)
+
+__version__ = about['__version__']
+
+with open('README.md') as f:
+    long_description = f.read()
+
+extensions = [Extension(
+          name='read_field',
+          sources=[os.path.join('lampy', 'fastread', 'read_field.pyx')]),
+          Extension(
+          name='read_phase_space',
+          sources=[os.path.join('lampy', 'fastread', 'read_phase_space.pyx')])
+]
+
+lib_read_binary = ('lib_read_binary',
+                   {'sources': [os.path.join('lampy', 'fastread', 'clibs',
+                                             'lib_read_binary.c')]})
+lib_read_phase_space = ('lib_read_phase_space',
+                        {'sources':
+                         [os.path.join('lampy', 'fastread', 'clibs',
+                                       'lib_read_phase_space.c')]})
+
+requirements = ['numpy', 'matplotlib', 'Cython', 'scipy']
+setup(
+    name='lampy',
+    version=__version__,
+    description="Python suite to access, manipulate and plot ALaDyn's datas",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages('.'),
+    author='ALaDyn Collaboration',
+    author_email='davide.terzani@ino.it',
+    maintainer='Davide Terzani',
+    maintainer_email='davide.terzani@ino.it',
+    license='GNU GPLv3',
+    url='https://github.com/ALaDyn/ALaDyn',
+    classifiers=[
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Programming Language :: Python :: 3.7'],
+    ext_modules=cythonize(extensions),
+    install_requires=[requirements],
+    ext_package=os.path.join('lampy', 'compiled_cython'),
+    libraries=[lib_read_binary, lib_read_phase_space],
+    include_dirs=[numpy.get_include()]
+)
