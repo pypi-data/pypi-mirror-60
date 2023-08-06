4f30057b53fd8b0bb42bc9257f7f65e6f6f86990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:23 +0000

    Add package sspo-db/5.2.21/sspo_db-5.2.21.tar.gz

---
+++ packages/sspo-db/sspo_db-5.2.21.tar.gz/sspo_db-5.2.21/sspo_db/application/stakeholder/application.py
@@ -0,0 +1,40 @@
+from sspo_db.model.stakeholder.models import Person, Developer, ScrumMaster
+from sspo_db.service.stakeholder.service import PersonService, DeveloperService, ScrumMasterService, TeamMemberService
+from sspo_db.application.core.application import ApplicationApplicationReference
+from sspo_db.application.abstract_application import AbstractApplication
+from pprint import pprint
+
+class ApplicationPerson(AbstractApplication):
+    
+    def __init__(self):
+        super().__init__(PersonService())
+        self.application_reference_service = ApplicationApplicationReference()
+    
+    def retrive_by_uuid(self, uuid):
+        return self.service.get_by_uuid(uuid)
+        
+class ApplicationTeamMember(AbstractApplication):
+    
+    def __init__(self):
+        super().__init__(TeamMemberService())
+        
+    def retrive_by_external_id_and_project_name(self, external_id, project_name):
+        return self.service.retrive_by_external_id_and_project_name(external_id,project_name)
+
+class ApplicationDeveloper(AbstractApplication):
+
+    def __init__(self):
+        super().__init__(DeveloperService())
+    
+    def create_with_project_name(self, external_id, project_name):
+        application_person = ApplicationPerson()
+        person = application_person.retrive_by_external_uuid(external_id, "person")
+        if person is not None:
+            return self.service.create_with_project_name(person, project_name)
+        return None
+        
+class ApplicationScrumMaster(AbstractApplication):
+    
+    def __init__(self):
+        super().__init__(ScrumMasterService())
+        
