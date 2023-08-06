c88ede36d6631f9aaef672a9111641a627222953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:12 +0000

    Add package PickPlace3D/0.0.1/PickPlace3D-0.0.1.tar.gz

---
+++ packages/PickPlace3D/PickPlace3D-0.0.1.tar.gz/PickPlace3D-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PickPlace3D",
+    version="0.0.1",
+    author="Ben Knight",
+    author_email="bknight@i3drobotics.com",
+    description="Pick and place using 3D data to inform robotic movement",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/i3drobotics/PickPlace3D",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy','opencv-python','stereocapture'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
