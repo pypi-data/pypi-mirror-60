1704d4dc3526b7a4bf9823ddad570cf2df14de0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:03 +0000

    Add package mindaffectBCI/0.9.1/mindaffectBCI-0.9.1.tar.gz

---
+++ packages/mindaffectBCI/mindaffectBCI-0.9.1.tar.gz/mindaffectBCI-0.9.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.rst", encoding='utf-8') as fh:
+    long_description = fh.read()
+print(long_description)
+
+setup(name='mindaffectBCI',
+      version='0.9.1',
+      description='The MindAffect BCI python SDK',
+      long_description_content_type='text/markdown',      
+      long_description=long_description,
+      url='http://github.com/mindaffect/pymindaffectBCI',
+      author='Jason Farquhar',
+      author_email='jason@mindaffect.nl',
+      license='MIT',
+      packages=['mindaffectBCI'],
+      #package_data={'mindaffectBCI':['codebooks/*.txt']},
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.5',      
+      zip_safe=False)
