ebdf08f4389315728b9aede5ec2dfd109df564b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:16 +0000

    Add package pervane/0.0.35/pervane-0.0.35.tar.gz

---
+++ packages/pervane/pervane-0.0.35.tar.gz/pervane-0.0.35/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pervane", # Replace with your own username
+    version="0.0.35",
+    author="hakanu",
+    author_email="hi@hakanu.net",
+    description="Plain text backed web based note taking app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hakanu/pervane",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "flask>=1.1.1",
+        "Flask-Caching>=1.8.0",
+        "Flask-HTTPAuth>=3.3.0",
+        "mistune>=0.8.4",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    #scripts=['bin/pervane'],
+    entry_points={"console_scripts": ["pervane = pervane.cli:main"]},
+    include_package_data=True,
+)
