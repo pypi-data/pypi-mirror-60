a675b94f103aa112114685752645473dfbc9c51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:39 +0000

    Add package cellpose/0.0.1/cellpose-0.0.1.tar.gz

---
+++ packages/cellpose/cellpose-0.0.1.tar.gz/cellpose-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cellpose",
+    version="0.0.1",
+    author="Marius Pachitariu and Carsen Stringer",
+    author_email="stringerc@janelia.hhmi.org",
+    description="anatomical segmentation algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MouseLand/cellpose",
+    packages=setuptools.find_packages(),
+    install_requires = ['pyqtgraph', 'PyQt5', 'numpy', 'scipy', 'natsort', 'tqdm',
+                        'numba', 'scikit-image', 'matplotlib', 'mxnet_mkl', 'opencv_python',
+                        'google-cloud-storage'],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
