44d2f8ce3cf66105734ac8fb066cb8b7c5f03e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:09 +0000

    Add package py-money/0.5.0/py-money-0.5.0.tar.gz

---
+++ packages/py-money/py-money-0.5.0.tar.gz/py-money-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='py-money',
+    packages=[
+        'money'
+    ],
+    version='0.5.0',
+    description='Money module for python',
+    long_description=long_description,
+    url='https://github.com/vimeo/py-money',
+    author='J.C. Manzo',
+    author_email='jnmanzo@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='money currency',
+    install_requires=[
+        'babel >= 2.4.0, < 3.0'
+    ],
+)
