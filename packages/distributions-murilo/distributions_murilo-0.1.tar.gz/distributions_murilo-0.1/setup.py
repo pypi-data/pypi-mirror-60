23995c4e90877b1b181507dececc8ad3eddde31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:45 +0000

    Add package distributions-murilo/0.1/distributions_murilo-0.1.tar.gz

---
+++ packages/distributions-murilo/distributions_murilo-0.1.tar.gz/distributions_murilo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_murilo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_murilo'],
+      zip_safe=False)
