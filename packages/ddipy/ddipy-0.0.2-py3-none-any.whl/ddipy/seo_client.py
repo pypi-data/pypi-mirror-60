5ebff8d78a74612236ade45f79314b7780b9be47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:11 +0000

    Add package ddipy/0.0.2/ddipy-0.0.2-py3-none-any.whl

---
+++ packages/ddipy/ddipy-0.0.2-py3-none-any.whl/ddipy/seo_client.py
@@ -0,0 +1,59 @@
+import requests
+
+from ddipy.verify_utils import VerifyUtils
+
+
+class SeoClient:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/54.0.2840.99 Safari/537.36"}
+    seoHomeUrl = "https://www.omicsdi.org/ws/seo/home"
+    seoSearchUrl = "https://www.omicsdi.org/ws/seo/search"
+    seoApiUrl = "https://www.omicsdi.org/ws/seo/api"
+    seoSchemaUrl = "https://www.omicsdi.org/ws/seo/schema"
+    seoDatabaseUrl = "https://www.omicsdi.org/ws/seo/database"
+    seoDatasetUrl = "https://www.omicsdi.org/ws/seo/dataset"
+    seoAboutUrl = "https://www.omicsdi.org/ws/seo/about"
+
+    def __init__(self):
+        pass
+
+    def get_seo_home(self):
+        res = requests.get(self.seoHomeUrl, headers=self.headers)
+        return res
+
+    def get_seo_search(self):
+        res = requests.get(self.seoSearchUrl, headers=self.headers)
+        return res
+
+    def get_seo_api(self):
+        res = requests.get(self.seoApiUrl, headers=self.headers)
+        return res
+
+    def get_seo_schema(self, acc, domain):
+        if not acc:
+            return VerifyUtils.empty_param_error("acc")
+
+        if not domain:
+            return VerifyUtils.empty_param_error("domain")
+
+        res = requests.get(self.seoSchemaUrl + "/" + domain + "/" + acc, headers=self.headers)
+        return res
+
+    def get_seo_database(self):
+        res = requests.get(self.seoDatabaseUrl, headers=self.headers)
+        return res
+
+    def get_seo_dataset(self, domain, acc):
+        if not acc:
+            return VerifyUtils.empty_param_error("acc")
+
+        if not domain:
+            return VerifyUtils.empty_param_error("domain")
+
+        res = requests.get(self.seoDatasetUrl + "/" + domain + "/" + acc, headers=self.headers)
+        return res
+
+    def get_seo_about(self):
+        res = requests.get(self.seoAboutUrl, headers=self.headers)
+        return res
+
---
+++ packages/ddipy/ddipy-0.0.2-py3-none-any.whl/ddipy/term_client.py
@@ -0,0 +1,34 @@
+import requests
+
+from ddipy.verify_utils import VerifyUtils
+
+
+class TermClient:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/54.0.2840.99 Safari/537.36"}
+    getTermByPatternUrl = "https://www.omicsdi.org/ws/term/getTermByPattern"
+    getFrequentlyTermListUrl = "https://www.omicsdi.org/ws/term/frequentlyTerm/list"
+
+    def __init__(self):
+        pass
+
+    def get_term_by_pattern(self, q="*:*", size=10):
+        res = requests.get(self.getTermByPatternUrl, params={
+            "q": q,
+            "size": size
+        }, headers=self.headers)
+        return res
+
+    def get_term_frequently_term_list(self, domain, field, size=20):
+        if not domain:
+            return VerifyUtils.empty_param_error("domain")
+
+        if not field:
+            return VerifyUtils.empty_param_error("field")
+
+        res = requests.get(self.getFrequentlyTermListUrl, params={
+            "size": size,
+            "domain": domain,
+            "field": field
+        }, headers=self.headers)
+        return res
