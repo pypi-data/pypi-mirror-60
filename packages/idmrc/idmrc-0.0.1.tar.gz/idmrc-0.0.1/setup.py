52dc1d2afaa71a7bf7b9043e079b70a7df9b2255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:39 +0000

    Add package idmrc/0.0.1/idmrc-0.0.1.tar.gz

---
+++ packages/idmrc/idmrc-0.0.1.tar.gz/idmrc-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='idmrc',
+    version='0.0.1',
+    packages=['idmrc'],
+    author="Lukas Jurk",
+    author_email="lukas.jurk@tu-dresden.de",
+    description="a simple idmrc",
+    license="GPLv3",
+    keywords="requests rest",
+    url="https://gitlab.hrz.tu-chemnitz.de/slm/rc",
+    entry_points={
+        'console_scripts': ['rc=idmrc.idmrc:cli']
+    },
+    install_requires=[
+        'Click==7.0',
+        'requests==2.22.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
