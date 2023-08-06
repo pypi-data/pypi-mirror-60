ac48839fb061a58a39b7c6bd50219cfac1186eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:13 +0000

    Add package gasync/0.0.1/gasync-0.0.1.tar.gz

---
+++ packages/gasync/gasync-0.0.1.tar.gz/gasync-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="gasync",
+    version="0.0.1",
+    install_requires=['hy>=0.15'],
+    packages=find_packages(exclude=['tests']),
+    package_data={
+        'gasync': ['*.hy'],
+    },
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    author="Hirotaka Niitsuma",
+    author_email="hirotaka.niitsuma@gmail.com",
+    long_description="""generic async func in Hy.""",
+    license="GNU Affero General Public License",
+    url="https://github.com/niitsuma/gasync",
+    platforms=['any'],
+    python_requires='>=3.6',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Lisp",
+        "Topic :: Software Development :: Libraries",
+    ]
+)
