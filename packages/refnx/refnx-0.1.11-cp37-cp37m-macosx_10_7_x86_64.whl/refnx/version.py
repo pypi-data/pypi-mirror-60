9bd21819eb3e46a9070272efd74ab6a757091b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:42 +0000

    Add package refnx/0.1.11/refnx-0.1.11-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/refnx/refnx-0.1.11-cp37-cp37m-macosx_10_7_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.11'
+version = '0.1.11'
+full_version = '0.1.11'
+git_revision = '7c8d9e9ef6e70d17084d59489382b993d8c07092'
+release = True
+if not release:
+    version = full_version
