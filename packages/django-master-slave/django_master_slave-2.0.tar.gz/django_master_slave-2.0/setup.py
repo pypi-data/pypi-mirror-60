d9efbed00a91cc1af3385f1881880755d7b741df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:05 +0000

    Add package django-master-slave/2.0/django_master_slave-2.0.tar.gz

---
+++ packages/django-master-slave/django_master_slave-2.0.tar.gz/django_master_slave-2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django_master_slave',
+    version='2.0',
+    packages=['masterslave'],
+    url='',
+    license='Beer Licence',
+    author='tumbler',
+    author_email='zimbler@gmail.com',
+    description='''Provides master-slave routing
+        and force master context manager for Django'''
+)
