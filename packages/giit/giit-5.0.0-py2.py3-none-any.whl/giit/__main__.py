a2a688a60c806b3f1c54e8d63ca9471561e0c7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:13 +0000

    Add package giit/5.0.0/giit-5.0.0-py2.py3-none-any.whl

---
+++ packages/giit/giit-5.0.0-py2.py3-none-any.whl/giit/__main__.py
@@ -0,0 +1,45 @@
+import sys
+import click
+import colorama
+
+import giit.build
+import giit.logs
+import giit.factory
+
+
+@click.command()
+@click.option('--build_path')
+@click.option('--giit_path')
+@click.option('--config_branch')
+@click.option('--config_path')
+@click.option('-v', '--verbose', is_flag=True)
+@click.argument('step')
+@click.argument('repository')
+def cli(step, repository, build_path, giit_path, config_branch, config_path,
+        verbose):
+
+    build = giit.build.Build(
+        step=step,
+        repository=repository,
+        factory=giit.factory,
+        build_path=build_path,
+        giit_path=giit_path,
+        config_branch=config_branch,
+        config_path=config_path,
+        verbose=verbose)
+
+    try:
+        build.run()
+    except Exception as e:
+
+        if verbose:
+            # We just propagate the exception out
+            raise
+
+        colorama.init()
+        print(colorama.Fore.RED + str(e))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/giit/giit-5.0.0-py2.py3-none-any.whl/giit/variables_reader.py
@@ -0,0 +1,49 @@
+import string
+import os
+
+
+class VariablesReader(object):
+
+    def __init__(self, variables, context):
+        self.variables = variables
+        self.context = context
+
+    def _find_item(self, key):
+
+        scope = self.context['scope']
+
+        if 'checkout' in self.context:
+            checkout = self.context['checkout']
+
+            # We first look in the variables dict using the following order:
+            #
+            # 1. scope:checkout:variable
+            # 2. scope:variable
+            # 3. variable
+            explicit = ':'.join([scope, checkout, key])
+
+            if explicit in self.variables:
+                return self.variables[explicit]
+
+        partial = ':'.join([scope, key])
+
+        if partial in self.variables:
+            return self.variables[partial]
+
+        if key in self.variables:
+            return self.variables[key]
+
+        # Check if the variable is defined in the context
+        if key in self.context:
+            return self.context[key]
+
+        raise AttributeError("Not found {}".format(key))
+
+    def __getitem__(self, key):
+
+        variable = self._find_item(key=key)
+        return string.Template(variable).substitute(self)
+
+    def expand(self, element):
+
+        return str(string.Template(element).substitute(self))
