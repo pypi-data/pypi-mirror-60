9957c3ab2039e6af6a635ea45bfb649ad6fa07f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:31 +0000

    Add package sgasayhello/0.1/sgasayhello-0.1.tar.gz

---
+++ packages/sgasayhello/sgasayhello-0.1.tar.gz/sgasayhello-0.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 13:17:53 2020
+
+@author: abdulkadarb
+"""
+
+from setuptools import setup
+
+setup(name='sgasayhello',
+      version='0.1',
+      description='Just say Hello',
+      #url='http://github.com/storborg/funniest',
+      author='AbdulkadarBabbar',
+      author_email='abdulkadar.babbar@sganalytics.com',
+      license='MIT',
+      packages=['sgasayhello'],
+      long_description="firstpackagetotest",
+      zip_safe=False)
---
+++ packages/sgasayhello/sgasayhello-0.1.tar.gz/sgasayhello-0.1/sgasayhello/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 13:21:20 2020
+
+@author: abdulkadarb
+"""
+
+def hello():
+    print("hello")
