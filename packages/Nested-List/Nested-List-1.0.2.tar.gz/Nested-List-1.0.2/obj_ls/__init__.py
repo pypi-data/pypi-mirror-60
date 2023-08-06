8550b552c67a333293a9cefa6815a614c388f882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:18 +0000

    Add package Nested-List/1.0.2/Nested-List-1.0.2.tar.gz

---
+++ packages/Nested-List/Nested-List-1.0.2.tar.gz/Nested-List-1.0.2/obj_ls/__init__.py
@@ -0,0 +1,28 @@
+"""
+A package that operates on a list whose type of element is other object.
+It's built based on the operator.attrgetter.
+"""
+from operator import attrgetter
+
+
+def sort(ol, order=False, *keys):
+    ol.sort(key=attrgetter(*keys), reverse=order if order is True or order == "DESC" else False)
+
+
+def delete_items(dl, compare, keys, values):
+    get_values = attrgetter(*keys)
+    for d in dl:
+        if compare == 'L' and get_values(d) <= values:
+            dl.remove(d)
+        elif compare == 'G' and get_values(d) >= values:
+            dl.remove(d)
+        elif get_values(d) == values:
+            dl.remove(d)
+
+
+def max(dl, *keys):
+    return dl.max(key=attrgetter(*keys))
+
+
+def min(dl, *keys):
+    return dl.min(key=attrgetter(*keys))
