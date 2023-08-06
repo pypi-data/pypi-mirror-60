a46813f656fc4bc7d3349c3af0845e4dbf9eceaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:09 +0000

    Add package fitgrid/0.4.10/fitgrid-0.4.10.tar.gz

---
+++ packages/fitgrid/fitgrid-0.4.10.tar.gz/fitgrid-0.4.10/fitgrid/defaults.py
@@ -0,0 +1,25 @@
+#: default epoch identifier
+EPOCH_ID = 'Epoch_idx'
+
+#: default time identifier
+TIME = 'Time'
+
+#: default generic channel list
+CHANNELS = [
+    'ch00',
+    'ch01',
+    'ch02',
+    'ch03',
+    'ch04',
+    'ch05',
+    'ch06',
+    'ch07',
+    'ch08',
+    'ch09',
+    'ch10',
+    'ch11',
+    'ch12',
+    'ch13',
+    'ch14',
+    'ch15',
+]
