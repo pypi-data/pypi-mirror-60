d004edd4a026ed12cf489da59da29755ab436eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package followthemoney/1.25.0/followthemoney-1.25.0.tar.gz

---
+++ packages/followthemoney/followthemoney-1.25.0.tar.gz/followthemoney-1.25.0/tests/types/test_entity.py
@@ -0,0 +1,24 @@
+import unittest
+
+from followthemoney.types import registry
+
+entities = registry.entity
+
+
+class EntityTest(unittest.TestCase):
+
+    def test_parse(self):
+        self.assertEqual(entities.clean('88'), '88')
+        self.assertEqual(entities.clean(88), '88')
+        self.assertEqual(entities.clean({'id': 88}), '88')
+        self.assertEqual(entities.clean(None), None)
+        self.assertEqual(entities.clean('With spaces'), None)
+        self.assertEqual(entities.clean('With!special'), None)
+        self.assertEqual(entities.clean('with.dot'), 'with.dot')
+
+    def test_normalize(self):
+        self.assertEqual(entities.normalize('FOO'), ['FOO'])
+        self.assertEqual(entities.normalize(None), [])
+
+    def test_funcs(self):
+        self.assertEqual(entities.specificity('bla'), 1)
