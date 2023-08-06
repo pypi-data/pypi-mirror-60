67c0e954280c7925dedc3fbaff2864c399a22bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:05 +0000

    Add package girder-ess-dive/1.0.0/girder-ess-dive-1.0.0.tar.gz

---
+++ packages/girder-ess-dive/girder-ess-dive-1.0.0.tar.gz/girder-ess-dive-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="girder-ess-dive",
+    version="1.0.0",
+    description="Allows access to ESS-DIVE files through a read-only assetstore.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/girder/girder-ess-dive",
+    packages=["girder_ess_dive"],
+    python_requires=">=3.5",
+    install_requires=[
+        "girder",
+        "pytest",
+        "requests",
+        "xmltodict",
+        "shapely",
+        "pyproj",
+    ],
+    entry_points={"girder.plugin": ["ess-dive = girder_ess_dive:GirderESSDive"]},
+)
