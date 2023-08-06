ed94107fe8a8137487dd278ec5f63ca0ab8caf76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:11 +0000

    Add package django-custom-user-models/0.1.0/django-custom-user-models-0.1.0.tar.gz

---
+++ packages/django-custom-user-models/django-custom-user-models-0.1.0.tar.gz/django-custom-user-models-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("CustomAuth/README.md", "r") as fh:
+    README = fh.read()
+
+setuptools.setup(
+    name="django-custom-user-models",
+    version="0.1.0",
+    author="mohammad hosein shamsaei",
+    author_email="mh.shamsaei.ms@gmail.com",
+    description="A custom user model for django authentication",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MHolger77/django-auth",
+    include_package_data=True,
+    install_requires=['django-authlib', 'jwt'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    python_requires='>=3.6',
+)
+
+# py setup.py sdist bdist_wheel
+# twine upload dist/*
