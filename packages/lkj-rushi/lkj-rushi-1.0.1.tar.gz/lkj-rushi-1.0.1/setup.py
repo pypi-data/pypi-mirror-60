77a5d3fc8835f40114aa1319a4c3e0edc9909431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:28 +0000

    Add package lkj-rushi/1.0.1/lkj-rushi-1.0.1.tar.gz

---
+++ packages/lkj-rushi/lkj-rushi-1.0.1.tar.gz/lkj-rushi-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="lkj-rushi",
+    version="1.0.1",
+    description="A Python package to get random number between 10 to 20",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tirth-2001/lkj-ru.git",
+    author="Tirth Patel",
+    author_email="tirthgpatel.27@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["lkj"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "lkj-rushi=lkj.fun:guess",
+        ]
+   
+    },
+)
