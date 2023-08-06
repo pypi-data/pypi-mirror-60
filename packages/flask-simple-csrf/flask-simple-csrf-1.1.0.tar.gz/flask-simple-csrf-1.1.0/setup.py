c4edc721e8ce1af2f71a7b4b3b4c2b44aef172ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:48 +0000

    Add package flask-simple-csrf/1.1.0/flask-simple-csrf-1.1.0.tar.gz

---
+++ packages/flask-simple-csrf/flask-simple-csrf-1.1.0.tar.gz/flask-simple-csrf-1.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as r:
+    README = r.read()
+
+setup(
+    name='flask-simple-csrf',
+    version='1.1.0',
+    description='Extremely simple, "Good Enough" csrf implemention for flask forms, no server side sessions required.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cc-d/flask-simple-csrf',
+    author='Cary Carter',
+    author_email='ccarterdev@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=['Werkzeug==0.16.0']
+)
