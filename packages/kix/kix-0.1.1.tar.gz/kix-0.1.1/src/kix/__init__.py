aed17619b7955bd4cd854dbc33161fe3073b453e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:36 +0000

    Add package kix/0.1.1/kix-0.1.1.tar.gz

---
+++ packages/kix/kix-0.1.1.tar.gz/kix-0.1.1/src/kix/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.1"
+
+
+from .color import red, green, blue, yellow, purple, black
+from .log import info, debug, warning, error, critical
+from .dict_expander import expand
+
