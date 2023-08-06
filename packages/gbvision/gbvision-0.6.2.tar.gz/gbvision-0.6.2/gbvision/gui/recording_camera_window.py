1230ce2cab77a4c801d5cc2e4bfb8fac20bfe531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:18 +0000

    Add package gbvision/0.6.2/gbvision-0.6.2.tar.gz

---
+++ packages/gbvision/gbvision-0.6.2.tar.gz/gbvision-0.6.2/gbvision/gui/recording_camera_window.py
@@ -0,0 +1,8 @@
+from .recording_wrapper_opencv_window import RecordingWrapperOpenCVWindow
+from .camera_window import CameraWindow
+
+
+class RecordingCameraWindow(RecordingWrapperOpenCVWindow, CameraWindow):
+    """
+    a basic window that displays the stream from a stream receiver
+    """
