94dedf1cf73a6de8b9f94b4794a993429fa647a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:07 +0000

    Add package pagermaid/2020.2.post11/pagermaid-2020.2.post11.tar.gz

---
+++ packages/pagermaid/pagermaid-2020.2.post11.tar.gz/pagermaid-2020.2.post11/pagermaid/interface/views.py
@@ -0,0 +1,88 @@
+""" Static views generated for PagerMaid. """
+
+from os.path import join
+from pathlib import Path
+from os.path import exists
+from flask import render_template, request, url_for, redirect, send_from_directory
+from flask_login import login_user, logout_user, current_user
+from pagermaid import logs
+from pagermaid.interface import app, login
+from pagermaid.interface.modals import User
+from pagermaid.interface.forms import LoginForm, SetupForm
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/setup", methods=['GET', 'POST'])
+def setup():
+    form = SetupForm(request.form)
+    msg = None
+    if request.method == 'GET':
+        if exists('data/.user_configured'):
+            return redirect(url_for('login'), code=302)
+        return render_template('pages/setup.html', form=form, msg=msg)
+    if form.validate_on_submit():
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str)
+        email = request.form.get('email', '', type=str)
+        user = User.query.filter_by(user=username).first()
+        user_by_email = User.query.filter_by(email=email).first()
+        if user or user_by_email:
+            msg = 'This email already exists on this system, sign in if it is yours.'
+        else:
+            pw_hash = password
+            user = User(username, email, pw_hash)
+            user.save()
+            Path('data/.user_configured').touch()
+            return redirect(url_for('login'), code=302)
+    else:
+        msg = 'Invalid input.'
+    return render_template('pages/setup.html', form=form, msg=msg)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if not exists('data/.user_configured'):
+        return redirect(url_for('setup'), code=302)
+    form = LoginForm(request.form)
+    msg = None
+    if form.validate_on_submit():
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str)
+        user = User.query.filter_by(user=username).first()
+        if user:
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                msg = "Incorrect username or password."
+        else:
+            msg = "This user does not exist."
+    return render_template('pages/login.html', form=form, msg=msg)
+
+
+@app.route('/')
+def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    return render_template('pages/index.html')
+
+
+@app.errorhandler(404)
+def no_such_file_or_directory(exception):
+    return render_template('pages/404.html')
+
+
+@app.errorhandler(500)
+def internal_server_error(exception):
+    logs.error(exception)
+    return render_template('pages/500.html')
