d955f3b6f7c3dc73cca941fb90be6333ab087256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:46 +0000

    Add package Jobtimize/0.0.4a0/Jobtimize-0.0.4a0.tar.gz

---
+++ packages/Jobtimize/Jobtimize-0.0.4a0.tar.gz/Jobtimize-0.0.4a0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Jobtimize
+
+
+setuptools.setup(
+    name = "Jobtimize",
+    packages = ["Jobtimize"],
+    version = Jobtimize.__version__,
+    install_requires = ["beautifulsoup4", "pandas", "jsonschema", "lxml", "requests"],
+    #setup_requires = ["bs4", "jsonschema", "lxml","numpy", "pandas"],
+    author = "Loïc Rakotoson",
+    author_email = "contact@loicrakotoson.com",
+    url = "https://loicrakotoson.com/Jobtimize/",
+    description = "Collect and standardize data on job posting platforms.",
+    long_description = open("README.md", encoding="utf8").read(),
+    long_description_content_type = "text/markdown",
+    license = "MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+
+)
