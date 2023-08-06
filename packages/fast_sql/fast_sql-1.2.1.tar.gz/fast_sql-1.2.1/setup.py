e5d3c895f5f7c4e42212ba5fa5f3e54546e97627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package fast_sql/1.2.1/fast_sql-1.2.1.tar.gz

---
+++ packages/fast_sql/fast_sql-1.2.1.tar.gz/fast_sql-1.2.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md','r',encoding='utf-8') as fp:
+    md = fp.read()
+
+
+setuptools.setup(
+    name="fast_sql",
+    version="1.2.1",
+    author="wuwukai",
+    author_email="1286345540@qq.com",
+    description="Multithreaded packaging based on python3.6, for fast read read SQL loaded as DataFrame, fast read read SQL written to CSV, Quick read migration table.",
+    long_description=open('./README.md','r',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "pandas>=0.23.0",
+        "DBUtils>=1.3",
+        'tqdm>=4.29',
+        'sqlalchemy==1.2.15',
+        'cx_Oracle>=6.0',
+        'pymysql>=0.9.2',
+    ],
+    url="https://github.com/tosmart01/fastsql",
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+       "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    data_files=['README.md']
+)
