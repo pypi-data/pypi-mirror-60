9fabe7c5731aa2d90aa3c3e11ca68d00920249c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:19 +0000

    Add package open-telemetry-kit/0.2.2/open_telemetry_kit-0.2.2.tar.gz

---
+++ packages/open-telemetry-kit/open_telemetry_kit-0.2.2.tar.gz/open_telemetry_kit-0.2.2/open_telemetry_kit/parser.py
@@ -0,0 +1,33 @@
+from .telemetry import Telemetry
+from .element import Element
+from abc import ABCMeta
+from abc import abstractmethod
+
+class Parser(metaclass=ABCMeta):
+  def __init__(self, source, 
+               convert_to_epoch: bool = False,
+               require_timestamp: bool = False):
+    self.source = source
+    self.convert_to_epoch = convert_to_epoch
+    self.require_timestamp = require_timestamp
+    self.element_dict = {}
+    for subcls in Element.__subclasses__():
+      for el in subcls.__subclasses__():
+        for name in el.names:
+          self.element_dict[name] = el
+
+  def __str__(self) -> str:
+    return "{}('{}')".format(self.__class__.__name__, self.source)
+
+  def __repr__(self) -> str:
+    return "{}('{}')".format(self.__class__.__name__, self.source)
+
+  @property
+  @classmethod
+  @abstractmethod
+  def tel_type(self) -> str:
+    pass
+
+  @abstractmethod
+  def read(self) -> Telemetry:
+    pass
