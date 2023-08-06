748a76fa4281b52d7a5347edefaaf9568e2d9878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:04 +0000

    Add package KeralaPyApi/2.3.2020/KeralaPyApi-2.3.2020.tar.gz

---
+++ packages/KeralaPyApi/KeralaPyApi-2.3.2020.tar.gz/KeralaPyApi-2.3.2020/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+from io import open
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+setup(name='KeralaPyApi',
+      version='2.3.2020',
+      description='Telegram bot api using Python. ',
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      author='Mario',
+      author_email='keralapyapi@gmail.com',
+      url='https://github.com/KeralaPyApi/KeralaPyApi',
+      packages=['keralabot'],
+      license='GPLv2',
+      keywords='telegram bot api using python',
+      install_requires=['requests', 'six'],
+      extras_require={
+          'json': 'ujson',
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      ]
+      )
