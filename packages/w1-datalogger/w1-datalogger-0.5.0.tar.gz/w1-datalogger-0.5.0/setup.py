07253a6218847639dea98291076e1ef071d372c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:01 +0000

    Add package w1-datalogger/0.5.0/w1-datalogger-0.5.0.tar.gz

---
+++ packages/w1-datalogger/w1-datalogger-0.5.0.tar.gz/w1-datalogger-0.5.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name="w1-datalogger",
+    version="0.5.0",
+    packages=find_packages(),
+    author="Steve Work",
+    author_email="steve@work.renlabs.com",
+    description="Log data from local w1 busses to a cloud endpoint",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License"
+    ],
+    zip_safe=True,
+    install_requires = [
+        "requests>=2,<3"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "w1logger = w1datalogger.logger:main",
+        ]
+    }
+)
