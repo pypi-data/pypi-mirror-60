69ab0b1facd2a965b67df7fdcf456a990ed2cd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package qb-sdk/0.0.3/qb_sdk-0.0.3-py3-none-any.whl

---
+++ packages/qb-sdk/qb_sdk-0.0.3-py3-none-any.whl/qbsdk/error.py
@@ -0,0 +1,24 @@
+
+
+class QiibeeError(Exception):
+    def __init__(self, message, http_status_code=None):
+        self.http_status_code = http_status_code
+        self.message = message
+
+class InvalidRequestError(QiibeeError):
+    pass
+
+class AuthorizationError(QiibeeError):
+    pass
+
+class ConfigError(QiibeeError):
+    pass
+
+class NotFoundError(QiibeeError):
+    pass
+
+class ConflictError(QiibeeError):
+    pass
+
+class ServerResponseParseError(QiibeeError):
+    pass
