bb576f6059db00da7a14287a9cfbc0614316465c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package img2org/0.2.2/img2org-0.2.2.tar.gz

---
+++ packages/img2org/img2org-0.2.2.tar.gz/img2org-0.2.2/docs/index.rst
@@ -0,0 +1,20 @@
+Welcome to img2org's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   readme
+   installation
+   usage
+   modules
+   contributing
+   authors
+   history
+
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/img2org/img2org-0.2.2.tar.gz/img2org-0.2.2/tests/test_img2org.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `img2org` package."""
+
+
+import unittest
+
+from img2org import img2org
+
+
+class TestImg2org(unittest.TestCase):
+    """Tests for `img2org` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
