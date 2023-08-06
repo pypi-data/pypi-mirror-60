61bd6bf382c788a3ed3e7633081b9f0e84d9e906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:37 +0000

    Add package p3-data/2.0.2/p3_data-2.0.2.tar.gz

---
+++ packages/p3-data/p3_data-2.0.2.tar.gz/p3_data-2.0.2/setup.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
+from setuptools import setup
+
+setup(
+    name='p3_data',
+    version='2.0.2',
+    description='A variety of utility functions to analyze results data produced by the P3 Test Driver.',
+    url='http://github.com/pravega/p3_test_driver',
+    author='Claudio Fahey',
+    author_email='claudio.fahey@dell.com',
+    packages=['p3_data'],
+    install_requires=[
+        'IPython',
+        'ipywidgets',
+        'pandas',
+        'PyYaml',
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'six',
+        'sklearn',
+    ],
+    zip_safe=True,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
