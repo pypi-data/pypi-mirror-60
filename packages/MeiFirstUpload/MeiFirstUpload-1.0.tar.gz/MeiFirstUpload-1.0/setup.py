23f6b75e6287e0f260335ad66b3ccd9057f9384c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:49 +0000

    Add package MeiFirstUpload/1.0/MeiFirstUpload-1.0.tar.gz

---
+++ packages/MeiFirstUpload/MeiFirstUpload-1.0.tar.gz/MeiFirstUpload-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='MeiFirstUpload',
+    version='1.0',
+    description='这是第一个对外发布的模块，此处是描述。 测试哦',
+    author='mlwtc',
+    author_email='meistudent@163.com',
+    py_modules=['Duiwaifabumokuai.demo1','Duiwaifabumokuai.demo2'] #需要发布的模块
+)
