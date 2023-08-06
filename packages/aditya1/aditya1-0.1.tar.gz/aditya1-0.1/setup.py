45e3d53b3b5b730e24b5772c4db9c46c11aedbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:59 +0000

    Add package aditya1/0.1/aditya1-0.1.tar.gz

---
+++ packages/aditya1/aditya1-0.1.tar.gz/aditya1-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(name='aditya1',
+      version='0.1',
+      description='add the number',
+      author='adityasg',
+      author_email="adityatiwari@sganalytics.com",
+      license='MIT',
+      packages=['aditya1'],
+      long_description="firstpackage",
+      zip_safe=False)
