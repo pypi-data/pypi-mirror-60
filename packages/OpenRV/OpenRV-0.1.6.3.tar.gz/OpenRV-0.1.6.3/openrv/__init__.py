6fc9357e0127594145cb6ec9d802592757211ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:05 +0000

    Add package OpenRV/0.1.6.3/OpenRV-0.1.6.3.tar.gz

---
+++ packages/OpenRV/OpenRV-0.1.6.3.tar.gz/OpenRV-0.1.6.3/openrv/__init__.py
@@ -0,0 +1,7 @@
+from openrv.app import App
+from openrv.colors import *
+from openrv.contour import Contour, ContourSet
+from openrv.image import Image
+# import openrv.ml as ml
+import openrv.utils
+import cv2
