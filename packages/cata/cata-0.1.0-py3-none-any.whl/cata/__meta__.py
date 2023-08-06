6e2e016e04687286a05afb04a7b44ba08d9fbb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:07 +0000

    Add package cata/0.1.0/cata-0.1.0-py3-none-any.whl

---
+++ packages/cata/cata-0.1.0-py3-none-any.whl/cata/__meta__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# catalog (c) Ian Dennis Miller
+
+__project__ = 'catalog'
+__version__ = '0.1.0'
+__author__ = 'Ian Dennis Miller'
+__email__ = 'ian@iandennismiller.com'
+__url__ = 'https://catalog-data.readthedocs.io'
+__copyright__ = 'Ian Dennis Miller'
