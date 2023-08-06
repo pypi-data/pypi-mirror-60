dbf41e46d2c5debccb68220ff326f92afc0da768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:01 +0000

    Add package draft-sport/0.0.6/draft-sport-0.0.6.tar.gz

---
+++ packages/draft-sport/draft-sport-0.0.6.tar.gz/draft-sport-0.0.6/draft_sport/security/session.py
@@ -0,0 +1,14 @@
+"""
+Draft Sport
+Session Module
+author: hugh@blinkybeach.com
+"""
+from nozomi import Decodable
+
+
+class Session(Decodable):
+    """
+    An authenticated session with the Draft Sport API, storing credentials
+    for use by API requests.
+    """
+    pass
