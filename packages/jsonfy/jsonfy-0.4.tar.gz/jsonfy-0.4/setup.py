7ed381c3f5db48dc1721cd499554a698707f0496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:08 +0000

    Add package jsonfy/0.4/jsonfy-0.4.tar.gz

---
+++ packages/jsonfy/jsonfy-0.4.tar.gz/jsonfy-0.4/setup.py
@@ -0,0 +1,29 @@
+# -*- coding utf-8 -*-#
+# ------------------------------------------------------------------
+# Name:      setup
+# Author:    liangbaikai
+# Date:      2020/1/17
+# Desc:      there is a setup.py file
+# ------------------------------------------------------------------
+from setuptools import setup, find_packages
+
+setup(
+    name="jsonfy",
+    version="0.4",
+    keywords=["pip", "json", "jsonfy", "obj"],
+    description="Jsonfy: Lightweight two-way binding of JSON and object without third-party dependency",
+    long_description="Jsonfy: Lightweight two-way binding of JSON and object without third-party dependency, suppoert nesting,complex obj"
+                     "[Jsonfy: 一款轻量级的无第三方依赖的json和对象的双向绑定的工具，支持复杂类型，嵌套类型，自定义某个字段是否序列化等,欢迎使用]",
+
+    license="MIT Licence",
+
+    url="https://github.com/1144388620lq/jsonfy",
+    author="liangbaikai",
+    author_email="1144388620@qq.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+
+)
