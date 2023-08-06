7ee7aee14c60e2362a28eab0600b729b1dac9b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:40 +0000

    Add package sga-imdb/0.1/sga_imdb-0.1.tar.gz

---
+++ packages/sga-imdb/sga_imdb-0.1.tar.gz/sga_imdb-0.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 13:17:53 2020
+
+@author: abdulkadarb
+"""
+
+from setuptools import setup
+
+setup(name='sga_imdb',
+      version='0.1',
+      description='Extract IMDB data',
+      #url='http://github.com/storborg/funniest',
+      author='AbdulkadarBabbar',
+      author_email='abdulkadar.babbar@sganalytics.com',
+      license='MIT',
+      packages=['sga_imdb'],
+      long_description="Get episode Count",
+      zip_safe=False)
