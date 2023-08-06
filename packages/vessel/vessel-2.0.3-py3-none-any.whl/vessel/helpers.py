3f1d03f36f23e4f6c371c1345e9a5ce6e8e88015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:27 +0000

    Add package vessel/2.0.3/vessel-2.0.3-py3-none-any.whl

---
+++ packages/vessel/vessel-2.0.3-py3-none-any.whl/vessel/helpers.py
@@ -0,0 +1,53 @@
+import functools
+
+
+__all__ = ('resolve',)
+
+
+def imitate(value, name = None, module = None, doc = None):
+
+    if name:
+
+        value.__name__ = value.__qualname__ = name
+
+    if module:
+
+        value.__module__ = module
+
+    if doc:
+
+        value.__doc__ = doc
+
+
+def simulate(cls, name = None, module = None, doc = None):
+
+    class meta(type):
+
+        __subclasscheck__ = cls.__subclasscheck__
+
+        __instancecheck__ = cls.__instancecheck__
+
+        __bases__ = (cls,)
+
+    class value(metaclass = meta):
+
+        """.."""
+
+        __slots__ = ()
+
+        def __new__(cls, *args, **kwargs):
+
+            return cls.__bases__[0](*args, **kwargs)
+
+    imitate(value, name = name or cls.__name__, module = module, doc = doc)
+
+    return value
+
+
+def resolve(value):
+
+    if isinstance(value, functools.partial):
+
+        return resolve(value.func)
+
+    return value
