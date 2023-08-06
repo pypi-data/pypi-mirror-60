e6e60b504f3b9eb04b15cfa302c44679b40fc2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:31 +0000

    Add package check-ip/0.4.1/check-ip-0.4.1.tar.gz

---
+++ packages/check-ip/check-ip-0.4.1.tar.gz/check-ip-0.4.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    README = f.read()
+
+setup(
+    name="check-ip",
+    version="0.4.1",
+    description="Check your public IP address and update DNS records on Cloudflare.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Samuel Searles-Bryant",
+    author_email="devel@samueljsb.co.uk",
+    license="MIT",
+    url="https://github.com/samueljsb/check-ip/",
+    py_packages=["check_ip"],
+    install_requires=["requests", "click", "pyyaml"],
+    entry_points={"console_scripts": ["check-ip=check_ip.cli:main"]},
+)
