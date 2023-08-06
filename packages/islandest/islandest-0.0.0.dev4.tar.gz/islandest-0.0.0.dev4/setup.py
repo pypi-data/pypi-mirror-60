625525bbcf0d9fa9b2111cdd4b9eec50759e8936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:23 +0000

    Add package islandest/0.0.0.dev4/islandest-0.0.0.dev4.tar.gz

---
+++ packages/islandest/islandest-0.0.0.dev4.tar.gz/islandest-0.0.0.dev4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="islandest",
+    version='0.0.0_dev4',
+    description='A command line tool to identify and annotate tRNA-targeting genomic islands',
+    url='https://github.com/bhattlab/islandest',
+    author="Matt Durrant",
+    author_email="mdurrant@stanford.edu",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'biopython',
+    ],
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'islandest = islandest.main:cli'
+        ],
+}
+)
