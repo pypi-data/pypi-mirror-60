235ff5b4f13bd873e07ff529bffd78c8273dc38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:36 +0000

    Add package resguard/0.3/resguard-0.3.tar.gz

---
+++ packages/resguard/resguard-0.3.tar.gz/resguard-0.3/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+
+# The text of the README file
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="resguard",
+    author="Daniel Hilst Selli",
+    author_email="danielhilst@gmail.com",
+    version="0.3",
+    py_modules=["resguard"],
+    python_requires=">=3.6",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="Apache-2.0",
+    install_requires=[
+        "dataclasses;python_version<'3.7'",
+        "typing_extensions;python_version<'3.8'",
+    ],
+    extras_require={
+        'tests': ['requests', 'dataclass;python_version<="3.6"'],
+    },
+    test_suite="resguard",
+)
