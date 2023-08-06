017ae58456796acea3a022e053127127c69e0cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:04 +0000

    Add package flask-app/1.0.2.dev1/flask-app-1.0.2.dev1.tar.gz

---
+++ packages/flask-app/flask-app-1.0.2.dev1.tar.gz/flask-app-1.0.2.dev1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("requirements.txt") as f:
+    PACKAGES = f.read().splitlines()
+with open("README.md", "r") as fh:
+    README = fh.read()
+setuptools.setup(
+    name="flask-app",
+    author="Kimbugwe Simon Peter",
+    author_email="kimbsimon2@gmail.com",
+    install_requires=PACKAGES,
+    description="A tool to create flask boilerplate code",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    entry_points={"console_scripts": ["flask-app=creator.main:main"]},
+    url="https://github.com/kimbugp/flask_app",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=["pbr"],
+    pbr=True,
+)
