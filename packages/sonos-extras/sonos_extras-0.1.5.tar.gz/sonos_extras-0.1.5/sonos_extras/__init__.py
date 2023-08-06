9a746e5f04198f4f9f423db3bdb2cc28701bd904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:18 +0000

    Add package sonos-extras/0.1.5/sonos_extras-0.1.5.tar.gz

---
+++ packages/sonos-extras/sonos_extras-0.1.5.tar.gz/sonos_extras-0.1.5/sonos_extras/__init__.py
@@ -0,0 +1,7 @@
+"""sonos_extras - Some extra useful commands for Sonos systems, based on SoCo."""
+
+__version__ = '0.1.5'
+__author__ = 'MaziarA <maziara2@gmail.com>'
+__all__ = []
+
+from .sonos_extras import *
