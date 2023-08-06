538acf57d0f485cd6e70558c4a8eacb88ca3fd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:37 +0000

    Add package pdb2sql/0.2.5/pdb2sql-0.2.5-py3-none-any.whl

---
+++ packages/pdb2sql/pdb2sql-0.2.5-py3-none-any.whl/pdb2sql/__init__.py
@@ -0,0 +1,6 @@
+from .pdb2sqlcore import pdb2sql
+from .interface import interface
+from .StructureSimilarity import StructureSimilarity
+from . import transform
+
+from .__version__ import __version__
