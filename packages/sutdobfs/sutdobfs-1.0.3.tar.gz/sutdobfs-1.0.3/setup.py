fce481a0588b802949018455720f3cfef714a29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:36 +0000

    Add package sutdobfs/1.0.3/sutdobfs-1.0.3.tar.gz

---
+++ packages/sutdobfs/sutdobfs-1.0.3.tar.gz/sutdobfs-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name="sutdobfs",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    entry_points={"console_scripts": ["sutdobfs = sutdobfs.__main__:main"]},
+    version="1.0.3",
+    description="Establish your variable names in collaboration with MIT",
+    long_description=long_descr,
+    long_description_content_type="text/markdown",
+    author="Chester Koh",
+    author_email="chester8991@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    url="https://github.com/OpenSUTD/sutdobfs",
+)
