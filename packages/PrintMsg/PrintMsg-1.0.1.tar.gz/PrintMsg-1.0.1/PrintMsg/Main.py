2af5b26ed1047ef7bf6cb165da8d2410020b176b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:49 +0000

    Add package PrintMsg/1.0.1/PrintMsg-1.0.1.tar.gz

---
+++ packages/PrintMsg/PrintMsg-1.0.1.tar.gz/PrintMsg-1.0.1/PrintMsg/Main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 12:57:31 2020
+
+@author: Sadanand
+"""
+
+from utils import *
+
+def printtext(var):
+    
+    printmsg(var)
+
+
+if __name__=="__main__":
+    printtext("sadanand")
+    
---
+++ packages/PrintMsg/PrintMsg-1.0.1.tar.gz/PrintMsg-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="PrintMsg",
+    version="1.0.1",
+    description="A Python package print the message",
+    author="Sadanand Prajapati",
+    author_email="sadanandp04@gmail.com",
+    long_description="asfajasklfhfad",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["PrintMsg"],
+    include_package_data=True,
+    install_requires=["requests"],
+)
