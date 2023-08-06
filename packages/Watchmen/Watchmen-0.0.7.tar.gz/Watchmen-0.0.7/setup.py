cee698b58499455e37beb6ee598e7482ad1fb6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:38 +0000

    Add package Watchmen/0.0.7/Watchmen-0.0.7.tar.gz

---
+++ packages/Watchmen/Watchmen-0.0.7.tar.gz/Watchmen-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Watchmen",
+    version="0.0.7",
+    author="John",
+    author_email="jfortik@gmail.com",
+    description="Package for nothing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["Pillow", "numpy", "scikit-image", "imutils", "opencv-python"],
+)
