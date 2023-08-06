fd5a72e3f04a5d96c15cfa871a83f3639a90ef7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:42 +0000

    Add package vkbee/0.1/vkbee-0.1-py3-none-any.whl

---
+++ packages/vkbee/vkbee-0.1-py3-none-any.whl/vkbee/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: asyncvk
+import api
+import longpoll
+
+__copyright__ = "2020"
+__version__ = "0.1"
+__authors__ = ["YamkaFox","sergeyfillipov1"]
+
+if __name__ == '__main__':
+    print('Dont run me')
