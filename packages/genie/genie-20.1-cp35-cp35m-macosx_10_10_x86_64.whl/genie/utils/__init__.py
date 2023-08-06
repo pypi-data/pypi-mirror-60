0fde8451dc9a999dd62edebd27cf46494fbb8ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:37 +0000

    Add package genie/20.1/genie-20.1-cp35-cp35m-macosx_10_10_x86_64.whl

---
+++ packages/genie/genie-20.1-cp35-cp35m-macosx_10_10_x86_64.whl/genie/utils/__init__.py
@@ -0,0 +1,12 @@
+'''
+    Module:
+        Utils
+
+    Description:
+        This is a sub-component of Genie that provides various nice-to-use utilities.
+
+'''
+
+# metadata
+__copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
+
