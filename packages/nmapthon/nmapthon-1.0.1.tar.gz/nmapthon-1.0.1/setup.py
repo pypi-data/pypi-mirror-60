f9bd6c1b880c86a713eae4afae07fc90d613e729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:16 +0000

    Add package nmapthon/1.0.1/nmapthon-1.0.1.tar.gz

---
+++ packages/nmapthon/nmapthon-1.0.1.tar.gz/nmapthon-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nmapthon", # Replace with your own username
+    version="1.0.1",
+    author="cblopez",
+    author_email="noeroiff@protonmail.com",
+    description="A simple, high level Nmap library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cblopez/nmapthon",
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Networking",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
