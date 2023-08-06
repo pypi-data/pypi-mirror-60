54a5f9aea32eaf1704b3624cf8372bed74a2db5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:06 +0000

    Add package disko/0.7.0b1/disko-0.7.0b1.tar.gz

---
+++ packages/disko/disko-0.7.0b1.tar.gz/disko-0.7.0b1/disko/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright Tim Molteno 2019 tim@elec.ac.nz
+#
+# Init for the DiSkO imaging algorithm
+from .disko import DiSkO, get_source_list
+from .disko_ms import disko_from_ms
+from .sphere import HealpixSphere, HealpixSubSphere
+from .sphere_mesh import AdaptiveMeshSphere, area
+from .telescope_operator import TelescopeOperator, normal_svd, dask_svd, plot_spectrum, plot_uv
+from .draw_sky import mask_to_sky
+from .projection_lsqr import plsqr
---
+++ packages/disko/disko-0.7.0b1.tar.gz/disko-0.7.0b1/disko/dmsh_test.py
@@ -0,0 +1,3 @@
+import pygmsh
+
+geo = pygmsh.opencascade.geometry.Geometry(characteristic_length_min=0.01, characteristic_length_max=0.1)
