43ce0f986059f3e7856c6bbb7dc004cc479a9162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:42 +0000

    Add package mukham/1.0.2/mukham-1.0.2.tar.gz

---
+++ packages/mukham/mukham-1.0.2.tar.gz/mukham-1.0.2/mukham/__main__.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+from mukham.detector import detect_largest_face
+
+
+"""
+    Argument parser for command line interface
+"""
+my_parser = argparse.ArgumentParser(description="mukham: Crop the largest face from an image.")
+my_parser.add_argument('-i', '--input', type=str, required=True, help='path to input image file')
+my_parser.add_argument('-o', '--output', type=str, required=True, help='path to save output image file')
+my_parser.add_argument(
+    '-c', '--conf', type=float, required=False, 
+    help='confidence threshold for face detected',
+    default=0.8
+    )
+
+# parse arguments
+args =  my_parser.parse_args()
+
+# run
+detect_largest_face(args.input, out_path=args.output, min_conf=args.conf)
---
+++ packages/mukham/mukham-1.0.2.tar.gz/mukham-1.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    dependencies = list(map(lambda x: x.replace('==', '>='), fh.read().split('\n')))
+
+setuptools.setup(
+    name="mukham",
+    version="1.0.2",
+    author="Anish Krishna Vallapuram",
+    author_email="akvallapuram@connect.ust.hk",
+    description="A basic library to crop largest face from the images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akvallapuram/mukham",
+    packages=['mukham'],
+    install_requires=dependencies,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
