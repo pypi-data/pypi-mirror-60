9906180da1b057c9ee3e329d28e4bdbaea2edab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:40 +0000

    Add package keras-mixed-sequence/1.0.3/keras_mixed_sequence-1.0.3.tar.gz

---
+++ packages/keras-mixed-sequence/keras_mixed_sequence-1.0.3.tar.gz/keras_mixed_sequence-1.0.3/keras_mixed_sequence/utils/sequence_length.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def sequence_length(sequence: List, batch_size: int) -> int:
+    """Return number of batch sizes contained in sequence.
+
+    Parameters
+    -----------
+    sequence: List,
+        Iterable to split into batches.
+    batch_size: int,
+        Size of the batches.
+
+    Returns
+    -----------
+    Return number of batch size contained in given sequence, by excess.
+    """
+    return len(sequence) // batch_size
