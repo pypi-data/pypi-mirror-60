73e109a8fdbf36fc723f5efdd1f444299cd7b9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:51 +0000

    Add package nota/0.8.5/nota-0.8.5.tar.gz

---
+++ packages/nota/nota-0.8.5.tar.gz/nota-0.8.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='nota',
+      version='0.8.5',
+      description='Text-based note taker',
+      url='https://github.com/dankelley/nota',
+      author='Dan Kelley',
+      author_email='kelley.dan@gmail.com',
+      license='GPL3',
+      packages=['nota'],
+      python_requires='>=3.6',
+      classifiers=['Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
+      test_suite="tests",
+      entry_points={ 'console_scripts':
+          [ 'nota = nota.main:nota' ]
+          },
+      zip_safe=True)
