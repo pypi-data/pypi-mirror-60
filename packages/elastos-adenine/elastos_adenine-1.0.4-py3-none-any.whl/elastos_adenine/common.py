2b75843195036096fd9838238fa499e6bf88934a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:50 +0000

    Add package elastos-adenine/1.0.4/elastos_adenine-1.0.4-py3-none-any.whl

---
+++ packages/elastos-adenine/elastos_adenine-1.0.4-py3-none-any.whl/elastos_adenine/common.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import grpc
+from decouple import config
+
+from .stubs import common_pb2, common_pb2_grpc
+from elastos_adenine.settings import REQUEST_TIMEOUT
+
+
+class Common:
+
+    def __init__(self):
+        host = config('GRPC_SERVER_HOST')
+        port = config('GRPC_SERVER_PORT')
+        production = config('PRODUCTION', default=False, cast=bool)
+        if not production:
+            self._channel = grpc.insecure_channel('{}:{}'.format(host, port))
+        else:
+            credentials = grpc.ssl_channel_credentials()
+            self._channel = grpc.secure_channel('{}:{}'.format(host, port), credentials)
+        self.stub = common_pb2_grpc.CommonStub(self._channel)
+
+    def close(self):
+        self._channel.close()
+
+    def generate_api_request_mnemonic(self, mnemonic):
+        response = self.stub.GenerateAPIRequestMnemonic(common_pb2.RequestMnemonic(mnemonic=mnemonic), timeout=REQUEST_TIMEOUT)
+        return response
+
+    def generate_api_request(self, secret_key, did):
+        response = self.stub.GenerateAPIRequest(common_pb2.Request(secret_key=secret_key, did=did), timeout=REQUEST_TIMEOUT)
+        return response
+
+    def get_api_request_mnemonic(self, mnemonic):
+        response = self.stub.GetAPIKeyMnemonic(common_pb2.RequestMnemonic(mnemonic=mnemonic), timeout=REQUEST_TIMEOUT)
+        return response
+
+    def get_api_key_request(self, secret_key, did):
+        response = self.stub.GetAPIKey(common_pb2.Request(secret_key=secret_key, did=did), timeout=REQUEST_TIMEOUT)
+        return response
