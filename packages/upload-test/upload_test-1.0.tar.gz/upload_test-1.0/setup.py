77b1a2cf73642f43b0f2611bd642db7f7656445d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:18 +0000

    Add package upload-test/1.0/upload_test-1.0.tar.gz

---
+++ packages/upload-test/upload_test-1.0.tar.gz/upload_test-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name="upload_test",
+    version=1.0,
+    description="这是一个测试",
+    author='zhu',
+    author_email='zhu_gxu@qq.com',
+    py_modules={
+        "zhu_01.send_test"
+    }
+
+)
