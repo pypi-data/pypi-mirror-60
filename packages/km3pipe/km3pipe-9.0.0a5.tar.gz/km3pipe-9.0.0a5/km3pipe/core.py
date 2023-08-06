8f911334828bb96fe0018f2e2d0d7959b427965d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:03 +0000

    Add package km3pipe/9.0.0a5/km3pipe-9.0.0a5.tar.gz

---
+++ packages/km3pipe/km3pipe-9.0.0a5.tar.gz/km3pipe-9.0.0a5/km3pipe/core.py
@@ -0,0 +1,12 @@
+# Filename: core.py
+# pylint: disable=locally-disabled
+"""
+The core of the KM3Pipe framework.
+
+"""
+from __future__ import absolute_import, print_function, division
+
+import gzip
+from thepipe import Blob, Module, Pipeline
+
+Pump = Module
