40fc3dbef02703421a55aebf285b0fd465afc8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:59 +0000

    Add package enjoyml/0.1.2/enjoyml-0.1.2.tar.gz

---
+++ packages/enjoyml/enjoyml-0.1.2.tar.gz/enjoyml-0.1.2/enjoyml/multiclass.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from collections import Counter
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import precision_recall_fscore_support as prfc_metrics
+
+def multiclass_cross_val_results(features_matrix, labels, model, 
+                                 n_folds=3, random_state=42, shuffle=True, 
+                                 labels_names=None):
+    res = []
+    stratified_k_fold = StratifiedKFold(n_splits=n_folds, random_state=random_state, shuffle=shuffle)
+    for train_index, test_index in stratified_k_fold.split(features_matrix, labels):
+        scaler = StandardScaler()
+        train_features_matrix = scaler.fit_transform(features_matrix[train_index])
+        test_features_matrix = scaler.transform(features_matrix[test_index])
+        model = model.fit(train_features_matrix, labels[train_index])
+        test_labels_predict = model.predict(test_features_matrix)
+        metrics_per_label = prfc_metrics(labels[test_index], test_labels_predict, average=None)[:-1]
+        res.append(metrics_per_label)
+    res = np.asarray(res).mean(axis=0).T
+    res = np.vstack((res, res.mean(axis=0)[np.newaxis, :]))
+    return pd.DataFrame.from_records(
+        res, columns=['precision', 'recall', 'f1-score'],
+        index=tuple(labels_names) + ('mean',)
+    )
+
+def calc_class_weights(labels, mode='ratio_from_max'):
+    labels_counter = Counter(labels)
+    max_label_count = max(labels_counter.values())
+    return {label: max_label_count/label_count 
+            for label, label_count in labels_counter.items()}
---
+++ packages/enjoyml/enjoyml-0.1.2.tar.gz/enjoyml-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='enjoyml',
+      version='0.1.2',
+      description='',
+      url='https://github.com/ByMyTry/enjoyml.git',
+      author='anton-taleckij',
+      author_email='anton.taleckij.job@gmail.com',
+      license='MIT',
+      packages=['enjoyml'],
+      install_requires=[
+          'numpy-1.18.1',
+          'pandas-1.0.1',
+          'scikit-learn-0.22.1',
+      ],
+      zip_safe=False)
