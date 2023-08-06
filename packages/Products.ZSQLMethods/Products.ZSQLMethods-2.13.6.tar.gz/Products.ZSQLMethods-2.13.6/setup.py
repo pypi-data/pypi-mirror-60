2885c52ba92e594664ab895e31dbd7236e35701a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:07 +0000

    Add package Products.ZSQLMethods/2.13.6/Products.ZSQLMethods-2.13.6.tar.gz

---
+++ packages/Products.ZSQLMethods/Products.ZSQLMethods-2.13.6.tar.gz/Products.ZSQLMethods-2.13.6/setup.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+__version__ = '2.13.6'
+
+setup(
+    name='Products.ZSQLMethods',
+    version=__version__,
+    url='http://pypi.python.org/pypi/Products.ZSQLMethods',
+    license='ZPL 2.1',
+    description="SQL method support for Zope 2.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    packages=find_packages('src'),
+    namespace_packages=['Products', 'Shared', 'Shared.DC'],
+    package_dir={'': 'src'},
+    install_requires=[
+        'setuptools',
+        'Acquisition',
+        'DateTime',
+        'ExtensionClass',
+        'Missing',
+        'Persistence',
+        'Record',
+        'transaction',
+        'ZODB3',
+        'zope.interface',
+        'Zope2 > 2.12.8',
+        # These are only available with Zope >= 2.13.0a1
+        # 'AccessControl',
+        'DocumentTemplate >= 2.13.5',
+        # 'zExceptions',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
