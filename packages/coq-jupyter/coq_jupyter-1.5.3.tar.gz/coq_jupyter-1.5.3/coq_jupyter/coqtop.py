d5d9a2b1f36ed34acdc03b6d004e793a756fef6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:31 +0000

    Add package coq-jupyter/1.5.3/coq_jupyter-1.5.3.tar.gz

---
+++ packages/coq-jupyter/coq_jupyter-1.5.3.tar.gz/coq_jupyter-1.5.3/coq_jupyter/coqtop.py
@@ -0,0 +1,301 @@
+from __future__ import unicode_literals
+
+import pexpect
+import re
+import xml.etree.ElementTree as ET
+
+from future.utils import raise_with_traceback
+from builtins import zip
+from future.moves.itertools import zip_longest
+from collections import deque
+from pexpect.exceptions import ExceptionPexpect
+from subprocess import check_output
+
+LANGUAGE_VERSION_PATTERN = re.compile(r'version (\d+(\.\d+)+)')
+
+SEPARATOR_PATTERN = r"((?:\[\s*[a-zA-Z][a-zA-Z0-9_']*\s*\]\s*\:\s*\{)|(?:\d+\s*\:\s*\{)|(?:\{)|(?:\})|(?:\++)|(?:\-+)|(?:\*+)|(?:\.))"
+
+INIT_COMMAND = '<call val="Init"> <option val="none"/> </call>'
+STATUS_COMMAND = '<call val="Status"> <bool val="true"/> </call>'
+GOAL_COMMAND = '<call val="Goal"> <unit/> </call>'
+
+ADD_COMMAND_TEMPLATE = """
+<call val="Add">
+  <pair>
+    <pair> <string></string> <int>0</int> </pair>
+    <pair> <state_id val="" /> <bool val="false" /> </pair>
+  </pair>
+</call>
+"""
+
+EDIT_AT_COMMAND_TEMPLATE = '<call val="Edit_at"> <state_id val="{0}"/> </call>'
+
+REPLY_PATTERNS = [
+    re.compile(r'\<{0}.*?\>.+?\<\/{0}\>'.format(t), re.DOTALL)
+    for t in [
+        "feedback",
+        "value",
+        "message" # older versions of coqtop wont wrap 'message' inside 'feedback'
+    ]
+]
+
+class CoqtopError(Exception): pass
+
+class Coqtop:
+
+    def __init__(self, kernel, coqtop_args):
+        try:
+            self.log = kernel.log
+
+            # locate coqtop executable
+            for cmd in ["coqidetop", "coqtop"]:
+                try:
+                    banner = check_output([cmd, '--version']).decode('utf-8')
+                    break
+                except:
+                    cmd = None
+
+            if cmd is None:
+                raise CoqtopError("Failed to locate 'coqidetop' or 'coqtop' executables")
+
+            version = LANGUAGE_VERSION_PATTERN.search(banner).group(1)
+            (parsed_version, version_8_9) = zip(*zip_longest(map(int, version.split(".")), [8, 9], fillvalue=0))
+
+            if cmd == "coqtop" and parsed_version >= version_8_9:
+                raise CoqtopError("Failed to locate 'coqidetop' executable ('coqtop' has been found but is insufficient since v8.9)")
+
+            self.cmd = cmd
+            self.version = version
+            self.banner = banner
+
+            # run coqtop executable
+            spawn_args = {
+                "echo": False,
+                "encoding": "utf-8",
+                "codec_errors": "replace"
+            }
+            if self.cmd == "coqidetop":
+                self._coqtop = pexpect.spawn("coqidetop -main-channel stdfds {}".format(coqtop_args), **spawn_args)
+            else:
+                self._coqtop = pexpect.spawn("coqtop -toploop coqidetop -main-channel stdfds {}".format(coqtop_args), **spawn_args)
+
+            # perform init
+            (reply, _) = self._execute_command(INIT_COMMAND)
+            self.tip = reply.find("./state_id").get("val")
+
+        except Exception as e:
+            raise_with_traceback(CoqtopError("Cause: {}".format(repr(e))))
+
+    def eval(self, code):
+        try:
+            tip_before = self.tip
+
+            sentences = deque(self._get_sentences(code))
+
+            # Attempt to evaluate sentences in code
+            code_evaluated = True
+            outputs = []
+            while len(sentences) > 0:
+                sentence = sentences.popleft()
+
+                (add_reply, _) = self._execute_command(self._build_add_command(sentence, self.tip), allow_fail=True)
+                (status_reply, out_of_band_status_replies) = self._execute_command(STATUS_COMMAND, allow_fail=True)
+
+                sentence_evaluated = self._is_good(add_reply) and self._is_good(status_reply)
+                errors = [
+                    self._get_error_content(r)
+                    for r in [add_reply, status_reply]
+                    if self._has_error(r)
+                ]
+                out_of_band_status_messsages = [
+                    self._get_message_content(r)
+                    for r in out_of_band_status_replies
+                    if self._is_message(r)
+                ]
+
+                if not sentence_evaluated:
+                    # In some cases (for example if there is invalid reference) erroneus command
+                    # can be accepted by parser, increase coqtop tip and fail late.
+                    # To ensure consistent state it is better to roll back to predictable state
+                    self.roll_back_to(self.tip)
+
+                if not sentence_evaluated and len(sentences) > 0:
+                    # Attempt to fix error by joining erroneus sentence with next one.
+                    # This should fix any errors caused by headless splitting
+                    # of code into sentences
+                    sentences.appendleft(sentence + sentences.popleft())
+                    continue
+
+                if not sentence_evaluated and len(sentences) == 0 and self._is_end_of_input_error(add_reply):
+                    # It is ok to ignore failute and output generated by evaluating
+                    # 'effectively empty' leftover sentence
+                    break
+
+                if not sentence_evaluated and len(sentences) == 0:
+                    # Upon reaching this state it we can definitely say that there is
+                    # an error in cell code
+                    code_evaluated = False
+                    outputs.extend(errors)
+                    break
+
+                self.tip = self._get_nexttip(add_reply)
+                outputs.extend(out_of_band_status_messsages)
+
+            if code_evaluated:
+                # Get data about theorem being proven
+                if self._is_proving(status_reply):
+                    outputs.append("Proving: {}".format(self._get_proof_name(status_reply)))
+
+                # Get goal state
+                (goal_reply, _) = self._execute_command(GOAL_COMMAND)
+                if self._has_goals(goal_reply):
+                    outputs.append(self._get_goals_content(goal_reply))
+            else:
+                # roll back any side effects of code
+                self.roll_back_to(tip_before)
+
+            return (code_evaluated, outputs)
+
+        except Exception as e:
+            raise_with_traceback(CoqtopError("Cause: {}".format(repr(e))))
+
+    def roll_back_to(self, state_id):
+        self._execute_command(self._build_edit_at_command(state_id))
+        self.tip = state_id
+
+    def _get_sentences(self, code):
+        parts = re.split(SEPARATOR_PATTERN, code)
+        pi = iter(parts)
+        sentences = [p + next(pi, '') for p in pi]
+        if sentences[-1].strip(" \t\n\r") != "":
+            return sentences
+        else:
+            return sentences[:-1]
+
+    def _execute_command(self, command, allow_fail=False):
+        self.log.debug("Executing coqtop command: {}".format(repr(command)))
+        self._coqtop.send(command + "\n")
+        out_of_band_replies = []
+        while True:
+            self._coqtop.expect(REPLY_PATTERNS)
+
+            if self._coqtop.before.strip(" \t\n\r") != "":
+                self.log.warning("Skipping unexpected coqtop output: {}".format(repr(self._coqtop.before)))
+
+            reply = self._parse(self._coqtop.match.group(0))
+            self.log.debug("Received coqtop reply: {}".format(ET.tostring(reply)))
+
+            if reply.tag == "value" and not allow_fail and not self._is_good(reply):
+                raise CoqtopError("Unexpected reply: {}".format(ET.tostring(reply)))
+            elif reply.tag == "value":
+                return (reply, out_of_band_replies)
+            else:
+                out_of_band_replies.append(reply)
+
+    def _parse(self, reply):
+        return ET.fromstring(reply.replace("&nbsp;", " "))
+
+    def _get_nexttip(self, reply):
+        state_id = reply.find("[@val='good']/pair/pair/union/state_id")
+        if state_id is None:
+            state_id = reply.find("[@val='good']/pair/state_id")
+        return state_id.get("val")
+
+    def _is_good(self, reply):
+        return reply.get("val") == "good"
+
+    def _unwrap_error(self, reply):
+        return reply.find(".[@val='fail']/richpp/..")
+
+    def _has_error(self, reply):
+        return self._unwrap_error(reply) is not None
+
+    def _get_error_content(self, reply):
+        # TODO add error context using loc_s, loc_e
+        error_content = self._format_richpp(self._unwrap_error(reply).find("./richpp"))
+        error_prefix = "Error: "
+        if error_content.startswith(error_prefix):
+            return error_content
+        else:
+            return error_prefix + error_content
+
+    def _unwrap_message(self, reply):
+        return reply if reply.tag == "message" else reply.find(".//message")
+
+    def _is_message(self, reply):
+        return self._unwrap_message(reply) is not None
+
+    def _get_message_content(self, reply):
+        message = self._unwrap_message(reply)
+        message_content = self._format_richpp(message.find("./richpp"))
+        message_level = message.find("./message_level").get("val")
+        message_level_prefix = "{}: ".format(message_level.capitalize())
+
+        if message_level == "notice" or message_content.startswith(message_level_prefix):
+            return message_content
+        else:
+            return message_level_prefix + message_content
+
+    def _unwrap_goals(self, reply):
+        return reply.find("./option/goals")
+
+    def _has_goals(self, reply):
+        return self._unwrap_goals(reply) is not None
+
+    def _get_goals_content(self, reply):
+        goals = self._unwrap_goals(reply)
+        current_goals = list(goals.find("./list").findall("./goal"))
+        background_before_goals = list(goals.find("./list[2]").findall("./pair/list[1]/goal"))
+        background_after_goals = list(goals.find("./list[2]").findall("./pair/list[2]/goal"))
+        background_goals = background_before_goals + background_after_goals
+
+        if len(current_goals) == 0 and len(background_goals) == 0:
+            return "No more subgoals"
+        elif len(current_goals) == 0 and len(background_goals) > 0:
+            header_content = "This subproof is complete, but there are some unfocused goals:"
+            hypotheses_content = ""
+            display_goals = background_goals
+        else:
+            header_content = "1 subgoal" if len(current_goals) == 1 else "{} subgoals".format(len(current_goals))
+            hypotheses_content = "\n".join(map(self._format_richpp, current_goals[0].findall("./list/richpp")))
+            display_goals = current_goals
+
+        goals_content = "\n".join(map(
+            lambda d: "{}\n{}".format(
+                "-------------- ({}/{})".format(d[0] + 1, len(display_goals))[-20:],
+                self._format_richpp(d[1].find("./richpp"))
+            ),
+            enumerate(display_goals)
+        ))
+
+        return "\n\n".join(filter(lambda c: c != "", [header_content, hypotheses_content, goals_content]))
+
+    def _is_proving(self, reply):
+        return reply.find(".status/option/string") is not None
+
+    def _get_proof_name(self, reply):
+        return reply.find(".status/option/string").text
+
+    def _format_richpp(self, richpp):
+        return ET.tostring(richpp, encoding='utf8', method='text').decode('utf8').strip("\n\r")
+
+    def _is_end_of_input_error(self, reply):
+        if self._is_good(reply):
+            return False
+        else:
+            error_content = self._get_error_content(reply)
+            if "Anomaly" in error_content and "Stm.End_of_input" in error_content:
+                return True
+            elif "Anomaly" in error_content and 'Invalid_argument("vernac_parse")' in error_content: # for older coqtop versions
+                return True
+            else:
+                return False
+
+    def _build_edit_at_command(self, state_id):
+        return EDIT_AT_COMMAND_TEMPLATE.format(state_id)
+
+    def _build_add_command(self, sentence, tip):
+        command = ET.fromstring(ADD_COMMAND_TEMPLATE)
+        command.find("./pair/pair/string").text = sentence
+        command.find("./pair/pair[2]/state_id").set("val", tip)
+        return ET.tostring(command, encoding='utf8').decode('utf8')
---
+++ packages/coq-jupyter/coq_jupyter-1.5.3.tar.gz/coq_jupyter-1.5.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+readme = """Jupyter kernel for Coq.
+See: https://github.com/EugeneLoy/coq_jupyter
+"""
+
+setup(
+    name='coq_jupyter',
+    version='1.5.3',
+    packages=['coq_jupyter'],
+    description='Coq kernel for Jupyter',
+    long_description=readme,
+    author='Eugene Loy',
+    author_email='eugeny.loy@gmail.com',
+    url='https://github.com/EugeneLoy/coq_jupyter',
+    include_package_data=True,
+    install_requires=[
+        'jupyter_client',
+        'IPython',
+        'ipykernel',
+        'future',
+        'pexpect>=4.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+        'Framework :: Jupyter',
+        'Intended Audience :: Education',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development'
+    ],
+)
