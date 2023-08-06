0d61b4dab3b78ca3de9d50aeadfec26a774f406e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:40 +0000

    Add package pymodore/1.1.0/pymodore-1.1.0.tar.gz

---
+++ packages/pymodore/pymodore-1.1.0.tar.gz/pymodore-1.1.0/README.md
@@ -0,0 +1,18 @@
+# pymodore
+
+Simple pomodore to run in terminal with sound.
+
+Install this module with:
+
+```
+pip3 install pymodore
+```
+
+A example of log generated from this script:
+```
+🍅 Begin:03/02/2020 16:04:07 | Learn Python | TIME: 25 minutes
+🍅 Begin:03/02/2020 16:05:01 | Hello World | TIME: 25 minutes
+🍅 Begin:03/02/2020 16:06:04 | Test interrupt | TIME: 4 minutes
+🍅 Begin:03/02/2020 16:06:39 | It worked in my machine | TIME: 25 minutes
+|-🍅-🍅- REST -🍅-🍅-|
+```
---
+++ packages/pymodore/pymodore-1.1.0.tar.gz/pymodore-1.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from sys import version_info
+
+if version_info.major < 3:
+    raise Exception("Please install this with: pip3 install pymodore")
+
+setup(
+    name="pymodore",
+    version="1.1.0",
+    author="Natan 'Albrigs' Fernandes dos Santos",
+    author_email="natanfs013@gmail.com",
+    py_modules=['pymodore'],
+    description='A simple terminal pomodore timer.',
+    url='https://github.com/NatanFernandesSantos/pymodore',
+    license='GNU 3.0',
+    keywords=['pomodoro','console','profit', 'command', 'line', 'timer'],
+    python_requires='>=3',
+    install_requires=[
+        'Click',
+        'termcolor',
+        'python-vlc',
+        'gtts'
+    ],
+    entry_points='''
+        [console_scripts]
+        pymodore=pymodore:pomodoro
+    ''',
+     classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+
+)
