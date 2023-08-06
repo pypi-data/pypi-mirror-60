c8cf23bb04576d191b31a20df28ff6ab88445613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:23 +0000

    Add package topsispack-NG/0.0.4/topsispack-NG-0.0.4.tar.gz

---
+++ packages/topsispack-NG/topsispack-NG-0.0.4.tar.gz/topsispack-NG-0.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsispack-NG",
+    version="0.0.4",
+    author="Nikhil Gupta",
+    author_email="ngupta_be17@thapar.edu",
+    description="Multiple Criteria Decision Making(MCDM) using TOPSIS",
+	url='https://github.com/CachingNik/niklib',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	scripts=['bin/topcli'],
+	keywords = ['CLI', 'TOPSIS', 'Data'], 
+	python_requires='>=2.7', 
+)
