48d514290ec0913d83808efa57812bbed6197b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:17 +0000

    Add package parakh-user/0.1/parakh_user-0.1.tar.gz

---
+++ packages/parakh-user/parakh_user-0.1.tar.gz/parakh_user-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(name='parakh_user',
+      version='0.1',
+      description='User made pkg',
+      author='parakht',
+      author_email="parakh.tayal@sganalytics.com",
+      license='MIT',
+      packages=['parakh_user'],
+      long_description="firstpackage",
+      zip_safe=False)
