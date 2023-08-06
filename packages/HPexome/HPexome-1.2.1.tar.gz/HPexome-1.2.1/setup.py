497e49872977ebe75e4a2fc03db9a7482bdbf7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:53 +0000

    Add package HPexome/1.2.1/HPexome-1.2.1.tar.gz

---
+++ packages/HPexome/HPexome-1.2.1.tar.gz/HPexome-1.2.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='HPexome',
+    version='1.2.1',
+    author="Welliton Souza",
+    author_email="well309@gmail.com",
+    description="An automated tool for processing whole-exome sequencing data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://bcblab.org/hpexome",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires=[
+        'Click'
+    ],
+    entry_points='''
+        [console_scripts]
+        hpexome=hpexome.hpexome:hpexome
+    ''',
+    project_urls={
+        "Source Code": "https://github.com/labbcb/hpexome",
+        "Bug Tracker": "https://github.com/labbcb/hpexome/issues"
+    }
+)
