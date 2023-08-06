fb1c0384bdb19330eb449b87a3847e5221a8b214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:42 +0000

    Add package distributions-egm/0.1/distributions_egm-0.1.tar.gz

---
+++ packages/distributions-egm/distributions_egm-0.1.tar.gz/distributions_egm-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_egm',
+      version='0.1',
+      description='different distribution classes',
+      packages=['distributions_egm'],
+      zip_safe=False)
