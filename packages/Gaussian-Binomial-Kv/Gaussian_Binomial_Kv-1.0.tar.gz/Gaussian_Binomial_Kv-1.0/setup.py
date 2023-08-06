3e3707c1e9afb767e4a359550ac4d339b05ee894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:07 +0000

    Add package Gaussian-Binomial-Kv/1.0/Gaussian_Binomial_Kv-1.0.tar.gz

---
+++ packages/Gaussian-Binomial-Kv/Gaussian_Binomial_Kv-1.0.tar.gz/Gaussian_Binomial_Kv-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Gaussian_Binomial_Kv',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['Gaussian_Binomial_Kv'],
+      zip_safe=False)
