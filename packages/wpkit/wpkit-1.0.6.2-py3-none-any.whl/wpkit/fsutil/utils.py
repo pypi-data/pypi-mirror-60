714227e90b86df068842b45d6f52ccee6e81db0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:31 +0000

    Add package wpkit/1.0.6.2/wpkit-1.0.6.2-py3-none-any.whl

---
+++ packages/wpkit/wpkit-1.0.6.2-py3-none-any.whl/wpkit/fsutil/utils.py
@@ -0,0 +1,33 @@
+import os,shutil,glob
+
+def copy_files_to(files,dst,overwrite=False):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for i,f in enumerate(files):
+        fn=os.path.basename(f)
+        f2=dst+'/'+fn
+        if os.path.exists(f2):
+            if os.path.samefile(f, f2):
+                print("ignoring same file:", f, f2)
+                continue
+            if not overwrite:
+                raise Exception("file %s already exists."%(f2))
+            else:
+                print("overwriting %s to %s ..."%(f,f2))
+                os.remove(f2)
+        shutil.copy(f,f2)
+def tranverse_files(dir,func):
+    fs=glob.glob(dir+'/*')
+    for f in fs:
+        if os.path.isfile(f):
+            func(f)
+        elif os.path.isdir(f):
+            tranverse_files(f,func)
+def tranverse_dirs(dir,func):
+    fs=glob.glob(dir+'/*')
+    for f in fs:
+        if os.path.isfile(f):
+            pass
+        elif os.path.isdir(f):
+            func(f)
+            tranverse_dirs(f,func)
