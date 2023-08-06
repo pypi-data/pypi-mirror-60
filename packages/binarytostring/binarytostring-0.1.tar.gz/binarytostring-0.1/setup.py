e5e29daaed93f005b52c0515ca3225f36f3e2636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:16 +0000

    Add package binarytostring/0.1/binarytostring-0.1.tar.gz

---
+++ packages/binarytostring/binarytostring-0.1.tar.gz/binarytostring-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(name='binarytostring',
+      version='0.1',
+      description='change string to binary',
+      author='adityasg',
+      author_email="aditya.tiwari@sganalytics.com",
+      license='MIT',
+      packages=['binarytostring'],
+      long_description="Give 8 bit binary number to string",
+      zip_safe=False)
