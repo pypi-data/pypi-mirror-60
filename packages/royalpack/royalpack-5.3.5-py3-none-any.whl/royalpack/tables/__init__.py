246b77c173cc406c4b5123581160b480e89502bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:09 +0000

    Add package royalpack/5.3.5/royalpack-5.3.5-py3-none-any.whl

---
+++ packages/royalpack/royalpack-5.3.5-py3-none-any.whl/royalpack/tables/__init__.py
@@ -0,0 +1,34 @@
+# Imports go here!
+from .diario import Diario
+from .aliases import Alias
+from .wikipages import WikiPage
+from .wikirevisions import WikiRevision
+from .bios import Bio
+from .reminders import Reminder
+from .triviascores import TriviaScore
+from .mmevents import MMEvent
+from .mmresponse import MMResponse
+from .leagueoflegends import LeagueOfLegends
+from .fiorygi import Fiorygi
+from .steam import Steam
+from .dota import Dota
+
+# Enter the tables of your Pack here!
+available_tables = [
+    Diario,
+    Alias,
+    WikiPage,
+    WikiRevision,
+    Bio,
+    Reminder,
+    TriviaScore,
+    MMEvent,
+    MMResponse,
+    LeagueOfLegends,
+    Fiorygi,
+    Steam,
+    Dota,
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [table.__name__ for table in available_tables]
