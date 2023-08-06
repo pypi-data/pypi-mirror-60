eb60298137cd01d4da9f2969809eee8aaee658ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:00 +0000

    Add package django-reactive/0.0.4/django_reactive-0.0.4-py3-none-any.whl

---
+++ packages/django-reactive/django_reactive-0.0.4-py3-none-any.whl/django_reactive/forms/widgets.py
@@ -0,0 +1,38 @@
+import json
+
+from django.conf import settings
+from django.forms.widgets import Widget
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+
+class ReactJSONSchemaFormWidget(Widget):
+    class Media:
+        css = {
+            'all': (
+                settings.STATIC_URL + 'css/django_reactive.css',
+            )
+        }
+        js = (
+            settings.STATIC_URL + 'dist/react.js',
+            settings.STATIC_URL + 'dist/react-dom.js',
+            settings.STATIC_URL + 'dist/react-jsonschema-form.js',
+            settings.STATIC_URL + 'js/django_reactive.js',
+        )
+
+    template_name = 'django_reactive.html'
+
+    def __init__(self, schema, ui_schema=None, **kwargs):
+        self.schema = schema
+        self.ui_schema = ui_schema
+        super(ReactJSONSchemaFormWidget, self).__init__(**kwargs)
+
+    def render(self, name, value, attrs=None, renderer=None):
+        context = {
+            'data': value,
+            'name': name,
+            'schema': json.dumps(self.schema),
+            'ui_schema': json.dumps(self.ui_schema) if self.ui_schema else '{}',
+        }
+
+        return mark_safe(render_to_string(self.template_name, context))
