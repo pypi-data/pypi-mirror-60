088b544240ee33adaf9f5ded23ce69df4e823bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:36 +0000

    Add package pyinfra/0.13.4/pyinfra-0.13.4.tar.gz

---
+++ packages/pyinfra/pyinfra-0.13.4.tar.gz/pyinfra-0.13.4/pyinfra/api/connectors/__init__.py
@@ -0,0 +1,25 @@
+import six
+
+from . import ansible, docker, local, mech, ssh, vagrant
+
+
+# Connectors that handle execution of pyinfra operations
+EXECUTION_CONNECTORS = {  # pragma: no cover
+    'docker': docker,
+    'local': local,
+    'ssh': ssh,
+}
+
+# Connectors that handle generation of inventories
+INVENTORY_CONNECTORS = {  # pragma: no cover
+    'ssh': ssh,
+    'docker': docker,
+    'mech': mech,
+    'vagrant': vagrant,
+    'ansible': ansible,
+}
+
+ALL_CONNECTORS = (  # pragma: no cover
+    list(six.iterkeys(EXECUTION_CONNECTORS))
+    + list(six.iterkeys(INVENTORY_CONNECTORS))
+)
