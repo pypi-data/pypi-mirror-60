4dc838fe900c02c1c8363ad9e72eff454b34c006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:32 +0000

    Add package trell-data-pipeline/0.2.5/trell_data_pipeline-0.2.5-py2.py3-none-any.whl

---
+++ packages/trell-data-pipeline/trell_data_pipeline-0.2.5-py2.py3-none-any.whl/pipeline/producers/__init__.py
@@ -0,0 +1,11 @@
+from pipeline import settings
+from .base import Producer
+from .kafka import KafkaProducer
+from .mock import MockProducer
+
+
+def get_producer() -> Producer:
+    if settings.PRODUCER_DRIVER == 'kafka':
+        return KafkaProducer()
+    else:
+        return MockProducer()
---
+++ packages/trell-data-pipeline/trell_data_pipeline-0.2.5-py2.py3-none-any.whl/pipeline/settings.py
@@ -0,0 +1,15 @@
+# system config
+LOG_DIR = None
+LOG_LEVEL = 'DEBUG'
+
+# LoRa server settings
+LORA_MQTT_TOPICS = None
+LORA_MQTT_URI = None
+LORA_DRIVER = None
+LORA_MQTT_USERNAME = None
+LORA_MQTT_PASSWORD = None
+
+# Producer settings
+PRODUCER_DRIVER = None
+PRODUCER_TOPIC = None
+PRODUCER_HOSTS = None
