b282726b8192671fdf8c70745f5ea59b70ae7283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package commontaskz/0.5.12/commontaskz-0.5.12.tar.gz

---
+++ packages/commontaskz/commontaskz-0.5.12.tar.gz/commontaskz-0.5.12/commontaskz/query_rankings.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+
+from prefect.engine.result_handlers import JSONResultHandler
+from prefect.utilities.tasks import defaults_from_attrs
+from .query_service import Query
+
+
+class QueryRankingsCompany(Query):
+    def __init__(self, key, url=None, max_retries=3, retry_delay=timedelta(minutes=1), **kwargs):
+        super().__init__(key, url, max_retries, retry_delay, **kwargs)
+        self.name = "Query Rankings"
+        self.url = url
+        self.key = key
+        self.log_prefix = self.build_log_prefix()
+
+    def prep(self, url: str, rankings_id: str):
+        if "error" in rankings_id:
+            return rankings_id
+        return "{}/companies?company={}".format(url, rankings_id)
+
+    @defaults_from_attrs('url')
+    def run(self, data, url=None, **kwargs):
+        full_url = self.prep(url, data)
+        response = self.query(data=data, full_url=full_url)
+        result = self.process(data=data, full_url=full_url, response=response)
+        result = self.add_api_call(full_url=full_url, result=result)
+        return result
