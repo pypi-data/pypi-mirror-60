726c38458d4e4679ae85f3c3df7c739a7e1f8ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:22 +0000

    Add package TOPSIS-Pratyaksh-Verma-101703402/0.0.1/TOPSIS_Pratyaksh_Verma_101703402-0.0.1.tar.gz

---
+++ packages/TOPSIS-Pratyaksh-Verma-101703402/TOPSIS_Pratyaksh_Verma_101703402-0.0.1.tar.gz/TOPSIS_Pratyaksh_Verma_101703402-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name="TOPSIS_Pratyaksh_Verma_101703402", # Replace with your own username
+    version="0.0.1",
+    author="Pratyaksh Verma",
+    author_email="pverma_be17@thapar.edu",
+    description="TOPSIS for PyPi",
+    url="https://github.com/pratyakshverma/TOPSIS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+python_requires='>=3.6',
+)
