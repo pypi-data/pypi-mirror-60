149947736335b9e99799c182e3e4eb48b24f2580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package DiaVirtualPicture/1.1.2/DiaVirtualPicture-1.1.2.tar.gz

---
+++ packages/DiaVirtualPicture/DiaVirtualPicture-1.1.2.tar.gz/DiaVirtualPicture-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'DiaVirtualPicture',
+    version = '1.1.2',
+    py_modules = ['DiaVirtualPicture'],
+    author = 'Ceekayshen',
+    author_email = 'ceekayshen@foxmail.com',
+    url = 'https://github.com/shenliuming/yys',
+    description = 'None',
+)
