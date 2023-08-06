e197989e21cf66201499617610fd7a2a77e3db70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:38 +0000

    Add package royalpack/5.4.1/royalpack-5.4.1.tar.gz

---
+++ packages/royalpack/royalpack-5.4.1.tar.gz/royalpack-5.4.1/royalpack/stars/api_user_list.py
@@ -0,0 +1,14 @@
+from starlette.requests import Request
+from starlette.responses import *
+from royalnet.constellation import *
+from royalnet.utils import *
+from royalnet.backpack.tables import *
+
+
+class ApiUserListStar(PageStar):
+    path = "/api/user/list"
+
+    async def page(self, request: Request) -> JSONResponse:
+        async with self.alchemy.session_acm() as session:
+            users: typing.List[User] = await asyncify(session.query(self.alchemy.get(User)).all)
+        return JSONResponse([user.json() for user in users])
