4c13cdf0362109fdb041ccb3e484ac3592cdc8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:52 +0000

    Add package data-toolkit/0.2.21/data-toolkit-0.2.21.tar.gz

---
+++ packages/data-toolkit/data-toolkit-0.2.21.tar.gz/data-toolkit-0.2.21/dt/ext/python_exec.py
@@ -0,0 +1,40 @@
+import pandas as pd 
+import swifter # ==0.289
+import os, sys, io
+import subprocess as sp
+
+# def called_function(row: pd.Series, command: str, TARGET_DIR: str):
+#     if 'noisy' in row['full_path']:
+#         os.system(f"mv {row['full_path']} {TARGET_DIR1}")
+#     else:
+#         os.system(f"mv {row['full_path']} {TARGET_DIR2}")
+
+def execute(command: str, filt_cond: str):
+    '''
+    Runs a python `command` on filenames that pass -f 
+    filt_cond: filter all files in in this directory and below by this expression.
+    command: E.g.
+    `dt py 'f"mv {x} real_A/ "' -f "real_A"`
+    '''
+    df = get_files_as_pd()
+
+    target_files = pd.Series(filter(lambda x: filt_cond in x, df.full_path.values))
+    # mv_fcn = lambda x: os.system(f"mv {x} {command}/{x.replace('/','_')}")
+    target_fcn = lambda x: os.system(eval(command))
+    target_files.swifter.apply(lambda x: target_fcn(x))
+
+
+def get_files_as_pd(directory='.'):
+    '''
+    Gets all files from recursive directories and returns this as
+    a `DataFrame`
+    '''
+    all_files = os.listdir(directory)
+
+    proc = str(sp.check_output('find $PWD/* -type f', shell=True))[2:] 
+    df = pd.DataFrame(str(proc).split('\\n')[:-1] )
+    df.columns = ['full_path']
+
+    level_paths = df.full_path.str.split('/',expand=True)
+    df = pd.concat([df, level_paths], axis=1)
+    return df
