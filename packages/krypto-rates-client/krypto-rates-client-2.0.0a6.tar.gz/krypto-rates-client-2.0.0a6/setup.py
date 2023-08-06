509cce72baa56abbe0d47fcc5e8d145eae7a4bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:16 +0000

    Add package krypto-rates-client/2.0.0a6/krypto-rates-client-2.0.0a6.tar.gz

---
+++ packages/krypto-rates-client/krypto-rates-client-2.0.0a6.tar.gz/krypto-rates-client-2.0.0a6/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['krypto_rates_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests-toolbelt>=0.9.1,<0.10.0',
+ 'requests>=2.22.0,<3.0.0',
+ 'typing-extensions>=3.7.4,<4.0.0']
+
+setup_kwargs = {
+    'name': 'krypto-rates-client',
+    'version': '2.0.0a6',
+    'description': 'Krypto Rates Python Client',
+    'long_description': '# @raptorsystems/krypto-rates-python\n\n> Krypto Rates Python Client\n',
+    'author': 'Raptor Systems SpA',
+    'author_email': 'raptor@raptorsystems.cl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/raptorsystems/krypto-rates',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
