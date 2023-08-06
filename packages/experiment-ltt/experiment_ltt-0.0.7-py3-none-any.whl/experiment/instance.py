7df46c84c73fb493323f71b5b0d52fb793666096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:13 +0000

    Add package experiment-ltt/0.0.7/experiment_ltt-0.0.7-py3-none-any.whl

---
+++ packages/experiment-ltt/experiment_ltt-0.0.7-py3-none-any.whl/experiment/instance.py
@@ -0,0 +1,20 @@
+import pymysql
+
+def add_instance(conn, config, name, kind):
+    cur = conn.cursor()
+    query = '''INSERT INTO instance (name, kind) VALUES ('%s', '%s')''' % (name, kind)
+    try:
+        cur.execute(query)
+        conn.commit()
+    except pymysql.err.IntegrityError:
+        if config['verbose']:
+            print('[instance|error] add_instance(%s, %s), reason: instance duplicated' % (name, kind))
+    else:
+        if config['verbose']:
+            print('[instance|ok] add_instance(%s, %s)' % (name, kind))
+
+def get_instance_id(conn, config, name, kind):
+    cur = conn.cursor()
+    query = '''SELECT id FROM instance WHERE name='%s' AND kind='%s' ''' % (name, kind)
+    cur.execute(query)
+    return cur.fetchone()[0]
