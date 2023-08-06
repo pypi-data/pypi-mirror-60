f49ef202a677338d2064455423cf2f0f905f02ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:47 +0000

    Add package Pointa/0.2.32/Pointa-0.2.32.tar.gz

---
+++ packages/Pointa/Pointa-0.2.32.tar.gz/Pointa-0.2.32/Pointa/Client/__init__.py
@@ -0,0 +1,67 @@
+import getopt
+import json
+import time
+from os import system
+from platform import system
+from sys import argv
+
+import requests
+
+from .Game import Client
+from .locales import en_US, zh_CN
+
+locales = {
+    'zh_CN': zh_CN,
+    'en_US': en_US
+}
+
+opts,args = getopt.getopt(argv[1:],'-l:',['locale='])
+for opt_name,opt_value in opts:
+    if opt_name in ('-l', '--locale'):
+        locale = locales[opt_value]
+
+URL_STRUCT = '{0}/inGame/{1}?fts={2}&r={3}&p={4}'
+
+localVar = {
+    'progress': {
+        'round': 0,
+        'phase': 0
+    },
+    'playerStat': {},
+    'localLog': [],
+    'targetUri': '',
+    'key': 'null',
+    'username': ''
+}
+anotherPlayer = {
+    'key': '',
+    'name': ''
+}
+clientStatus = {
+    'inGame': False,
+    'loggedIn': False,
+    'waiting': False,
+}
+
+varsBundle = [localVar, anotherPlayer, clientStatus]
+
+client = Client(system(), locale, varsBundle, URL_STRUCT)
+
+Version = 'Pointa! v0.24-alpha [2020/2/4]'
+
+print(Version)
+
+# Game Begin
+if client.Login():
+    while not client.clientStatus['inGame']:
+        client.Menu()
+    preprocess = True
+else:
+    preprocess = False
+
+while preprocess:
+    if not client.Game():
+        break
+
+client.ARinput(locale.QUIT)
+quit()
