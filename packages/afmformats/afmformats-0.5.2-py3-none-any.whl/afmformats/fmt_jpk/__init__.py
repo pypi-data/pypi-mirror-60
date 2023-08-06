5644541a2dde84bb8fc24c38717070194a769eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package afmformats/0.5.2/afmformats-0.5.2-py3-none-any.whl

---
+++ packages/afmformats/afmformats-0.5.2-py3-none-any.whl/afmformats/fmt_jpk/__init__.py
@@ -0,0 +1,43 @@
+import pathlib
+
+import numpy as np
+from .read_jpk import load_jpk
+
+
+def prepare_jpk_data(path, callback=None):
+    """Convert the data from `load_jpk` to something afmformats understands"""
+    # load the data
+    measurements = load_jpk(path=path, callback=callback)
+    # convert join the segment data
+    dataset = []
+    for enum, mm in enumerate(measurements):
+        (app, metadata, _), (ret, _, _) = mm
+        metadata["enum"] = enum
+        metadata["path"] = pathlib.Path(path)
+
+        # join segments
+        lenapp = len(app[list(app.keys())[0]])
+        lenret = len(ret[list(ret.keys())[0]])
+        ret["time"] += metadata["duration"]
+        data = {}
+        for key in app.keys():
+            data[key] = np.concatenate((app[key], ret[key]))
+        data["segment"] = np.concatenate((np.zeros(lenapp, dtype=bool),
+                                          np.ones(lenret, dtype=bool)))
+        dataset.append({"data": data,
+                        "metadata": metadata,
+                        })
+    return dataset
+
+
+recipe_jpk_force = {
+    "loader": prepare_jpk_data,
+    "suffix": ".jpk-force",
+    "mode": "force-distance",
+}
+
+recipe_jpk_force_map = {
+    "loader": prepare_jpk_data,
+    "suffix": ".jpk-force-map",
+    "mode": "force-distance",
+}
