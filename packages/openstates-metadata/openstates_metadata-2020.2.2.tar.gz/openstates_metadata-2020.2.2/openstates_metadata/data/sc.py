a1e4d456d47fc06c4eebee7c2b163ecc9f930806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:23 +0000

    Add package openstates-metadata/2020.2.2/openstates_metadata-2020.2.2.tar.gz

---
+++ packages/openstates-metadata/openstates_metadata-2020.2.2.tar.gz/openstates_metadata-2020.2.2/openstates_metadata/data/sc.py
@@ -0,0 +1,34 @@
+from ..models import State, Chamber, simple_numbered_districts
+
+SC = State(
+    name="South Carolina",
+    abbr="SC",
+    capital="Columbia",
+    capital_tz="America/New_York",
+    fips="45",
+    unicameral=False,
+    legislature_name="South Carolina Legislature",
+    division_id="ocd-division/country:us/state:sc",
+    jurisdiction_id="ocd-jurisdiction/country:us/state:sc/government",
+    url="http://www.scstatehouse.gov/",
+    lower=Chamber(
+        chamber_type="lower",
+        name="House",
+        organization_id="ocd-organization/11144d16-6b61-4e2a-94fe-6f9c24e36193",
+        num_seats=124,
+        title="Representative",
+        districts=simple_numbered_districts(
+            "ocd-division/country:us/state:sc", "lower", 124
+        ),
+    ),
+    upper=Chamber(
+        chamber_type="upper",
+        name="Senate",
+        organization_id="ocd-organization/705ec497-4b18-4a39-a0e0-ce114a2f3c78",
+        num_seats=46,
+        title="Senator",
+        districts=simple_numbered_districts(
+            "ocd-division/country:us/state:sc", "upper", 46
+        ),
+    ),
+)
