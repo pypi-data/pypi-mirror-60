2b4158b15a3a469c26c60db0f2e4eb71602cc451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:21 +0000

    Add package mukham/1.0.7/mukham-1.0.7.tar.gz

---
+++ packages/mukham/mukham-1.0.7.tar.gz/mukham-1.0.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    dependencies = list(map(lambda x: x.replace('==', '>='), fh.read().split('\n')))
+
+setuptools.setup(
+    name="mukham",
+    version="1.0.7",
+    author="Anish Krishna Vallapuram",
+    author_email="akvallapuram@connect.ust.hk",
+    description="A basic library to crop largest face from the images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akvallapuram/mukham",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=dependencies,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
