51a66358e432292ca482cf7f70d95c4a8fd01699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:48 +0000

    Add package atspy/0.0.5/atspy-0.0.5.tar.gz

---
+++ packages/atspy/atspy-0.0.5.tar.gz/atspy-0.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, Command
+import os
+import sys
+
+setup(name='atspy',
+      version='0.0.5',
+      description='Automated Time Series in Python',
+      url='https://github.com/firmai/atspy',
+      author='snowde',
+      author_email='d.snow@firmai.org',
+      license='MIT',
+      packages=['atspy','atspy.TS'],
+      install_requires=[
+          'pandas',
+          'scipy',
+          'numba',
+          'datetime',
+          'pmdarima',
+          'pydot',
+          'dill',
+          'pathos',
+          'sqlalchemy',
+          'matplotlib',
+          'xgboost',
+          'sklearn',
+          'mxnet==1.4.1',
+          'gluonts',
+          'nbeats-pytorch',
+          'seasonal',
+          'tbats',
+          'tsfresh',
+          'python-dateutil==2.8.0',
+          'numpy==1.17.4',
+
+      ],
+      zip_safe=False)
