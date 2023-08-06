1c6b56002f716e1065bb6bfa598ddfa6b0c9503f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:07 +0000

    Add package results/0.1.1580967064/results-0.1.1580967064-py3-none-any.whl

---
+++ packages/results/results-0.1.1580967064-py3-none-any.whl/results/__init__.py
@@ -0,0 +1,38 @@
+from logx import log
+from sqlbag import create_database  # noqa
+from sqlbag.pg import use_pendulum_for_time_types  # noqa
+
+from .cleaning import standardize_key, standardized_key_mapping  # noqa
+from .connections import db  # noqa
+from .fileutil import file_text, files, from_file, from_files  # noqa
+from .itercsv import fast_csv_it  # noqa
+from .openers import (  # noqa
+    csv_column_names,
+    detect_enc,
+    detect_string_enc,
+    dicts_from_rows,
+    first_n_lines,
+    from_csv,
+    from_xls,
+    from_xlsx,
+    save_xlsx_sheets,
+    smart_open,
+)
+from .resources import (  # noqa
+    resource_data,
+    resource_path,
+    resource_stream,
+    resource_text,
+)
+from .result import Result  # noqa
+from .resultset import Results  # noqa
+from .sqlutil import create_table_statement  # noqa
+from .typeguess import (  # noqa
+    guess_column_type,
+    guess_sql_column_type,
+    guess_value_type,
+)
+from .uuids import deterministic_uuid  # noqa
+
+log.set_null_handler()
+use_pendulum_for_time_types()
