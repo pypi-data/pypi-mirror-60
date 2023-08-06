09f26d2bcd69e8fa77cf6f5f5bbc43a0933af653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:24 +0000

    Add package requre/0.2.2/requre-0.2.2.tar.gz

---
+++ packages/requre/requre-0.2.2.tar.gz/requre-0.2.2/requre/__init__.py
@@ -0,0 +1,8 @@
+from requre.import_system import decorate, replace, replace_module, log_imports
+
+__all__ = [
+    decorate.__name__,
+    replace.__name__,
+    replace_module.__name__,
+    log_imports.__name__,
+]
