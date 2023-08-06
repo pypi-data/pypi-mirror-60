7fff866c3baef0742f20c0352d4c6ce0e6441bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:49 +0000

    Add package xiaomi-mi-scale/0.0.1/xiaomi_mi_scale-0.0.1.tar.gz

---
+++ packages/xiaomi-mi-scale/xiaomi_mi_scale-0.0.1.tar.gz/xiaomi_mi_scale-0.0.1/README.md
@@ -0,0 +1,9 @@
+# Xiaomi Mi Scale
+
+Libraries to read weight measurements from [Mi Body Composition Scale](https://www.mi.com/global/mi-body-composition-scale/) (aka Xiaomi Mi Scale V2)
+Also works with [Mi Body Composition Scale 2](https://c.mi.com/thread-2289389-1-0.html) (Model # XMTZC05HM)
+
+
+Note: Framework is present to also read from Xiaomi Scale V1, although I do not own one to test so the code has not been maintained
+
+See my [github](https://github.com/lolouk44/xiaomi_mi_scale) page for more information
---
+++ packages/xiaomi-mi-scale/xiaomi_mi_scale-0.0.1.tar.gz/xiaomi_mi_scale-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xiaomi_mi_scale", # Replace with your own username
+    version="0.0.1",
+    author="Laurent Mahe",
+    author_email="laurent.mahe@gmail.com",
+    description="Labraries to read weight measurements from Xiaomi Mi Body Composition Scale (aka Xiaomi Mi Scale V2)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lolouk44/xiaomi_mi_scale",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
