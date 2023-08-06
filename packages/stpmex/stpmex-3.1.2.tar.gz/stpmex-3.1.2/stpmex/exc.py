928bdf66b11973b1c6278a74250a13b675733de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:33 +0000

    Add package stpmex/3.1.2/stpmex-3.1.2.tar.gz

---
+++ packages/stpmex/stpmex-3.1.2.tar.gz/stpmex-3.1.2/stpmex/exc.py
@@ -0,0 +1,37 @@
+from pydantic.errors import PydanticValueError
+
+
+class StpmexException(Exception):
+    def __init__(self, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+
+    def __repr__(self):
+        return (
+            self.__class__.__name__
+            + '('
+            + ', '.join(
+                [
+                    f'{attr}={repr(value)}'
+                    for attr, value in self.__dict__.items()
+                ]
+            )
+            + ')'
+        )
+
+    def __str__(self):
+        return repr(self)
+
+
+class InvalidPassphrase(StpmexException):
+    """El passphrase es incorrecto"""
+
+
+class BankCodeValidationError(PydanticValueError):
+    code = 'clabe.bank_code'
+    msg_template = 'código de banco no es válido'
+
+
+class ClabeControlDigitValidationError(PydanticValueError):
+    code = 'clabe.control_digit'
+    msg_template = 'clabe dígito de control no es válido'
