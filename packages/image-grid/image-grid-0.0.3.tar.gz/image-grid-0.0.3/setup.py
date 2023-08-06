a971545eb944913d95105d6212b9227c08756f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:00 +0000

    Add package image-grid/0.0.3/image-grid-0.0.3.tar.gz

---
+++ packages/image-grid/image-grid-0.0.3.tar.gz/image-grid-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("readme.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='image-grid',
+    version='0.0.3',
+    author='Rasmus Laurvig Haugaard',
+    author_email='rasmus.l.haugaard@gmail.com',
+    description='assembles images in a grid',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RasmusHaugaard/image-grid',
+    scripts=[
+        'bin/image-grid',
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'natsort',
+    ],
+    python_requires='>=3.6',
+)
