1519ff92b57e86625c1791aad29b34e345a7d7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:59 +0000

    Add package pept/0.1.5/pept-0.1.5.tar.gz

---
+++ packages/pept/pept-0.1.5.tar.gz/pept-0.1.5/pept/tracking/peptml/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# File   : __init__.py
+# License: License: GNU v3.0
+# Author : Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
+# Date   : 22.08.2019
+
+
+from    .peptml     import  Cutpoints
+from    .peptml     import  HDBSCANClusterer
+
+
+__all__ = [
+    "Cutpoints",
+    "HDBSCANClusterer"
+]
+
+
+__author__ =        "Andrei Leonard Nicusan"
+__credits__ =       ["Andrei Leonard Nicusan", "Kit Windows-Yule", "Sam Manger"]
+__license__ =       "GNU v3.0"
+__version__ =       "0.1"
+__maintainer__ =    "Andrei Leonard Nicusan"
+__email__ =         "a.l.nicusan@bham.ac.uk"
+__status__ =        "Development"
+
+
+
+
