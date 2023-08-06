18d2f5e6bede03a0b8c5b95c1879f6576d906b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:58 +0000

    Add package pythx/1.5.1/pythx-1.5.1-py2.py3-none-any.whl

---
+++ packages/pythx/pythx-1.5.1-py2.py3-none-any.whl/pythx/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pythx."""
+
+__author__ = """Dominik Muhs"""
+__email__ = "dominik.muhs@consensys.net"
+__version__ = "1.5.1"
+
+from mythx_models.exceptions import MythXAPIError, MythXBaseException, ValidationError
+from pythx.api.client import Client
