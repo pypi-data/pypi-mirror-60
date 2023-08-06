e09f92e4d6bee2a2f4ca8497a78b65ed08b956c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:38 +0000

    Add package Py-Rate/0.4.0/Py-Rate-0.4.0.tar.gz

---
+++ packages/Py-Rate/Py-Rate-0.4.0.tar.gz/Py-Rate-0.4.0/pyrate/core/user_experience.py
@@ -0,0 +1,34 @@
+import pathlib
+
+def delete_tsincr_files(params):
+
+    outDirPath = pathlib.Path(params["outdir"])
+    for filePath in outDirPath.iterdir():
+        if "tsincr" in str(filePath):
+            filePath.unlink()
+
+def break_number_into_factors(n, memo={}, left=2):
+    if (n, left) in memo:
+        return memo[(n, left)]
+    if left == 1:
+        return (n, [n])
+    i = 2
+    best = n
+    bestTuple = [n]
+    while i * i <= n:
+        if n % i == 0:
+            rem = break_number_into_factors(n / i, memo, left - 1)
+            if rem[0] + i < best:
+                best = rem[0] + i
+                bestTuple = [i] + rem[1]
+        i += 1
+
+    # handle edge case when only one processor is available
+    if bestTuple == [4]:
+        return 2, 2
+
+    if len(bestTuple) == 1:
+        bestTuple.append(1)
+        return bestTuple
+
+    return bestTuple
