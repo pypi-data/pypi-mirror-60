afc1745039a95fdd1dc8b45c991ea44c34720373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:18 +0000

    Add package parakh1/0.1/parakh1-0.1.tar.gz

---
+++ packages/parakh1/parakh1-0.1.tar.gz/parakh1-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(name='parakh1',
+      version='0.1',
+      description='add the number',
+      author='parakhsg',
+      author_email="parakh.tayal@sganalytics.com",
+      license='MIT',
+      packages=['parakh1'],
+      long_description="firstpackage",
+      zip_safe=False)
