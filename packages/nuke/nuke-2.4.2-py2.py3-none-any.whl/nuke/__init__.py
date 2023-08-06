692feec01068d4191a54d5ca1b766e1b7973b1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:38 +0000

    Add package nuke/2.4.2/nuke-2.4.2-py2.py3-none-any.whl

---
+++ packages/nuke/nuke-2.4.2-py2.py3-none-any.whl/nuke/__init__.py
@@ -0,0 +1,9 @@
+"""Project information."""
+
+__author__ = "Varun Agrawal"
+__copyright__ = "Copyright 2017."
+__license__ = "MIT"
+__version__ = "2.4.2"
+__maintainer__ = "Varun Agrawal"
+__email__ = "varagrawal@gmail.com"
+__status__ = "Production"
