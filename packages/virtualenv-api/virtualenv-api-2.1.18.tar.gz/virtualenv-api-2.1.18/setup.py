bb4302b9be4328a94be8bc83d5b3f24198052de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:36 +0000

    Add package virtualenv-api/2.1.18/virtualenv-api-2.1.18.tar.gz

---
+++ packages/virtualenv-api/virtualenv-api-2.1.18.tar.gz/virtualenv-api-2.1.18/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+from virtualenvapi import __version__
+
+setup(
+    name='virtualenv-api',
+    version=__version__,
+    license='BSD',
+    author='Sam Kingston and AUTHORS',
+    author_email='sam@sjkwi.com.au',
+    description='An API for virtualenv/pip',
+    long_description=open('README.rst', 'r').read(),
+    url='https://github.com/sjkingo/virtualenv-api',
+    install_requires=['six'],
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
