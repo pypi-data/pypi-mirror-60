c232be8b42337190c0017cfcdb70416da9c251b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:26 +0000

    Add package edx-enterprise-data/1.3.13/edx-enterprise-data-1.3.13.tar.gz

---
+++ packages/edx-enterprise-data/edx-enterprise-data-1.3.13.tar.gz/edx-enterprise-data-1.3.13/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function
+
+import os
+import re
+import sys
+
+from setuptools import setup
+
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+VERSION = get_version("enterprise_data", "__init__.py")
+
+if sys.argv[-1] == "tag":
+    print("Tagging the version on github:")
+    os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+base_path = os.path.dirname(__file__)
+
+REQUIREMENTS = open(os.path.join(base_path, 'requirements', 'base.txt')).read().splitlines()
+
+setup(
+    name="edx-enterprise-data",
+    version=VERSION,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="""Enterprise Reporting""",
+    author="edX",
+    author_email="oscm@edx.org",
+    url="https://github.com/edx/edx-enterprise-data",
+    packages=[
+        'enterprise_data',
+        'enterprise_reporting',
+        'enterprise_data_roles',
+    ],
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    license="AGPL 3.0",
+    zip_safe=False,
+)
