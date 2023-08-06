563661d5ed778c5885947d1aaa4bc984123e1b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:53 +0000

    Add package jupyter-repo2docker/0.11.0/jupyter-repo2docker-0.11.0.tar.gz

---
+++ packages/jupyter-repo2docker/jupyter-repo2docker-0.11.0.tar.gz/jupyter-repo2docker-0.11.0/repo2docker/buildpacks/conda/install-miniconda.bash
@@ -0,0 +1,82 @@
+#!/bin/bash
+# This downloads and installs a pinned version of miniconda
+set -ex
+
+cd $(dirname $0)
+MINICONDA_VERSION=4.7.12.1
+CONDA_VERSION=4.7.12
+# Only MD5 checksums are available for miniconda
+# Can be obtained from https://repo.continuum.io/miniconda/
+MD5SUM="81c773ff87af5cfac79ab862942ab6b3"
+
+URL="https://repo.continuum.io/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh"
+INSTALLER_PATH=/tmp/miniconda-installer.sh
+
+# make sure we don't do anything funky with user's $HOME
+# since this is run as root
+unset HOME
+
+wget --quiet $URL -O ${INSTALLER_PATH}
+chmod +x ${INSTALLER_PATH}
+
+# check md5 checksum
+if ! echo "${MD5SUM}  ${INSTALLER_PATH}" | md5sum  --quiet -c -; then
+    echo "md5sum mismatch for ${INSTALLER_PATH}, exiting!"
+    exit 1
+fi
+
+bash ${INSTALLER_PATH} -b -p ${CONDA_DIR}
+export PATH="${CONDA_DIR}/bin:$PATH"
+
+# Allow easy direct installs from conda forge
+conda config --system --add channels conda-forge
+
+# Do not attempt to auto update conda or dependencies
+conda config --system --set auto_update_conda false
+conda config --system --set show_channel_urls true
+
+# bug in conda 4.3.>15 prevents --set update_dependencies
+echo 'update_dependencies: false' >> ${CONDA_DIR}/.condarc
+
+# install conda itself
+if [[ "${CONDA_VERSION}" != "${MINICONDA_VERSION}" ]]; then
+    conda install -yq conda==${CONDA_VERSION}
+fi
+
+# avoid future changes to default channel_priority behavior
+conda config --system --set channel_priority "flexible"
+
+echo "installing notebook env:"
+cat /tmp/environment.yml
+conda env create -p ${NB_PYTHON_PREFIX} -f /tmp/environment.yml
+
+# empty conda history file,
+# which seems to result in some effective pinning of packages in the initial env,
+# which we don't intend.
+# this file must not be *removed*, however
+echo '' > ${NB_PYTHON_PREFIX}/conda-meta/history
+
+if [[ -f /tmp/kernel-environment.yml ]]; then
+    # install kernel env and register kernelspec
+    echo "installing kernel env:"
+    cat /tmp/kernel-environment.yml
+
+    conda env create -p ${KERNEL_PYTHON_PREFIX} -f /tmp/kernel-environment.yml
+    ${KERNEL_PYTHON_PREFIX}/bin/ipython kernel install --prefix "${NB_PYTHON_PREFIX}"
+    echo '' > ${KERNEL_PYTHON_PREFIX}/conda-meta/history
+    conda list -p ${KERNEL_PYTHON_PREFIX}
+fi
+
+# Clean things out!
+conda clean --all -f -y
+
+# Remove the big installer so we don't increase docker image size too much
+rm ${INSTALLER_PATH}
+
+# Remove the pip cache created as part of installing miniconda
+rm -rf /root/.cache
+
+chown -R $NB_USER:$NB_USER ${CONDA_DIR}
+
+conda list -n root
+conda list -p ${NB_PYTHON_PREFIX}
