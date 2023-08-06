b1f5f168ea5cc675134a84ddc234dc42f6cd9583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:12 +0000

    Add package itexus-aleno-test-2/0.0.13/itexus_aleno_test_2-0.0.13.tar.gz

---
+++ packages/itexus-aleno-test-2/itexus_aleno_test_2-0.0.13.tar.gz/itexus_aleno_test_2-0.0.13/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="itexus_aleno_test_2",  # How you named your package folder (MyLib)
+    packages=["itexus_aleno_test"],  # Chose the same as "name"
+    version="0.0.13",  # Start with a small number and increase it with every change you make
+    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description="Aleno Marketplace test package description",  # Give a short description about your library
+    author="Maksim Holasau",  # Type in your name
+    author_email="maksim.holasau@itexus.com",  # Type in your E-Mail
+    url="https://github.com/maksim-holasau/itexus_aleno_test",  # Provide either the link to your github or to your website
+    download_url="https://github.com/maksim-holasau/itexus_aleno_test/archive/0.0.13.tar.gz",  # I explain this later on
+    keywords=["SOME", "MEANINGFULL", "KEYWORDS"],
+    classifiers=[],
+)
