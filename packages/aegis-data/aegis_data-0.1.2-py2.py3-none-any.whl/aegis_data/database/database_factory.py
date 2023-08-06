36bb1fd14997e3acf47f20cddb24e16b3693c235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:48 +0000

    Add package aegis-data/0.1.2/aegis_data-0.1.2-py2.py3-none-any.whl

---
+++ packages/aegis-data/aegis_data-0.1.2-py2.py3-none-any.whl/aegis_data/database/database_factory.py
@@ -0,0 +1,8 @@
+from aegis_data.database.mongo_db import MongoDB
+
+
+def database_factory(db_type, config_file="config.json"):
+    if 'mongo' in db_type:
+        return MongoDB(config_file)
+    else:
+        raise NotImplementedError("Database {} is not supported!".format(db_type))
