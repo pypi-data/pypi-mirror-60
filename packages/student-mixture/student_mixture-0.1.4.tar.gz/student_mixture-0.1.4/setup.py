b63757a99a58340bc9064c72296429006f83b82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:20 +0000

    Add package student-mixture/0.1.4/student_mixture-0.1.4.tar.gz

---
+++ packages/student-mixture/student_mixture-0.1.4.tar.gz/student_mixture-0.1.4/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages, Extension
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Omri Tomer",
+    author_email='omritomer1@mail.tau.ac.il',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A package for fitting a Student's t-mixture model",
+    install_requires=requirements,
+    license="MIT license",
+    long_description_content_type='text/markdown',
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='student_mixture',
+    name='student_mixture',
+    packages=find_packages(include=['student_mixture', 'student_mixture.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/omritomer/student_mixture',
+    version='0.1.4',
+    zip_safe=False,
+)
