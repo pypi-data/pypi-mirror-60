0be9eadeac00b0f1d05cd73b698f0aab06868b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:25 +0000

    Add package 101703375-p2/0.0.8/101703375_p2-0.0.8.tar.gz

---
+++ packages/101703375-p2/101703375_p2-0.0.8.tar.gz/101703375_p2-0.0.8/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="101703375_p2",
+    version="0.0.8",
+    author="Nishant Dhanda",
+    author_email="ndhanda_be17@thapar.edu",
+    description="Removal of outliers using pandas",
+	url='https://github.com/NishantDhanda/101703375_p2/',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	scripts=['bin/outcli'],
+	keywords = ['CLI', 'OUTLIER', 'Data', 'outlier removal'], 
+	python_requires='>=3.6', 
+)
