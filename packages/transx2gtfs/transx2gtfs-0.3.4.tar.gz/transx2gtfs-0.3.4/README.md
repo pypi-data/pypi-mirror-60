df1d52e620fdcfd91d516784cec6377408a593fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:36 +0000

    Add package transx2gtfs/0.3.4/transx2gtfs-0.3.4.tar.gz

---
+++ packages/transx2gtfs/transx2gtfs-0.3.4.tar.gz/transx2gtfs-0.3.4/README.md
@@ -0,0 +1,56 @@
+# transx2gtfs 
+[![PyPI version](https://badge.fury.io/py/transx2gtfs.svg)](https://badge.fury.io/py/transx2gtfs) [![build status](https://travis-ci.com/HTenkanen/transx2gtfs.svg?branch=master)](https://travis-ci.com/HTenkanen/transx2gtfs) [![Coverage Status](https://codecov.io/gh/HTenkanen/transx2gtfs/branch/master/graph/badge.svg)](https://codecov.io/gh/HTenkanen/transx2gtfs) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3628737.svg)](https://doi.org/10.5281/zenodo.3628737) [![Gitter](https://badges.gitter.im/transx2gtfs/community.svg)](https://gitter.im/transx2gtfs/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
+This is a small library to convert transit data from TransXchange format into GTFS -format that
+can be used with various routing engines such as OpenTripPlanner. 
+
+## Note!
+
+This package is still in a Beta-phase, so use it at your own risk. 
+Requires more testing. If you find an issue, you can contribute and 
+help solving them by [raising an issue](https://github.com/HTenkanen/transx2gtfs/issues).
+
+## Features
+
+ - Reads TransXchange xml-files and converts into GTFS feed with all necessary information 
+ according the General Transit Feed Specification.
+ - Combines multiple TransXchange files into a single GTFS feed if present in the same folder.
+ - Uses multiprocessing to parallelize the conversion process.
+ - Parses dates of non-operation (bank holidays etc.) which are written to calendar_dates.txt.
+ - Uses NaPTAN stops.  
+
+## Install
+
+
+The package is available at PyPi and you can install it with:
+
+`$ pip install transx2gtfs`
+
+Library works and is being tested with Python versions 3.6, 3.7 and 3.8.  
+
+## Basic usage
+
+After you have installed the library you can use it in a similar manner as any Python
+library:
+
+```python
+>>> import transx2gtfs
+>>> data_dir_for_transxchange_files = "data/my_transxchange_files"
+>>> output_path = "data/my_converted_gtfs.zip"
+>>> transx2gtfs.convert(data_dir_for_transxchange_files, output_path)
+```
+
+After you have successfully converted the TransXchange into GTFS, you can start doing
+multimodal routing with your favourite routing engine such as OpenTripPlanner:
+
+![OTP_example_in_London](img/London_multimodal_route.PNG)
+
+## Citation
+
+If you use this tool for research purposes, we encourage you to cite this work:
+
+ - Henrikki Tenkanen. (2020, January 27). HTenkanen/transx2gtfs: transx2gtfs v0.3.3.1 (Version v0.3.3.1). Zenodo. http://doi.org/10.5281/zenodo.3628737
+
+## Developers
+
+- Henrikki Tenkanen, University College London
---
+++ packages/transx2gtfs/transx2gtfs-0.3.4.tar.gz/transx2gtfs-0.3.4/transx2gtfs/stops.py
@@ -0,0 +1,190 @@
+import os
+import pandas as pd
+import pyproj
+
+def download_naptan_stops(url="http://naptan.app.dft.gov.uk/datarequest/GTFS.ashx", local_path=None):
+    """Download a fresh set of Naptan stop points, or use a locally downloaded NAPTAN stopset csv-file"""
+    if local_path is not None:
+        if os.path.exists(local_path):
+            # Read csv
+            stops = pd.read_csv(local_path, sep='\t')
+            return stops
+        else:
+            raise ValueError("Could not find stop file at:", local_path)
+    else:
+        NotImplementedError("TODO: Downloading stops from NAPTAN website")
+
+
+def _get_tfl_style_stops(data, naptan_stops_fp):
+    """"""
+    # Helper projections for transformations
+    # Define the projection
+    # The .srs here returns the Proj4-string presentation of the projection
+    wgs84 = pyproj.Proj("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    osgb36 = pyproj.Proj(
+        "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.999601 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894 +units=m +no_defs <>")
+
+    # Attributes
+    _stop_id_col = 'stop_id'
+
+    # Container
+    stop_data = pd.DataFrame()
+
+    # Get stop database
+    naptan_stops = download_naptan_stops(local_path=naptan_stops_fp)
+
+    # Iterate over stop points
+    for p in data.TransXChange.StopPoints.StopPoint:
+        # Name of the stop
+        stop_name = p.Descriptor.CommonName.cdata
+
+        # Stop_id
+        stop_id = p.AtcoCode.cdata
+
+        # Get stop info
+        stop = naptan_stops.loc[naptan_stops[_stop_id_col] == stop_id]
+
+        # If NAPTAN db does not contain the info, try to parse from the data directly
+
+        if len(stop) == 0:
+            # print("Could not find stop_id '%s' from Naptan database. Using coordinates directly from TransXChange." % stop_id)
+            # X and y coordinates - Notice: these might not be available! --> Use NAPTAN database
+            # Spatial reference - TransXChange might use:
+            #   - OSGB36 (epsg:7405) spatial reference: https://spatialreference.org/ref/epsg/osgb36-british-national-grid-odn-height/
+            #   - WGS84 (epsg:4326)
+            # Detected epsg
+            detected_epsg = None
+            x = float(p.Place.Location.Easting.cdata)
+            y = float(p.Place.Location.Northing.cdata)
+
+            # Detect the most probable CRS at the first iteration
+            if detected_epsg is None:
+                # Check if the coordinates are in meters
+                if x > 180:
+                    detected_epsg = 7405
+                else:
+                    detected_epsg = 4326
+
+            # Convert point coordinates to WGS84 if they are in OSGB36
+            if detected_epsg == 7405:
+                x, y = pyproj.transform(p1=osgb36, p2=wgs84, x=x, y=y)
+
+            # Create row
+            stop = dict(stop_id=stop_id,
+                        stop_code=None,
+                        stop_name=stop_name,
+                        stop_lat=y,
+                        stop_lon=x,
+                        stop_url=None
+                        )
+
+        elif len(stop) > 1:
+            raise ValueError("Had more than 1 stop with identical stop reference.")
+
+        # Add to container
+        stop_data = stop_data.append(stop, ignore_index=True, sort=False)
+
+    return stop_data
+
+def _get_txc_21_style_stops(data, naptan_stops_fp):
+    # Helper projections for transformations
+    # Define the projection
+    # The .srs here returns the Proj4-string presentation of the projection
+    wgs84 = pyproj.Proj("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    osgb36 = pyproj.Proj(
+        "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.999601 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894 +units=m +no_defs <>")
+
+    # Attributes
+    _stop_id_col = 'stop_id'
+
+    # Container
+    stop_data = pd.DataFrame()
+
+    # Get stop database
+    naptan_stops = download_naptan_stops(local_path=naptan_stops_fp)
+
+    # Iterate over stop points using TransXchange version 2.1
+    for p in data.TransXChange.StopPoints.AnnotatedStopPointRef:
+        # Name of the stop
+        stop_name = p.CommonName.cdata
+
+        # Stop_id
+        stop_id = p.StopPointRef.cdata
+
+        # Get stop info
+        stop = naptan_stops.loc[naptan_stops[_stop_id_col] == stop_id]
+
+        # If NAPTAN db does not contain the info, try to parse from the data directly
+        # Note: this seems not to be part of the data in TXC 2.1 schema
+        # TODO: Remove?
+        try:
+            if len(stop) == 0:
+                # print("Could not find stop_id '%s' from Naptan database. Using coordinates directly from TransXChange." % stop_id)
+                # X and y coordinates - Notice: these might not be available! --> Use NAPTAN database
+                # Spatial reference - TransXChange might use:
+                #   - OSGB36 (epsg:7405) spatial reference: https://spatialreference.org/ref/epsg/osgb36-british-national-grid-odn-height/
+                #   - WGS84 (epsg:4326)
+                # Detected epsg
+                detected_epsg = None
+                x = float(p.Place.Location.Easting.cdata)
+                y = float(p.Place.Location.Northing.cdata)
+
+                # Detect the most probable CRS at the first iteration
+                if detected_epsg is None:
+                    # Check if the coordinates are in meters
+                    if x > 180:
+                        detected_epsg = 7405
+                    else:
+                        detected_epsg = 4326
+
+                # Convert point coordinates to WGS84 if they are in OSGB36
+                if detected_epsg == 7405:
+                    x, y = pyproj.transform(p1=osgb36, p2=wgs84, x=x, y=y)
+
+                    # Create row
+                stop = dict(stop_id=stop_id,
+                            stop_code=None,
+                            stop_name=stop_name,
+                            stop_lat=y,
+                            stop_lon=x,
+                            stop_url=None
+                            )
+            elif len(stop) > 1:
+                raise ValueError("Had more than 1 stop with identical stop reference.")
+        # If stop location cannot be determined do not add it to the stops frame.
+        except Exception as e:
+            print(e)
+            continue
+
+        # Add to container
+        stop_data = stop_data.append(stop, ignore_index=True, sort=False)
+
+    return stop_data
+
+
+def get_stops(data, naptan_stops_fp=None):
+    """Parse stop data from TransXchange elements"""
+    required_cols = ['stop_id', 'stop_name',
+                     'stop_lat', 'stop_lon']
+
+    if 'StopPoint' in data.TransXChange.StopPoints.__dir__():
+        stop_data = _get_tfl_style_stops(data, naptan_stops_fp)
+    elif 'AnnotatedStopPointRef' in data.TransXChange.StopPoints.__dir__():
+        stop_data = _get_txc_21_style_stops(data, naptan_stops_fp)
+    else:
+        raise ValueError("Could not parse Stop information from the TransXchange.")
+
+    # Check that stops were found
+    if len(stop_data) == 0:
+        return None
+
+    # Check that required columns exist
+    for col in required_cols:
+        if col not in stop_data.columns:
+            return None
+
+    # Vehicle type is not needed
+    if 'vehicle_type' in stop_data.columns:
+        stop_data = stop_data.drop('vehicle_type', axis=1)
+
+    return stop_data
