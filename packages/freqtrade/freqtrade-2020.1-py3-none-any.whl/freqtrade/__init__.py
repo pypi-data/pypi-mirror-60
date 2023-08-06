dc4898b8597daea2c8fa8ff0e6aed1e4aa9225a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:11 +0000

    Add package freqtrade/2020.1/freqtrade-2020.1-py3-none-any.whl

---
+++ packages/freqtrade/freqtrade-2020.1-py3-none-any.whl/freqtrade/__init__.py
@@ -0,0 +1,13 @@
+""" FreqTrade bot """
+__version__ = '2020.01'
+
+if __version__ == 'develop':
+
+    try:
+        import subprocess
+        __version__ = 'develop-' + subprocess.check_output(
+            ['git', 'log', '--format="%h"', '-n 1'],
+            stderr=subprocess.DEVNULL).decode("utf-8").rstrip().strip('"')
+    except Exception:
+        # git not available, ignore
+        pass
