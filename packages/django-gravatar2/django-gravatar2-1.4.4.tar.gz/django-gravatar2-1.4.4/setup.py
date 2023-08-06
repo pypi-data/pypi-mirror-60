abab0a2f4bdce022a3337b5a60f9eae1909fe0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:34 +0000

    Add package django-gravatar2/1.4.4/django-gravatar2-1.4.4.tar.gz

---
+++ packages/django-gravatar2/django-gravatar2-1.4.4.tar.gz/django-gravatar2-1.4.4/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-gravatar2',
+    version='1.4.4',
+    description='Essential Gravatar support for Django. Features helper'
+                ' methods, templatetags and a full test suite!',
+    long_description=open('README.rst').read(),
+    keywords='django gravatar avatar',
+    license='MIT',
+    author='Tristan Waddington',
+    author_email='tristan.waddington@gmail.com',
+    url='https://github.com/twaddington/django-gravatar',
+    packages=['django_gravatar', 'django_gravatar.templatetags'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable', # 4 Beta, 5 Production/Stable
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ]
+)
