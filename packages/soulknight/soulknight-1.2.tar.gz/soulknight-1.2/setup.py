ab5244770ad9d49cf6ec8d83b720be4c91b9fca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:56 +0000

    Add package soulknight/1.2/soulknight-1.2.tar.gz

---
+++ packages/soulknight/soulknight-1.2.tar.gz/soulknight-1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="soulknight",
+    version="1.2",
+    description="The funniest joke in the world",
+    long_description="pygame",
+    classifiers=["Programming Language :: Python :: 3.7"],
+    keywords="game for yandex lyceum",
+    url="https://github.com/9kin/soul-knight",
+    author="9kin",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["tiledtmxloader", "pygame", "pygame-menu"],
+    entry_points={"console_scripts": ["soulknight = soulknight:main"]},
+    include_package_data=True,
+    setup_requires=["wheel"] ,
+    zip_safe=False,
+)
