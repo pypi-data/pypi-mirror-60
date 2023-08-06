307a24d9d59a2b8eb4b7aef2c13dbeee8125a475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:28 +0000

    Add package saga-vcs/0.0.21/saga_vcs-0.0.21-py3-none-any.whl

---
+++ packages/saga-vcs/saga_vcs-0.0.21-py3-none-any.whl/saga/operations/login.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from requests import Session
+from getpass import getpass
+
+def login(login_url: str) -> Optional[Session]:
+    session = Session()
+
+    # TODO: in the future, we can read these
+    # from the global saga config file
+    username = input("Username: ")
+    password = input("Password: ")
+
+    try:
+        r = session.post(
+            login_url,
+            data = {
+                "username": username,
+                "password": password
+            }
+        )
+
+        if r.status_code == 200:
+            return session
+        # login failed
+        return None
+    except Exception as e:
+        print(e)
+        return None
