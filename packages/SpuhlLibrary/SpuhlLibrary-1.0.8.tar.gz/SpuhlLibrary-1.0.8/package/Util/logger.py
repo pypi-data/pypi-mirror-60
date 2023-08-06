a5150bdd704159ee2604fb86c6e4ec7b139ab996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:39 +0000

    Add package SpuhlLibrary/1.0.8/SpuhlLibrary-1.0.8.tar.gz

---
+++ packages/SpuhlLibrary/SpuhlLibrary-1.0.8.tar.gz/SpuhlLibrary-1.0.8/package/Util/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def setup_logger(level=logging.DEBUG):
+    """
+    Setup the logger for a single module. This setup formats the log-messages. The root logger logs only to the
+    console, according to the azure standard specifications.
+
+    :param level: The Log-level of the messages, which shall be printed to the console
+    """
+    logging.basicConfig(format='%(asctime)s %(name)-12s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S', level=logging.DEBUG)
+    root_logger = logging.getLogger('')
+    root_logger.setLevel(level)
+
+
+
+
+setup_logger()
+logging.log(logging.ERROR, "Connection error to drive")
