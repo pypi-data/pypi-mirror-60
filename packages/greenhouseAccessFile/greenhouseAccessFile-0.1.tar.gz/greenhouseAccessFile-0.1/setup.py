161ec40165c90088ad9aa03bbea7772a8d208291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:39 +0000

    Add package greenhouseAccessFile/0.1/greenhouseAccessFile-0.1.tar.gz

---
+++ packages/greenhouseAccessFile/greenhouseAccessFile-0.1.tar.gz/greenhouseAccessFile-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='greenhouseAccessFile',
+      version='0.1',
+      description='access any file in the zip file of Green House',
+      url='https://github.com/cseseniordesign/plant-phenotyping/tree/master/greenhouseEI',
+      author='YinchaoH',
+      author_email='yhe@huskers.unl.edu',
+      license='APACHE2.0',
+      packages=['greenhouseEI','greenhouseEI'],
+      zip_safe=False)
