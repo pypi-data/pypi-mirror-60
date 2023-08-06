48f82a7e112ba186b86585f718ee1eadcbb36f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:25 +0000

    Add package ddeployer/0.0.8/ddeployer-0.0.8.tar.gz

---
+++ packages/ddeployer/ddeployer-0.0.8.tar.gz/ddeployer-0.0.8/dd/__init__.py
@@ -0,0 +1,9 @@
+
+
+__all__ = ['run','main']
+__title__ = 'dockerDeployer'
+__version__ = '0.0.8'
+__author__ = 'stosc lee'
+__copyright__ = 'Copyright 2020 stosc lee'
+__serverName__='ddeployer'
+__daemonName__='ddd'
