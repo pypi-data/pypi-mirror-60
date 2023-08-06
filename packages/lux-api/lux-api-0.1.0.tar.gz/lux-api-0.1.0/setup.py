687a68989abf0a2b058a37e8b1f2ad64b9445a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:56 +0000

    Add package lux-api/0.1.0/lux-api-0.1.0.tar.gz

---
+++ packages/lux-api/lux-api-0.1.0.tar.gz/lux-api-0.1.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='lux-api',  # PyPI Name (pip install [name])
+    version='0.1.0',  # Required
+    description='A Python API for Intelligent Data Discovery',  # Project description (Optional)
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/lux-org/lux',  # Optional
+    maintainer='Doris Jung-Lin Lee',  # Optional
+    maintainer_email='dorisjunglinlee@gmail.com',  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='visualization analytics data-science insight discovery',  # Optional
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={'': 'lux'},  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.5',
+    install_requires=['pandas','altair'],  # Optional
+    extras_require={  # Optional
+        'test': ['pytest']
+    }
+)
