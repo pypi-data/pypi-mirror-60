d90c4a31aaf86a154364905c0aa55dd6ce9e6d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:36 +0000

    Add package autoprop/1.0.0/autoprop-1.0.0.tar.gz

---
+++ packages/autoprop/autoprop-1.0.0.tar.gz/autoprop-1.0.0/autoprop.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+class autoprop(object):
+
+    __version__ = '1.0.0'
+    property = type('property', (property,), {})
+
+    class _Cache:
+        __slots__ = ('value', 'is_stale')
+
+        def __init__(self):
+            self.value = None
+            self.is_stale = True
+
+    @classmethod
+    def cache(cls, f):
+        if not f.__name__.startswith('get_'):
+            raise ValueError("only getters can be cached")
+
+        # In python2, you can assign attributes to functions but not to bound/ 
+        # unbound methods (see PEP 232).  However, methods allow read-only 
+        # access to attributes of the underlying function.  So here we give the 
+        # function a mutable cache instance that can be manipulated later.
+        f._autoprop_cache = cls._Cache()
+        return f
+
+    def __new__(self, cls):
+        # These imports have to be inside autoprop(), otherwise the sys.modules 
+        # hack below somehow makes them unavailable when the decorator is 
+        # applied.
+        import inspect, re
+        from collections import defaultdict
+        from functools import wraps
+
+        if not hasattr(cls, '__class__'):
+            raise TypeError('@autoprop can only be used with new-style classes')
+
+        accessors = defaultdict(dict)
+        expected_num_args = {'get': 0, 'set': 1, 'del': 0}
+
+        # The accessors we're searching for are considered methods in python2 
+        # and functions in python3.  They behave the same either way.
+        ismethod = lambda x: inspect.ismethod(x) or inspect.isfunction(x)
+
+        for method_name, method in inspect.getmembers(cls, ismethod):
+            accessor_match = re.match('(get|set|del)_(.+)', method_name)
+            if not accessor_match:
+                continue
+
+            # Suppress a warning by using getfullargspec() if it's available 
+            # and getargspec() if it's not.
+            try: from inspect import getfullargspec as getargspec
+            except ImportError: from inspect import getargspec
+
+            prefix, name = accessor_match.groups()
+            arg_spec = getargspec(method)
+            num_args = len(arg_spec.args) - len(arg_spec.defaults or ())
+            num_args_minus_self = num_args - 1
+
+            if num_args_minus_self != expected_num_args[prefix]:
+                continue
+
+            accessors[name][prefix] = method
+
+        def use_cache(getter):
+
+            @wraps(getter)
+            def wrapper(*args, **kwargs):
+                if getter._autoprop_cache.is_stale:
+                    getter._autoprop_cache.value = getter(*args, **kwargs)
+                    getter._autoprop_cache.is_stale = False
+                return getter._autoprop_cache.value
+
+            return wrapper
+
+        def clear_cache(f, getter):
+            if f is None:
+                return None
+
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+                getter._autoprop_cache.is_stale = True
+                return f(*args, **kwargs)
+
+            return wrapper
+
+        for name in accessors:
+            try:
+                attr = getattr(cls, name)
+                ok_to_overwrite = isinstance(attr, self.property)
+            except AttributeError:
+                ok_to_overwrite = True
+
+            getter  = accessors[name].get('get')
+            setter  = accessors[name].get('set')
+            deleter = accessors[name].get('del')
+
+            # Cache the return value of the getter, if requested.  For some 
+            # reason setting attributes on method objects 
+            if getter and getattr(getter, '_autoprop_cache', False):
+                setter  = clear_cache(setter,  getter)
+                deleter = clear_cache(deleter, getter)
+                getter  = use_cache(getter)
+
+            if ok_to_overwrite:
+                property = self.property(getter, setter, deleter)
+                setattr(cls, name, property)
+
+        return cls
+
+
+# Abuse the import system so that the module itself can be used as a decorator.  
+# This is a simple module intended only to cut-down on boilerplate, so I think 
+# the trade-off between magicalness and ease-of-use is justified in this case.
+import sys
+sys.modules[__name__] = autoprop
---
+++ packages/autoprop/autoprop-1.0.0.tar.gz/autoprop-1.0.0/tests/test_autoprop.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+
+import pytest
+import autoprop
+
+def test_get():
+    @autoprop
+    class Example(object): #
+        def get_attr(self): #
+            return 'attr'
+
+    ex = Example()
+    assert ex.attr == 'attr'
+
+def test_set():
+    @autoprop
+    class Example(object): #
+        def set_attr(self, attr): #
+            self._attr = 'new ' + attr
+
+    ex = Example()
+    ex.attr = 'attr'
+    assert ex._attr == 'new attr'
+
+def test_del():
+    @autoprop
+    class Example(object): #
+        def del_attr(self): #
+            self._attr = None
+
+    ex = Example()
+    del ex.attr
+    assert ex._attr is None
+
+def test_get_set():
+    @autoprop
+    class Example(object): #
+        def get_attr(self): #
+            return self._attr
+        def set_attr(self, attr): #
+            self._attr = 'new ' + attr
+
+    ex = Example()
+    ex.attr = 'attr'
+    assert ex.attr == 'new attr'
+
+def test_get_set_del():
+    @autoprop
+    class Example(object): #
+        def get_attr(self): #
+            return self._attr
+        def set_attr(self, attr): #
+            self._attr = 'new ' + attr
+        def del_attr(self): #
+            self._attr = None
+
+    ex = Example()
+    ex.attr = 'attr'
+    assert ex.attr == 'new attr'
+    del ex.attr
+    assert ex.attr is None
+
+def test_cache_get():
+    @autoprop
+    class Example(object): #
+        def __init__(self): #
+            self._attr = 0
+        @autoprop.cache
+        def get_attr(self): #
+            self._attr += 1
+            return self._attr
+
+    ex = Example()
+
+    # Calling the getter multiple times only call the underlying method once.
+    assert ex.attr == 1
+    assert ex.attr == 1
+
+def test_cache_get_set_del():
+    @autoprop
+    class Example(object): #
+        def __init__(self): #
+            self._attr = 0
+        @autoprop.cache
+        def get_attr(self): #
+            self._attr += 1
+            return self._attr
+        def set_attr(self, attr): #
+            self._attr = attr
+        def del_attr(self): #
+            self._attr = 0
+
+    ex = Example()
+
+    # Calling the getter multiple times only call the underlying method once.
+    assert ex.attr == 1
+    assert ex.attr == 1
+
+    # Calling the setter clears the cache.
+    ex.attr = 2
+    assert ex.attr == 3
+    assert ex.attr == 3
+
+    # Calling the deleter also clears the cache.
+    del ex.attr
+    assert ex.attr == 1
+    assert ex.attr == 1
+
+def test_ignore_similar_names():
+    @autoprop #
+    class Example(object):
+        def getattr(self):
+            return 'attr'
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        ex.attr
+
+def test_ignore_empty_names():
+    @autoprop #
+    class Example(object):
+        def get_(self):
+            return 'get'
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        getattr(ex, '')
+
+def test_ignore_non_methods():
+    @autoprop #
+    class Example(object):
+        get_attr = 'attr'
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        ex.attr
+
+def test_dont_overwrite_existing_attributes():
+    @autoprop #
+    class Example(object):
+        attr = 'class var'
+        def get_attr(self):
+            return 'attr'
+
+    ex = Example()
+    assert ex.attr == 'class var'
+
+def test_dont_overwrite_inherited_attributes():
+    @autoprop #
+    class Parent(object):
+        attr = 'class var'
+        def get_attr(self):
+            return 'parent'
+
+    @autoprop #
+    class Child(Parent):
+        def get_attr(self):
+            return 'child'
+
+    parent = Parent()
+    child = Child()
+
+    assert parent.attr == 'class var'
+    assert child.attr == 'class var'
+
+def test_overwrite_inherited_autoprops():
+    @autoprop #
+    class Parent(object):
+        def get_attr(self):
+            return 'parent'
+        def get_overloaded_attr(self):
+            return 'parent'
+
+    @autoprop #
+    class Child(Parent):
+        def get_overloaded_attr(self):
+            return 'child'
+
+    parent = Parent()
+    child = Child()
+
+    assert parent.attr == 'parent'
+    assert parent.overloaded_attr == 'parent'
+    assert child.attr == 'parent'
+    assert child.overloaded_attr == 'child'
+
+def test_optional_arguments():
+    @autoprop #
+    class Example(object):
+        def get_attr(self, pos=None, *args, **kwargs):
+            return self._attr
+        def set_attr(self, new_value, pos=None, *args, **kwargs):
+            self._attr = 'new ' + new_value
+        def del_attr(self, pos=None, *args, **kwargs):
+            self._attr = None
+
+    ex = Example()
+    ex.attr = 'attr'
+    assert ex.attr == 'new attr'
+    del ex.attr
+    assert ex.attr == None
+
+def test_getters_need_one_argument():
+    @autoprop #
+    class Example(object):
+        def get_attr():
+            return 'attr'
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        ex.attr
+
+    @autoprop #
+    class Example(object):
+        def get_attr(self, more_info):
+            return 'attr'
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        ex.attr
+
+def test_setters_need_two_arguments():
+    @autoprop #
+    class Example(object):
+        def set_attr(self):
+            self._attr = 'no args'
+
+    ex = Example()
+    ex.attr = 'attr'
+    with pytest.raises(AttributeError):
+        assert ex._attr != 'no args'
+
+    @autoprop #
+    class Example(object):
+        def set_attr(self, new_value, more_info):
+            self._attr = 'two args'
+
+    ex = Example()
+    ex.attr = 'attr'
+    with pytest.raises(AttributeError):
+        assert ex._attr != 'two args'
+
+def test_deleters_need_one_argument():
+    @autoprop #
+    class Example(object):
+        def del_attr():
+            pass
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        del ex.attr
+
+    @autoprop #
+    class Example(object):
+        def del_attr(self, more_info):
+            pass
+
+    ex = Example()
+    with pytest.raises(AttributeError):
+        del ex.attr
+
+def test_docstrings():
+
+    @autoprop #
+    class Example(object):
+        def get_attr(self): #
+            "get attr"
+            return self._attr
+        def set_attr(self, attr): #
+            "set attr"
+            self._attr = 'new ' + attr
+        def del_attr(self): #
+            "del attr"
+            self._attr = None
+
+    # The docstrings on the setter and deleter are ignored, as per the default 
+    # behavior of the `property()` decorator.
+    assert Example.attr.__doc__ == "get attr"
