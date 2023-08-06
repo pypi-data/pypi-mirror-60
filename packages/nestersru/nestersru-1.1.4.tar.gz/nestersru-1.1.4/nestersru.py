992abab695db56ce37e9944bb1d2fc3d32a3b873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:16 +0000

    Add package nestersru/1.1.4/nestersru-1.1.4.tar.gz

---
+++ packages/nestersru/nestersru-1.1.4.tar.gz/nestersru-1.1.4/nestersru.py
@@ -0,0 +1,15 @@
+"""This is nester.py module. This module has the iter() function. This function is used for iterating through the lists."""
+# print_nestList(l) function takes one list parameter. It prints out each element from the provided list, even if the list has multiple nested lists.
+
+
+def print_nestList(l, level=0):
+# Second argument is for providing the number of tab stops while printing the nested list.
+# second argument has a default value making it as an optional argument.
+
+	for i in l:
+		if isinstance(i, list):
+			print_nestList(i,level+1)
+		else:
+			for tab in range(level):
+				print("\t" , end='')
+			print(i)	
