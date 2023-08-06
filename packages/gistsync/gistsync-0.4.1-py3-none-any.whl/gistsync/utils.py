189f68b15b5b6f1fe8ffadbdd74d1432ad9554ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:32 +0000

    Add package gistsync/0.4.1/gistsync-0.4.1-py3-none-any.whl

---
+++ packages/gistsync/gistsync-0.4.1-py3-none-any.whl/gistsync/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import re
+
+def format_gist_updated_at(gist) -> str:
+    '''
+    format gist update_at as string.
+    '''
+    return gist.updated_at.isoformat(timespec='seconds')
+
+def find_gist_id(input_str: str) -> str:
+    'try find a gist id from user input string.'
+
+    m = re.match('https://gist.github.com/[^/]+/(.+)', input_str, re.I)
+    if m:
+        return m[0]
+    return input_str
+
