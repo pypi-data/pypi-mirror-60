120141b5cc0d6d8103032ed14cc4d85ff8cb4ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:54 +0000

    Add package extra-keras-datasets/0.1.5/extra_keras_datasets-0.1.5.tar.gz

---
+++ packages/extra-keras-datasets/extra_keras_datasets-0.1.5.tar.gz/extra_keras_datasets-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'extra_keras_datasets',
+  packages = ['extra_keras_datasets'],
+  version = '0.1.5',
+  license='MIT',
+  description = 'Extending the Keras Datasets module with extra ones.',
+  long_description = 'Extending the Keras Datasets module with extra ones.',
+  author = 'Christian Versloot',
+  author_email = 'chris@machinecurve.com',
+  url = 'https://github.com/christianversloot/extra_keras_datasets',
+  download_url = 'https://github.com/christianversloot/extra_keras_datasets/archive/0.1.5.tar.gz',
+  keywords = ['keras', 'datasets', 'machine learning'],
+  install_requires=[
+    'tensorflow>=2.0.0',
+    'numpy',
+    'scipy'
+  ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
