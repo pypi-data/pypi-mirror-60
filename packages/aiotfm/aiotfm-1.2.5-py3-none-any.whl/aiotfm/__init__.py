3f3b60bf06c27059dec6cb387279411fee545915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:24 +0000

    Add package aiotfm/1.2.5/aiotfm-1.2.5-py3-none-any.whl

---
+++ packages/aiotfm/aiotfm-1.2.5-py3-none-any.whl/aiotfm/__init__.py
@@ -0,0 +1,9 @@
+from aiotfm import utils, errors, enums
+from aiotfm.client import Client
+from aiotfm.connection import Connection
+from aiotfm.packet import Packet
+from aiotfm.player import Player, Profile, Stats
+from aiotfm.tribe import *
+
+from aiotfm.__version__ import __author__, __title__, __description__
+from aiotfm.__version__ import __url__, __version__, __credits__, __license__
