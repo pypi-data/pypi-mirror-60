0db9d64ec73a908bf0415c0e70aa1e023fca344f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:47 +0000

    Add package tfea/1.0.1/tfea-1.0.1.tar.gz

---
+++ packages/tfea/tfea-1.0.1.tar.gz/tfea-1.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tfea",
+    version="1.0.1",
+    python_requires=">=3",
+    description="Transcription Factor Enrichment Analysis",
+    url="https://github.com/Dowell-Lab/TFEA.git",
+    author="Jonathan Rubin",
+    author_email="jonathan.rubin@colorado.edu",
+    license="GPL-3.0",
+    packages=setuptools.find_packages(),
+    package_data={"": ["test/test_files/*", "*sbatch"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    scripts=["bin/TFEA", "bin/TFEA-annotate", "bin/TFEA-simulate"],
+    install_requires=[
+        "matplotlib>=3.1.1",
+        "scipy",
+        "numpy",
+        "pybedtools",
+        "htseq",
+        "psutil",
+        "ujson",
+        "pysam==0.15.2"
+    ],
+    zip_safe=False,
+)
