0192c389c81267d66bbaac80063ed3283ffd9ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:28 +0000

    Add package pipelinewise-transform-field/1.1.2/pipelinewise-transform-field-1.1.2.tar.gz

---
+++ packages/pipelinewise-transform-field/pipelinewise-transform-field-1.1.2.tar.gz/pipelinewise-transform-field-1.1.2/setup.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pipelinewise-transform-field',
+      version='1.1.2',
+      description='Singer.io simple field transformator between taps and targets - PipelineWise compatible',
+      author="TransferWise",
+      url='https://github.com/transferwise/pipelinewise-transform-field',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=['transform_field'],
+      install_requires=[
+          'singer-python==5.2.0',
+      ],
+      entry_points='''
+          [console_scripts]
+          transform-field=transform_field:main
+      ''',
+      packages=['transform_field']
+)
