ab566d5d0a1a534cc591f09b21208183da0365be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:09 +0000

    Add package basepackage/0.0.1/basepackage-0.0.1.tar.gz

---
+++ packages/basepackage/basepackage-0.0.1.tar.gz/basepackage-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="basepackage", # Replace with your own username
+    version="0.0.1",
+    author="Lucifer Monao",
+    author_email="Lucifermonao@gmx.de",
+    description="This package includes some, for me, helpful small things.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
