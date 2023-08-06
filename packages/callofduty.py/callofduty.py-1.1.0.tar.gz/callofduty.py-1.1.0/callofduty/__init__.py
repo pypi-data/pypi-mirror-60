72308e43b38daf6543ce6f7bfa6eeb6b981f1d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:07 +0000

    Add package callofduty.py/1.1.0/callofduty.py-1.1.0.tar.gz

---
+++ packages/callofduty.py/callofduty.py-1.1.0.tar.gz/callofduty.py-1.1.0/callofduty/__init__.py
@@ -0,0 +1,31 @@
+"""
+CallofDuty.py is an asynchronous, object-oriented Python wrapper for
+the Call of Duty API.
+
+GitHub: https://github.com/EthanC/CallofDuty.py
+"""
+
+import logging
+
+from .auth import Login
+from .client import Client
+from .enums import *
+from .errors import *
+from .leaderboard import Leaderboard, LeaderboardEntry
+from .loadout import Loadout, LoadoutItem, LoadoutWeapon
+from .loot import LootItem, Season
+from .match import Match
+from .player import Player
+from .squad import Squad
+from .stamp import AuthenticityStamp
+
+try:
+    from logging import NullHandler
+except ImportError:
+
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+
+logging.getLogger(__name__).addHandler(NullHandler())
