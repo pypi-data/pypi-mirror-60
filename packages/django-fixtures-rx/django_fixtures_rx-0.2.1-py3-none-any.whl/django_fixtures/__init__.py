e215287a7c052c47a87beee124dadb0c0f233fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:10 +0000

    Add package django-fixtures-rx/0.2.1/django_fixtures_rx-0.2.1-py3-none-any.whl

---
+++ packages/django-fixtures-rx/django_fixtures_rx-0.2.1-py3-none-any.whl/django_fixtures/__init__.py
@@ -0,0 +1,3 @@
+from .dataset import CommonDatasetMixin, TupleDatasetMixin, DictDatasetMixin, GeneratedDatasetMixin
+from .fixtures import FixturesData, JSONFixture, ModelFixture
+from .unittests import FixturesTestCase
