89182dc4f1cf3d98ce0023b58ffe7d9577c5066b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:18 +0000

    Add package edit-distance-lte1/0.0.1/edit_distance_lte1-0.0.1.tar.gz

---
+++ packages/edit-distance-lte1/edit_distance_lte1-0.0.1.tar.gz/edit_distance_lte1-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup, Extension
+
+lte1_edit_distance_module = Extension(
+    '_edit_distance_lte1',
+    sources = ['./edit_distance_lte1/_edit_distance_lte1.c']
+)
+
+
+setup(
+    name='edit_distance_lte1',
+    version='0.0.1',
+    description='Fast string distance function for the case where the distance is less or equal to 1',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+
+    author='Romain SENESI',
+    author_email='romain.senesi@mapado.com',
+    maintainer='Romain SENESI',
+    maintainer_email='romain.senesi@mapado.com',
+    url='https://github.com/mapado/edit_distance_lte1',
+    packages=['edit_distance_lte1'],
+    license=['MIT'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    ext_modules=[lte1_edit_distance_module]
+)
