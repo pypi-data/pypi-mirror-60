fd8576cfed1e8e728cba323e8cf234af448cd973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package netsuite-db-connector/0.2.0/netsuite-db-connector-0.2.0.tar.gz

---
+++ packages/netsuite-db-connector/netsuite-db-connector-0.2.0.tar.gz/netsuite-db-connector-0.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='netsuite-db-connector',
+    version='0.2.0',
+    author='Siva Narayanan',
+    author_email='siva@fyle.in',
+    description='Connects NetSuite to a database connector to transfer information to and fro',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['netsuite', 'api', 'python', 'sdk', 'sqlite'],
+    url='https://github.com/fylein/netsuite-db-connector',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['netsuitesdk==1.1.0'],
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
