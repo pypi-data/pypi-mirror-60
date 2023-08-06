0c4db4877e8638c578b0bcd73fd0a3d80bded476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:06 +0000

    Add package credstash-envvar-helper/0.1.1/credstash_envvar_helper-0.1.1.tar.gz

---
+++ packages/credstash-envvar-helper/credstash_envvar_helper-0.1.1.tar.gz/credstash_envvar_helper-0.1.1/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Thomas Knox <tknox@umich.edu>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/credstash-envvar-helper/credstash_envvar_helper-0.1.1.tar.gz/credstash_envvar_helper-0.1.1/tests/test_credstash_envvar_helper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `credstash_envvar_helper` package."""
+
+
+import unittest
+
+from credstash_envvar_helper import credstash_envvar_helper
+
+
+class TestCredstash_envvar_helper(unittest.TestCase):
+    """Tests for `credstash_envvar_helper` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
