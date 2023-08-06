3ce323dafa9390e6fa19c241f7bb533b6b69f9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:27 +0000

    Add package slack-bundle/1.1/slack_bundle-1.1-py3-none-any.whl

---
+++ packages/slack-bundle/slack_bundle-1.1-py3-none-any.whl/slack_bundle/bundle.py
@@ -0,0 +1,21 @@
+from applauncher.kernel import ConfigurationReadyEvent
+from slackclient import SlackClient
+import inject
+
+
+class SlackBundle(object):
+    def __init__(self):
+        self.config_mapping = {
+            "slack": {
+                "token": None,
+            }
+        }
+
+        self.event_listeners = [
+            (ConfigurationReadyEvent, self.configuration_ready)
+        ]
+
+        self.injection_bindings = {}
+
+    def configuration_ready(self, event):
+        self.injection_bindings[SlackClient] = SlackClient(token=event.configuration.slack.token)
