89c395a54e239a5a54a2502ed80c90ba202fa2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:39 +0000

    Add package astrocyte/0.2.2/astrocyte-0.2.2-py3-none-any.whl

---
+++ packages/astrocyte/astrocyte-0.2.2-py3-none-any.whl/astrocyte/templates.py
@@ -0,0 +1,28 @@
+import os
+from . import get_minimum_glia_version
+
+
+def get_constants():
+    return {"glia_version": get_minimum_glia_version()}
+
+
+def parse_template(name, locals={}):
+    template_file = name + ".txt"
+    if name.startswith("."):
+        template_file = "_" + template_file[1:]
+    file = open(os.path.join(os.path.dirname(__file__), "templates", template_file), "r")
+    template_string = file.read()
+    file.close()
+    for local, value in locals.items():
+        template_string = template_string.replace("{{" + local + "}}", str(value))
+    for const, value in get_constants().items():
+        template_string = template_string.replace("{|" + const + "|}", str(value))
+
+    return template_string
+
+
+def create_template(name, target, locals={}):
+    content = parse_template(name, locals)
+    template = open(os.path.join(target, name), "w")
+    template.write(content)
+    template.close()
