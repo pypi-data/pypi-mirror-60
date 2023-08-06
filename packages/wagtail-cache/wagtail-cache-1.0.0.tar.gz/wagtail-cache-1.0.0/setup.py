78a1f11f4c769de2508671423834a9dde7822cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:54 +0000

    Add package wagtail-cache/1.0.0/wagtail-cache-1.0.0.tar.gz

---
+++ packages/wagtail-cache/wagtail-cache-1.0.0.tar.gz/wagtail-cache-1.0.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from wagtailcache import __version__
+
+with open('README.md', encoding='utf8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='wagtail-cache',
+    version=__version__,
+    author="CodeRed LLC",
+    author_email='info@coderedcorp.com',
+    url='https://github.com/coderedcorp/wagtail-cache',
+    description="A simple page cache for Wagtail based on the Django cache middleware.",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license="BSD license",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'wagtail>=2.0'
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'mypy',
+            'pytest',
+            'pytest-cov',
+            'pytest-django',
+            'sphinx',
+            'twine',
+            'wheel',
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 2',
+    ],
+)
