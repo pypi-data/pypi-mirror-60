223c6cd1a2b092ae445c3fbf2690cb34d970ef0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:00 +0000

    Add package poetry-publish/0.2.2/poetry-publish-0.2.2.tar.gz

---
+++ packages/poetry-publish/poetry-publish-0.2.2.tar.gz/poetry-publish-0.2.2/poetry_publish/self.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from creole.setup_utils import update_rst_readme
+
+import poetry_publish
+import poetry_publish.publish
+
+
+def update_poetry_publish_readme():
+    return update_rst_readme(
+        package_root=Path(__file__).parent.parent,
+        filename='README.creole'
+    )
+
+
+def publish_poetry_publish():
+    """
+        Publish 'poetry-publish' to PyPi
+        Call this via:
+            $ poetry run publish
+    """
+    poetry_publish.publish.poetry_publish(
+        package_root=Path(poetry_publish.__file__).parent.parent,
+        version=poetry_publish.__version__,
+    )
