9e80013e19d3e615dfe35dd17f92194e44d26754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package lgt-common/0.3/lgt-common-0.3.tar.gz

---
+++ packages/lgt-common/lgt-common-0.3.tar.gz/lgt-common-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lgt-common',
+      version='0.3',
+      description='LGT common builds',
+      packages=['lgt'],
+      include_package_data=True,
+      author_email='alexander.grin1997@gmail.com',
+      zip_safe=False)
