e9fbb5c0c3bd4e37498e31ace6e95b7c666095a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:49 +0000

    Add package cfgrep/1.0.5/cfgrep-1.0.5.tar.gz

---
+++ packages/cfgrep/cfgrep-1.0.5.tar.gz/cfgrep-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cfgrep",
+    version="1.0.5",
+    description="cfgrep is Cisco-like Config Context-aware Check grep",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="toshayas",
+    author_email='toshayas@cisco.com',
+    url='https://github.com/tk-hayashi/cfgrep',
+    packages=find_packages(),
+    install_requires=["wheel",
+                      "ciscoconfparse",
+                      "netaddr",
+                      "docopt"],
+    entry_points={
+        "console_scripts": [
+            "cfgrep = bin.main:main",
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
