ad96f8cf45d5403bbb49428b93662ec6eeea8fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:43 +0000

    Add package pigpio-encoder/0.2.2/pigpio_encoder-0.2.2.tar.gz

---
+++ packages/pigpio-encoder/pigpio_encoder-0.2.2.tar.gz/pigpio_encoder-0.2.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+file_path = path.abspath(path.dirname(__file__))
+
+with open(path.join(file_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pigpio_encoder',
+      version='0.2.2',
+      description='Python module for for quickly interface a KY040 rotary encoder with Raspberry Pi.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+      ],
+      keywords='rotary encoder switch ky040 gpio raspberry',
+      url='https://github.com/vash3d/pigpio_encoder',
+      download_url='https://github.com/vash3d/pigpio_encoder/archive/0.2.2.tar.gz',
+      author='vash',
+      author_email='vash.3d@gmail.com',
+      license='GNU GPLv3',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
