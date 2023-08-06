f6542c7d594db56688d92f644ab366d77ba42ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:26 +0000

    Add package atspy/0.2.2/atspy-0.2.2.tar.gz

---
+++ packages/atspy/atspy-0.2.2.tar.gz/atspy-0.2.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Command
+import os
+import sys
+
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+print("Req:")
+print(install_requires)
+
+setup(name='atspy',
+      version='0.2.2',
+      description='Automated Time Series in Python',
+      url='https://github.com/firmai/atspy',
+      author='snowde',
+      author_email='d.snow@firmai.org',
+      license='MIT',
+      packages=['atspy'],
+      install_requires=
+          install_requires
+    ,
+      zip_safe=False)
