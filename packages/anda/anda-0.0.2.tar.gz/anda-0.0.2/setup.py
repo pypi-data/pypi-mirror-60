249070be8a23e463e87e899e358b99e32f5a3d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:22 +0000

    Add package anda/0.0.2/anda-0.0.2.tar.gz

---
+++ packages/anda/anda-0.0.2.tar.gz/anda-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anda", # Replace with your own username
+    version="0.0.2",
+    author="Vojtech Kase",
+    author_email="vojtech.kase@gmail.com",
+    description="A package collecting various functions to work with ancient Mediterranean datasets (textual, spatial, etc.)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sdam-au/anda",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
