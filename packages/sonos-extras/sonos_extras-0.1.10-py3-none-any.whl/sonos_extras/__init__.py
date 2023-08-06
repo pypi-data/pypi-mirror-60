f96e7280dd5996a20f01e308a50ef616e77ff811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:07 +0000

    Add package sonos-extras/0.1.10/sonos_extras-0.1.10-py3-none-any.whl

---
+++ packages/sonos-extras/sonos_extras-0.1.10-py3-none-any.whl/sonos_extras/__init__.py
@@ -0,0 +1,8 @@
+"""sonos_extras - Some extra useful commands for Sonos systems, based on SoCo."""
+
+__version__ = '0.1.10'
+__author__ = 'MaziarA <maziara2@gmail.com>'
+__all__ = []
+
+from .module import *
+from .bin import SonosExtrasCLI
