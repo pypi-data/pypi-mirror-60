d1b393df69b023c61885a0a73525e1b7111efb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:32 +0000

    Add package mreddata/0.3.5/mreddata-0.3.5-py3-none-any.whl

---
+++ packages/mreddata/mreddata-0.3.5-py3-none-any.whl/mreddata/__init__.py
@@ -0,0 +1,10 @@
+from .datatools import options
+from .datatools import plot_options as po
+#from .pkldata import PklData 
+#from .txtdata import TxtData 
+#from .hdf5data import Hdf5Data 
+from .hdf5data import Hdf5Data as Data
+
+#define common symbols used in plot titles/axes/legends etc.
+mu = '\u03bc'
+deg = '\u03B1'
