daa8c5f378747ab8db48885a49b2802283c1d30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:56 +0000

    Add package deepali/0.1/deepali-0.1.tar.gz

---
+++ packages/deepali/deepali-0.1.tar.gz/deepali-0.1/assignment2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 18:44:48 2020
+
+@author: deepaliS
+"""
+
+
+
+from setuptools import setup
+setup(name='deepali',
+      version='0.1',
+      description='sum of two number',
+      author='deepali',
+      author_email="deepali.sharma@sganalytics.com",
+      license='MIT',
+      packages=['deepali'],
+      long_description="firstpackage",
+      zip_safe=False)
---
+++ packages/deepali/deepali-0.1.tar.gz/deepali-0.1/deepali/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 18:40:10 2020
+
+@author: deepaliS
+"""
+
+def addfunc(num1, num2):
+    sum = num1+num2
+    return sum
