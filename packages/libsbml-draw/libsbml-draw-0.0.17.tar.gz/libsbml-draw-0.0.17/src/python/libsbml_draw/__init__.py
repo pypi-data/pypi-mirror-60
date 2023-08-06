6cd589c2a898468201b8737da931d483a9b7c0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:07 +0000

    Add package libsbml-draw/0.0.17/libsbml-draw-0.0.17.tar.gz

---
+++ packages/libsbml-draw/libsbml-draw-0.0.17.tar.gz/libsbml-draw-0.0.17/src/python/libsbml_draw/__init__.py
@@ -0,0 +1,9 @@
+from libsbml_draw.sbml_layout import SBMLlayout
+from libsbml_draw.styles import Style, black_and_white
+from libsbml_draw.utils import biomodels_download
+
+MAJOR = 0
+MINOR = 0
+MICRO = 17
+
+__version__ = f"{MAJOR}.{MICRO}.{MICRO}"
