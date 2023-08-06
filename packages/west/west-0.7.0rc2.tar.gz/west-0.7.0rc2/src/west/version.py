56f0b999b89da874904a444e43c555a3d5154e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:44 +0000

    Add package west/0.7.0rc2/west-0.7.0rc2.tar.gz

---
+++ packages/west/west-0.7.0rc2.tar.gz/west-0.7.0rc2/src/west/version.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2019, Nordic Semiconductor ASA
+#
+# Don't put anything else in here!
+#
+# This is the Python 3 version of option 3 in:
+# https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
+
+__version__ = '0.7.0rc2'
