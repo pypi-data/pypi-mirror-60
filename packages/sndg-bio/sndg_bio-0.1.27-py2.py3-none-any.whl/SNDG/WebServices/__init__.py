39253adc8f98606ee9cbf713b60a4eb12a26ab4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:46 +0000

    Add package sndg-bio/0.1.27/sndg_bio-0.1.27-py2.py3-none-any.whl

---
+++ packages/sndg-bio/sndg_bio-0.1.27-py2.py3-none-any.whl/SNDG/WebServices/__init__.py
@@ -0,0 +1,48 @@
+"""
+Utilities for file download
+"""
+
+import hashlib
+import logging
+import os
+
+
+from SNDG import execute
+
+_log = logging.getLogger(__name__)
+
+PROXIES = {}
+def proxy_vars():
+    vars = ""
+    for k,v in PROXIES.items():
+        vars= k + "=" + v
+    return vars
+def OvewriteFileException(Exception):
+    pass
+
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def md5_equal(fname, known_md5):
+    match = md5(fname) == known_md5
+    if not match:
+        _log.error("error in %s checksum" % fname)
+    return match
+
+
+def download_file(complete_url, target, ovewrite=False, retries=3):
+    if not target.strip():
+        target = "./"
+    if not os.path.exists(os.path.dirname(os.path.abspath(target))):
+        raise Exception("%s does not exists" % os.path.dirname(target))
+    if os.path.exists(target) and not ovewrite:
+        raise OvewriteFileException("%s already exists" % target)
+
+    execute(proxy_vars() + ' wget  --timeout=20 --tries={retries} -O {target} "{url}"',
+            url=complete_url, retries=retries, target=target)
