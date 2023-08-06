49ae898b0f6dfcead26bc5311dba252a68ed6432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:39 +0000

    Add package integration-cli-test/0.0.1b5/integration-cli-test-0.0.1b5.tar.gz

---
+++ packages/integration-cli-test/integration-cli-test-0.0.1b5.tar.gz/integration-cli-test-0.0.1b5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='integration-cli-test',
+    version='0.0.1b5',
+    packages=find_packages(),
+    py_modules=['smart_integration_cli', '/smart_cli', '/smart_cli/generators'],
+    install_requires=[
+        'Click>=5.1',
+        'Jinja2>=2.10.3',
+    ],
+    entry_points="""
+        [console_scripts]
+        integration=smart_integration_cli:cli
+    """,
+    include_package_data=True,
+)
