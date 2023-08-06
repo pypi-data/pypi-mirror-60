63e2da49040faeb1b9efcdf4f1c9877009c88644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:17 +0000

    Add package schema-validator-halpa/0.0.6rc1/schema-validator-halpa-0.0.6rc1.tar.gz

---
+++ packages/schema-validator-halpa/schema-validator-halpa-0.0.6rc1.tar.gz/schema-validator-halpa-0.0.6rc1/validator/decorators/decorators.py
@@ -0,0 +1,8 @@
+from .. import is_valid, validate as base_validate
+
+def validate(schema, data):
+    def decorator(f):
+        def validate_f(*args, **kwargs):
+            return base_validate(schema, data)
+        return validate_f
+    return decorator
