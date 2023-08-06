2fc92ebde2ba4f15664a7137849900c6db45e053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package trollformatter/0.0.1/trollformatter-0.0.1.tar.gz

---
+++ packages/trollformatter/trollformatter-0.0.1.tar.gz/trollformatter-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="trollformatter",
+    version="0.0.1",
+    author="Vincent Wang",
+    author_email="vwangsf@gmail.com",
+    description="A troll code formatter for C, C++ and Java to make your compsci teacher angry.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/bbworld1/trollformatter",
+    download_url="https://gitlab.com/bbworld1/trollformatter/-/archive/v1.0.0/trollformatter-v1.0.0.tar.gz",
+    packages=setuptools.find_packages(),
+    scripts=["bin/trollformatter"],
+    keywords="troll code-formatter formatter c c++ java",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3'
+    ],
+)
