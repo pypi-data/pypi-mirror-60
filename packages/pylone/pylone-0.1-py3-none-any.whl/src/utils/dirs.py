37551ccd515dfc5e91acda2735006416c6c61688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:27 +0000

    Add package pylone/0.1/pylone-0.1-py3-none-any.whl

---
+++ packages/pylone/pylone-0.1-py3-none-any.whl/src/utils/dirs.py
@@ -0,0 +1,10 @@
+import os
+
+def makedirs(directories):
+	for directory in directories:
+		try:
+			os.mkdir(directory)
+		except FileExistsError:
+			print(directory, 'already exists')
+		except Exception as e:
+			print(e)
