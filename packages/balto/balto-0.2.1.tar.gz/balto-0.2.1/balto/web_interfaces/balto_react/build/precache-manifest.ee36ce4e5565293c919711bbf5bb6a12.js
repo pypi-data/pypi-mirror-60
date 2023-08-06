cdf1f81f85f8889295bff6539177ba93079386c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:43 +0000

    Add package balto/0.2.1/balto-0.2.1.tar.gz

---
+++ packages/balto/balto-0.2.1.tar.gz/balto-0.2.1/balto/web_interfaces/balto_react/build/precache-manifest.ee36ce4e5565293c919711bbf5bb6a12.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4257a0c86e5a4949ec2c",
+    "url": "./static/css/main.fbf7ee4c.chunk.css"
+  },
+  {
+    "revision": "4257a0c86e5a4949ec2c",
+    "url": "./static/js/main.4257a0c8.chunk.js"
+  },
+  {
+    "revision": "11154dd893ca271b0950",
+    "url": "./static/css/1.049c078b.chunk.css"
+  },
+  {
+    "revision": "11154dd893ca271b0950",
+    "url": "./static/js/1.11154dd8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d0ef7417d2e2ac0ed9dd32f9f45f1fd2",
+    "url": "./index.html"
+  }
+];
