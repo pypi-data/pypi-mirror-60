d2c5c17ce3242ce3081f8f2a30d350741df85dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:54 +0000

    Add package py2graphql/0.9.0/py2graphql-0.9.0-py2.py3-none-any.whl

---
+++ packages/py2graphql/py2graphql-0.9.0-py2.py3-none-any.whl/py2graphql/__init__.py
@@ -0,0 +1,12 @@
+from .core import (
+    Aliased,
+    AutoSubscriptingClient,
+    Client,
+    GraphQLError,
+    GraphQLEndpointError,
+    InfinityNotSupportedError,
+    Literal,
+    Mutation,
+    Query,
+    Variable,
+)
