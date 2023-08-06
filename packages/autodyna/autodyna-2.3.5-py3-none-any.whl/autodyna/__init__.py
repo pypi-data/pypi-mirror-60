e738ed66a905115a902ccf02ccdde3b8ccda6952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:54 +0000

    Add package autodyna/2.3.5/autodyna-2.3.5-py3-none-any.whl

---
+++ packages/autodyna/autodyna-2.3.5-py3-none-any.whl/autodyna/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Insert relevant path
+initPath = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, initPath)
+
+# Import project libraries
+from . import Beams
