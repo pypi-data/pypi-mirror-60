86d21bd6fb9e7610cb56d526f60c2340c3a5f818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:38 +0000

    Add package li-privacy/1.2.3/li_privacy-1.2.3.tar.gz

---
+++ packages/li-privacy/li_privacy-1.2.3.tar.gz/li_privacy-1.2.3/li_privacy/__init__.py
@@ -0,0 +1,10 @@
+from .DSRProcessor import DSRProcessor
+from .InitProcessor import InitProcessor
+from .DeleteProcessor import DeleteProcessor
+from .OptoutProcessor  import OptoutProcessor
+from .UserFormatError import UserFormatError
+from .Request import Request
+from .ApiClient import ApiClient
+
+from .__version__ import __version__
+
