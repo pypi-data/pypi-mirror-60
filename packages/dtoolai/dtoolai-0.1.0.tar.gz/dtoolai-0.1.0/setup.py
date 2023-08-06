8cf3ffde31d8bd0d114b28f2e2fdb1f22000ce7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:08 +0000

    Add package dtoolai/0.1.0/dtoolai-0.1.0.tar.gz

---
+++ packages/dtoolai/dtoolai-0.1.0.tar.gz/dtoolai-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='dtoolai',
+      version='0.1.0',
+      description='dtool AI utils',
+      url='http://github.com/JIC-CSB/dtoolai',
+      author='Matthew Hartley',
+      author_email='Matthew.Hartley@jic.ac.uk',
+      license='MIT',
+      install_requires=[
+        'dtoolcore',
+        'dtool-http',
+        'click',
+        'pillow',
+        'torch',
+        'torchvision',
+      ],
+      packages=['dtoolai'],
+      entry_points={
+        'console_scripts': [
+          'dtoolai-provenance = dtoolai.utils:print_provenance',
+          'create-image-dataset-from-dirtree = dtoolai.utils:image_dataset_from_dirtree_cli'
+        ]
+      },
+      zip_safe=False)
