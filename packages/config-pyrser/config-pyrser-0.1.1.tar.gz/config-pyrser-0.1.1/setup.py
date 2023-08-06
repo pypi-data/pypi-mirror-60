d6ae001a7fa4dec9ea2dae339d2ae5bc2c5f2ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:35 +0000

    Add package config-pyrser/0.1.1/config-pyrser-0.1.1.tar.gz

---
+++ packages/config-pyrser/config-pyrser-0.1.1.tar.gz/config-pyrser-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['config_pyrser']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'config-pyrser',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'wadamek65',
+    'author_email': 'wadamek65@gmail.com',
+    'maintainer': 'wadamek65',
+    'maintainer_email': 'wadamek65@gmail.com',
+    'url': 'https://github.com/wadamek65/config-pyrser',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
