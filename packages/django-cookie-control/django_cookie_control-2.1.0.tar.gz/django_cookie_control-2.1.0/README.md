4a9a2be64349047252f3608080d9a7066680ed1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:16 +0000

    Add package django-cookie-control/2.1.0/django_cookie_control-2.1.0.tar.gz

---
+++ packages/django-cookie-control/django_cookie_control-2.1.0.tar.gz/django_cookie_control-2.1.0/README.md
@@ -0,0 +1,89 @@
+# django-cookie-control
+A Django plugin to integrate the Civic UK Cookie Control (https://www.civicuk.com/cookie-control)
+
+The plugin is compatible with Django, DjangoCMS and Django/CMS multisite.
+
+## To install
+Install the application via pip:
+
+`pip install django-cookie-control`
+
+Add 'django_cookie_control' to installed apps:
+
+`INSTALLED_APPS = [
+...
+'django_cookie_control',
+...]`
+
+Add/Edit the following to `settings.py` if required with required cookies:
+```
+# DEFAULT if missing:
+COOKIE_CONTROL_REQUIRED_COOKIES = ['csrftoken','django_language','sessionid','django_privacy_mgmt_preferences','AWSALB']
+```
+
+Run or make migrations:
+
+`python.exe manage.py migrate`
+
+## To configure
+
+For a full list of configuration settings see the Civic UK site, here:
+
+https://www.civicuk.com/cookie-control/v8/documentation
+
+Plugin configuration overview:
+
+1. Add a new configuration - custom configuration for different sites via multisite.
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/add_control.jpg)
+
+
+2. Configure Main Settings
+- Site name
+- API key
+- Cookie Control product version: Community, Pro, Pro Multisite
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/main_settings.jpg)
+
+3. Display Options
+- Position
+- Theme style
+- Toggle type
+- Close style
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/display_options.jpg)
+
+
+4. Text and Accessibility
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/configure_accessibility.jpg)
+
+
+5. Custom Settings
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/custom_settings.jpg)
+
+
+6. Pro and Pro_Multisite Settings
+A series of settings for the paid Pro plugins, including:
+- Custom text
+- Custom formatting
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/pro_settings_only.jpg)
+
+
+7. Cookies
+
+* Necessary Cookies
+  * A simple list of cookie names, including wildcards that are necessary for the site
+  * A default list for Django, DjangoCMS, and AWS are already included.
+* Optional Cookies
+  * Add an optional cookie or package of cookies that can be turned off for the site
+  * Using separate functions On Accept or for On Revoke the cookies can be customised to be disable or enabled.
+
+![](https://raw.githubusercontent.com/mcldev/django-cookie-control/master/docs/images/optional_cookie_control.jpg)
+
+
+## To Test
+Use IE11's emulation to test this tool -
+
---
+++ packages/django-cookie-control/django_cookie_control-2.1.0.tar.gz/django_cookie_control-2.1.0/django_cookie_control/admin.py
@@ -0,0 +1,136 @@
+from django.contrib import admin
+from parler.admin import TranslatableAdmin
+from parler.forms import TranslatableModelForm
+
+from django_cookie_control.models import *
+from django_cookie_control import cache
+
+
+# Standard Models
+# -----------------
+class CookiesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Cookie, CookiesAdmin)
+
+
+class ThirdPartyCookiesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ThirdPartyCookie, ThirdPartyCookiesAdmin)
+
+
+class CallbackFunctionsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CallbackFunction, CallbackFunctionsAdmin)
+
+
+class AccessibilityValuesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Accessibility, AccessibilityValuesAdmin)
+
+
+class BrandingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Branding, BrandingAdmin)
+
+
+# Translated Models
+# -----------------
+
+class StatementAdmin(TranslatableAdmin):
+    form = TranslatableModelForm
+    fieldsets = (
+        (None, {
+           'fields': ('name', 'url', 'updated', 'description')
+        }),
+    )
+admin.site.register(Statement, StatementAdmin)
+
+
+class TextValuesAdmin(TranslatableAdmin):
+    form = TranslatableModelForm
+
+admin.site.register(TextValue, TextValuesAdmin)
+
+
+class PurposeObjectAdmin(TranslatableAdmin):
+    form = TranslatableModelForm
+    verbose_name = 'Optional Cookie Control'
+    verbose_name_plural = 'Optional Cookie Controls'
+    fieldsets = (
+        (None, {
+           'fields': ('name', 'label', 'description')
+        }),
+        ('Cookies', {
+            'fields': ('cookies', 'thirdPartyCookies', 'recommendedState', 'lawfulBasis')
+        }),
+        ('Callback Functions', {
+            'fields': ('onAccept', 'onRevoke')
+        }),
+    )
+
+admin.site.register(PurposeObject, PurposeObjectAdmin)
+
+
+# Optional Cookie Inline.. maybe
+class OptionalCookieInline(admin.TabularInline):
+    model = CookieControl.optionalCookies.through
+    verbose_name = 'Optional Cookie'
+    verbose_name_plural = 'Optional Cookies'
+
+
+class OptionalCookieAdmin(admin.ModelAdmin):
+    inlines = [
+        OptionalCookieInline,
+    ]
+    verbose_name = 'Optional Cookie'
+    verbose_name_plural = 'Optional Cookies'
+
+
+
+# Main Cookie Control Model
+# -----------------
+class CookieControlAdmin(admin.ModelAdmin):
+    inlines = [
+        OptionalCookieInline,
+    ]
+    exclude = [
+        'optionalCookies',
+    ]
+    fieldsets = (
+        (None, {
+           'fields': ('site', 'name', 'apiKey', 'product')
+        }),
+        ('Display Options', {
+            'classes': ('collapse',),
+            'fields': ('position', 'theme', 'toggleType', 'closeStyle'),
+        }),
+        ('Text and Accessibility', {
+            'classes': ('collapse',),
+            'fields': ('statement', 'text', 'accessibility'),
+        }),
+        ('Custom Settings', {
+            'classes': ('collapse',),
+            'fields': ('consentCookieExpiry', 'logConsent', 'notifyOnce', 'onLoad'),
+        }),
+        ('Pro & Pro_Multisite Settings Only', {
+            'classes': ('collapse',),
+            'fields': ('initialState', 'layout', 'excludedCountries', 'branding'),
+        }),
+        ('Cookies', {
+            'fields': ('necessaryCookies', ),
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        super(CookieControlAdmin, self).save_model(request, obj, form, change)
+        cache.delete(obj.site.id)
+
+admin.site.register(CookieControl, CookieControlAdmin)
+
+
+
