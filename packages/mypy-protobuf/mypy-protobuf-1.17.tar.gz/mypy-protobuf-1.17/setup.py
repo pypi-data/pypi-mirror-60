f490a98b05e185a664179fe606765ae0c82d4316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:49 +0000

    Add package mypy-protobuf/1.17/mypy-protobuf-1.17.tar.gz

---
+++ packages/mypy-protobuf/mypy-protobuf-1.17.tar.gz/mypy-protobuf-1.17/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='mypy-protobuf',
+    version='1.17',
+    description='Generate mypy stub files from protobuf specs',
+    keywords='mypy proto dropbox',
+    license='Apache License 2.0',
+    author='Nipunn Koorapati',
+    author_email='nipunn@dropbox.com',
+    py_modules=[],
+    url='https://github.com/dropbox/mypy-protobuf',
+    download_url='https://github.com/dropbox/mypy-protobuf/archive/v1.17.tar.gz',
+    install_requires=['protobuf>=3.6.0'],
+
+    scripts=['protoc-gen-mypy', 'protoc_gen_mypy.bat'],
+)
