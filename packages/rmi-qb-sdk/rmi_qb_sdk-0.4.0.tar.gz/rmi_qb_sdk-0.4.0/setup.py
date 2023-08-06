6cf1197ad889ef5c59052ff82683dae4c69e487d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:50 +0000

    Add package rmi-qb-sdk/0.4.0/rmi_qb_sdk-0.4.0.tar.gz

---
+++ packages/rmi-qb-sdk/rmi_qb_sdk-0.4.0.tar.gz/rmi_qb_sdk-0.4.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='rmi_qb_sdk',
+      version='0.4.0',
+      description='Reading & Math Inc Python SDK for Quick Base',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/ReadingandMath/Quickbase-Python-SDK',
+      author='Kevin Connor',
+      author_email='kevin.connor@servetogrow.org',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+      ],
+)
