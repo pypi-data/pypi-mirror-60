75aceba30cc5828dadf496f7fc53323b7fd177ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:40 +0000

    Add package pip-outdated/0.4.0/pip_outdated-0.4.0-py3-none-any.whl

---
+++ packages/pip-outdated/pip_outdated-0.4.0-py3-none-any.whl/pip_outdated/check_outdated.py
@@ -0,0 +1,56 @@
+from packaging.utils import canonicalize_name
+from packaging.version import parse as parse_version
+from pkg_resources import get_distribution, DistributionNotFound
+
+from .verbose import verbose
+
+class OutdateResult:
+    def __init__(self, requirement, version, all_versions):
+        self.requirement = requirement
+        self.name = requirement.name
+        self.version = version
+        self.wanted = None
+        self.latest = None
+        
+        if all_versions:
+            try:
+                self.wanted = next(v for v in reversed(all_versions)
+                                   if v in requirement.specifier)
+            except StopIteration:
+                pass
+            self.latest = all_versions[-1]
+        
+    def install_not_found(self):
+        return self.version is None
+        
+    def install_not_wanted(self):
+        return self.version not in self.requirement.specifier
+        
+    def pypi_not_found(self):
+        return self.latest is None
+        
+    def outdated(self):
+        return self.version != self.wanted or self.version != self.latest
+
+def get_current_version(name):
+    try:
+        return parse_version(get_distribution(name).version)
+    except DistributionNotFound:
+        pass
+        
+async def get_pypi_versions(name, session):
+    async with session.get(f"https://pypi.org/pypi/{name}/json") as r:
+        if r.status != 200:
+            return None
+        keys = [parse_version(v) for v in (await r.json())["releases"].keys()]
+        keys = [v for v in keys if not v.is_prerelease]
+        keys.sort()
+        return keys
+    
+async def check_outdated(require, session):
+    if verbose():
+        print(f"Checking: {require.name} {require.specifier}")
+    name = canonicalize_name(require.name)
+    current_version = get_current_version(name)
+    pypi_versions = await get_pypi_versions(name, session)
+    return OutdateResult(require, current_version, pypi_versions)
---
+++ packages/pip-outdated/pip_outdated-0.4.0-py3-none-any.whl/pip_outdated/print_outdated.py
@@ -0,0 +1,60 @@
+import sys
+import colorama
+from termcolor import colored
+from terminaltables import AsciiTable as Table
+
+def make_row(outdate):
+    if not outdate.outdated():
+        return None
+        
+    def colored_current():
+        if outdate.install_not_found() or outdate.install_not_wanted():
+            return colored(str(outdate.version), "red", attrs=["bold"])
+        return str(outdate.version)
+        
+    def colored_wanted():
+        if outdate.pypi_not_found() or not outdate.wanted:
+            return colored("None", "red", attrs=["bold"])
+        if not outdate.install_not_found() and outdate.version < outdate.wanted:
+            return colored(str(outdate.wanted), "green", attrs=["bold"])
+        return str(outdate.wanted)
+        
+    def colored_latest():
+        if outdate.pypi_not_found():
+            return colored("None", "red", attrs=["bold"])
+        if not outdate.install_not_found() and outdate.version < outdate.latest:
+            return colored(str(outdate.latest), "green", attrs=["bold"])
+        return str(outdate.latest)
+    
+    return [
+        outdate.name,
+        colored_current(),
+        colored_wanted(),
+        colored_latest()
+    ]
+    
+async def print_outdated(outdates, quiet: bool):
+    colorama.init()
+    
+    data = [["Name", "Installed", "Wanted", "Latest"]]
+    count = 0
+    for count, outdate in enumerate(outdates, 1):
+        row = make_row(await outdate)
+        if row:
+            data.append(row)
+            
+    if not count:
+        print(colored("No requirements found.", "red"))
+        return
+        
+    if len(data) == 1:
+        print(colored("Everything is up-to-date!", "cyan", attrs=["bold"]))
+        return
+        
+    print(colored("Red = unavailable/outdated/out of version specifier", "red", attrs=["bold"]))
+    print(colored("Green = updatable", "green", attrs=["bold"]))
+    table = Table(data)
+    print(table.table)
+    if not quiet:
+        sys.exit(1)
+    
