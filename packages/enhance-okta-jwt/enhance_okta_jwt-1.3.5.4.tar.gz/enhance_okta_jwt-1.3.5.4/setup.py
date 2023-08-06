7b28b227b9610c44e5239acd654b75e3fe7bd0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:57 +0000

    Add package enhance-okta-jwt/1.3.5.4/enhance_okta_jwt-1.3.5.4.tar.gz

---
+++ packages/enhance-okta-jwt/enhance_okta_jwt-1.3.5.4.tar.gz/enhance_okta_jwt-1.3.5.4/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import okta_jwt
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    long_description = readme.read()
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, '__init__.py'))
+    ]
+
+
+extras_require = {}
+
+setup(
+    name="enhance_okta_jwt",
+    version=okta_jwt.__version__,
+    author="Hua Luo fork and update",
+    author_email="luohuazju@gmail.com",
+    description="Okta JWT Access Token verifier, fork and update",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luohuazju/okta_jwt",
+    packages=get_packages('okta_jwt'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities'
+    ],
+    extras_require=extras_require,
+    tests_require=['mock', 'ddt'],
+    install_requires=['six <2.0', 'requests', 'python-jose']
+)
