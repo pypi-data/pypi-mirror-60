980a667d2c57eb1956fe8c1bd48c33799562c79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:42 +0000

    Add package deep-talk/0.1.4/deep_talk-0.1.4.tar.gz

---
+++ packages/deep-talk/deep_talk-0.1.4.tar.gz/deep_talk-0.1.4/setup.py
@@ -0,0 +1,26 @@
+import subprocess
+from sys import platform
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+with open("README.md","r") as f:
+    long_description = f.read()
+
+version = "0.1.4"
+setup(name='deep_talk',
+  version=version,
+  description='DeepTalk: Python and Prolog based dialogue agent',
+  long_description = long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/ptarau/DeepTalk.git',
+  author='Paul Tarau, Andrea Cortis',
+  author_USER_EMAIL='<paul.tarau@gmail.com>; andrea.cortis@gmail.com>',
+  license='Apache',
+  packages=['deep_talk'],
+  package_data={'deep_talk': ['*.pro']},
+  include_package_data=True,
+  install_requires = required,
+  zip_safe=False
+  )
