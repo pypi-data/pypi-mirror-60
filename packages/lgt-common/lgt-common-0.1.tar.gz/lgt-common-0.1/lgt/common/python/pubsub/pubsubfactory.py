6a595ca8f45b1beca13af5bec6896ffb21467c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:00 +0000

    Add package lgt-common/0.1/lgt-common-0.1.tar.gz

---
+++ packages/lgt-common/lgt-common-0.1.tar.gz/lgt-common-0.1/lgt/common/python/pubsub/pubsubfactory.py
@@ -0,0 +1,54 @@
+import time
+import env
+
+from google.cloud import pubsub_v1
+from google.api_core.exceptions import GoogleAPICallError
+from google.auth.transport.grpc import *
+from google.auth import jwt
+
+
+class PubSubFactory:
+
+    __cred: jwt.Credentials
+    __delay: 3
+
+    def __init__(self):
+        self.cred = jwt.Credentials.from_service_account_file(env.google_application_credential)
+
+    def create_publisher(self):
+        return pubsub_v1.PublisherClient(credentials=self.cred)
+
+    def create_subscriber(self):
+        return pubsub_v1.SubscriberClient(credentials=self.cred)
+
+    def get_topic_path(self, project_id, topic_name):
+        topic = self.create_topic_if_doesnt_exist(topic_name)
+        return pubsub_v1.PublisherClient.api.topic_path(project_id, topic)
+
+    def get_subscription_path(self, project_id, subscriber_name, topic_name):
+        subscription = self.create_subscription_if_doesnt_exist(subscriber_name, topic_name)
+        subscriber = self.create_subscriber()
+        return subscriber.api.subscription_path(project_id, subscription)
+
+    def create_topic_if_doesnt_exist(self, topic_name):
+        time.sleep(self.__delay)
+        publisher = pubsub_v1.PublisherClient(credentials=self.cred)
+        try:
+            return publisher.api.get_topic(topic_name)
+        except GoogleAPICallError as ex:
+            if ex.grpc_status_code == grpc.StatusCode.NOT_FOUND:
+                return publisher.api.create_topic(name=topic_name)
+            else:
+                raise
+
+    def create_subscription_if_doesnt_exist(self, subscriber_name, topic_name):
+        time.sleep(self.__delay)
+        subscriber = pubsub_v1.SubscriberClient(credentials=self.cred)
+        try:
+            return subscriber.api.get_subscription(subscriber_name)
+        except GoogleAPICallError as ex:
+            if ex.grpc_status_code == grpc.StatusCode.NOT_FOUND:
+                return subscriber.api.create_subscription(name=subscriber_name, topic=topic_name,
+                                                          push_config=None, ack_deadline_seconds=60)
+            else:
+                raise
