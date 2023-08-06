3f2e9f4bdbd236ad7a9a8d076f10b64118233983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:15 +0000

    Add package strict_functions/2020.2.4/strict_functions-2020.2.4.tar.gz

---
+++ packages/strict_functions/strict_functions-2020.2.4.tar.gz/strict_functions-2020.2.4/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-11-09 11:50:41
+# @Last Modified 2018-03-15
+# @Last Modified time: 2019-09-02 12:41:32
+
+from distutils.core import setup
+
+version = '2020.2.4'
+
+setup(
+  name = 'strict_functions',
+  packages = ['strict_functions'], # this must be the same as the name above
+  version = version,
+  description = 'Decorators for function scope control, overloading, type safety, thread safety, cache control, tracing and even self awareness!',
+  author = 'Cody Kochmann',
+  author_email = 'kochmanncody@gmail.com',
+  url = 'https://github.com/CodyKochmann/strict_functions',
+  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/{}'.format(version),
+  keywords = ['strict_functions', 'scope', 'overload', 'overloading', 'type', 'enforce', 'strict', 'restrict', 'strict defaults', 'defaults'],
+  classifiers = [],
+)
