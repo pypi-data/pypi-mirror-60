8d55a87203af97dda21aac9809cc650d868ad1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package Trolls/0.3.1/Trolls-0.3.1-py36-none-any.whl

---
+++ packages/Trolls/Trolls-0.3.1-py36-none-any.whl/trolls/wrappers/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "Christian Heider Nielsen"
+
+from .buffered import *
+from .clipped import *
+from .noisy import *
+from .skip import *
+from .normalised import *
+from .bonus import *
+from .vector_environments import *
