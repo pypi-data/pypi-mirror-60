a874a4b70177cd418c24473bf25cbd4018c929ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:52 +0000

    Add package elasticsearch-follow/0.1.3/elasticsearch_follow-0.1.3-py3-none-any.whl

---
+++ packages/elasticsearch-follow/elasticsearch_follow-0.1.3-py3-none-any.whl/elasticsearch_follow/follower.py
@@ -0,0 +1,24 @@
+import datetime
+
+from dateutil import tz
+
+
+class Follower:
+    def __init__(self, elasticsearch_follow, index, time_delta=60, processor=None):
+        self.elasticsearch_follow = elasticsearch_follow
+        self.index = index
+        self.time_delta = time_delta
+        self.processor = processor
+
+    def generator(self):
+        now = datetime.datetime.utcnow()
+        now = now.replace(tzinfo=tz.UTC)
+        delta = datetime.timedelta(seconds=self.time_delta)
+
+        for line in self.elasticsearch_follow.get_new_lines(self.index, now - delta):
+            self.elasticsearch_follow.prune_before(now - delta)
+
+            if self.processor:
+                yield self.processor.process_line(line)
+            else:
+                yield line
