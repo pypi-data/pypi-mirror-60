08b50b969260f124270de4fd7f26d47ec06f4ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:46 +0000

    Add package python-duplicate/1.0.3/python-duplicate-1.0.3.tar.gz

---
+++ packages/python-duplicate/python-duplicate-1.0.3.tar.gz/python-duplicate-1.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('PYPI_README.md') as file:
+    long_description = file.read()
+
+setup(
+    name="python-duplicate",
+    version="1.0.3",
+    url="https://github.com/Clement-O/python-duplicate",
+    license="MIT",
+    author="Clément Omont--Agnes",
+    author_email="omont.clement@gmail.com",
+    description="Handle duplicate in python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    zip_safe=False,
+    install_requires=["psycopg2-binary", "PyMySQL"],
+    project_urls={
+        "Documentation": "https://python-duplicate.readthedocs.io/en/latest/",
+        "Source": "https://github.com/Clement-O/python-duplicate",
+    },
+)
