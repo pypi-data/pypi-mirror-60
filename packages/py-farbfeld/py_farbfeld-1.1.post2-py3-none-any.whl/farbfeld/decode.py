4d16d15a8586713df5dbcbd55d0bcf2179ba7c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:50 +0000

    Add package py-farbfeld/1.1.post2/py_farbfeld-1.1.post2-py3-none-any.whl

---
+++ packages/py-farbfeld/py_farbfeld-1.1.post2-py3-none-any.whl/farbfeld/decode.py
@@ -0,0 +1,33 @@
+from struct import unpack, Struct
+
+class FarbfeldDecodeError(Exception):
+    pass
+
+class FarbfeldDecoder:
+    "Decoder to decode the Farbfeld image format into 16-bit pixels."
+
+    def __init__(self, infile):
+        """
+        infile
+            a file-like object that must be readable via
+            the `read()` call and should return bytes.
+        """
+        if hasattr(infile, 'read'):
+            self._infile = infile
+        else:
+            raise FarbfeldDecodeError("file-like object doesn't support read() calls")
+
+        if infile.read(8) != b'farbfeld':
+            raise FarbfeldDecodeError("invalid signature found while parsing")
+
+        (self.width, self.height) = unpack("<II", infile.read(8))
+        
+        self._row_unpacker = Struct(f'<{self.width * 4}H')
+
+    def decode(self) -> map:
+        """
+        Creates a iterable 2D object representing an image frame
+        (row-major), pixels are RGBA, bitdepth is 16-bit.
+        """
+        return map(lambda row: self._row_unpacker.unpack(row)[0],
+                    iter(lambda: self._infile.read(self.width * 4), b''))
---
+++ packages/py-farbfeld/py_farbfeld-1.1.post2-py3-none-any.whl/farbfeld/encode.py
@@ -0,0 +1,49 @@
+from struct import pack, Struct
+from itertools import starmap
+from functools import reduce
+
+class FarbfeldEncodeError(Exception):
+    pass
+
+class FarbfeldEncoder:
+    "Encoder to encode 16-bit pixels into the Farbfeld image format."
+    
+    def __init__(self, width, height):
+        self.width = abs(width)
+        self.height = abs(height)
+
+        self._rowutil = Struct(f'<{width * 4}H')
+
+    def encode(self, outfile, imageframe):
+        """
+        outfile
+            a file-like object that supports write() calls and
+            support bytes as the first argument.
+
+        imageframe
+            an 2D iterable object representing an image frame
+            (row-major), pixels are RGBA, bitdepth is 16-bit.
+        """
+        if not hasattr(outfile, 'write'):
+            raise FarbfeldEncodeError("file-like object doesn't support write() calls")
+
+        try:
+            height = len(imageframe)
+            width = int(len(imageframe[0]) / 4)
+        except (IndexError, TypeError):
+            raise FarbfeldEncodeError("layout of pixels is non-standard")
+
+        if width % 4 != 0:
+            raise FarbfeldEncodeError("pixelformat isn't RGBA")
+
+        # slice upto the limit if slice_frame is supplied.
+        if self.width > width or self.height > height:
+            imageframe = imageframe[:height, :width * 4]
+        
+        elif self.width < width or self.height < height:
+            raise FarbfeldEncodeError(f"pixels supplied is lesser than the amount of pixels specified")
+
+        outfile.write(b'farbfeld' + pack('<II', width, height))
+        # writes image data row by row to the output file.
+        starmap(outfile.write, map(lambda row: self._rowutil.pack(row), imageframe))
+        outfile.flush()
