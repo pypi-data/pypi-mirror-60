c2806243dae7c0d25adf454854b4320d3736c6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:48 +0000

    Add package nflyway/0.0.2/nflyway-0.0.2.tar.gz

---
+++ packages/nflyway/nflyway-0.0.2.tar.gz/nflyway-0.0.2/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*_
+# Author: nie<bellondragon@gmail.com>
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='nflyway',
+    version='0.0.2',
+    description='this package is used to migrate db in project',
+    author='nie',
+    author_email='bellondragon@gmail.com',
+    url='',
+    packages=setuptools.find_packages(exclude=['test', 'examples', 'script', 'tutorials']),
+
+    install_requires=[
+        'os',
+        'hashlib',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=True,
+)
