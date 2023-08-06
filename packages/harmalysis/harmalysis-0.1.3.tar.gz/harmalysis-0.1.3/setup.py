c3651cd684eed50428376f957490a889e1b9c88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:44 +0000

    Add package harmalysis/0.1.3/harmalysis-0.1.3.tar.gz

---
+++ packages/harmalysis/harmalysis-0.1.3.tar.gz/harmalysis-0.1.3/setup.py
@@ -0,0 +1,44 @@
+'''
+    harmalysis - a language for harmonic analysis and roman numerals
+    Copyright (C) 2020  Nestor Napoles Lopez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="harmalysis", # Replace with your own username
+    version="0.1.3",
+    author="Nestor Napoles Lopez",
+    author_email="napulen@gmail.com",
+    description="A language for harmonic analysis and roman numerals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/napulen/harmalysis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_data={'': ["*.lark"]},
+    install_requires=[
+          'lark-parser',
+    ],
+    python_requires='>=3.6',
+)
