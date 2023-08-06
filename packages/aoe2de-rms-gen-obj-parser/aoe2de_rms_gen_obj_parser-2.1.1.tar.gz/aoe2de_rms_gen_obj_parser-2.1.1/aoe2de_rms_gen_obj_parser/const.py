027e182eed71b070188f97bb7b581692079dd90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:23 +0000

    Add package aoe2de-rms-gen-obj-parser/2.1.1/aoe2de_rms_gen_obj_parser-2.1.1.tar.gz

---
+++ packages/aoe2de-rms-gen-obj-parser/aoe2de_rms_gen_obj_parser-2.1.1.tar.gz/aoe2de_rms_gen_obj_parser-2.1.1/aoe2de_rms_gen_obj_parser/const.py
@@ -0,0 +1,30 @@
+def get_dict_first_key(dictionary):
+    return next(iter(dictionary))
+
+
+# ### MAP SIZE
+MAP_SIZE_DICT = {
+    "TINY_MAP": "Tiny map",
+    "SMALL_MAP": "Small map",
+    "MEDIUM_MAP": "Medium map",
+    "LARGE_MAP": "Large map",
+    "HUGE_MAP": "Huge map",
+    "GIGANTIC_MAP": "Gigantic map",
+    "LUDIKRIS_MAP": "Ludikris map"
+}
+
+# ### MAP RESOURCES
+MAP_RESOURCES_DICT = {
+    "OTHER_RESOURCES": "Other",
+    "INFINITE_RESOURCES": "Infinite"
+}
+
+# ### MAP TYPES
+GAME_TYPE_DICT = {
+    "OTHER_GAME": "Other",
+    "KING_OT_HILL": "King of the Hill",
+    "EMPIRE_WARS": "Empire Wars",
+    "BATTLE_ROYALE": "Battle Royal",
+    "REGICIDE": "Regicide",
+    "DEATH_MATCH": "Death match"
+}
