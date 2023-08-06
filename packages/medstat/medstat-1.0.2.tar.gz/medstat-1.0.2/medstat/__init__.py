6761ce6c2e6b0e9fbd07f6834d140d7eeeffcd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:01 +0000

    Add package medstat/1.0.2/medstat-1.0.2.tar.gz

---
+++ packages/medstat/medstat-1.0.2.tar.gz/medstat-1.0.2/medstat/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for medstat."""
+
+__author__ = """Pierre Monferrand"""
+__version__ = '1.0.2'
+
+from medstat.medstat import test_hypothesis, analyse_dataset
+
+
+__all__ = ['test_hypothesis', 'analyse_dataset']
