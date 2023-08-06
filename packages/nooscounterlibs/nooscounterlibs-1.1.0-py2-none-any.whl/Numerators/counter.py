00e900bb59db0974e4cd1fdda0c431ee8ee48762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:14 +0000

    Add package nooscounterlibs/1.1.0/nooscounterlibs-1.1.0-py2-none-any.whl

---
+++ packages/nooscounterlibs/nooscounterlibs-1.1.0-py2-none-any.whl/Numerators/counter.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class Numerator(object):
+    """ Noos Sistem Numerator API arayüzü """
+
+    def __init__(self, api_code, api_path):
+        self.api_code = api_code
+        self.api_path = api_path
+        self._api_url = "https://{}.execute-api.eu-central-1.amazonaws.com/Prod/{}".format(self.api_code, self.api_path)
+
+    def counter(self, numerator_code):
+        """
+        Yeni bir numara çeker
+        :param numerator_code: Counter Kodu
+        :return: {"counter":..,"format"}
+        """
+        response = requests.post(self._api_url, json={"op": "counter", "numerator": numerator_code})
+        if response.status_code == 200:
+            response_data = response.json()
+            if response_data.get("complete"):
+                return {
+                    "counter": response_data.get("numerator_counter"),
+                    "format": response_data.get("numeretor_format")
+                }
+
+        return None
