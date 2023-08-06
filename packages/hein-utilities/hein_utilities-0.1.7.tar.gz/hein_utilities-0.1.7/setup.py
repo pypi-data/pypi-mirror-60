7b61f2a1f3602baa8cfa5dfccde3c7abd9793069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:17 +0000

    Add package hein-utilities/0.1.7/hein_utilities-0.1.7.tar.gz

---
+++ packages/hein-utilities/hein_utilities-0.1.7.tar.gz/hein_utilities-0.1.7/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+NAME = 'hein_utilities'
+VERSION = '0.1.7'
+AUTHOR = 'Lars Yunker / Hein Group'
+
+PACKAGES = find_packages()
+# KEYWORDS = ', '.join([
+# ])
+
+with open('LICENSE') as f:
+    lic = f.read()
+    lic.replace('\n', ' ')
+
+# with open('README.MD') as f:
+#     long_description = f.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='Methods and Classes used in a variety of Hein projects',
+    # long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    url='https://gitlab.com/heingroup/hein_utilities',
+    packages=PACKAGES,
+    license=lic,
+    python_requires='>=3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Operating System :: Microsoft :: Windows',
+        'Natural Language :: English'
+    ],
+    # keywords=KEYWORDS,
+    install_requires=[
+        'numpy',
+        'slackclient'
+    ],
+)
