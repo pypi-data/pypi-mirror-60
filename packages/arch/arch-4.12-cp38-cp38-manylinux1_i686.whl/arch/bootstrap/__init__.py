cf99cf2d81901537340a11006c08906150106523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:50 +0000

    Add package arch/4.12/arch-4.12-cp38-cp38-manylinux1_i686.whl

---
+++ packages/arch/arch-4.12-cp38-cp38-manylinux1_i686.whl/arch/bootstrap/__init__.py
@@ -0,0 +1,35 @@
+"""
+Tools for implementing statistical bootstraps
+"""
+from arch.bootstrap import _samplers_python
+from arch.bootstrap.base import (
+    CircularBlockBootstrap,
+    IIDBootstrap,
+    IndependentSamplesBootstrap,
+    MovingBlockBootstrap,
+    StationaryBootstrap,
+)
+from arch.bootstrap.multiple_comparison import MCS, SPA, RealityCheck, StepM
+
+COMPILED_SAMPLERS = True
+try:
+    from arch.bootstrap import _samplers
+except ImportError:
+    COMPILED_SAMPLERS = False
+
+
+__all__ = [
+    "IIDBootstrap",
+    "CircularBlockBootstrap",
+    "MovingBlockBootstrap",
+    "StationaryBootstrap",
+    "IndependentSamplesBootstrap",
+    "SPA",
+    "RealityCheck",
+    "StepM",
+    "MCS",
+    "_samplers_python",
+]
+
+if COMPILED_SAMPLERS:
+    __all__ += ["_samplers"]
