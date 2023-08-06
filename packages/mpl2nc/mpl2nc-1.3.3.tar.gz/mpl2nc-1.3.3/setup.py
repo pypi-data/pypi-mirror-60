e4ffb9ed0704db85d4944397b94d90fea5e8d540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package mpl2nc/1.3.3/mpl2nc-1.3.3.tar.gz

---
+++ packages/mpl2nc/mpl2nc-1.3.3.tar.gz/mpl2nc-1.3.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mpl2nc',
+    version='1.3.3',
+    description='Convert Sigma Space Micro Pulse Lidar (MPL) data files to NetCDF',
+    author='Peter Kuma',
+    author_email='peter.kuma@fastmail.com',
+    license='MIT',
+    scripts=['mpl2nc'],
+    data_files=[('share/man/man1', ['mpl2nc.1'])],
+    install_requires=['netCDF4>=1.2.9'],
+    keywords=['sigmaspace', 'mpl', 'lidar', 'netcdf'],
+    url='https://github.com/peterkuma/mpl2nc',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+    ]
+)
