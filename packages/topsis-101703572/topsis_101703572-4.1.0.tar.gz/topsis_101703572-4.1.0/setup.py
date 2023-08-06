d697ba7c0bec90ee7dcb9eb86cbe6c0baf7d9af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:42 +0000

    Add package topsis-101703572/4.1.0/topsis_101703572-4.1.0.tar.gz

---
+++ packages/topsis-101703572/topsis_101703572-4.1.0.tar.gz/topsis_101703572-4.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="topsis_101703572",
+    version="4.1.0",
+    description="Python package to implement TOPSIS",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sunvir72/Topsis",
+    author="Sunvir Singh",
+    author_email="sunvirsingh72@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis_101703572"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "topsis-101703572=topsis_101703572.__init__:main",
+        ]
+    },
+)
