bb97296be92cce63769c14b48d7eebe07f4b8be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:43 +0000

    Add package NlpToolkit-ParseTree/1.0.0/NlpToolkit-ParseTree-1.0.0.tar.gz

---
+++ packages/NlpToolkit-ParseTree/NlpToolkit-ParseTree-1.0.0.tar.gz/NlpToolkit-ParseTree-1.0.0/ParseTree/NodeCondition/NodeCondition.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod
+
+from ParseTree.ParseNode import ParseNode
+
+
+class NodeCondition:
+
+
+    @abstractmethod
+    def satisfies(self, parseNode: ParseNode) -> bool:
+        pass
