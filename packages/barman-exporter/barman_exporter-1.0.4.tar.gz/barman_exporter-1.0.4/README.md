10b7ff5920e61ff156cea600967f0ad698e2539c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:17 +0000

    Add package barman-exporter/1.0.4/barman_exporter-1.0.4.tar.gz

---
+++ packages/barman-exporter/barman_exporter-1.0.4.tar.gz/barman_exporter-1.0.4/README.md
@@ -0,0 +1,152 @@
+# Barman exporter for Prometheus
+
+`barman-exporter` runs `barman` shell command with _experimental_ JSON output I added to Barman 2.9. JSON output may change in the future and break some of functionalities in the exporter.
+
+By default barman exporter outputs metrics to stdout. If everything seems right you want to save it as textfile with `-f /var/lib/prometheus/node_exporter/barman.prom` and set up `node_exporter` to read from this path (`--collector.textfile.directory` option).
+
+## Grafana dashboard
+
+You can find basic grafana dashboard in `grafana-dashboard.json`. It is open for improvements.
+
+![Grafana screenshot](grafana-screenshot.png?raw=true "Grafana screenshot")
+
+## Usage
+
+```
+usage: barman-exporter [-h] [-f TEXTFILE_PATH] [-u USER] [-g GROUP]
+                          [-m MODE]
+                          [servers [servers ...]]
+
+Barman exporter
+
+positional arguments:
+  servers               Space separated list of servers to check (default:
+                        ['all'])
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f TEXTFILE_PATH, --file TEXTFILE_PATH
+                        Save output to textfile (default: None)
+  -u USER, --user USER  Textfile owner (default: prometheus)
+  -g GROUP, --group GROUP
+                        Textfile group (default: prometheus)
+  -m MODE, --mode MODE  Textfile mode (default: 0644)
+```
+
+For example:
+
+- `$ barman-exporter postgres-01`
+- `$ barman-exporter postgres-01 postgres-02`
+- `$ barman-exporter all`
+- `$ barman-exporter -f /var/lib/prometheus/node_exporter/barman.prom -u prometheus -g prometheus -m 0644 all`
+
+## Requirements
+
+You need Python3 along with following modules:
+
+- prometheus_client
+- sh
+
+All dependencies will be installed with pip command (see Installation).
+
+## Installation
+
+```
+pip3 install barman-exporter
+```
+
+### Cron job to run barman-exporter
+
+Set up cron job to run every hour:
+
+```
+0 * * * * root barman-exporter -f /var/lib/prometheus/node_exporter/barman.prom
+```
+
+## Prometheus configuration
+
+Please note that backup listing is rather heavy IO operation and can take a while. **Definitely do not run barman exporter every minute**.
+
+Barman exporter does not require any Prometheus configuration because it uses **node-exporter** to get metrics from a textfile. Remember to use `--collector.textfile.directory` in node-exporter to point a directory with textfiles.
+
+## Metrics
+
+The label `number=1` determines the newest backup.
+
+The metrics `barman_bacukps_size` and `barman_backups_wal_size` show only successful backups. Failed backups will not be listed here.
+
+The metric `barman_backups_total` includes failed backups. A number of failed backups is exposed in `barman_backups_failed`. `barman_last_backup_copy_time` shows how long did it take to make the latest backup.
+
+The metric `barman_up` shows checks as in command `barman check SERVER_NAME`. Output `OK` is `1.0`, `FAILED` is `0.0`.
+
+By using timestamps from metrics `barman_last_backup` and `barman_first_backup`, you can easily calculate how long ago backup completed:
+
+`time() - barman_last_backup{instance="$instance", server="$server"}`
+
+### Raw metrics
+
+```
+# HELP barman_backups_size Size of available backups
+# TYPE barman_backups_size gauge
+barman_backups_size{number="1",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="2",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="3",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="4",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="5",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="6",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="7",server="postgres-01"} 1.429365116108e+012
+barman_backups_size{number="8",server="postgres-01"} 1.429365116108e+012
+
+# HELP barman_backups_wal_size WAL size of available backups
+# TYPE barman_backups_wal_size gauge
+barman_backups_wal_size{number="1",server="postgres-01"} 1.94347270144e+011
+barman_backups_wal_size{number="2",server="postgres-01"} 3.06553290752e+011
+barman_backups_wal_size{number="3",server="postgres-01"} 3.05479548928e+011
+barman_backups_wal_size{number="4",server="postgres-01"} 4.79318350233e+011
+barman_backups_wal_size{number="5",server="postgres-01"} 2.87333312102e+011
+barman_backups_wal_size{number="6",server="postgres-01"} 2.73267294208e+011
+barman_backups_wal_size{number="7",server="postgres-01"} 3.65501716889e+011
+barman_backups_wal_size{number="8",server="postgres-01"} 2.34075717632e+011
+
+# HELP barman_backups_total Total number of backups
+# TYPE barman_backups_total gauge
+barman_backups_total{server="postgres-01"} 9.0
+
+# HELP barman_backups_failed Number of failed backups
+# TYPE barman_backups_failed gauge
+barman_backups_failed{server="postgres-01"} 1.0
+
+# HELP barman_last_backup Last successful backup timestamp
+# TYPE barman_last_backup gauge
+barman_last_backup{server="postgres-01"} 1.562537102e+09
+
+# HELP barman_last_backup_copy_time Last successful backup copy time
+# TYPE barman_last_backup_copy_time gauge
+barman_last_backup_copy_time{server="postgres-01"} 18706.918297
+
+# HELP barman_first_backup First successful backup timestamp
+# TYPE barman_first_backup gauge
+barman_first_backup{server="postgres-01"} 1.561154701e+09
+
+# HELP barman_up Barman status checks
+# TYPE barman_up gauge
+barman_up{check="archiver_errors",server="postgres-01"} 1.0
+barman_up{check="backup_maximum_age",server="postgres-01"} 1.0
+barman_up{check="compression_settings",server="postgres-01"} 1.0
+barman_up{check="directories",server="postgres-01"} 1.0
+barman_up{check="failed_backups",server="postgres-01"} 1.0
+barman_up{check="is_superuser",server="postgres-01"} 1.0
+barman_up{check="minimum_redundancy_requirements",server="postgres-01"} 1.0
+barman_up{check="pg_basebackup",server="postgres-01"} 1.0
+barman_up{check="pg_basebackup_compatible",server="postgres-01"} 1.0
+barman_up{check="pg_basebackup_supports_tablespaces_mapping",server="postgres-01"} 1.0
+barman_up{check="pg_receivexlog",server="postgres-01"} 1.0
+barman_up{check="pg_receivexlog_compatible",server="postgres-01"} 1.0
+barman_up{check="postgresql",server="postgres-01"} 1.0
+barman_up{check="postgresql_streaming",server="postgres-01"} 1.0
+barman_up{check="receive_wal_running",server="postgres-01"} 1.0
+barman_up{check="replication_slot",server="postgres-01"} 1.0
+barman_up{check="retention_policy_settings",server="postgres-01"} 1.0
+barman_up{check="systemid_coherence",server="postgres-01"} 1.0
+barman_up{check="wal_level",server="postgres-01"} 1.0
+```
---
+++ packages/barman-exporter/barman_exporter-1.0.4.tar.gz/barman_exporter-1.0.4/barman_exporter/barman_exporter.py
@@ -0,0 +1,182 @@
+import sys
+import argparse
+import time
+import json
+import shutil
+from sh import barman as barman_cli
+from datetime import datetime
+from prometheus_client import core, write_to_textfile, generate_latest
+
+
+class Barman:
+
+    @staticmethod
+    def cli(*args, **kwargs):
+        output = barman_cli('-f', 'json', *args, **kwargs)
+        output = json.loads(str(output))
+        return output
+
+    @classmethod
+    def version(cls):
+        version = barman_cli('-v').split()
+        return version[0]
+
+    def servers(self):
+        servers = self.cli('list-server')
+        return list(servers.keys())
+
+    def server_status(self, server_name):
+        status = self.cli('status', server_name)
+        status = {k: v['message'] for k, v in status[server_name].items()}
+        return status
+
+    def server_check(self, server_name):
+        check = self.cli('check', server_name, _ok_code=[0, 1])
+        check = {k: 1 if v['status'] == "OK" else 0 for k,
+                 v in check[server_name].items()}
+        return check
+
+    def list_backup(self, server_name):
+        backups = self.cli('list-backup', server_name)
+        backups_done = [backup for backup in backups[server_name]
+                        if backup['status'] == 'DONE']
+        backups_failed = [
+            backup for backup in backups[server_name] if backup['status'] == 'FAILED']
+        return backups_done, backups_failed
+
+    def show_backup(self, server_name, backup_id):
+        backup = self.cli('show-backup', server_name, backup_id)
+        return backup[server_name]
+
+
+class BarmanCollector:
+
+    def __init__(self, servers):
+        self.servers = servers
+
+    @staticmethod
+    def pretty_size_to_bytes(size, suffixes="KMGTPEZY"):
+        size, suffix = size.split()
+        unit = 1024 if "iB" in suffix else 1000
+        exponent = suffixes.find(suffix[0].upper()) + 1
+        size_bytes = float(size) * (unit ** exponent)
+        return int(size_bytes)
+
+    def collect(self):
+        collectors = dict(
+            barman_backups_size=core.GaugeMetricFamily(
+                'barman_backups_size', "Size of available backups",
+                labels=['server', 'number']),
+            barman_backups_wal_size=core.GaugeMetricFamily(
+                'barman_backups_wal_size', "WAL size of available backups",
+                labels=['server', 'number']),
+            barman_backups_total=core.GaugeMetricFamily(
+                "barman_backups_total", "Total number of backups",
+                labels=["server"]),
+            barman_backups_failed=core.GaugeMetricFamily(
+                "barman_backups_failed", "Number of failed backups",
+                labels=["server"]),
+            barman_last_backup=core.GaugeMetricFamily(
+                "barman_last_backup", "Last successful backup timestamp",
+                labels=["server"]),
+            barman_last_backup_copy_time=core.GaugeMetricFamily(
+                "barman_last_backup_copy_time", "Last successful backup copy time",
+                labels=["server"]),
+            barman_first_backup=core.GaugeMetricFamily(
+                "barman_first_backup", "First successful backup timestamp",
+                labels=["server"]),
+            barman_up=core.GaugeMetricFamily(
+                "barman_up", "Barman status checks",
+                labels=["server", "check"])
+        )
+
+        barman = Barman()
+
+        if self.servers[0] == "all":
+            self.servers = barman.servers()
+
+        for server_name in self.servers:
+            server_status = barman.server_status(server_name)
+
+            if server_status['first_backup']:
+                first_backup = datetime.strptime(
+                    server_status['first_backup'], "%Y%m%dT%H%M%S")
+                collectors['barman_first_backup'].add_metric(
+                    [server_name], first_backup.strftime("%s"))
+
+            if server_status['last_backup']:
+                last_backup = datetime.strptime(
+                    server_status['last_backup'], "%Y%m%dT%H%M%S")
+                collectors['barman_last_backup'].add_metric(
+                    [server_name], last_backup.strftime("%s"))
+
+            backups_done, backups_failed = barman.list_backup(server_name)
+
+            collectors['barman_backups_total'].add_metric(
+                [server_name], len(backups_done) + len(backups_failed))
+
+            collectors['barman_backups_failed'].add_metric(
+                [server_name], len(backups_failed))
+
+            if len(backups_done) > 0:
+                last_backup = barman.show_backup(
+                    server_name, backups_done[0]['backup_id'])
+                last_backup_copy_time = last_backup['base_backup_information']['copy_time_seconds']
+
+                collectors['barman_last_backup_copy_time'].add_metric(
+                    [server_name], last_backup_copy_time)
+
+            for number, backup in enumerate(backups_done, 1):
+                collectors['barman_backups_size'].add_metric(
+                    [server_name, str(number)], backup['size_bytes'])
+
+                collectors['barman_backups_wal_size'].add_metric(
+                    [server_name, str(number)], backup['wal_size_bytes'])
+
+            server_check = barman.server_check(server_name)
+            for check_name, check_value in server_check.items():
+                collectors['barman_up'].add_metric(
+                    [server_name, check_name], check_value)
+
+        for collector in collectors.values():
+            yield collector
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Barman exporter",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-f', '--file',
+                        metavar="TEXTFILE_PATH",
+                        required=False,
+                        help="Save output to textfile")
+    parser.add_argument('servers', nargs="*", default=['all'],
+                        help="Space separated list of "
+                             "servers to check")
+    parser.add_argument('-u', '--user', metavar='USER',
+                        default='prometheus', help="Textfile owner")
+    parser.add_argument('-g', '--group', metavar='GROUP',
+                        default='prometheus', help="Textfile group")
+    parser.add_argument('-m', '--mode', metavar='MODE',
+                        default='0644', help="Textfile mode")
+
+    args = parser.parse_args()
+
+    barman_version = tuple(int(v) for v in Barman.version().split('.'))
+    if barman_version < (2, 9):
+        print("ERROR: Barman version 2.9+ required")
+        sys.exit(1)
+
+    registry = BarmanCollector(args.servers)
+
+    if args.file:
+        write_to_textfile(args.file, registry)
+        shutil.chown(args.file, user=args.user, group=args.group)
+        os.chmod(args.file, mode=int(args.mode, 8))
+    else:
+        print(generate_latest(registry).decode())
+
+
+if __name__ == "__main__":
+    main()
