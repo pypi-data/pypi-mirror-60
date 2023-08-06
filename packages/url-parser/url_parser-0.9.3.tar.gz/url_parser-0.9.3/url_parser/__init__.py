503100e96bbe251acb8491243b07ad174cb7c2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:48 +0000

    Add package url-parser/0.9.3/url_parser-0.9.3.tar.gz

---
+++ packages/url-parser/url_parser-0.9.3.tar.gz/url_parser-0.9.3/url_parser/__init__.py
@@ -0,0 +1,38 @@
+import re
+
+
+def parse_url(item):
+    http = None
+    www = None
+    sub_domain = None
+    domain = None
+    top_domain = None
+    sub_dir = None
+    regex = r"(?P<http>http\:\/\/|https\:\/\/|)(?P<www>[www.]*(?=[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+))" \
+            r"(?P<sub_domain>(?=[a-zA-Z0-9-]+\.[a-zA-Z0-9]{2,4}|[a-zA-Z0-9-]+\.[a-zA-Z0-9]{2,4}\/.*)" \
+            r"[a-zA-Z0-9-]*\.)?(?P<domain>(?=[a-zA-Z0-9]{2,4}|[a-zA-Z0-9]{2,4}\/.*)[a-zA-Z0-9-]+(?=\.)" \
+            r")\.(?P<top_domain>(?<=\.)[a-zA-Z0-9]{2,4}(?=$|\/))(?P<dir>\/.*(?=$|\/))?"
+    matches = re.finditer(regex, item)
+    for match in matches:
+        http = match.group('http')
+        if http == '':
+            http = None
+        www = match.group('www')
+        if www == '':
+            www = None
+        if www != None:
+            if '.' in www:
+                www = www.replace('.', '')
+        sub_domain = match.group('sub_domain')
+        if sub_domain == '':
+            sub_domain = None
+        if sub_domain != None:
+            if '.' in sub_domain:
+                sub_domain = sub_domain.replace('.', '')
+        domain = match.group('domain')
+        top_domain = match.group('top_domain')
+        sub_dir = match.group('dir')
+        if sub_dir == '':
+            sub_dir = None
+
+    return {'http': http, 'www': www, 'sub_domain': sub_domain, 'domain': domain, 'top_domain': top_domain, 'dir': sub_dir}
