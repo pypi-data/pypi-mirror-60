098f050c90511f657ac310b6de8589a8443109dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:59 +0000

    Add package markdown-ratings/0.1.0/markdown_ratings-0.1.0-py3-none-any.whl

---
+++ packages/markdown-ratings/markdown_ratings-0.1.0-py3-none-any.whl/markdown_ratings/main.py
@@ -0,0 +1,36 @@
+# from re import Pattern, Match
+from re import Match
+
+from markdown import Markdown
+from markdown.extensions import Extension
+from markdown.inlinepatterns import InlineProcessor
+from markdown.util import etree
+
+__all__ = ["makeExtension"]
+
+RATING_RE = r"\[rating: *([0-5]) *\]"
+
+
+class RatingsProcessor(InlineProcessor):
+    def handleMatch(self, m: Match, data):
+        rating_parent = etree.Element("span")
+
+        rating_value = int(m.group(1))
+
+        for r in range(0, 5):
+            class_ = "fa fa-star star-checked" if r < rating_value else "fa fa-star"
+            rating_item = etree.Element("span", {"class": class_})
+            rating_parent.append(rating_item)
+
+        return rating_parent, m.start(0), m.end(0)
+
+
+class RatingsExtension(Extension):
+    def extendMarkdown(self, mdown: Markdown):
+        print(type(mdown))
+        mdown.registerExtension(self)
+        mdown.inlinePatterns.register(RatingsProcessor(RATING_RE), "stars-rating", 75)
+
+
+def makeExtension(**kwargs):
+    return RatingsExtension(**kwargs)
