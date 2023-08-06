4f33b0b19877e062d48494d3cd3cce60126f5f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:15 +0000

    Add package FAPSDemonstratorAPI/0.0.2/FAPSDemonstratorAPI-0.0.2.tar.gz

---
+++ packages/FAPSDemonstratorAPI/FAPSDemonstratorAPI-0.0.2.tar.gz/FAPSDemonstratorAPI-0.0.2/FAPSDemonstratorAPI/__init__.py
@@ -0,0 +1,6 @@
+from .command import Command
+from .command_mode import CommandMode
+from .parameter_mode import ParameterMode
+from .program import Program
+from .utils import *
+from .APIService import *
---
+++ packages/FAPSDemonstratorAPI/FAPSDemonstratorAPI-0.0.2.tar.gz/FAPSDemonstratorAPI-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+      name='FAPSDemonstratorAPI',
+      version='0.0.2',
+      description='FAPS Demonstrator FAPSDemonstratorAPI api',
+      license='MIT',
+      author='FAU - FAPS',
+      author_email='jupiter.bakakeu@faps.fau.de',
+      url='https://faps.de/',
+      packages=setuptools.find_packages(),
+      install_requires=required,
+      classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+          ],
+      long_description='FAPS Demonstrator API. This api enable the control of the demonstrator from the cloud.'
+)
