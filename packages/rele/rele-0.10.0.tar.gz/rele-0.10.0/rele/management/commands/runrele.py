8cf7b7d935c08ddd4f0217783adfc8ee33e9734b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:24 +0000

    Add package rele/0.10.0/rele-0.10.0.tar.gz

---
+++ packages/rele/rele-0.10.0.tar.gz/rele-0.10.0/rele/management/commands/runrele.py
@@ -0,0 +1,52 @@
+import logging
+import signal
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+import rele
+from rele import Worker
+from rele.config import Config
+
+from rele.management.discover import discover_subs_modules
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Start subscriber threads to consume messages from Relé topics."
+    config = Config(settings.RELE)
+
+    def handle(self, *args, **options):
+        if all(map(lambda x: x.get("CONN_MAX_AGE"), settings.DATABASES.values())):
+            self.stderr.write(
+                self.style.WARNING(
+                    "WARNING: settings.CONN_MAX_AGE is not set to 0. "
+                    "This may result in slots for database connections to "
+                    "be exhausted."
+                )
+            )
+        subs = self._autodiscover_subs()
+        self.stdout.write(f"Configuring worker with {len(subs)} " f"subscription(s)...")
+        for sub in subs:
+            self.stdout.write(f"  {sub}")
+        worker = Worker(
+            subs,
+            self.config.gc_project_id,
+            self.config.credentials,
+            self.config.ack_deadline,
+            self.config.threads_per_subscription,
+        )
+
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        signal.signal(signal.SIGTERM, worker.stop)
+        signal.signal(signal.SIGTSTP, worker.stop)
+
+        worker.run_forever()
+
+    def _autodiscover_subs(self):
+        return rele.config.load_subscriptions_from_paths(
+            discover_subs_modules(),
+            self.config.sub_prefix,
+            settings.RELE.get("FILTER_SUBS_BY"),
+        )
---
+++ packages/rele/rele-0.10.0.tar.gz/rele-0.10.0/rele/worker.py
@@ -0,0 +1,110 @@
+import logging
+import sys
+import time
+from concurrent import futures
+
+from google.cloud.pubsub_v1.subscriber.scheduler import ThreadScheduler
+
+from .client import Subscriber
+from .middleware import run_middleware_hook
+from .subscription import Callback
+
+logger = logging.getLogger(__name__)
+
+
+class Worker:
+    """A Worker manages the subscriptions which consume Google PubSub messages.
+
+    Facilitates the creation of subscriptions if not already created,
+    and the starting and stopping the consumption of them.
+
+    :param subscriptions: list :class:`~rele.subscription.Subscription`
+    """
+
+    def __init__(
+        self,
+        subscriptions,
+        gc_project_id=None,
+        credentials=None,
+        default_ack_deadline=None,
+        threads_per_subscription=None,
+    ):
+        self._subscriber = Subscriber(gc_project_id, credentials, default_ack_deadline)
+        self._futures = []
+        self._subscriptions = subscriptions
+        self.threads_per_subscription = threads_per_subscription
+
+    def setup(self):
+        """Create the subscriptions on a Google PubSub topic.
+
+        If the subscription already exists, the subscription will not be
+        re-created. Therefore, it is idempotent.
+        """
+        for subscription in self._subscriptions:
+            self._subscriber.create_subscription(subscription.name, subscription.topic)
+
+    def start(self):
+        """Begin consuming all subscriptions.
+
+        When consuming a subscription, a ``StreamingPullFuture`` is returned from
+        the Google PubSub client library. This future can be used to
+        manage the background stream.
+
+        The futures are stored so that they can be cancelled later on
+        for a graceful shutdown of the worker.
+        """
+        run_middleware_hook("pre_worker_start")
+        for subscription in self._subscriptions:
+            executor_kwargs = {
+                "thread_name_prefix": "ThreadPoolExecutor-ThreadScheduler"
+            }
+            executor = futures.ThreadPoolExecutor(
+                max_workers=self.threads_per_subscription, **executor_kwargs
+            )
+            scheduler = ThreadScheduler(executor=executor)
+            self._futures.append(
+                self._subscriber.consume(
+                    subscription_name=subscription.name,
+                    callback=Callback(subscription),
+                    scheduler=scheduler,
+                )
+            )
+        run_middleware_hook("post_worker_start")
+
+    def run_forever(self, sleep_interval=1):
+        """Shortcut for calling setup, start, and _wait_forever.
+
+        :param sleep_interval: Number of seconds to sleep in the ``while True`` loop
+        """
+        self.setup()
+        self.start()
+        self._wait_forever(sleep_interval=sleep_interval)
+
+    def stop(self, signal=None, frame=None):
+        """Manage the shutdown process of the worker.
+
+        This function has two purposes:
+
+            1. Cancel all the futures created.
+            2. And close all the database connections
+               opened by Django. Even though we cancel the connections
+               for every execution of the callback, we want to be sure
+               that all the database connections are closed
+               in this process.
+
+        Exits with code 0 for a clean exit.
+
+        :param signal: Needed for `signal.signal <https://docs.python.org/3/library/signal.html#signal.signal>`_  # noqa
+        :param frame: Needed for `signal.signal <https://docs.python.org/3/library/signal.html#signal.signal>`_  # noqa
+        """
+        run_middleware_hook("pre_worker_stop", self._subscriptions)
+        for future in self._futures:
+            future.cancel()
+
+        run_middleware_hook("post_worker_stop")
+        sys.exit(0)
+
+    def _wait_forever(self, sleep_interval):
+        logger.info("Consuming subscriptions...")
+        while True:
+            time.sleep(sleep_interval)
