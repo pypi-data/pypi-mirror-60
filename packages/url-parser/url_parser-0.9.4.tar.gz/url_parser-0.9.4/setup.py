8ef7ac84b247a542c24b49411e0cf9cee4fb7c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:08 +0000

    Add package url-parser/0.9.4/url_parser-0.9.4.tar.gz

---
+++ packages/url-parser/url_parser-0.9.4.tar.gz/url_parser-0.9.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='url_parser',
+      version='0.9.4',
+      description='Parse url and get all the different parts out of it',
+      url='https://bitbucket.org/OddAdapted/url_parser/src/master/',
+      author='Odd Jøren Røland',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author_email='odd@adapted.no',
+      license='MIT',
+      packages=['url_parser'],
+      zip_safe=False)
