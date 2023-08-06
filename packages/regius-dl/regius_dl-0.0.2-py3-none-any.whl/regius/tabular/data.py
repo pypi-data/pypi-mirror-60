1b6e5bea98d1c69c39de8b987d824bfc946fb7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:37 +0000

    Add package regius-dl/0.0.2/regius_dl-0.0.2-py3-none-any.whl

---
+++ packages/regius-dl/regius_dl-0.0.2-py3-none-any.whl/regius/tabular/data.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/30 下午2:33
+# @Author  : RegiusQuant <315135833@qq.com>
+# @Project : regius
+# @File    : data.py
+# @Desc    : 表格数据所需的Dataset
+
+
+from ..core import *
+
+
+class WideDeepDataset(Dataset):
+    """Wide&Deep模型所需的数据集结构
+
+    Args:
+        x_wide (np.ndarray): Wide模块的输入特征
+        x_deepdense (np.ndarray): DeepDense模块的输入特征
+        y (np.ndarray): 预测目标值
+    """
+    def __init__(self,
+                 x_wide: np.ndarray,
+                 x_deepdense: np.ndarray,
+                 y: Optional[np.ndarray] = None):
+        self.x_wide = x_wide
+        self.x_deepdense = x_deepdense
+        self.y = y
+
+    def __getitem__(self, idx: int):
+        x = Bunch(wide=self.x_wide[idx], deepdense=self.x_deepdense[idx])
+        if self.y is not None:
+            return x, self.y[idx]
+        return x
+
+    def __len__(self):
+        return len(self.x_deepdense)
