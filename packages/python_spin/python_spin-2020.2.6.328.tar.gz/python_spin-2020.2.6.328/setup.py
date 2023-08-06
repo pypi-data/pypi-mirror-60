e025be2dd1dc5aeed77c449e1dbd58d203479a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:46 +0000

    Add package python_spin/2020.2.6.328/python_spin-2020.2.6.328.tar.gz

---
+++ packages/python_spin/python_spin-2020.2.6.328.tar.gz/python_spin-2020.2.6.328/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+    setuptools.setup(
+        name                 = 'python_spin',
+        version              = '2020.02.06.0328',
+        description          = 'a small utility to assist in setting usage modes of laptop-tablet devices',
+        long_description     = long_description(),
+        url                  = 'https://github.com/wdbm/spin',
+        author               = 'Will Breaden Madden',
+        author_email         = 'wbm@protonmail.ch',
+        license              = 'GPLv3',
+        packages             = setuptools.find_packages(),
+        install_requires     = [
+                               'docopt',
+                               'technicolor'
+                               ],
+        entry_points         = {
+                               'console_scripts': ('spin = python_spin.__init__:main')
+                               },
+        include_package_data = True,
+        zip_safe             = False,
+        python_requires      = '>=3.6'
+    )
+
+def long_description(filename = 'README.md'):
+    if os.path.isfile(os.path.expandvars(filename)):
+      try:
+          import pypandoc
+          long_description = pypandoc.convert_file(filename, 'rst')
+      except ImportError:
+          long_description = open(filename).read()
+    else:
+        long_description = ''
+    return long_description
+
+if __name__ == '__main__':
+    main()
