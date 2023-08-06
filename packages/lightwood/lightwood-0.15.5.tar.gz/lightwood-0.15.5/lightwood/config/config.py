0c4a3f72453ea8e1d0ad2f4ffae7687bed2e73e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:01 +0000

    Add package lightwood/0.15.5/lightwood-0.15.5.tar.gz

---
+++ packages/lightwood/lightwood-0.15.5.tar.gz/lightwood-0.15.5/lightwood/config/config.py
@@ -0,0 +1,28 @@
+import torch
+
+
+class CONFIG:
+    """General"""
+    USE_CUDA = False
+    if torch.cuda.device_count() > 0:
+        USE_CUDA = True
+
+    USE_DEVICE = None
+    CACHE_ENCODED_DATA = True
+
+    # Enable deterministic cuda flag and use seeds everywhere (static or based on features of the dataset)
+    DETERMINISTIC = True
+    OVERSAMPLE = False
+    SELFAWARE = True
+    HELPER_MIXERS = False
+    ENABLE_DROPOUT = True
+
+    """Probabilistic FC layers"""
+    USE_PROBABILISTIC_LINEAR = False # change weights in mixer to be probabilistic
+
+    """Bayesian Network"""
+    NUMBER_OF_PROBABILISTIC_MODELS = 2
+
+    """Encoder options"""
+    TRAIN_TO_PREDICT_TARGET = True
+    MAX_ENCODER_TRAINING_TIME = 3600 * 2
