2a0278755a121195c8d378f04dae6152f2801268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:07 +0000

    Add package pynidus/0.0.111/pynidus-0.0.111.tar.gz

---
+++ packages/pynidus/pynidus-0.0.111.tar.gz/pynidus-0.0.111/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pynidus",
+    version="0.0.111",
+    author="Keurcien Luu",
+    author_email="keurcien@appchoose.io",
+    description="A handful of utilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/appchoose/pynidus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
