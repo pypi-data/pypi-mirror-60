0bef6509aa3758cf7bdb344d8a090c145e9170af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:03 +0000

    Add package vote4film/1.2.3/vote4film-1.2.3.tar.gz

---
+++ packages/vote4film/vote4film-1.2.3.tar.gz/vote4film-1.2.3/src/films/core/types.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum, unique
+from typing import Optional
+
+
+@unique
+class AgeRating(Enum):
+    UNIVERSAL = "U"
+    PARENTAL_GUIDANCE = "PG"
+    AGE_12 = "12"  # Including 12A
+    AGE_15 = "15"
+    AGE_18 = "18"  # Including R18
+
+
+@dataclass
+class Film:
+    imdb: str
+    title: str
+    year: int
+    imdb_rating: float
+    age_rating: Optional[AgeRating] = None
+    trailer: Optional[str] = None
+    genre: Optional[str] = None
+    runtime_mins: Optional[int] = None
+    plot: Optional[str] = None
+    poster_url: Optional[str] = None
