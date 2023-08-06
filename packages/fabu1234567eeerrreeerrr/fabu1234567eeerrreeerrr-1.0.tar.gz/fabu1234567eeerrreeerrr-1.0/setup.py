edebde9c19325c5e59a0578d51813e8232fd128f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:52 +0000

    Add package fabu1234567eeerrreeerrr/1.0/fabu1234567eeerrreeerrr-1.0.tar.gz

---
+++ packages/fabu1234567eeerrreeerrr/fabu1234567eeerrreeerrr-1.0.tar.gz/fabu1234567eeerrreeerrr-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='fabu1234567eeerrreeerrr',
+    version='1.0',
+    description='这是一个发布的测试包',
+    author='yuzhang',
+    author_email='yuzhang@nwcdcloud.cn',
+    py_modules=['fabu1234567eeerrreeerrr.test1','fabu1234567eeerrreeerrr.test2']
+)
