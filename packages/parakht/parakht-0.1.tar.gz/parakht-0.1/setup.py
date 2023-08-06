92f1c1479c8d2cca3db237e1534ff239a86e8d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:00 +0000

    Add package parakht/0.1/parakht-0.1.tar.gz

---
+++ packages/parakht/parakht-0.1.tar.gz/parakht-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(name='parakht',
+      version='0.1',
+      description='User made pkg',
+      author='parakht',
+      author_email="parakh.tayal@sganalytics.com",
+      license='MIT',
+      packages=['parakht'],
+      long_description="firstpackage",
+      zip_safe=False)
