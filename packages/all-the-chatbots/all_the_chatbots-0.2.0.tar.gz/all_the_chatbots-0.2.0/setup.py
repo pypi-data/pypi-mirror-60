7ba44191ce347e0add42f440e27f5bd5be5d7d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:42 +0000

    Add package all-the-chatbots/0.2.0/all_the_chatbots-0.2.0.tar.gz

---
+++ packages/all-the-chatbots/all_the_chatbots-0.2.0.tar.gz/all_the_chatbots-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='all_the_chatbots',
+    version='0.2.0',
+    packages=['all_the_chatbots'],
+    url='https://github.com/OpenJarbas/all_the_chatbots',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unofficial apis for all the chatbots'
+)
