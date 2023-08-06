f3ebba3bd3eaea817c2e45401a0027551024daf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:57 +0000

    Add package configtype/0.0.8/configtype-0.0.8.tar.gz

---
+++ packages/configtype/configtype-0.0.8.tar.gz/configtype-0.0.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+__version__ = "0.0.8"
+
+
+setup(
+    # package name in pypi
+    name='configtype',
+    # extract version from module.
+    version=__version__,
+    description="Configtype bridges json configuration to python.",
+    long_description="Configure your app with json, but import from python",
+    classifiers=[],
+    keywords='',
+    author='Lars van de Kerkhof',
+    author_email='lars@permanentmarkers.nl',
+    url='https://github.com/specialunderwear/configtype',
+    license='GPLv3',
+    # include all packages in the egg, except the test package.
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'test']),
+    # for avoiding conflict have one namespace for all related eggs.
+    namespace_packages=[],
+    # include non python files
+    include_package_data=True,
+    zip_safe=False,
+    # specify dependencies
+    install_requires=[
+        'setuptools',
+        'six',
+    ],
+    # mark test target to require extras.
+    extras_require = {
+        'test':  ["nose"]
+    },
+)
