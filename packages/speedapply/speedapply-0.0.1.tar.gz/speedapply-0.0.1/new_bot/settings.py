186eb5efa4613c41b24430e909c2ab9d13c29f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Add package speedapply/0.0.1/speedapply-0.0.1.tar.gz

---
+++ packages/speedapply/speedapply-0.0.1.tar.gz/speedapply-0.0.1/new_bot/settings.py
@@ -0,0 +1,16 @@
+# driver
+DRIVER = 'chrome'
+DRIVER_OPTIONS = ['--headless']
+
+# job titles
+TITLES = [
+    'database developer',
+    'neo4j',
+    'postgresql'
+]
+
+# job locations
+LOCATIONS = [
+    'Irvine, CA',
+    'Los Angeles, CA'
+]
