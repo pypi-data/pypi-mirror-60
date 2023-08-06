49486f289d608ea8cc20453201b5d310ce940ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:54 +0000

    Add package graphbrain/0.3.1/graphbrain-0.3.1.tar.gz

---
+++ packages/graphbrain/graphbrain-0.3.1.tar.gz/graphbrain-0.3.1/graphbrain/agents/actors.py
@@ -0,0 +1,35 @@
+from collections import Counter
+from graphbrain.meaning.concepts import *
+from graphbrain.meaning.corefs import main_coref
+from graphbrain.agents.agent import Agent
+
+
+class Actors(Agent):
+    def __init__(self, hg, lang, sequence=None):
+        super().__init__(hg, lang, sequence)
+        self.actor_counter = None
+
+    def name(self):
+        return 'actors'
+
+    def languages(self):
+        return set()
+
+    def start(self):
+        self.actor_counter = Counter()
+
+    def input_edge(self, edge):
+        if not edge.is_atom():
+            ct = edge.connector_type()
+            if ct[:2] == 'pd':
+                subjects = edge.edges_with_argrole('s')
+                if len(subjects) == 1:
+                    subject = strip_concept(subjects[0])
+                    if subject and has_proper_concept(subject):
+                        actor = main_coref(self.hg, subject)
+                        self.actor_counter[actor] += 1
+
+    def end(self):
+        for actor in self.actor_counter:
+            if self.actor_counter[actor] > 1:
+                self.add(('actor/p/.', actor))
