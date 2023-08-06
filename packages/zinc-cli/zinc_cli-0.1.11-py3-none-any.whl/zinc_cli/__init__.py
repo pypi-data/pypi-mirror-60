16a58d74163150b55f259539ee87bd669d32d635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:36 +0000

    Add package zinc-cli/0.1.11/zinc_cli-0.1.11-py3-none-any.whl

---
+++ packages/zinc-cli/zinc_cli-0.1.11-py3-none-any.whl/zinc_cli/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+__version__ = "0.1.11"
+
+from .zinc import main
+from .commands import zinc_create, zinc_transform
---
+++ packages/zinc-cli/zinc_cli-0.1.11-py3-none-any.whl/zinc_cli/infrastructure/models/infrastructure_service_model.py
@@ -0,0 +1,59 @@
+from typing import Dict, List
+
+import kix
+
+from .infrastructure_service_field import InfrastructureServiceField as Field
+
+
+class InfrastructureServiceModel:
+    def __init__(self):
+        self._all_fields: Dict[str, Field] = {}
+
+        # General.
+        self.aws_account_id: Field = self._add_field("Z_AWS_ACCOUNT_ID")
+        self.aws_region: Field = self._add_field("Z_AWS_REGION")
+        self.project_name: Field = self._add_field("Z_PROJECT_NAME")
+
+        # Static site creation.
+        self.static_site_root_domain: Field = self._add_field("Z_STATIC_SITE_ROOT_DOMAIN")
+        self.static_site_sub_domain: Field = self._add_field("Z_STATIC_SITE_SUB_DOMAIN")
+        self.with_https: Field = self._add_field("Z_WITH_HTTPS")
+
+    def _add_field(self, key: str, default: str = "") -> Field:
+        field = Field(key, default)
+        self._all_fields[key] = field
+        return field
+
+    def save_to_environ(self):
+        for field in self._all_fields.values():
+            field.write_to_environ()
+
+    def load_from_environ(self):
+        data = {}
+        for field in self._all_fields.values():
+            field.load_from_environ()
+            data[field.key] = field.value
+
+        kix.info("Loading Infrastructure Model from Environment", data)
+
+    def append(self, new_model: 'InfrastructureServiceModel'):
+
+        # Absorb the edited fields from the new model.
+        for k, field in self._all_fields.items():
+            new_field = new_model._all_fields[k]
+            if new_field.was_edited:
+                field.set(new_field.value)
+
+    def get_command_line_args(self) -> str:
+        # Turn all the non-default fields into environment arguments.
+        commands: Dict[str, str] = self.get_command_line_dict()
+        commands_list = [f"{k}={v}" for k, v in commands.items()]
+        return " ".join(commands_list)
+
+    def get_command_line_dict(self) -> Dict[str, str]:
+        # Turn all the non-default fields into environment arguments.
+        commands: Dict[str, str] = {}
+        for field in self._all_fields.values():
+            if field.was_edited:
+                commands[field.key] = field.value
+        return commands
