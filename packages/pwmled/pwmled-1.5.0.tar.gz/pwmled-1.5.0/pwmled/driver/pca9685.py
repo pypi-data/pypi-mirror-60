790b58c9b347a40d70cb9880b56b81c62a5243ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:01 +0000

    Add package pwmled/1.5.0/pwmled-1.5.0.tar.gz

---
+++ packages/pwmled/pwmled-1.5.0.tar.gz/pwmled-1.5.0/pwmled/driver/pca9685.py
@@ -0,0 +1,32 @@
+"""PCA9685 pwm driver."""
+from Adafruit_PCA9685 import PCA9685
+
+from pwmled.driver import Driver
+
+
+class Pca9685Driver(Driver):
+    """Represents a pwm driver, which uses the pins of an PCA9685 I2C board."""
+
+    RESOLUTION = 12
+
+    def __init__(self, pins, freq=200, address=0x40):
+        """
+        Initialize the driver.
+
+        :param pins: The pin numbers, that should be controlled.
+        :param freq: The pwm frequency.
+        :param address: The address of the PCA9685.
+        """
+        super().__init__(pins, self.RESOLUTION, freq)
+
+        self._device = PCA9685(address)
+        self._device.set_pwm_freq(self._freq)
+
+    def _set_pwm(self, raw_values):
+        """
+        Set pwm values on the controlled pins.
+
+        :param raw_values: Raw values to set (0-4095).
+        """
+        for i in range(len(self._pins)):
+            self._device.set_pwm(self._pins[i], 0, raw_values[i])
---
+++ packages/pwmled/pwmled-1.5.0.tar.gz/pwmled-1.5.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pwmled',
+    version='1.5.0',
+    description='Control LEDs connected to a micro controller using pwm.',
+    url='https://github.com/soldag/python-pwmled/',
+    license='MIT',
+    author='soldag',
+    author_email='soren.oldag@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pigpio==1.35',
+        'Adafruit-PCA9685==1.0.1',
+        'python-singleton==0.1.2',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
