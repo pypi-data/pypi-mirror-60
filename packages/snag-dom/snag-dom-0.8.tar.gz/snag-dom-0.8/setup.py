ae901efce2f1e45cd0ed4f0bf9db5e628ed3bc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package snag-dom/0.8/snag-dom-0.8.tar.gz

---
+++ packages/snag-dom/snag-dom-0.8.tar.gz/snag-dom-0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+        name='snag-dom',
+        version='0.8',
+        author='Cassandra Gentry',
+        author_email='cassiegentlekitty@gmail.com',
+        description='An HTML utility package',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=setuptools.find_packages(),
+        install_requires= [
+            'requests',
+            'bs4'
+        ],
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Operating System :: OS Independent'
+        ]
+)
