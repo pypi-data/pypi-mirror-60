5884a6fa301f88fa633111cfc757a3b0fbf94874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:00 +0000

    Add package hola/1.1.54/hola-1.1.54.tar.gz

---
+++ packages/hola/hola-1.1.54.tar.gz/hola-1.1.54/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='hola',
+    version='1.1.54',
+    author='smd',
+    author_email= '"asdf,df" <aasimD@redf.com>',
+    maintainer_email='trentm@gmail.com',
+    description='This is the summary (or short description)',
+    long_description='This is the long description, which can be in the following formats: text/plain, text/markdown, or text/x-rst',
+    python_requires='>2.9',
+    download_url="http://www.ggogasd.fom",
+   
+    project_urls={
+        "sadsdsddfgsfgdf": "https://www.goog.com",
+        "safd": "https://www.google.com"
+    },
+    packages=['greetings'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    keywords='sample test blah',
+    install_requires="sampleproject > 1.0",
+    platforms='linux_x86_32'
+)
