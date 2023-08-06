2425f3af99f70c28ad226990b6a04b4848e3d359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:12 +0000

    Add package elm-kernel/0.21.1/elm_kernel-0.21.1.tar.gz

---
+++ packages/elm-kernel/elm_kernel-0.21.1.tar.gz/elm_kernel-0.21.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='elm_kernel',
+    version='0.21.1',
+    packages=['elm_kernel'],
+    description='Jupyter kernel for executing Elm code',
+    long_description=readme,
+    author='Austin Bingham',
+    author_email='austin@sixty-north.com',
+    url='https://github.com/abingham/jupyter-elm-kernel',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['jupyter'],
+)
